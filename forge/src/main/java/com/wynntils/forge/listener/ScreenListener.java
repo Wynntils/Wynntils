1989f701bf860a042d19d2f957ebc171c2595081
Magnus Ihse Bursie <mag@icus.se>
2021-12-21 11:40:40 +0100

    Add copyright header

--- forge/src/main/java/com/wynntils/forge/listener/ScreenListener.java
+++ forge/src/main/java/com/wynntils/forge/listener/ScreenListener.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright © Wynntils 2021.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
 package com.wynntils.forge.listener;
 
 import com.wynntils.mc.event.ScreenEvents;
