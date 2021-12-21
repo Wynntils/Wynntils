12f87c8051faf7000241ee681f78906d6de18d2f
Magnus Ihse Bursie <mag@icus.se>
2021-12-21 14:32:50 +0100

    Remove non-needed example code

--- common/src/main/java/com/wynntils/WynntilsMod.java
+++ common/src/main/java/com/wynntils/WynntilsMod.java
@@ -8,10 +8,6 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import java.util.List;
 import java.util.function.Consumer;
-import me.shedaniel.architectury.registry.CreativeTabs;
-import me.shedaniel.architectury.registry.DeferredRegister;
-import me.shedaniel.architectury.registry.Registries;
-import me.shedaniel.architectury.registry.RegistrySupplier;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
@@ -20,37 +16,14 @@ import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.multiplayer.ServerData;
-import net.minecraft.core.Registry;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.LazyLoadedValue;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 
 public class WynntilsMod {
     public static final String MOD_ID = "wynntils";
-    // We can use this if we don't want to use DeferredRegister
-    public static final LazyLoadedValue<Registries> REGISTRIES =
-            new LazyLoadedValue<>(() -> Registries.get(MOD_ID));
-    // Registering a new creative tab
-    public static final CreativeModeTab EXAMPLE_TAB =
-            CreativeTabs.create(
-                    new ResourceLocation(MOD_ID, "example_tab"),
-                    () -> new ItemStack(WynntilsMod.EXAMPLE_ITEM.get()));
-
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(MOD_ID, Registry.ITEM_REGISTRY);
-    public static final RegistrySupplier<Item> EXAMPLE_ITEM =
-            ITEMS.register(
-                    "example_item",
-                    () -> new Item(new Item.Properties().tab(WynntilsMod.EXAMPLE_TAB)));
 
     public static void init() {
-        ITEMS.register();
-
-        System.out.println(
-                ExampleExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
+        System.out.println("Wynntils initialized");
     }
 
     private static class WynncraftButton extends Button {
--- forge/src/main/java/com/wynntils/forge/WynntilsModForge.java
+++ forge/src/main/java/com/wynntils/forge/WynntilsModForge.java
@@ -5,19 +5,11 @@
 package com.wynntils.forge;
 
 import com.wynntils.WynntilsMod;
-import com.wynntils.forge.listener.ScreenListener;
-import me.shedaniel.architectury.platform.forge.EventBuses;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(WynntilsMod.MOD_ID)
 public class WynntilsModForge {
     public WynntilsModForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(
-                WynntilsMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         WynntilsMod.init();
-        MinecraftForge.EVENT_BUS.register(new ScreenListener());
     }
 }
