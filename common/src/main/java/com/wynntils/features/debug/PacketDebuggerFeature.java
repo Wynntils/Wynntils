be064a8543a58fa71dfd6f8095020f2e6bb2b885
Magnus Ihse Bursie <mag@icus.se>
2022-01-02 03:40:30 +0100

    Ignore spammy packets in debugger. Disable debugger by default.

--- common/src/main/java/com/wynntils/features/debug/PacketDebuggerFeature.java
+++ common/src/main/java/com/wynntils/features/debug/PacketDebuggerFeature.java
@@ -9,12 +9,47 @@ import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
 import com.wynntils.core.features.AbstractFeature;
 import com.wynntils.mc.event.PacketEvent.PacketReceivedEvent;
 import com.wynntils.mc.event.PacketEvent.PacketSentEvent;
+import java.util.Arrays;
+import java.util.List;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.*;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 public class PacketDebuggerFeature extends AbstractFeature {
-    public static final boolean DEBUG_PACKETS = true;
+    public static final boolean DEBUG_PACKETS = false;
+
+    /* These packets just spam the log; ignore them. */
+    private static final List<Class<? extends Packet>> IGNORE_LIST =
+            Arrays.asList(
+                    // General
+                    ServerboundKeepAlivePacket.class,
+                    ClientboundKeepAlivePacket.class,
+                    ClientboundSetTimePacket.class,
+                    ClientboundUpdateAdvancementsPacket.class,
+                    ClientboundUpdateAttributesPacket.class,
+                    ClientboundLevelParticlesPacket.class,
+                    ClientboundPlayerInfoPacket.class,
+                    ClientboundSetEquipmentPacket.class,
+                    // Chunks
+                    ClientboundForgetLevelChunkPacket.class,
+                    ClientboundLevelChunkPacket.class,
+                    ClientboundLightUpdatePacket.class,
+                    ClientboundSetChunkCacheCenterPacket.class,
+                    // Entities
+                    ClientboundAddEntityPacket.class,
+                    ClientboundAddMobPacket.class,
+                    ClientboundMoveEntityPacket.Pos.class,
+                    ClientboundMoveEntityPacket.PosRot.class,
+                    ClientboundMoveEntityPacket.Rot.class,
+                    ClientboundRotateHeadPacket.class,
+                    ClientboundSetEntityDataPacket.class,
+                    ClientboundSetEntityMotionPacket.class,
+                    ClientboundTeleportEntityPacket.class,
+                    // Client movement
+                    ServerboundMovePlayerPacket.Pos.class,
+                    ServerboundMovePlayerPacket.PosRot.class,
+                    ServerboundMovePlayerPacket.Rot.class);
 
     private String describePacket(Packet packet) {
         return ReflectionToStringBuilder.toString(packet, SHORT_PREFIX_STYLE)
@@ -25,13 +60,21 @@ public class PacketDebuggerFeature extends AbstractFeature {
     public void onPacketSent(PacketSentEvent e) {
         if (!DEBUG_PACKETS) return;
 
-        System.out.println("SENT packet: " + describePacket(e.getPacket()));
+        Packet packet = e.getPacket();
+
+        if (IGNORE_LIST.contains(packet.getClass())) return;
+
+        System.out.println("SENT packet: " + describePacket(packet));
     }
 
     @SubscribeEvent
     public void onPacketReceived(PacketReceivedEvent e) {
         if (!DEBUG_PACKETS) return;
 
-        System.out.println("RECV packet: " + describePacket(e.getPacket()));
+        Packet packet = e.getPacket();
+
+        if (IGNORE_LIST.contains(packet.getClass())) return;
+
+        System.out.println("RECV packet: " + describePacket(packet));
     }
 }
