0c495ad705547cc9ebf1216df368f81f9d643979
kristofbolyai <bolyai.bolyai@gmail.com>
2022-02-28 16:27:50 +0100

    Spotless

--- common/src/main/java/com/wynntils/commands/WynntilsCommand.java
+++ common/src/main/java/com/wynntils/commands/WynntilsCommand.java
@@ -8,14 +8,11 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.wynntils.core.webapi.WebManager;
 import com.wynntils.mc.utils.commands.WynntilsCommandBase;
+import java.util.List;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.*;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class WynntilsCommand extends WynntilsCommandBase {
     @Override
@@ -30,138 +27,146 @@ public class WynntilsCommand extends WynntilsCommandBase {
     }
 
     private int reloadApi(CommandContext<CommandSourceStack> context) {
-            WebManager.reset();
+        WebManager.reset();
 
-            boolean success = WebManager.setupUserAccount();
+        boolean success = WebManager.setupUserAccount();
 
-            success &= WebManager.reloadUsedRoutes();
+        success &= WebManager.reloadUsedRoutes();
 
-            if (success) {
-                context.getSource()
-                        .sendSuccess(
-                                new TextComponent("Successfully reloaded all used API routes.")
-                                        .withStyle(ChatFormatting.GREEN),
-                                false);
-            } else {
-                context.getSource()
-                        .sendFailure(
-                                new TextComponent("One or more API routes failed to reload")
-                                        .withStyle(ChatFormatting.RED));
-            }
+        if (success) {
+            context.getSource()
+                    .sendSuccess(
+                            new TextComponent("Successfully reloaded all used API routes.")
+                                    .withStyle(ChatFormatting.GREEN),
+                            false);
+        } else {
+            context.getSource()
+                    .sendFailure(
+                            new TextComponent("One or more API routes failed to reload")
+                                    .withStyle(ChatFormatting.RED));
+        }
 
-            return 1;
+        return 1;
     }
 
     private int donateLink(CommandContext<CommandSourceStack> context) {
-            MutableComponent c =
-                    new TextComponent("You can donate to Wynntils at: ")
-                            .withStyle(ChatFormatting.AQUA);
-            MutableComponent url =
-                    new TextComponent("https://www.patreon.com/Wynntils")
-                            .withStyle(
-                                    Style.EMPTY
-                                            .withColor(ChatFormatting.LIGHT_PURPLE)
-                                            .withUnderlined(true)
-                                            .withClickEvent(
-                                                    new ClickEvent(
-                                                            ClickEvent.Action.OPEN_URL,
-                                                            "https://www.patreon.com/Wynntils"))
-                                            .withHoverEvent(
-                                                    new HoverEvent(
-                                                            HoverEvent.Action.SHOW_TEXT,
-                                                            new TextComponent(
-                                                                    "Click here to open in your"
-                                                                            + " browser."))));
+        MutableComponent c =
+                new TextComponent("You can donate to Wynntils at: ").withStyle(ChatFormatting.AQUA);
+        MutableComponent url =
+                new TextComponent("https://www.patreon.com/Wynntils")
+                        .withStyle(
+                                Style.EMPTY
+                                        .withColor(ChatFormatting.LIGHT_PURPLE)
+                                        .withUnderlined(true)
+                                        .withClickEvent(
+                                                new ClickEvent(
+                                                        ClickEvent.Action.OPEN_URL,
+                                                        "https://www.patreon.com/Wynntils"))
+                                        .withHoverEvent(
+                                                new HoverEvent(
+                                                        HoverEvent.Action.SHOW_TEXT,
+                                                        new TextComponent(
+                                                                "Click here to open in your"
+                                                                        + " browser."))));
 
-            context.getSource().sendSuccess(c.append(url), false);
-            return 1;
+        context.getSource().sendSuccess(c.append(url), false);
+        return 1;
     }
 
     private int help(CommandContext<CommandSourceStack> context) {
-            MutableComponent text =
-                    new TextComponent("").withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD));
-            text.append("Wynntils' command list: ");
-            text.append("\n");
-            addCommandDescription(
-                    text,
-                    "wynntils",
-                    List.of("help"),
-                    "This shows a list of all available commands for Wynntils.");
-            text.append("\n");
-            addCommandDescription(
-                    text,
-                    "wynntils",
-                    List.of("discord"),
-                    "This provides you with an invite to our Discord server.");
-            text.append("\n");
-            //            addCommandDescription(text, "-wynntils", " version", "This shows the
-            // installed Wynntils version.");
-            //            text.append("\n");
-            //            addCommandDescription(text, "-wynntils", " changelog [major/latest]",
-            // "This shows the changelog of your installed version.");
-            //            text.append("\n");
-            addCommandDescription(text, "-wynntils", List.of("reloadapi"), "This reloads all API data.");
-            text.append("\n");
-            addCommandDescription(text, "-wynntils", List.of("donate"), "This provides our Patreon link.");
-            text.append("\n");
-            addCommandDescription(
-                    text,
-                    "token",
-                    List.of(),
-                    "This provides a clickable token for you to create a Wynntils account to manage"
-                            + " your cosmetics.");
-            text.append("\n");
-            addCommandDescription(
-                    text,
-                    "territory",
-                    List.of(),
-                    "This makes your compass point towards a specified territory.");
-            context.getSource().sendSuccess(text, false);
-            return 1;
+        MutableComponent text =
+                new TextComponent("").withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD));
+        text.append("Wynntils' command list: ");
+        text.append("\n");
+        addCommandDescription(
+                text,
+                "wynntils",
+                List.of("help"),
+                "This shows a list of all available commands for Wynntils.");
+        text.append("\n");
+        addCommandDescription(
+                text,
+                "wynntils",
+                List.of("discord"),
+                "This provides you with an invite to our Discord server.");
+        text.append("\n");
+        //            addCommandDescription(text, "-wynntils", " version", "This shows the
+        // installed Wynntils version.");
+        //            text.append("\n");
+        //            addCommandDescription(text, "-wynntils", " changelog [major/latest]",
+        // "This shows the changelog of your installed version.");
+        //            text.append("\n");
+        addCommandDescription(
+                text, "-wynntils", List.of("reloadapi"), "This reloads all API data.");
+        text.append("\n");
+        addCommandDescription(
+                text, "-wynntils", List.of("donate"), "This provides our Patreon link.");
+        text.append("\n");
+        addCommandDescription(
+                text,
+                "token",
+                List.of(),
+                "This provides a clickable token for you to create a Wynntils account to manage"
+                        + " your cosmetics.");
+        text.append("\n");
+        addCommandDescription(
+                text,
+                "territory",
+                List.of(),
+                "This makes your compass point towards a specified territory.");
+        context.getSource().sendSuccess(text, false);
+        return 1;
     }
 
     private int discordLink(CommandContext<CommandSourceStack> context) {
-            MutableComponent msg =
-                    new TextComponent("You're welcome to join our Discord server at:\n")
-                            .withStyle(ChatFormatting.GOLD);
-            String discordInvite =
-                    WebManager.getApiUrls() == null
-                            ? null
-                            : WebManager.getApiUrls().get("DiscordInvite");
-            MutableComponent link =
-                    new TextComponent(
-                                    discordInvite == null
-                                            ? "<Wynntils servers are down>"
-                                            : discordInvite)
-                            .withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_AQUA));
-            if (discordInvite != null) {
-                link.setStyle(
-                        link.getStyle()
-                                .withClickEvent(
-                                        new ClickEvent(ClickEvent.Action.OPEN_URL, discordInvite))
-                                .withHoverEvent(
-                                        new HoverEvent(
-                                                HoverEvent.Action.SHOW_TEXT,
-                                                new TextComponent(
-                                                        "Click here to join our Discord"
-                                                                + " server."))));
-            }
-            context.getSource().sendSuccess(msg.append(link), false);
-            return 1;
+        MutableComponent msg =
+                new TextComponent("You're welcome to join our Discord server at:\n")
+                        .withStyle(ChatFormatting.GOLD);
+        String discordInvite =
+                WebManager.getApiUrls() == null
+                        ? null
+                        : WebManager.getApiUrls().get("DiscordInvite");
+        MutableComponent link =
+                new TextComponent(
+                                discordInvite == null
+                                        ? "<Wynntils servers are down>"
+                                        : discordInvite)
+                        .withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_AQUA));
+        if (discordInvite != null) {
+            link.setStyle(
+                    link.getStyle()
+                            .withClickEvent(
+                                    new ClickEvent(ClickEvent.Action.OPEN_URL, discordInvite))
+                            .withHoverEvent(
+                                    new HoverEvent(
+                                            HoverEvent.Action.SHOW_TEXT,
+                                            new TextComponent(
+                                                    "Click here to join our Discord"
+                                                            + " server."))));
+        }
+        context.getSource().sendSuccess(msg.append(link), false);
+        return 1;
     }
 
     private static void addCommandDescription(
             MutableComponent text, String prefix, List<String> suffix, String description) {
         MutableComponent clickComponent = new TextComponent("");
         {
-            clickComponent.setStyle(clickComponent.getStyle()
-                    .withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/" + prefix + " " + String.join(" ", suffix)))
-                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                            new TextComponent("Click here to run this commannd."))));
+            clickComponent.setStyle(
+                    clickComponent
+                            .getStyle()
+                            .withClickEvent(
+                                    new ClickEvent(
+                                            ClickEvent.Action.SUGGEST_COMMAND,
+                                            "/" + prefix + " " + String.join(" ", suffix)))
+                            .withHoverEvent(
+                                    new HoverEvent(
+                                            HoverEvent.Action.SHOW_TEXT,
+                                            new TextComponent(
+                                                    "Click here to run this commannd."))));
 
             MutableComponent prefixText =
-                    new TextComponent("-" + prefix)
-                            .withStyle(ChatFormatting.DARK_GRAY);
+                    new TextComponent("-" + prefix).withStyle(ChatFormatting.DARK_GRAY);
             clickComponent.append(prefixText);
 
             if (!suffix.isEmpty()) {
