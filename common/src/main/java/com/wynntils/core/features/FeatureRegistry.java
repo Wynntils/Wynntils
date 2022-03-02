a6db5700462a710bb0992f3f999396b878727a8a
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2022-03-02 07:45:00 -0700

    change overlay impl

--- common/src/main/java/com/wynntils/core/features/FeatureRegistry.java
+++ common/src/main/java/com/wynntils/core/features/FeatureRegistry.java
@@ -15,37 +15,36 @@ import com.wynntils.mc.event.RenderEvent;
 import com.wynntils.mc.utils.CrashReportManager;
 import com.wynntils.mc.utils.McUtils;
 import com.wynntils.wc.utils.WynnUtils;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.LinkedList;
 import java.util.List;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 /** Loads {@link Feature}s */
 public class FeatureRegistry {
-    private static final List<Feature> FEATURES = new LinkedList<>();
-    private static final List<Overlay> OVERLAYS = new LinkedList<>();
+    private static final List<Feature> features = new LinkedList<>();
+    private static final OverlayListener overlayListener = new OverlayListener();
 
     public static void registerFeature(Feature feature) {
         if (feature instanceof Overlay overlay) {
-            OVERLAYS.add(overlay);
+            overlayListener.register(overlay);
         }
 
-        FEATURES.add(feature);
+        features.add(feature);
     }
 
     public static void registerFeatures(List<Feature> features) {
         features.forEach(FeatureRegistry::registerFeature);
     }
 
-    public static void registerOverlay(Overlay overlay) {
-        OVERLAYS.add(overlay);
-    }
-
     public static List<Feature> getFeatures() {
-        return FEATURES;
+        return features;
     }
 
     public static List<Overlay> getOverlays() {
-        return OVERLAYS;
+        return overlayListener.overlays;
     }
 
     public static void init() {
@@ -62,7 +61,7 @@ public class FeatureRegistry {
         registerFeature(new PlayerGhostTransparencyFeature());
         registerFeature(new ItemStatInfoFeature());
 
-        FEATURES.forEach(Feature::init);
+        features.forEach(Feature::init);
 
         WynntilsMod.getEventBus().register(OverlayListener.class);
 
@@ -81,7 +80,7 @@ public class FeatureRegistry {
                     public Object generate() {
                         StringBuilder result = new StringBuilder();
 
-                        for (Feature feature : FEATURES) {
+                        for (Feature feature : getFeatures()) {
                             if (feature.isEnabled()) {
                                 result.append("\n\t\t").append(feature.getClass().getName());
                             }
@@ -102,7 +101,7 @@ public class FeatureRegistry {
                     public Object generate() {
                         StringBuilder result = new StringBuilder();
 
-                        for (Overlay overlay : OVERLAYS) {
+                        for (Overlay overlay : getOverlays()) {
                             if (overlay.isEnabled()) {
                                 result.append("\n\t\t").append(overlay.getClass().getName());
                             }
@@ -114,39 +113,48 @@ public class FeatureRegistry {
     }
 
     private static class OverlayListener {
-        @SubscribeEvent
-        public static void onTick(ClientTickEvent e) {
-            if (e.getTickPhase() == ClientTickEvent.Phase.END) {
-                for (Overlay overlay : OVERLAYS) {
-                    overlay.tick();
-                }
+        private final List<Overlay> overlays = new LinkedList<>();
+        private final EnumMap<RenderEvent.ElementType, List<Overlay>> overlaysMap = new EnumMap<>(RenderEvent.ElementType.class);
+
+        public void register(Overlay overlay) {
+            overlays.add(overlay);
+
+            for (RenderEvent.ElementType type : overlay.hookElements) {
+                List<Overlay> overlayList = overlaysMap.computeIfAbsent(type, k -> new ArrayList<>());
+                overlayList.add(overlay);
             }
         }
 
         @SubscribeEvent
-        public static void onRenderPre(RenderEvent.Pre e) {
+        public void onTick(ClientTickEvent e) {
+            if (e.getTickPhase() == ClientTickEvent.Phase.END) {
+                McUtils.mc().getProfiler().push("tickOverlay");
+                for (Overlay overlay : overlays) {
+                    if (!overlay.isEnabled()) continue;
+
+                    overlay.tick();
+                }
+                McUtils.mc().getProfiler().pop();
+            }
+        }
+
+        @SubscribeEvent
+        public void onRenderPre(RenderEvent.Pre e) {
             if (!WynnUtils.onServer()) // || !McUtils.mc().playerController.isSpectator())
             return;
 
             McUtils.mc().getProfiler().push("preRenOverlay");
-            for (Overlay overlay : OVERLAYS) {
-                if (!overlay.visible) continue;
-                // if (!overlay.active) continue;
 
-                if (overlay.hookElements.length != 0) {
-                    boolean contained = false;
-                    for (RenderEvent.ElementType type : overlay.hookElements) {
-                        if (e.getType() == type) {
-                            contained = true;
-                            break;
-                        }
-                    }
+            List<Overlay> overlayList = overlaysMap.get(e.getType());
 
-                    if (contained && overlay.visible) {
-                        McUtils.mc().getProfiler().push(overlay.displayName);
-                        overlay.render(e);
-                        McUtils.mc().getProfiler().pop();
-                    }
+            if (overlayList != null) {
+                for (Overlay overlay : overlayList) {
+                    if (!overlay.visible) continue;
+                    if (!overlay.isEnabled()) continue;
+
+                    McUtils.mc().getProfiler().push(overlay.displayName);
+                    overlay.render(e);
+                    McUtils.mc().getProfiler().pop();
                 }
             }
 
@@ -156,26 +164,23 @@ public class FeatureRegistry {
         }
 
         @SubscribeEvent
-        public static void onRenderPost(RenderEvent.Post e) {
+        public void onRenderPost(RenderEvent.Post e) {
             if (!WynnUtils.onServer()) // || !McUtils.mc().playerController.isSpectator())
             return;
 
             McUtils.mc().getProfiler().push("postRenOverlay");
 
-            for (Overlay overlay : OVERLAYS) {
-                if (!overlay.visible)
-                    // if (!overlay.active) continue;
+            List<Overlay> overlayList = overlaysMap.get(e.getType());
 
-                    if (overlay.hookElements.length != 0) {
-                        for (RenderEvent.ElementType type : overlay.hookElements) {
-                            if (e.getType() == type) {
-                                McUtils.mc().getProfiler().push(overlay.displayName);
-                                overlay.render(e);
-                                McUtils.mc().getProfiler().pop();
-                                break;
-                            }
-                        }
-                    }
+            if (overlayList != null) {
+                for (Overlay overlay : overlayList) {
+                    if (!overlay.visible)
+                    if (!overlay.isEnabled()) continue;
+
+                    McUtils.mc().getProfiler().push(overlay.displayName);
+                    overlay.render(e);
+                    McUtils.mc().getProfiler().pop();
+                }
             }
 
             McUtils.mc().getProfiler().pop();
