883b15801bee0eb324e984b31052f319c703a61d
kristofbolyai <bolyai.bolyai@gmail.com>
2022-03-15 13:49:58 +0100

    Spotless

--- common/src/main/java/com/wynntils/mc/event/ContainerScreenInitEvent.java
+++ common/src/main/java/com/wynntils/mc/event/ContainerScreenInitEvent.java
@@ -1,16 +1,20 @@
+/*
+ * Copyright © Wynntils 2022.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
 package com.wynntils.mc.event;
 
+import java.util.function.Consumer;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 import net.minecraftforge.eventbus.api.Event;
-
-import java.util.function.Consumer;
 
 public class ContainerScreenInitEvent extends Event {
     private final ContainerScreen containerScreen;
     private final Consumer<AbstractWidget> addButton;
 
-    public ContainerScreenInitEvent(ContainerScreen containerScreen, Consumer<AbstractWidget> addButton) {
+    public ContainerScreenInitEvent(
+            ContainerScreen containerScreen, Consumer<AbstractWidget> addButton) {
         this.containerScreen = containerScreen;
         this.addButton = addButton;
     }
