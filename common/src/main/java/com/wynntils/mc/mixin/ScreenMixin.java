fb87ed39f4c318aaee8b6bce101bb138a967471b
kristofbolyai <bolyai.bolyai@gmail.com>
2022-04-05 19:51:12 +0200

    Fix failed mixin loading

--- common/src/main/java/com/wynntils/mc/mixin/ScreenMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/ScreenMixin.java
@@ -7,9 +7,7 @@ package com.wynntils.mc.mixin;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.wynntils.mc.EventFactory;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.Widget;
 import net.minecraft.client.gui.components.events.GuiEventListener;
-import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
     @Shadow
-    protected abstract <T extends GuiEventListener & Widget & NarratableEntry> T addRenderableWidget(T widget);
+    protected abstract GuiEventListener addRenderableWidget(GuiEventListener widget);
 
     @Inject(method = "init(Lnet/minecraft/client/Minecraft;II)V", at = @At("RETURN"))
     private void initPost(Minecraft client, int width, int height, CallbackInfo info) {
