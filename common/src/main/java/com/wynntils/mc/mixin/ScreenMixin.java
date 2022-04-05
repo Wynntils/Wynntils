44926ea849d9cf3cba2b1b05293cafc8db0d7b2b
kristofbolyai <bolyai.bolyai@gmail.com>
2022-04-06 00:38:28 +0200

    Fix @Shadow warning correctly.

--- common/src/main/java/com/wynntils/mc/mixin/ScreenMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/ScreenMixin.java
@@ -6,10 +6,14 @@ package com.wynntils.mc.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.wynntils.mc.EventFactory;
+import java.util.List;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Widget;
 import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,8 +22,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
+    @Final
     @Shadow
-    protected abstract GuiEventListener addRenderableWidget(GuiEventListener widget);
+    private List<GuiEventListener> children;
+
+    @Final
+    @Shadow
+    private List<NarratableEntry> narratables;
+
+    @Final
+    @Shadow
+    private List<Widget> renderables;
+
+    public <T extends GuiEventListener & Widget & NarratableEntry> T addRenderableWidget(T widget) {
+        renderables.add((Widget) widget);
+        return addWidget(widget);
+    }
+
+    public <T extends GuiEventListener & NarratableEntry> T addWidget(T listener) {
+        children.add(listener);
+        narratables.add((NarratableEntry) listener);
+        return listener;
+    }
 
     @Inject(method = "init(Lnet/minecraft/client/Minecraft;II)V", at = @At("RETURN"))
     private void initPost(Minecraft client, int width, int height, CallbackInfo info) {
