c53571cac2fa02ff8a86627cb3be0763bffb7623
Scyu_ <scyu@scyu.dev>
2022-04-05 17:43:36 +0100

    Test auto formatting

--- common/src/main/java/com/wynntils/commands/ServerCommand.java
+++ common/src/main/java/com/wynntils/commands/ServerCommand.java
@@ -12,16 +12,17 @@ import com.wynntils.core.webapi.WebManager;
 import com.wynntils.mc.utils.commands.CommandBase;
 import com.wynntils.utils.StringUtils;
 import com.wynntils.wc.utils.WynnUtils;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TextComponent;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class ServerCommand extends CommandBase {
     @Override
@@ -61,7 +62,7 @@ public class ServerCommand extends CommandBase {
     private int serverHelp(CommandContext<CommandSourceStack> context) {
         MutableComponent text =
                 new TextComponent(
-                                """
+                        """
                                 /s <command> [options]
 
                                 commands:
@@ -160,10 +161,10 @@ public class ServerCommand extends CommandBase {
 
             message.append(
                     new TextComponent(
-                                    StringUtils.capitalizeFirst(serverType)
-                                            + " ("
-                                            + currentTypeServers.size()
-                                            + "):\n")
+                            StringUtils.capitalizeFirst(serverType)
+                                    + " ("
+                                    + currentTypeServers.size()
+                                    + "):\n")
                             .withStyle(ChatFormatting.GOLD));
 
             message.append(
