d5243def39dec33819adac1d1d9c52146cc9c9af
kristofbolyai <bolyai.bolyai@gmail.com>
2022-04-06 13:53:03 +0200

    Fix Bossbar NPE

--- common/src/main/java/com/wynntils/mc/mixin/BossHealthOverlayMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/BossHealthOverlayMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.client.gui.components.LerpingBossEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundBossEventPacket;
 import net.minecraft.world.BossEvent;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -31,46 +32,52 @@ public abstract class BossHealthOverlayMixin {
                             target =
                                     "Lnet/minecraft/network/protocol/game/ClientboundBossEventPacket;dispatch(Lnet/minecraft/network/protocol/game/ClientboundBossEventPacket$Handler;)V"))
     private void updatePre(ClientboundBossEventPacket packet, ClientboundBossEventPacket.Handler handler) {
+        Map<UUID, LerpingBossEvent> _events = events;
+
         packet.dispatch(new ClientboundBossEventPacket.Handler() {
             public void add(
-                    UUID id,
-                    Component name,
+                    @NotNull UUID id,
+                    @NotNull Component name,
                     float progress,
-                    BossEvent.BossBarColor color,
-                    BossEvent.BossBarOverlay overlay,
+                    BossEvent.@NotNull BossBarColor color,
+                    BossEvent.@NotNull BossBarOverlay overlay,
                     boolean darkenScreen,
                     boolean playMusic,
                     boolean createWorldFog) {
-                events.put(
+                _events.put(
                         id,
                         new LerpingBossEvent(
                                 id, name, progress, color, overlay, darkenScreen, playMusic, createWorldFog));
             }
 
-            public void remove(UUID id) {
-                events.remove(id);
+            public void remove(@NotNull UUID id) {
+                _events.remove(id);
             }
 
-            public void updateProgress(UUID id, float progress) {
-                if (!events.containsKey(id)) return;
-                ((LerpingBossEvent) events.get(id)).setProgress(progress);
+            public void updateProgress(@NotNull UUID id, float progress) {
+                if (!_events.containsKey(id)) return;
+                (_events.get(id)).setProgress(progress);
             }
 
-            public void updateName(UUID id, Component name) {
-                if (!events.containsKey(id)) return;
-                ((LerpingBossEvent) events.get(id)).setName(name);
+            public void updateName(@NotNull UUID id, @NotNull Component name) {
+                if (!_events.containsKey(id)) return;
+                (_events.get(id)).setName(name);
             }
 
-            public void updateStyle(UUID id, BossEvent.BossBarColor color, BossEvent.BossBarOverlay overlay) {
-                if (!events.containsKey(id)) return;
-                LerpingBossEvent lerpingBossEvent = (LerpingBossEvent) events.get(id);
+            public void updateStyle(
+                    @NotNull UUID id,
+                    BossEvent.@NotNull BossBarColor color,
+                    BossEvent.@NotNull BossBarOverlay overlay) {
+                if (!_events.containsKey(id)) return;
+                LerpingBossEvent lerpingBossEvent = _events.get(id);
                 lerpingBossEvent.setColor(color);
                 lerpingBossEvent.setOverlay(overlay);
             }
 
-            public void updateProperties(UUID id, boolean darkenScreen, boolean playMusic, boolean createWorldFog) {
-                if (!events.containsKey(id)) return;
-                LerpingBossEvent lerpingBossEvent = (LerpingBossEvent) events.get(id);
+            public void updateProperties(
+                    @NotNull UUID id, boolean darkenScreen, boolean playMusic, boolean createWorldFog) {
+                if (!_events.containsKey(id)) return;
+                LerpingBossEvent lerpingBossEvent = _events.get(id);
                 lerpingBossEvent.setDarkenScreen(darkenScreen);
                 lerpingBossEvent.setPlayBossMusic(playMusic);
                 lerpingBossEvent.setCreateWorldFog(createWorldFog);
