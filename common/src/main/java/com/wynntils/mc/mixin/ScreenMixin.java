06b4c7b9cba7584a304f0c03aa0567c2e922e2fa
kristofbolyai <bolyai.bolyai@gmail.com>
2022-04-06 13:45:22 +0200

    Spotless

--- common/src/main/java/com/wynntils/mc/mixin/ScreenMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/ScreenMixin.java
@@ -24,7 +24,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class ScreenMixin {
     @Final
     @Shadow
-
     private List<GuiEventListener> children;
 
     @Final
