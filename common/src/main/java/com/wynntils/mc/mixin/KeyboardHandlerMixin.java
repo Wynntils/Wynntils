45167d0a2399b8bc0eb69c7d2c85ab25aea656e3
DonkeyBlaster69 <57310593+DonkeyBlaster@users.noreply.github.com>
2022-05-05 09:43:59 -0700

    Rename mixin, add translation

--- common/src/main/java/com/wynntils/mc/mixin/KeyboardHandlerMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/KeyboardHandlerMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyboardHandler.class)
-public abstract class KeyInputMixin {
+public abstract class KeyboardHandlerMixin {
     @Inject(method = "keyPress(JIIII)V", at = @At("RETURN"))
     private void handleKeyPressPost(
             long windowPointer, int key, int scanCode, int action, int modifiers, CallbackInfo ci) {
