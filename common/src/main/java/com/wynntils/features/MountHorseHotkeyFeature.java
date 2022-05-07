d7307814e9e8b08aab4b05cd4c28873bc8fe827c
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2022-05-07 05:20:43 -0700

    Cleanup, adds horse spawning

--- common/src/main/java/com/wynntils/features/MountHorseHotkeyFeature.java
+++ common/src/main/java/com/wynntils/features/MountHorseHotkeyFeature.java
@@ -6,13 +6,18 @@ import com.wynntils.core.features.properties.FeatureInfo;
 import com.wynntils.core.features.properties.GameplayImpact;
 import com.wynntils.core.features.properties.PerformanceImpact;
 import com.wynntils.core.features.properties.Stability;
+import com.wynntils.mc.event.PacketEvent.PacketReceivedEvent;
 import com.wynntils.mc.utils.McUtils;
 import com.wynntils.mc.utils.keybinds.KeyHolder;
 import com.wynntils.mc.utils.keybinds.KeyManager;
+import com.wynntils.utils.Delay;
+import com.wynntils.wc.utils.WynnUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.network.protocol.game.ServerboundUseItemPacket;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
@@ -21,6 +26,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.phys.AABB;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.List;
@@ -28,7 +34,9 @@ import java.util.List;
 @FeatureInfo(stability = Stability.STABLE, gameplay = GameplayImpact.MEDIUM, performance = PerformanceImpact.SMALL)
 public class MountHorseHotkeyFeature extends Feature {
 
-    private static final int searchRadius = 18;
+    private static final int searchRadius = 8;
+    private static final int summonAttempts = 3;
+    private static final int summonDelayTicks = 5;
     public static boolean mountNextHorseSpawn = false;
 
     public enum MountHorseStatus {
@@ -42,46 +50,70 @@ public class MountHorseHotkeyFeature extends Feature {
             return;
         }
 
-        Entity horse = searchForNearbyHorses();
+        Entity horse = searchForHorseNearby();
         Player player = McUtils.player();
         if (horse == null) { // Horse has not spawned, we should do that
             int horseInventorySlot = findHorseInInventory();
             if (horseInventorySlot > 8 || horseInventorySlot == -1) {
                 postHorseErrorMessage(MountHorseStatus.NO_HORSE);
+                return;
             }
-            int prevItem = McUtils.player().getInventory().selected;
-            player.getInventory().selected = horseInventorySlot;
-            McUtils.player().connection.send(new ServerboundUseItemPacket(InteractionHand.MAIN_HAND));
-            player.getInventory().selected = prevItem;
-            mountNextHorseSpawn = true;
+            System.out.println(horseInventorySlot);
+            trySummonHorse(horseInventorySlot, summonAttempts);
         } else { // Horse already exists, mount it
             mountHorse(horse);
         }
     });
 
-    public static MountHorseStatus mountHorse(Entity horse) {
+    private static void mountHorse(Entity horse) {
         // Horse should be nearby when this is called
         Player player = McUtils.player();
 
         // swap to soul points to avoid right click problems
         int prevItem = player.getInventory().selected;
         player.getInventory().selected = 8;
-        player.interactOn(horse, InteractionHand.MAIN_HAND);
+        McUtils.player().connection.send(ServerboundInteractPacket.createInteractionPacket(horse, false, InteractionHand.MAIN_HAND));
         player.getInventory().selected = prevItem;
     }
 
-    public static boolean isPlayersHorse(Entity horse, String playerName) {
+    private static void trySummonHorse(int horseInventorySlot, int attempts) {
+        if (attempts <= 0) {
+            postHorseErrorMessage(MountHorseStatus.NO_HORSE);
+            return;
+        }
+
+        Player player = McUtils.player();
+        int prevItem = McUtils.player().getInventory().selected;
+
+        new Delay(() -> {
+            player.getInventory().selected = horseInventorySlot;
+            System.out.println("sending rclick to slot " + player.getInventory().selected + " with attempts " + attempts);
+            McUtils.player().connection.send(new ServerboundUseItemPacket(InteractionHand.MAIN_HAND));
+
+            if (searchForHorseNearby() != null) {
+                System.out.println("success on " + attempts);
+                player.getInventory().selected = prevItem;
+                mountNextHorseSpawn = true;
+                return;
+            }
+            trySummonHorse(horseInventorySlot, attempts - 1);
+        }, summonDelayTicks);
+    }
+
+    private static boolean isPlayersHorse(Entity horse, String playerName) {
         return (horse instanceof AbstractHorse) && isPlayersHorse(horse.getCustomName(), playerName);
     }
 
-    public static boolean isPlayersHorse(Component horseName, String playerName) {
+    private static boolean isPlayersHorse(Component horseName, String playerName) {
         String defaultName = "§f" + playerName + "§7" + "'s horse";
         String customSuffix = "§7" + " [" + playerName + "]";
+
+        if (horseName == null) return false;
 
         return defaultName.equals(horseName.getString()) || horseName.getString().endsWith(customSuffix);
     }
 
-    private static Entity searchForNearbyHorses() {
+    private static Entity searchForHorseNearby() {
         Player player = McUtils.player();
 
         List<AbstractHorse> horses = McUtils.mc().level.getEntitiesOfClass(AbstractHorse.class, new AABB(
@@ -134,5 +166,20 @@ public class MountHorseHotkeyFeature extends Feature {
     @Override
     protected void onDisable() {
         KeyManager.unregisterKeybind(mountHorseKeybind);
+    }
+
+    @SubscribeEvent
+    public void onHorseDataReceived(PacketReceivedEvent<ClientboundSetEntityDataPacket> e) {
+        System.out.println("evt f");
+        mountNextHorseSpawn = true;
+        if (!WynnUtils.onWorld() || !mountNextHorseSpawn) return;
+        System.out.println("passed first return");
+        int id = e.getPacket().getId();
+        Entity entity = McUtils.mc().level.getEntity(id);
+        System.out.println(entity.getDisplayName().getString());
+        if (!(entity instanceof AbstractHorse) || e.getPacket().getUnpackedData().isEmpty()) return;
+
+        Player player = McUtils.player();
+        System.out.println(e.getPacket().getUnpackedData());
     }
 }
