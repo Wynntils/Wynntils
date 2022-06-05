2ed77380a4d01e328ac46f0eb51dbe343ca65c71
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2022-06-05 07:35:32 +0200

    MythicBlockerFeature: Make check stricter by checking item type (and util housekeeping) (#84)
    * MythicBlockerFeature: Make check stricter by checking item type
    
    * MythicBlockerFeature: Move methods to correct Util files

--- common/src/main/java/com/wynntils/features/user/MythicBlockerFeature.java
+++ common/src/main/java/com/wynntils/features/user/MythicBlockerFeature.java
@@ -9,9 +9,9 @@ import com.wynntils.core.features.properties.EventListener;
 import com.wynntils.core.features.properties.FeatureInfo;
 import com.wynntils.core.features.properties.FeatureInfo.Stability;
 import com.wynntils.mc.event.ContainerCloseEvent;
-import com.wynntils.mc.utils.ItemUtils;
 import com.wynntils.mc.utils.McUtils;
 import com.wynntils.wc.utils.ContainerUtils;
+import com.wynntils.wc.utils.WynnItemMatchers;
 import com.wynntils.wc.utils.WynnUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.NonNullList;
@@ -31,7 +31,8 @@ public class MythicBlockerFeature extends UserFeature {
         NonNullList<ItemStack> items = ContainerUtils.getItems(McUtils.mc().screen);
         for (int i = 0; i < 27; i++) {
             ItemStack stack = items.get(i);
-            if (ItemUtils.isMythic(stack)) {
+            if (!WynnItemMatchers.isWynnItem(stack) && !WynnItemMatchers.isUnidentified(stack)) continue;
+            if (WynnItemMatchers.isMythic(stack)) {
                 McUtils.sendMessageToClient(new TranslatableComponent("feature.wynntils.mythicBlocker.closingBlocked")
                         .withStyle(ChatFormatting.RED));
                 e.setCanceled(true);
