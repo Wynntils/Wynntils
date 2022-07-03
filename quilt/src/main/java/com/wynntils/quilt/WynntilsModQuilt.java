25d6ffcc1c10717fc86b2c16b600e4e7b30438e1
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-07-03 13:57:37 +0200

    All: Add quilt loader support :)

---
+++ quilt/src/main/java/com/wynntils/quilt/WynntilsModQuilt.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © Wynntils 2022.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.quilt;
+
+import com.wynntils.core.WynntilsMod;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class WynntilsModQuilt implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(ModContainer mod) {
+        WynntilsMod.init(mod.metadata().version().raw());
+    }
+}
