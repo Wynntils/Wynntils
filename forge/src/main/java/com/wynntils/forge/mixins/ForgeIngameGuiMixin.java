1ae272e57ca7be0726eff090a3c778413d95f6dc
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-07-08 03:47:43 +0200

    Forge: Add forge side mixins

---
+++ forge/src/main/java/com/wynntils/forge/mixins/ForgeIngameGuiMixin.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © Wynntils 2022.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.forge.mixins;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.wynntils.mc.EventFactory;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.gui.ForgeIngameGui;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ForgeIngameGui.class)
+public abstract class ForgeIngameGuiMixin {
+
+    // TODO: This is kinda hacky, but we can't shadow minecraft here
+    //       so we have to use the instance.
+
+    @Inject(method = "render", at = @At(value = "HEAD"))
+    public void onRenderGuiPre(PoseStack poseStack, float partialTick, CallbackInfo ci) {
+        EventFactory.onRenderGuiPre(
+                poseStack, partialTick, Minecraft.getInstance().getWindow());
+    }
+
+    @Inject(method = "render", at = @At(value = "RETURN"))
+    public void onRenderGuiPost(PoseStack poseStack, float partialTick, CallbackInfo ci) {
+        EventFactory.onRenderGuiPost(
+                poseStack, partialTick, Minecraft.getInstance().getWindow());
+    }
+}
