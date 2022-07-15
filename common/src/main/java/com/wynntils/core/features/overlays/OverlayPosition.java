a487f4a52bc5a11a29540db8b88877abc28c7a4b
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-07-15 21:03:23 +0200

    OverlayPosition: Change stupid comment

--- common/src/main/java/com/wynntils/core/features/overlays/OverlayPosition.java
+++ common/src/main/java/com/wynntils/core/features/overlays/OverlayPosition.java
@@ -124,7 +124,7 @@ public class OverlayPosition {
         float renderX = Overlay.getRenderX(newOverlayPositionTemp, overlay);
         float renderY = Overlay.getRenderY(newOverlayPositionTemp, overlay);
 
-        // 4. Calculate the offsets to match the current render position, but factor in offsets
+        // 4. Calculate the alignment offsets to match the current render position, but factor in argument offsets
         return new OverlayPosition(
                 oldRenderY - renderY + offsetY,
                 oldRenderX - renderX + offsetX,
