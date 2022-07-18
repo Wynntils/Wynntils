4f6d20dccbbbac46c923d176cd31e547183fff90
Toma <62384384+TomaSajt@users.noreply.github.com>
2022-07-18 11:54:51 +0200

    Add ActionBarManager (#168)
    * Add ActionBarParserFeature to update CharacterInfo
    
    * Use ActionBarManager instead of ActionBarParserFeature
    
    * Remove unused fields from CharacterInfo
    
    * Use [0-9] instead of \d in pattern
    
    * Use new health getters in HealthPotionBlockerFeature
    
    * Update regex pattern and field starting values
    
    Co-authored-by: Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>

---
+++ common/src/main/java/com/wynntils/wc/utils/ActionBarManager.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright © Wynntils 2022.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.wc.utils;
+
+import com.wynntils.core.WynntilsMod;
+import com.wynntils.mc.event.PacketEvent.PacketReceivedEvent;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.protocol.game.ClientboundChatPacket;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ActionBarManager {
+
+    private static final Pattern ACTIONBAR_PATTERN =
+            Pattern.compile("§❤ ([0-9]+)/([0-9]+)§ +(.+?) +§✺ ([0-9]+)/([0-9]+)".replace("§", "(?:§[0-9a-fklmnor])*"));
+
+    private static String previousActionBar = null;
+
+    private static int currentHealth = -1;
+    private static int maxHealth = -1;
+    private static int currentMana = -1;
+    private static int maxMana = -1;
+
+    // The server doesn't send SetActionBarPackets, instead it sends ChatPackets with the ChatType GAME_INFO
+    @SubscribeEvent
+    public static void onActionBarUpdate(PacketReceivedEvent<ClientboundChatPacket> e) {
+        if (!WynnUtils.onWorld()) return;
+
+        ClientboundChatPacket packet = e.getPacket();
+        if (packet.getType() != ChatType.GAME_INFO) return;
+
+        String actionBar = packet.getMessage().getString();
+        // don't parse unchanged actionbar
+        if (actionBar.equals(previousActionBar)) return;
+        previousActionBar = actionBar;
+
+        Matcher matcher = ACTIONBAR_PATTERN.matcher(actionBar);
+        if (!matcher.matches()) return;
+
+        currentHealth = Integer.parseInt(matcher.group(1));
+        maxHealth = Integer.parseInt(matcher.group(2));
+        currentMana = Integer.parseInt(matcher.group(4));
+        maxMana = Integer.parseInt(matcher.group(5));
+
+        String centerText = matcher.group(3);
+        // TODO: parse partial spell progress from centerText
+    }
+
+    public static void init() {
+        WynntilsMod.getEventBus().register(ActionBarManager.class);
+    }
+
+    public static int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public static int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public static int getCurrentMana() {
+        return currentMana;
+    }
+
+    public static int getMaxMana() {
+        return maxMana;
+    }
+}
