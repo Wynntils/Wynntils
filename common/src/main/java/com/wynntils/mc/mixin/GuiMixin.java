fe39b454ed802f3b8233f8e8caa16b8e6d947fd3
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2022-07-27 01:26:28 +0200

    Overlays: Add health bar and mana bar overlays (#173)
    * Overlays: Add CustomBarsFeature (health bar)
    
    * Overlays: Add CustomBarsFeature (health and mana bar)
    
    * Mixins: Fix forge gui mixins
    
    * Overlays: Fix field naming
    
    * RenderUtils: Refactor drawProgressBar

--- common/src/main/java/com/wynntils/mc/mixin/GuiMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/GuiMixin.java
@@ -4,10 +4,13 @@
  */
 package com.wynntils.mc.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.wynntils.mc.EventFactory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
@@ -15,6 +18,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Gui.class)
@@ -55,5 +59,47 @@ public abstract class GuiMixin {
                 .isCanceled()) {
             ci.cancel();
         }
+    }
+
+    @Inject(method = "renderHearts", at = @At(value = "HEAD"), cancellable = true)
+    public void onRenderHeartsPre(
+            PoseStack poseStack,
+            Player player,
+            int x,
+            int y,
+            int height,
+            int i,
+            float f,
+            int j,
+            int k,
+            int l,
+            boolean bl,
+            CallbackInfo ci) {
+        if (EventFactory.onRenderHearthsPre(poseStack, this.minecraft.getWindow())
+                .isCanceled()) {
+            ci.cancel();
+        }
+
+        RenderSystem.setShaderTexture(0, GuiComponent.GUI_ICONS_LOCATION); // we have to reset shader texture
+    }
+
+    // This doesn't work on forge. See ForgeIngameGuiMixin for replacement.
+    @Redirect(
+            method = "renderPlayerHealth",
+            at =
+                    @At(
+                            value = "INVOKE",
+                            target =
+                                    "Lnet/minecraft/client/gui/Gui;getVehicleMaxHearts(Lnet/minecraft/world/entity/LivingEntity;)I"))
+    public int onRenderFoodPre(Gui instance, LivingEntity mountEntity) {
+        if (EventFactory.onRenderFoodPre(new PoseStack(), this.minecraft.getWindow())
+                .isCanceled()) {
+
+            RenderSystem.setShaderTexture(0, GuiComponent.GUI_ICONS_LOCATION); // we have to reset shader texture
+            return 1;
+        }
+
+        RenderSystem.setShaderTexture(0, GuiComponent.GUI_ICONS_LOCATION); // we have to reset shader texture
+        return 0;
     }
 }
