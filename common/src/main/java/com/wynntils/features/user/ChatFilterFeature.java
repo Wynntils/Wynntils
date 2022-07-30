76c5c8c1c4f342f5568aceb9f8088284b03266d8
Magnus Ihse Bursie <mag@icus.se>
2022-07-31 00:32:32 +0200

    Refactor how recipient kinds are handled

--- common/src/main/java/com/wynntils/features/user/ChatFilterFeature.java
+++ common/src/main/java/com/wynntils/features/user/ChatFilterFeature.java
@@ -11,22 +11,13 @@ import com.wynntils.core.notifications.NotificationManager;
 import com.wynntils.mc.utils.ComponentUtils;
 import com.wynntils.wc.event.ChatMessageReceivedEvent;
 import com.wynntils.wc.utils.WynnUtils;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 @FeatureInfo
 public class ChatFilterFeature extends UserFeature {
-    private static final Pattern GLOBAL_CHAT =
-            Pattern.compile("^§8\\[(Lv\\. )?\\d+\\*?/\\d+/..(/[^]]+)?\\]§r§7 \\[[A-Z0-9]+\\]§r.*$");
-    private static final Pattern LOCAL_CHAT = Pattern.compile("^§7\\[(Lv. )?\\d+\\*?/\\d+/..(/[^]]+)\\]§r.*$");
-    private static final Pattern GUILD_CHAT = Pattern.compile("^(§r)?§3\\[(§b★+§3)?.*§3]§b .*$");
-    private static final Pattern PARTY_CHAT = Pattern.compile("^§7\\[§r§e.*§r§7\\] §r§f.*$");
-    private static final Pattern PRIVATE_CHAT = Pattern.compile("^§7\\[§r§5.*§r§6 ➤ §r§5.*§r§7\\] §r§f.*$");
-    private static final Pattern SHOUT = Pattern.compile("^§3.* \\[[A-Z0-9]+\\] shouts: §r§b.*$");
-
     private static final Pattern PRE_WELCOME_1 = Pattern.compile("^§7Loading Resource Pack...$");
     private static final Pattern PRE_WELCOME_2 =
             Pattern.compile("^§6Thank you for using the WynnPack. Enjoy the game!$");
@@ -37,8 +28,6 @@ public class ChatFilterFeature extends UserFeature {
     private static final Pattern WELCOME_2 = Pattern.compile("^ +§fplay.wynncraft.com §7-/-§f wynncraft.com$");
 
     private static final Pattern SYSTEM_INFO = Pattern.compile("^(§r)?§.\\[Info\\] .*$");
-    private static final Pattern GUILD_INFO = Pattern.compile("^§3\\[INFO§3\\]§b (.*)$");
-    private static final Pattern WAR_INFO = Pattern.compile("^§3\\[WAR§3\\]§c (.*)$");
 
     private static final Pattern SOUL_POINT_1 = Pattern.compile("^§5As the sun rises, you feel a little bit safer...$");
     private static final Pattern SOUL_POINT_2 = Pattern.compile("^§d\\[(\\+\\d+ Soul Points?)\\]$");
@@ -50,22 +39,11 @@ public class ChatFilterFeature extends UserFeature {
     private static final Pattern VIP_LOGIN =
             Pattern.compile("^§.\\[§r§.[A-Z+]+§r§.\\] §r§..*§r§. has just logged in!$");
 
-    private static final Pattern BACKGROUND_GLOBAL_CHAT = Pattern.compile(
-            "^(§r§8)?\\[(Lv\\. )?\\d+\\*?/\\d+/..(/[^]]+)?\\] \\[[A-Z0-9]+\\](§r§7)?( \\[(§k\\|)?§r§.[A-Z+]+§r§.(§k\\|§r§7)?\\])?(§r§7)? (§r§8)?.*$");
-    private static final Pattern BACKGROUND_LOCAL_CHAT = Pattern.compile(
-            "^(§r§8)?\\[(Lv. )?\\d+\\*?/\\d+/..(/[^]]+)\\]( \\[(§k\\|)?§r§.[A-Z+]+§r§.(§k\\|§r§7)?\\])?(§r§7)? (§r§8)?.*$");
-    private static final Pattern BACKGROUND_GUILD_CHAT = Pattern.compile("^(§r§8)?\\[(§r§7★+§r§8)?.*]§r§7 .*$");
-    private static final Pattern BACKGROUND_PARTY_CHAT = Pattern.compile("^§r§8\\[§r§7.*§r§8\\] §r§7.*$");
-    private static final Pattern BACKGROUND_PRIVATE_CHAT = Pattern.compile("^(§r§8)?\\[.* ➤ .*\\] §r§7.*$");
-    private static final Pattern BACKGROUND_SHOUT = Pattern.compile("^(§r§8)?.* \\[[A-Z0-9]+\\] shouts: §r§7.*$");
-
     private static final Pattern BACKGROUND_WELCOME_1 = Pattern.compile("^ +§6§lWelcome to Wynncraft!$");
     private static final Pattern BACKGROUND_WELCOME_2 =
             Pattern.compile("^ +§fplay.wynncraft.com §7-/-§f wynncraft.com$");
 
     private static final Pattern BACKGROUND_SYSTEM_INFO = Pattern.compile("^(§r§8)?\\[Info\\] .*$");
-    private static final Pattern BACKGROUND_GUILD_INFO = Pattern.compile("TODO_MISSING");
-    private static final Pattern BACKGROUND_WAR_INFO = Pattern.compile("^(§r§8)?\\[WAR\\]§r§7 (.*)$");
 
     private static final Pattern BACKGROUND_SOUL_POINT_1 =
             Pattern.compile("^(§r§8)?As the sun rises, you feel a little bit safer...$");
@@ -93,12 +71,6 @@ public class ChatFilterFeature extends UserFeature {
     @Config
     private boolean redirectSoulPoint = true;
 
-    @Config
-    private boolean redirectGuildInfo = true;
-
-    @Config
-    private boolean redirectWarInfo = true;
-
     @SubscribeEvent
     public void onChatMessage(ChatMessageReceivedEvent e) {
         if (!WynnUtils.onServer()) return;
@@ -107,32 +79,24 @@ public class ChatFilterFeature extends UserFeature {
 
         System.out.println("We got chat1: " + msg + "system:" + e.getType());
 
-        if (e.getType() == ChatMessageReceivedEvent.MessageType.NORMAL) {
-            if (GLOBAL_CHAT.matcher(msg).find()) {
-                handleGlobalChat(e);
-                return;
-            }
-            if (LOCAL_CHAT.matcher(msg).find()) {
-                handleLocalChat(e);
-                return;
-            }
-            if (GUILD_CHAT.matcher(msg).find()) {
-                handleGuildChat(e);
-                return;
-            }
-            if (PARTY_CHAT.matcher(msg).find()) {
-                handlePartyChat(e);
-                return;
-            }
-            if (PRIVATE_CHAT.matcher(msg).find()) {
-                handlePrivateChat(e);
-                return;
-            }
-            if (SHOUT.matcher(msg).find()) {
-                handleShout(e);
-                return;
+        if (e.getType() == ChatMessageReceivedEvent.MessageType.SYSTEM) {
+            // System type messages can only be "info" messages
+            handleChatType(e, msg, RecipientType.INFO, ChatMessageReceivedEvent.MessageType.SYSTEM);
+        } else {
+            for (RecipientType recipient : RecipientType.values()) {
+                if (recipient.matchPattern(msg, e.getType())) {
+                    handleChatType(e, msg, recipient, e.getType());
+                    return;
+                }
             }
 
+            // If no specific recipient matched, it is an "info" message
+            handleChatType(e, msg, RecipientType.INFO, e.getType());
+        }
+    }
+
+    private void filterInfoMessage(ChatMessageReceivedEvent e, String msg, ChatMessageReceivedEvent.MessageType type) {
+        if (type == ChatMessageReceivedEvent.MessageType.NORMAL) {
             if (hideSystemInfo) {
                 if (SYSTEM_INFO.matcher(msg).find()) {
                     e.setCanceled(true);
@@ -146,28 +110,7 @@ public class ChatFilterFeature extends UserFeature {
                     return;
                 }
             }
-
-            if (redirectGuildInfo) {
-                Matcher m = GUILD_INFO.matcher(msg);
-                if (m.find()) {
-                    e.setCanceled(true);
-                    NotificationManager.queueMessage(ComponentUtils.stripFormattingCodes(m.group(1)));
-                    return;
-                }
-            }
-
-            if (redirectWarInfo) {
-                Matcher m = WAR_INFO.matcher(msg);
-                if (m.find()) {
-                    e.setCanceled(true);
-                    NotificationManager.queueMessage(ComponentUtils.stripFormattingCodes(m.group(1)));
-                    return;
-                }
-            }
-
-            System.out.println("UNHANDLED_NORMAL: " + msg);
-            e.setMessage(new TextComponent("UNHANDLED_NORMAL: ").append(e.getMessage()));
-        } else if (e.getType() == ChatMessageReceivedEvent.MessageType.SYSTEM) {
+        } else if (type == ChatMessageReceivedEvent.MessageType.SYSTEM) {
             if (hideLevelUp) {
                 if (LEVEL_UP_1.matcher(msg).find() || LEVEL_UP_2.matcher(msg).find()) {
                     e.setCanceled(true);
@@ -205,59 +148,10 @@ public class ChatFilterFeature extends UserFeature {
                     return;
                 }
             }
-
-            System.out.println("UNHANDLED_SYSTEM: " + msg);
-            e.setMessage(new TextComponent("UNHANDLED_SYSTEM: ").append(e.getMessage()));
-        } else if (e.getType() == ChatMessageReceivedEvent.MessageType.BACKGROUND) {
-            System.out.println("BACKGROUND: " + msg);
-            e.setMessage(new TextComponent("BACKGROUND: ").append(e.getMessage()));
-
-            if (BACKGROUND_GLOBAL_CHAT.matcher(msg).find()) {
-                handleGlobalChat(e);
-                return;
-            }
-            if (BACKGROUND_LOCAL_CHAT.matcher(msg).find()) {
-                handleLocalChat(e);
-                return;
-            }
-            if (BACKGROUND_GUILD_CHAT.matcher(msg).find()) {
-                handleGuildChat(e);
-                return;
-            }
-            if (BACKGROUND_PARTY_CHAT.matcher(msg).find()) {
-                handlePartyChat(e);
-                return;
-            }
-            if (BACKGROUND_PRIVATE_CHAT.matcher(msg).find()) {
-                handlePrivateChat(e);
-                return;
-            }
-            if (BACKGROUND_SHOUT.matcher(msg).find()) {
-                handleShout(e);
-                return;
-            }
-
+        } else if (type == ChatMessageReceivedEvent.MessageType.BACKGROUND) {
             if (hideSystemInfo) {
                 if (BACKGROUND_SYSTEM_INFO.matcher(msg).find()) {
                     e.setCanceled(true);
-                    return;
-                }
-            }
-
-            if (redirectGuildInfo) {
-                Matcher m = BACKGROUND_GUILD_INFO.matcher(msg);
-                if (m.find()) {
-                    e.setCanceled(true);
-                    NotificationManager.queueMessage(ComponentUtils.stripFormattingCodes(m.group(1)));
-                    return;
-                }
-            }
-
-            if (redirectWarInfo) {
-                Matcher m = BACKGROUND_WAR_INFO.matcher(msg);
-                if (m.find()) {
-                    e.setCanceled(true);
-                    NotificationManager.queueMessage(ComponentUtils.stripFormattingCodes(m.group(1)));
                     return;
                 }
             }
@@ -299,33 +193,52 @@ public class ChatFilterFeature extends UserFeature {
                     return;
                 }
             }
-
-            System.out.println("UNHANDLED_BACKGROUND: " + msg);
-            e.setMessage(new TextComponent("UNHANDLED_BACKGROUND: ").append(e.getMessage()));
         }
     }
 
-    private void handlePartyChat(ChatMessageReceivedEvent e) {
-        e.setMessage(new TextComponent("PARTY: ").append(e.getMessage()));
+    private void handleChatType(
+            ChatMessageReceivedEvent e,
+            String msg,
+            RecipientType recipient,
+            ChatMessageReceivedEvent.MessageType type) {
+        if (recipient == RecipientType.INFO) {
+            filterInfoMessage(e, msg, type);
+            if (!e.isCanceled()) {
+                System.out.println("UNHANDLED-" + type.name() + ": " + msg);
+                e.setMessage(new TextComponent("UNHANDLED-" + type.name() + ": ").append(e.getMessage()));
+            }
+        } else {
+            e.setMessage(new TextComponent(type.name() + "-" + recipient.name() + ": ").append(e.getMessage()));
+        }
     }
 
-    private void handleGuildChat(ChatMessageReceivedEvent e) {
-        e.setMessage(new TextComponent("GUILD: ").append(e.getMessage()));
-    }
+    public enum RecipientType {
+        INFO(null, null),
+        GLOBAL(
+                "^§8\\[(Lv\\. )?\\d+\\*?/\\d+/..(/[^]]+)?\\]§r§7 \\[[A-Z0-9]+\\]§r.*$",
+                "^(§r§8)?\\[(Lv\\. )?\\d+\\*?/\\d+/..(/[^]]+)?\\] \\[[A-Z0-9]+\\](§r§7)?( \\[(§k\\|)?§r§.[A-Z+]+§r§.(§k\\|§r§7)?\\])?(§r§7)? (§r§8)?.*$"),
+        LOCAL(
+                "^§7\\[(Lv. )?\\d+\\*?/\\d+/..(/[^]]+)\\]§r.*$",
+                "^(§r§8)?\\[(Lv. )?\\d+\\*?/\\d+/..(/[^]]+)\\]( \\[(§k\\|)?§r§.[A-Z+]+§r§.(§k\\|§r§7)?\\])?(§r§7)? (§r§8)?.*$"),
+        GUILD("^(§r)?§3\\[(§b★+§3)?.*§3]§. .*$", "^(§r§8)?\\[(§r§7★+§r§8)?.*]§r§7 .*$"),
+        PARTY("^§7\\[§r§e.*§r§7\\] §r§f.*$", "^(§r§8)?\\[§r§7.*§r§8\\] §r§7.*$"),
+        PRIVATE("^§7\\[§r§5.*§r§6 ➤ §r§5.*§r§7\\] §r§f.*$", "^(§r§8)?\\[.* ➤ .*\\] §r§7.*$"),
+        SHOUT("^§3.* \\[[A-Z0-9]+\\] shouts: §r§b.*$", "^(§r§8)?.* \\[[A-Z0-9]+\\] shouts: §r§7.*$");
 
-    private void handleShout(ChatMessageReceivedEvent e) {
-        e.setMessage(new TextComponent("SHOUT: ").append(e.getMessage()));
-    }
+        private final Pattern normalPattern;
+        private final Pattern backgroundPattern;
 
-    private void handlePrivateChat(ChatMessageReceivedEvent e) {
-        e.setMessage(new TextComponent("PRIVATE: ").append(e.getMessage()));
-    }
+        RecipientType(String normalPattern, String backgroundPattern) {
+            this.normalPattern = (normalPattern == null ? null : Pattern.compile(normalPattern));
+            this.backgroundPattern = (backgroundPattern == null ? null : Pattern.compile(backgroundPattern));
+        }
 
-    private void handleLocalChat(ChatMessageReceivedEvent e) {
-        e.setMessage(new TextComponent("LOCAL: ").append(e.getMessage()));
-    }
-
-    private void handleGlobalChat(ChatMessageReceivedEvent e) {
-        e.setMessage(new TextComponent("GLOBAL: ").append(e.getMessage()));
+        public boolean matchPattern(String msg, ChatMessageReceivedEvent.MessageType type) {
+            assert (type == ChatMessageReceivedEvent.MessageType.NORMAL
+                    || type == ChatMessageReceivedEvent.MessageType.BACKGROUND);
+            Pattern pattern = (type == ChatMessageReceivedEvent.MessageType.NORMAL ? normalPattern : backgroundPattern);
+            if (pattern == null) return false;
+            return pattern.matcher(msg).find();
+        }
     }
 }
