d4837c1cfe09aefa20672e1e21d564f1a34fc52f
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-08-03 23:13:24 +0200

    Lootrun: Add line coloring, preparation for textured lines

--- common/src/main/java/com/wynntils/mc/render/Texture.java
+++ common/src/main/java/com/wynntils/mc/render/Texture.java
@@ -7,10 +7,11 @@ package com.wynntils.mc.render;
 import net.minecraft.resources.ResourceLocation;
 
 public enum Texture {
-    HIGHLIGHT("highlight.png", 256, 256),
     ARC("arc.png", 1024, 64),
-    ITEM_LOCK("item_lock.png", 16, 16),
     HEALTH_BAR("bars_health.png", 256, 256),
+    HIGHLIGHT("highlight.png", 256, 256),
+    ITEM_LOCK("item_lock.png", 16, 16),
+    LOOTRUN_LINE("path_arrow.png", 16, 16),
     MANA_BAR("bars_mana.png", 256, 256),
     OVERLAY_SELECTION_GUI("overlay_selection_gui.png", 195, 256);
 
