284ad1a57a39c7fa287a03cdaf923630ff6038d1
Magnus Ihse Bursie <mag@icus.se>
2022-08-03 16:16:12 +0200

    Fix the disconnect problems, for real this time (#205)
    Co-authored-by: Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>

--- common/src/main/java/com/wynntils/mc/mixin/ConnectionMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/ConnectionMixin.java
@@ -5,31 +5,18 @@
 package com.wynntils.mc.mixin;
 
 import com.wynntils.mc.EventFactory;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
 import net.minecraft.network.Connection;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.Packet;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Connection.class)
 public abstract class ConnectionMixin {
-    @Shadow
-    private Channel channel;
-
-    @Inject(method = "disconnect(Lnet/minecraft/network/chat/Component;)V", at = @At("HEAD"))
-    private void disconnectPre(Component message, CallbackInfo ci) {
-        if (!this.channel.isOpen()) return;
-
-        EventFactory.onDisconnect();
-    }
-
     @Inject(
             method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/protocol/Packet;)V",
             at = @At("HEAD"),
