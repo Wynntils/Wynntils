2a57d13015be02799ec04f6f019a92be3f66b7b0
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2022-08-07 15:39:35 -0700

    Load Main-map.png (#228)
    * map loading
    
    * test
    
    * spotless
    
    * fix exception temporarily
    
    * Fixes and more logging
    
    * change which is async
    
    * chore: spotless formatting
    
    * Hack is unnecessary
    
    * spotless
    
    * magicus changes
    
    * chore: spotless formatting
    
    * change return type of method
    
    * Final changes
    
    * Actual final commit
    
    * Update common/src/main/java/com/wynntils/core/webapi/WebManager.java
    
    Co-authored-by: Magnus Ihse Bursie <mag@icus.se>
    
    * Update common/src/main/java/com/wynntils/core/webapi/WebManager.java
    
    Co-authored-by: Magnus Ihse Bursie <mag@icus.se>
    
    * nullable
    
    * more changes
    
    * Update WebManager.java
    
    * fix npe
    
    * misspelling
    
    Co-authored-by: Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
    Co-authored-by: Incompleteusern <Incompleteusern@users.noreply.github.com>
    Co-authored-by: Magnus Ihse Bursie <mag@icus.se>
    Co-authored-by: HighCrit <35771251+HighCrit@users.noreply.github.com>

--- common/src/main/java/com/wynntils/core/webapi/request/RequestHandler.java
+++ common/src/main/java/com/wynntils/core/webapi/request/RequestHandler.java
@@ -61,6 +61,11 @@ public class RequestHandler {
         dispatch(false);
     }
 
+    /** Send all enqueued requests inside of a new thread and return that thread */
+    public Thread dispatchAsync() {
+        return dispatch(true);
+    }
+
     /** Enqueue a new {@link Request} and dispatches it */
     public void addAndDispatch(Request req, boolean async) {
         addRequest(req);
@@ -69,13 +74,12 @@ public class RequestHandler {
 
     /** Enqueue a new {@link Request} and dispatches it */
     public void addAndDispatch(Request req) {
-        addRequest(req);
-        dispatch(false);
+        addAndDispatch(req, false);
     }
 
-    /** Send all enqueued requests inside a new thread and return that thread */
-    public Thread dispatchAsync() {
-        return dispatch(true);
+    /** Enqueue a new {@link Request} and dispatches it */
+    public void addAndDispatchAsync(Request req) {
+        addAndDispatch(req, true);
     }
 
     public Thread dispatch(boolean async) {
@@ -145,7 +149,7 @@ public class RequestHandler {
                         }
                     } catch (FileNotFoundException ignore) {
                     } catch (Exception e) {
-                        WynntilsMod.warn("Error occurred whilst trying to use validated cache for " + req.id + " at "
+                        WynntilsMod.warn("Error occurred whilst trying to validate cache for " + req.id + " at "
                                 + req.cacheFile.getPath());
                         e.printStackTrace();
                     }
@@ -163,6 +167,8 @@ public class RequestHandler {
                                 req.onError();
                             }
                         } catch (FileNotFoundException ignore) {
+                            WynntilsMod.warn("Could not find file while trying to use cache as backup");
+                            req.onError();
                         } catch (Exception e) {
                             WynntilsMod.warn("Error occurred whilst trying to use cache for " + req.id + " at "
                                     + req.cacheFile.getPath());
@@ -171,9 +177,11 @@ public class RequestHandler {
                             req.onError();
                         }
                     } else {
+                        WynntilsMod.warn("Request was not satisfied before and using cache as backup is not set");
                         req.onError();
                     }
                 }
+
                 req.currentlyHandling = LoadingPhase.LOADED;
                 return null;
             });
@@ -228,6 +236,7 @@ public class RequestHandler {
         HttpURLConnection st;
         try {
             st = req.establishConnection();
+            st.setReadTimeout(0);
             if (st.getResponseCode() != 200) {
                 WynntilsMod.warn("Invalid response code for request");
                 st.disconnect();
