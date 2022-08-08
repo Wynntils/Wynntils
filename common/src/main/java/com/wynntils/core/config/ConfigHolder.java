7a5cc7fcff717602317626e6c16e967c5523ab7a
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-08-08 02:10:16 +0200

    ConfigHolder: Do not lose config type override

--- common/src/main/java/com/wynntils/core/config/ConfigHolder.java
+++ common/src/main/java/com/wynntils/core/config/ConfigHolder.java
@@ -55,7 +55,7 @@ public class ConfigHolder {
         this.defaultValue =
                 ConfigManager.getGson().fromJson(ConfigManager.getGson().toJson(getValue()), fieldTypeTemp);
 
-        if (this.defaultValue != null) {
+        if (typeOverride == null && this.defaultValue != null) {
             fieldTypeTemp = defaultValue.getClass();
         }
 
