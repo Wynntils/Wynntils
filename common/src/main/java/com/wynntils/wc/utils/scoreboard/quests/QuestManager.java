28c61bc6a821654c1de15b791b87c369f2574c45
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2022-08-09 15:47:15 +0200

    QuestManager: Add coloring to quest description

--- common/src/main/java/com/wynntils/wc/utils/scoreboard/quests/QuestManager.java
+++ common/src/main/java/com/wynntils/wc/utils/scoreboard/quests/QuestManager.java
@@ -10,8 +10,12 @@ import com.wynntils.wc.utils.scoreboard.ScoreboardHandler;
 import com.wynntils.wc.utils.scoreboard.ScoreboardManager;
 import com.wynntils.wc.utils.scoreboard.Segment;
 import java.util.List;
+import java.util.regex.Pattern;
+import net.minecraft.ChatFormatting;
 
 public class QuestManager implements ScoreboardHandler {
+    private static final Pattern COORDINATE_BRACKET_PATTERN = Pattern.compile("\\[-?\\d+,-?\\d+,-?\\d+\\]");
+
     private static QuestInfo currentQuest = null;
 
     public static QuestInfo getCurrentQuest() {
@@ -37,8 +41,48 @@ public class QuestManager implements ScoreboardHandler {
             }
         }
 
-        currentQuest = new QuestInfo(
-                questName.toString().trim(), description.toString().trim());
+        String descriptionTrimmed = description.toString().trim();
+
+        StringBuilder formattedDescriptionBuilder = new StringBuilder();
+
+        int openingBracket = descriptionTrimmed.indexOf('[');
+        int closingBracket = descriptionTrimmed.indexOf(']', openingBracket);
+
+        if (openingBracket != -1 && closingBracket != -1) {
+            formattedDescriptionBuilder.append(descriptionTrimmed, 0, openingBracket);
+
+            while (openingBracket != -1 && closingBracket != -1) {
+                String bracketPart = descriptionTrimmed.substring(openingBracket, closingBracket + 1);
+
+                ChatFormatting color =
+                        COORDINATE_BRACKET_PATTERN.matcher(bracketPart).matches()
+                                ? ChatFormatting.LIGHT_PURPLE
+                                : ChatFormatting.DARK_AQUA;
+                formattedDescriptionBuilder.append(color).append(bracketPart);
+
+                openingBracket = descriptionTrimmed.indexOf('[', closingBracket);
+
+                if (openingBracket != -1) {
+                    formattedDescriptionBuilder
+                            .append(ChatFormatting.RESET)
+                            .append(descriptionTrimmed, closingBracket + 1, openingBracket);
+                }
+
+                closingBracket = descriptionTrimmed.indexOf(']', openingBracket);
+            }
+
+            int lastClosing = descriptionTrimmed.lastIndexOf(']');
+
+            if (lastClosing != -1) {
+                formattedDescriptionBuilder
+                        .append(ChatFormatting.RESET)
+                        .append(descriptionTrimmed.substring(lastClosing + 1));
+            }
+        } else {
+            formattedDescriptionBuilder = new StringBuilder(descriptionTrimmed);
+        }
+
+        currentQuest = new QuestInfo(questName.toString().trim(), formattedDescriptionBuilder.toString());
     }
 
     @Override
