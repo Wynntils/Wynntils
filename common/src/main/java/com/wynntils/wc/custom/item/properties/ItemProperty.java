0f700b5c86d9fb94b351c7324eaaafa31639e146
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-08-10 10:09:30 +0200

    ItemProperties: Small fix

--- common/src/main/java/com/wynntils/wc/custom/item/properties/ItemProperty.java
+++ common/src/main/java/com/wynntils/wc/custom/item/properties/ItemProperty.java
@@ -15,7 +15,8 @@ public abstract class ItemProperty {
     public static final Class<DungeonKeyProperty> DUNGEON_KEY = DungeonKeyProperty.class;
     public static final Class<DurabilityProperty> DURABILITY = DurabilityProperty.class;
     public static final Class<ItemTierProperty> ITEM_TIER = ItemTierProperty.class;
-    public static final Class<TieredCraftingItemProperty> INGREDIENT = TieredCraftingItemProperty.class;
+    public static final Class<IngredientProperty> INGREDIENT = IngredientProperty.class;
+    public static final Class<MaterialProperty> MATERIAL = MaterialProperty.class;
     public static final Class<TeleportScrollProperty> TELEPORT_SCROLL = TeleportScrollProperty.class;
 
     public static final Class<HighlightProperty> HIGHLIGHT = HighlightProperty.class;
