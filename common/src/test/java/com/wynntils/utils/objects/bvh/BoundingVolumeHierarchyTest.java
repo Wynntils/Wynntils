228bb8d654106860218e286f58c3fff7da2e228c
Kepler-17c <17955785+Kepler-17c@users.noreply.github.com>
2022-08-12 22:09:23 +0200

    Improved test readability & added BVH 'benchmark'

--- common/src/test/java/com/wynntils/utils/objects/bvh/BoundingVolumeHierarchyTest.java
+++ common/src/test/java/com/wynntils/utils/objects/bvh/BoundingVolumeHierarchyTest.java
@@ -6,13 +6,23 @@ package com.wynntils.utils.objects.bvh;
 
 import com.wynntils.utils.objects.IBoundingBox;
 import com.wynntils.utils.objects.IBoundingBox.AxisAlignedBoundingBox;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Supplier;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 import net.minecraft.world.phys.Vec3;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -27,51 +37,49 @@ class BoundingVolumeHierarchyTest {
     @Test
     void addRemoveBulkTest() {
         final BoundingVolumeHierarchy<IBoundingBox> bvh = new BoundingVolumeHierarchy<>();
+        final Set<IBoundingBox> referenceSet = new HashSet<>();
         final Random random = new Random("Wynntils".hashCode()); // fixed seed for reproducible test
-        final Supplier<Vec3> nextPoint = () -> new Vec3(random.nextDouble(), random.nextDouble(), random.nextDouble());
-        final int cycles = 1 << 10;
-        final int inserts = 1 << 10;
-        IBoundingBox nearestTest;
-        Vec3 reference;
-        Vec3 offset = new Vec3(1, 0, 0);
+        final int cycles = 1 << 8;
+        final int insertCount = 1 << 8;
+        final int removalCount = 1 << 6;
         for (int c = 0; c < cycles; c++) {
-            nearestTest = null;
-            reference = nextPoint.get().add(offset);
-            bvh.clear();
-            // prepare bvh and reference element for findNearest()
-            final List<IBoundingBox> removals = new ArrayList<>(inserts);
-            for (int i = 0; i < inserts; i++) {
-                final IBoundingBox bb = new AxisAlignedBoundingBox(nextPoint.get(), nextPoint.get());
-                bvh.add(bb);
-                if (nearestTest == null || bb.squareDistance(reference) < nearestTest.squareDistance(reference)) {
-                    nearestTest = bb;
-                }
-                if (random.nextBoolean()) {
-                    removals.add(bb);
-                }
-            }
+            // fill BVH and collect elements to be removed later
+            final List<IBoundingBox> insertElements = IntStream.range(0, insertCount)
+                    .mapToObj(i -> generatePointBB(random))
+                    .toList();
+            final List<IBoundingBox> removals = IntStream.range(0, removalCount)
+                    .map(i -> random.nextInt(insertCount))
+                    .mapToObj(insertElements::get)
+                    .toList();
+            bvh.addAll(insertElements);
+            referenceSet.addAll(insertElements);
+            // create probe point and compare the results for nearest
+            final Vec3 probePoint = generatePoint(random);
+            final IBoundingBox referenceNearest = referenceSet.stream()
+                    .min(Comparator.comparingDouble(a -> a.squareDistance(probePoint)))
+                    .get();
             // test bvh against reference element
-            IBoundingBox bvhNearest = bvh.findNearest(reference);
+            IBoundingBox bvhNearest = bvh.findNearest(probePoint);
+            Assertions.assertTrue(bvh.contains(referenceNearest));
             Assertions.assertEquals(
-                    nearestTest,
+                    referenceNearest,
                     bvhNearest,
-                    "Cycle: " + c + ", expected dist: " + nearestTest.squareDistance(reference) + ", actual dist: "
-                            + bvhNearest.squareDistance(reference));
-            // remove marked elements
-            for (final IBoundingBox bb : removals) {
-                if (bb == nearestTest) {
-                    nearestTest = null;
-                }
-                bvh.remove(bb);
-            }
-            // test again after deletions
-            if (nearestTest != null) {
-                bvhNearest = bvh.findNearest(reference);
+                    "Cycle: " + c + ", expected dist: " + referenceNearest.squareDistance(probePoint)
+                            + ", actual dist: " + bvhNearest.squareDistance(probePoint));
+            // remove marked elements and test again
+            bvh.removeAll(removals);
+            referenceSet.removeAll(removals);
+            bvhNearest = bvh.findNearest(probePoint);
+            if (removals.contains(referenceNearest)) {
+                Assertions.assertNotEquals(
+                        referenceNearest, bvhNearest, "Cycle: " + c + " - expected unequal, but matched");
+            } else {
                 Assertions.assertEquals(
-                        nearestTest,
+                        referenceNearest,
                         bvhNearest,
-                        "Cycle: " + c + ", expected dist: " + nearestTest.squareDistance(reference) + ", actual dist: "
-                                + bvhNearest.squareDistance(reference));
+                        "Cycle: " + c + ", expected dist: " + referenceNearest.squareDistance(probePoint)
+                                + ", actual dist: "
+                                + bvhNearest.squareDistance(probePoint));
             }
         }
     }
@@ -90,13 +98,11 @@ class BoundingVolumeHierarchyTest {
                 expected[y][x] = bb;
             }
             bvh.addAll(Arrays.asList(expected[y]));
-            System.out.println("added " + (y * range));
         }
         for (int y = 0; y < range; y++) {
             for (int x = 0; x < range; x++) {
                 Assertions.assertEquals(expected[y][x], bvh.findNearest(new Vec3(x, y, 0)));
             }
-            System.out.println("checked " + (y * range));
         }
     }
 
@@ -113,24 +119,138 @@ class BoundingVolumeHierarchyTest {
             final int batch = random.nextInt(batchLimit);
             for (int b = 0; b < batch; b++, size++) {
                 bvh.add(generatePointBB(random));
-                try {
-                    Thread.sleep(10);
-                } catch (final InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
             Assertions.assertEquals(size, bvh.size());
         }
     }
 
     /**
-     * Creates a new random point element implementing IBoundingBox from a given generator.
-     *
-     * @param random
-     *            RNG source.
+     * Testing lookup efficiency of the BVH vs linear search.
+     */
+    @Test
+    @Disabled
+    void lookupTimesTest() throws IOException {
+        final Random random = new Random("Artemis".hashCode());
+        final int probeCount = 1 << 5;
+        final int maxElementCount = 1 << 20;
+        final Function<Integer, Integer> probeSizeSupplier = (index) -> maxElementCount * index / probeCount;
+        final int measurementCount = 1 << 8;
+        final List<Quadruple<Integer, Long, Long, Long>> timingResults = new ArrayList<>();
+        for (int i = 1; i <= probeCount; i++) {
+            final List<IBoundingBox> list = IntStream.range(0, probeSizeSupplier.apply(i))
+                    .mapToObj(x -> generatePointBB(random))
+                    .toList();
+            final long buildTimeStart = System.currentTimeMillis();
+            final BoundingVolumeHierarchy<IBoundingBox> bvh = new BoundingVolumeHierarchy<>();
+            bvh.addAll(list);
+            Assertions.assertTrue(waitForBvhRebuild(bvh));
+            final long buildTimeEnd = System.currentTimeMillis();
+            long buildTime = buildTimeEnd - buildTimeStart;
+            long listTimeSum = 0;
+            long bvhTimeSum = 0;
+            for (int n = 0; n < measurementCount; n++) {
+                final Vec3 probePoint = generatePoint(random);
+                final long listStartTime = System.nanoTime();
+                final IBoundingBox listMin = list.stream()
+                        .min(Comparator.comparingDouble(a -> a.squareDistance(probePoint)))
+                        .orElse(null);
+                final long listEndTime = System.nanoTime();
+                final long bvhStartTime = System.nanoTime();
+                final IBoundingBox bvhMin = bvh.findNearest(probePoint);
+                final long bvhEndTime = System.nanoTime();
+                Assertions.assertEquals(listMin, bvhMin);
+                listTimeSum += listEndTime - listStartTime;
+                bvhTimeSum += bvhEndTime - bvhStartTime;
+            }
+            timingResults.add(new Quadruple(list.size(), listTimeSum, bvhTimeSum, buildTime));
+        }
+        // export data
+        final File file = new File("src/test/resources/bvh_vs_linear_times.csv");
+        final FileWriter fw = new FileWriter(file);
+        final BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("\"Element Count\",\"List Times\",\"BVH Times\",\"BVH Build Time\"");
+        bw.newLine();
+        for (var t : timingResults) {
+            bw.append(t.first + "," + t.second + "," + t.third + "," + t.fourth);
+            bw.newLine();
+        }
+        bw.close();
+        fw.close();
+    }
+
+    /**
+     * Creates a new random point in the interval {@code [0,1)} using the given generator.
+     * @param random RNG source.
      * @return the random point.
      */
-    private static IBoundingBox generatePointBB(final Random random) {
-        return new AxisAlignedBoundingBox(new Vec3(random.nextDouble(), random.nextDouble(), random.nextDouble()));
+    private static Vec3 generatePoint(final Random random) {
+        return new Vec3(random.nextDouble(), random.nextDouble(), random.nextDouble());
     }
+
+    /**
+     * Encapsulates the point from {@link #generatePoint(Random)} in a bounding box.
+     *
+     * @param random RNG source.
+     * @return the random point bounding box.
+     */
+    private static IBoundingBox generatePointBB(final Random random) {
+        return new AxisAlignedBoundingBox(generatePoint(random));
+    }
+
+    /**
+     * Inspects the BVH via reflection to check the rebuild status.
+     * @param bvh to wait for.
+     */
+    private static boolean waitForBvhRebuild(BoundingVolumeHierarchy bvh) {
+        final Class<BoundingVolumeHierarchy> bvhClass = BoundingVolumeHierarchy.class;
+        final Field dataLockField;
+        final Field rebuildInProgressField;
+        final Field pendingInsertsField;
+        final Field pendingDeletesField;
+        final Object dataLock;
+        try {
+            dataLockField = bvhClass.getDeclaredField("dataLock");
+            dataLockField.setAccessible(true);
+            rebuildInProgressField = bvhClass.getDeclaredField("rebuildInProgress");
+            rebuildInProgressField.setAccessible(true);
+            pendingInsertsField = bvhClass.getDeclaredField("pendingInserts");
+            pendingInsertsField.setAccessible(true);
+            pendingDeletesField = bvhClass.getDeclaredField("pendingDeletes");
+            pendingDeletesField.setAccessible(true);
+            dataLock = dataLockField.get(bvh);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            return false;
+        }
+        int rebuildDoneCounter = 0;
+        while (true) {
+            synchronized (dataLock) {
+                boolean rebuildInProgress;
+                Set pendingInserts;
+                Set pendingDeletes;
+                try {
+                    rebuildInProgress = rebuildInProgressField.getBoolean(bvh);
+                    pendingInserts = (Set) pendingInsertsField.get(bvh);
+                    pendingDeletes = (Set) pendingDeletesField.get(bvh);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+                if (!rebuildInProgress && pendingInserts.size() == 0 && pendingDeletes.size() == 0) {
+                    rebuildDoneCounter++;
+                    if (rebuildDoneCounter == 10) {
+                        return true;
+                    }
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    // don't count this iteration and wait again
+                    rebuildDoneCounter--;
+                }
+            }
+        }
+    }
+
+    public record Quadruple<A, B, C, D>(A first, B second, C third, D fourth) {}
 }
