095ca1785462a0dd69819f484c78079032e0adc7
Magnus Ihse Bursie <mag@icus.se>
2022-08-12 21:53:09 +0200

    Fix isFoil mixin signature

--- common/src/main/java/com/wynntils/mc/mixin/PotionItemMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/PotionItemMixin.java
@@ -6,6 +6,7 @@ package com.wynntils.mc.mixin;
 
 import com.wynntils.mc.EventFactory;
 import com.wynntils.mc.event.DrawPotionGlintEvent;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PotionItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PotionItem.class)
 public abstract class PotionItemMixin {
-    @Inject(method = "isFoil()B", at = @At("HEAD"), cancellable = true)
-    private void isFoilPre(CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "isFoil(Lnet/minecraft/world/item/ItemStack;)Z", at = @At("HEAD"), cancellable = true)
+    private void isFoilPre(ItemStack item, CallbackInfoReturnable<Boolean> cir) {
         DrawPotionGlintEvent event = EventFactory.onPotionIsFoil((PotionItem) (Object) this);
         if (event.isCanceled()) {
             cir.setReturnValue(false);
