a58f73eee038d549a40627eeb7c502cb86943f2b
magicus <magicus@users.noreply.github.com>
2022-08-13 00:15:46 +0000

    chore: spotless formatting

--- common/src/main/java/com/wynntils/features/user/InfoMessageFilterFeature.java
+++ common/src/main/java/com/wynntils/features/user/InfoMessageFilterFeature.java
@@ -49,7 +49,8 @@ public class InfoMessageFilterFeature extends UserFeature {
             Pattern.compile("^(§r§8)?As the sun rises, you feel a little bit safer...$");
     private static final Pattern BACKGROUND_SOUL_POINT_2 = Pattern.compile("^§r§7\\[(\\+\\d+ Soul Points?)\\]$");
 
-    private static final Pattern BACKGROUND_LEVEL_UP_1 = Pattern.compile("^(?:§r§8)?.* is now (?:combat )?level .*(?: in §.*)?$");
+    private static final Pattern BACKGROUND_LEVEL_UP_1 =
+            Pattern.compile("^(?:§r§8)?.* is now (?:combat )?level .*(?: in §.*)?$");
     private static final Pattern BACKGROUND_LEVEL_UP_2 =
             Pattern.compile("^(§r§8)?\\[!\\] Congratulations to §r.* for reaching (combat )?§r§7level .*!$");
 
