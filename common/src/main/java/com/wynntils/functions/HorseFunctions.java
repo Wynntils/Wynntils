d187c9605aaedab6a75dc8742a59a9ce342a9420
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2022-08-15 02:36:50 -0700

    add deps

--- common/src/main/java/com/wynntils/functions/HorseFunctions.java
+++ common/src/main/java/com/wynntils/functions/HorseFunctions.java
@@ -5,10 +5,13 @@
 package com.wynntils.functions;
 
 import com.wynntils.core.functions.ActiveFunction;
+import com.wynntils.core.managers.Model;
 import com.wynntils.mc.utils.InventoryUtils;
 import com.wynntils.mc.utils.McUtils;
+import com.wynntils.wc.custom.item.ItemStackTransformModel;
 import com.wynntils.wc.custom.item.WynnItemStack;
 import com.wynntils.wc.custom.item.properties.ItemProperty;
+import java.util.List;
 import net.minecraft.world.item.ItemStack;
 
 public class HorseFunctions {
@@ -26,6 +29,11 @@ public class HorseFunctions {
             if (getHorse() == null) return "??";
             return String.valueOf(getHorse().getProperty(ItemProperty.HORSE).getLevel());
         }
+
+        @Override
+        public List<Class<? extends Model>> getModelDependencies() {
+            return List.of(ItemStackTransformModel.class);
+        }
     }
 
     public static class HorseLevelMaxFunction extends ActiveFunction<String> {
@@ -33,6 +41,11 @@ public class HorseFunctions {
         public String getValue(String argument) {
             if (getHorse() == null) return "??";
             return String.valueOf(getHorse().getProperty(ItemProperty.HORSE).getMaxLevel());
+        }
+
+        @Override
+        public List<Class<? extends Model>> getModelDependencies() {
+            return List.of(ItemStackTransformModel.class);
         }
     }
 
@@ -42,6 +55,11 @@ public class HorseFunctions {
             if (getHorse() == null) return null;
             return String.valueOf(getHorse().getProperty(ItemProperty.HORSE).getXp());
         }
+
+        @Override
+        public List<Class<? extends Model>> getModelDependencies() {
+            return List.of(ItemStackTransformModel.class);
+        }
     }
 
     public static class HorseTierFunction extends ActiveFunction<String> {
@@ -49,6 +67,11 @@ public class HorseFunctions {
         public String getValue(String argument) {
             if (getHorse() == null) return null;
             return String.valueOf(getHorse().getProperty(ItemProperty.HORSE).getTier());
+        }
+
+        @Override
+        public List<Class<? extends Model>> getModelDependencies() {
+            return List.of(ItemStackTransformModel.class);
         }
     }
 }
