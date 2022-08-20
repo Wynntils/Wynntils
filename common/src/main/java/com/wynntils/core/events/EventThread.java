0ac4a4f2f4956a93e86b73a13b2bb38075413a30
Magnus Ihse Bursie <mag@icus.se>
2022-08-20 02:06:47 +0200

    Add documentation

--- common/src/main/java/com/wynntils/core/events/EventThread.java
+++ common/src/main/java/com/wynntils/core/events/EventThread.java
@@ -9,15 +9,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Inform what type of thread the event is allowed to be sent on. Events without an
+ * explicit annotation is considered to be allowed on Type == RENDER only.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface EventThread {
     Type value() default Type.RENDER;
 
     enum Type {
-        RENDER,
-        IO,
-        WORKER,
-        ANY
+        RENDER, // The main thread a.k.a the Render thread
+        IO,     // Any Netty Client IO thread
+        WORKER, // A worker thread, from a Minecraft or Wynntils thread pool
+        ANY     // Any thread at all
     }
 }
