e63f460a68b5c5b9d76770efff5ed6673418dc9b
ekgame <ekgame1@gmail.com>
2022-09-08 15:06:14 +0300

    Fix casting spells while dropping items.

---
+++ common/src/main/java/com/wynntils/mc/mixin/PlayerMixin.java
@@ -0,0 +1,28 @@
+package com.wynntils.mc.mixin;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Player.class)
+public abstract class PlayerMixin {
+
+    @Redirect(
+        method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/player/Player;swing(Lnet/minecraft/world/InteractionHand;)V"
+        )
+    )
+    private void preventSwingOnDrop(Player player, InteractionHand interaction) {
+        /**
+         * This mixin fixes an issue where a Archers holding bows may start
+         * casting spells just by dropping items from the inventory screen.
+         *
+         * This method overrides the swing call in the function that drops items on the ground.
+         * This method is left empty intentionally, to effectively remove the swing method call.
+         */
+    }
+}
