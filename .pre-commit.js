1d20c645f3a73a1c3715be86148cce8c646c8b1f
HighCrit <35771251+HighCrit@users.noreply.github.com>
2022-09-09 22:49:51 +0200

    chore(ci): Port Publishing from Wynntils

---
+++ .pre-commit.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+exports.preCommit = (props) => {
+    const replace = (path, searchValue, replaceValue) => {
+        let content = fs.readFileSync(path, "utf-8");
+        if (content.match(searchValue)) {
+            fs.writeFileSync(path, content.replace(searchValue, replaceValue));
+            console.log(`"${path}" changed`);
+        }
+    };
+
+    const [major, minor, revision] = props.version.split(".");
+
+    replace("./build.gradle", /(?<=major: )\d+/g, major);
+    replace("./build.gradle", /(?<=minor: )\d+/g, minor);
+    replace("./build.gradle", /(?<=revision: )\d+/g, revision);
+};
