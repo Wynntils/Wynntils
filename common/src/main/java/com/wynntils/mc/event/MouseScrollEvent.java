1c47341ef221e2340dacce28977d575b20c1bf51
byBackfish <byBackfish@users.noreply.github.com>
2022-09-17 22:33:51 +0000

    chore: spotless formatting

--- common/src/main/java/com/wynntils/mc/event/MouseScrollEvent.java
+++ common/src/main/java/com/wynntils/mc/event/MouseScrollEvent.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright © Wynntils 2022.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
 package com.wynntils.mc.event;
 
 import net.minecraftforge.eventbus.api.Event;
@@ -6,10 +10,11 @@ public class MouseScrollEvent extends Event {
 
     private final boolean isScrollingUp;
 
-
     public MouseScrollEvent(boolean isScrollingUp) {
         this.isScrollingUp = isScrollingUp;
     }
 
-    public boolean isScrollingUp() { return isScrollingUp; }
+    public boolean isScrollingUp() {
+        return isScrollingUp;
+    }
 }
