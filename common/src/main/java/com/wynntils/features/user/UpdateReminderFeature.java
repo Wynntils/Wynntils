3f3b9483151c6e0a5872980c8eb5301008ffcdd5
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-09-22 21:26:19 +0200

    UpdateReminderFeature: Update text

--- common/src/main/java/com/wynntils/features/user/UpdateReminderFeature.java
+++ common/src/main/java/com/wynntils/features/user/UpdateReminderFeature.java
@@ -13,17 +13,24 @@ import com.wynntils.mc.utils.McUtils;
 import com.wynntils.wynn.event.WorldStateEvent;
 import com.wynntils.wynn.model.WorldStateManager;
 import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class UpdateReminderFeature extends UserFeature {
     private static final String CI_LINK =
             "https://ci.wynntils.com/job/Artemis/lastSuccessfulBuild/api/json?tree=number";
+    private static final String DOWNLOAD_LINK = "https://ci.wynntils.com/job/Artemis";
 
     private boolean firstJoin = true;
 
     @SubscribeEvent
-    public void onWorldEnter(WorldStateEvent event) {
+    public void onWorldStateChange(WorldStateEvent event) {
+        if (event.getNewState() == WorldStateManager.State.NOT_CONNECTED) {
+            firstJoin = false;
+            return;
+        }
+
         if (event.getNewState() != WorldStateManager.State.WORLD || !firstJoin) return;
 
         firstJoin = false;
@@ -33,10 +40,20 @@ public class UpdateReminderFeature extends UserFeature {
                     int buildNumber = jsonObject.getAsJsonPrimitive("number").getAsInt();
 
                     if (buildNumber != WynntilsMod.getBuildNumber()) {
+                        TextComponent clickable = new TextComponent("here.");
+                        clickable.setStyle(clickable
+                                .getStyle()
+                                .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, DOWNLOAD_LINK))
+                                .withUnderlined(true)
+                                .withBold(true));
                         McUtils.sendMessageToClient(new TextComponent("[Wynntils/Artemis]: Build " + buildNumber
                                         + " is the latest version, but you are using build "
-                                        + WynntilsMod.getBuildNumber() + ". Please consider updating.")
-                                .withStyle(ChatFormatting.RED));
+                                        + WynntilsMod.getBuildNumber() + ". Please consider updating ")
+                                .append(clickable)
+                                .append(
+                                        new TextComponent(
+                                                "\nPlease note that Artemis is in alpha, and newer builds might introduce bugs."))
+                                .withStyle(ChatFormatting.GREEN));
                     }
 
                     return true;
