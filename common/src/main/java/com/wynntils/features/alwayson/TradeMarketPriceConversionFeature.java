5206c907f05b5089faf7b1e131cb410aeb4a1322
byBackfish <byBackfish@users.noreply.github.com>
2022-09-24 19:30:20 +0000

    chore: spotless formatting

--- common/src/main/java/com/wynntils/features/alwayson/TradeMarketPriceConversionFeature.java
+++ common/src/main/java/com/wynntils/features/alwayson/TradeMarketPriceConversionFeature.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright © Wynntils 2022.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
 package com.wynntils.features.alwayson;
 
 import com.wynntils.core.features.StateManagedFeature;
@@ -5,25 +9,24 @@ import com.wynntils.mc.event.ChatSentEvent;
 import com.wynntils.utils.StringUtils;
 import com.wynntils.wynn.event.ChatMessageReceivedEvent;
 import com.wynntils.wynn.utils.WynnUtils;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-
 import java.util.regex.Pattern;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class TradeMarketPriceConversionFeature extends StateManagedFeature {
 
-    private static final Pattern PRICE_PATTERN = Pattern.compile("^§6Type the price in emeralds or type 'cancel' to cancel:$");
+    private static final Pattern PRICE_PATTERN =
+            Pattern.compile("^§6Type the price in emeralds or type 'cancel' to cancel:$");
 
     private boolean shouldConvert = false;
 
     @SubscribeEvent
     public void onChatMessageReceive(ChatMessageReceivedEvent event) {
-        if(!WynnUtils.onWorld()) return;
+        if (!WynnUtils.onWorld()) return;
 
         if (PRICE_PATTERN.matcher(event.getCodedMessage()).matches()) {
             shouldConvert = true;
         }
     }
-
 
     @SubscribeEvent
     public void onClientChat(ChatSentEvent event) {
@@ -33,6 +36,4 @@ public class TradeMarketPriceConversionFeature extends StateManagedFeature {
         String price = StringUtils.convertEmeraldPrice(event.getMessage());
         if (!price.isEmpty()) event.setMessage(price);
     }
-
-
 }
