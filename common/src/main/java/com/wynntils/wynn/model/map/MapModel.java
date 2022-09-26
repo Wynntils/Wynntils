e23924bc896e97c2bc8ffcb1ae36ecdc4dfa312c
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-09-26 23:20:23 +0200

    MapModel: Try to reduce hacks and add logging

--- common/src/main/java/com/wynntils/wynn/model/map/MapModel.java
+++ common/src/main/java/com/wynntils/wynn/model/map/MapModel.java
@@ -167,15 +167,22 @@ public final class MapModel extends Model {
                                 .build());
                     }
 
-                    // hacky way to know when handler has finished dispatching
-                    CompletableFuture.runAsync(handler::dispatch).whenComplete((a, b) -> {
-                        if (syncList.size() == mapArray.size()) {
-                            result.complete(true);
-                            maps = syncList;
-                        } else {
-                            result.complete(false);
-                        }
-                    });
+                    Thread thread = handler.dispatchAsync();
+
+                    try {
+                        thread.join();
+                    } catch (InterruptedException e) {
+                        WynntilsMod.error("Exception when loading map files.", e);
+                    }
+
+                    if (syncList.size() == mapArray.size()) {
+                        result.complete(true);
+                        maps = syncList;
+                    } else {
+                        WynntilsMod.error("MapModel: Expected " + mapArray.size() + " map pieces, got "
+                                + syncList.size() + " pieces.");
+                        result.complete(false);
+                    }
 
                     return true;
                 })
