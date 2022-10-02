0fedd4ad5d872d5fb83b671831c0be334428d24b
Magnus Ihse Bursie <mag@icus.se>
2022-10-02 15:38:56 +0200

    Fix multi maps for minimap as well

--- common/src/main/java/com/wynntils/features/user/overlays/map/MinimapFeature.java
+++ common/src/main/java/com/wynntils/features/user/overlays/map/MinimapFeature.java
@@ -118,10 +118,9 @@ public class MinimapFeature extends UserFeature {
             // Always draw a black background to cover transparent map areas
             RenderUtils.drawRect(poseStack, CommonColors.BLACK, renderX, renderY, 0, width, height);
 
-            Optional<MapTexture> mapOpt = MapModel.getMapForLocation(
-                    (int) McUtils.player().getX(), (int) McUtils.player().getZ());
-            if (mapOpt.isPresent()) {
-                MapTexture map = mapOpt.get();
+            List<MapTexture> maps =
+                    MapRenderer.getMapTextures((int) McUtils.player().getX(), (int) McUtils.player().getZ(), width, height, scale);
+            for (MapTexture map : maps) {
                 float textureX = map.getTextureXPosition(McUtils.player().getX());
                 float textureZ = map.getTextureZPosition(McUtils.player().getZ());
                 MapRenderer.renderMapQuad(
