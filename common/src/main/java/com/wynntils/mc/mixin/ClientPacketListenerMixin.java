b0e5a06e515e3bfb3aa0e350b117153546888752
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2022-10-08 15:45:25 +0200

    Features: Add LobbyUptimeFeature (#522)
    * Features: Add LobbyUptimeFeature
    
    * Fix typo
    
    * ServerProfile: Fix matchTime
    
    * LobbyUptimeFeature: Use item transformers
    
    Co-authored-by: Magnus Ihse Bursie <mag@icus.se>

--- common/src/main/java/com/wynntils/mc/mixin/ClientPacketListenerMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/ClientPacketListenerMixin.java
@@ -17,6 +17,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundCommandsPacket;
 import net.minecraft.network.protocol.game.ClientboundContainerClosePacket;
 import net.minecraft.network.protocol.game.ClientboundContainerSetContentPacket;
+import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket;
 import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket;
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket;
@@ -113,6 +114,12 @@ public abstract class ClientPacketListenerMixin {
         if (EventFactory.onContainerSetContent(packet).isCanceled()) {
             ci.cancel();
         }
+    }
+
+    @Inject(method = "handleContainerSetSlot", at = @At("HEAD"))
+    private void handleContainerSetSlot(ClientboundContainerSetSlotPacket packet, CallbackInfo ci) {
+        if (!isRenderThread()) return;
+        EventFactory.onContainerSetSlot(packet);
     }
 
     @Inject(
