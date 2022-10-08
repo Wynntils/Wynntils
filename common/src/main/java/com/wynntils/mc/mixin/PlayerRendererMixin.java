9a4745bcd9e45ff2629c363e92108a81dbf8b7e1
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-10-08 19:13:22 +0200

    WIP

---
+++ common/src/main/java/com/wynntils/mc/mixin/PlayerRendererMixin.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © Wynntils 2022.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.mc.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import java.util.List;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerRenderer.class)
+public abstract class PlayerRendererMixin extends EntityRenderer<Player> {
+    protected PlayerRendererMixin(EntityRendererProvider.Context context) {
+        super(context);
+    }
+
+    @Inject(
+            method =
+                    "renderNameTag(Lnet/minecraft/client/player/AbstractClientPlayer;Lnet/minecraft/network/chat/Component;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+            at = @At("HEAD"))
+    public void redirectNameTagRender(
+            AbstractClientPlayer entity,
+            Component displayName,
+            PoseStack matrixStack,
+            MultiBufferSource buffer,
+            int packedLight,
+            CallbackInfo ci) {
+        List<MutableComponent> injected = List.of(
+                new TextComponent("AItem").withStyle(ChatFormatting.AQUA),
+                new TextComponent("BItem").withStyle(ChatFormatting.AQUA),
+                new TextComponent("CItem").withStyle(ChatFormatting.AQUA),
+                new TextComponent("DItem").withStyle(ChatFormatting.AQUA),
+                new TextComponent("EItem").withStyle(ChatFormatting.AQUA),
+                new TextComponent("Wynntils Developer").withStyle(ChatFormatting.GOLD));
+
+        for (MutableComponent component : injected) {
+            super.renderNameTag(entity, component, matrixStack, buffer, packedLight);
+            matrixStack.translate(0.0, 0.25875f, 0.0);
+        }
+    }
+}
