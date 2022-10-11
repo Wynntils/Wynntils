54cbe3057e7a14052d1a902511a8773174945d00
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-10-11 18:37:07 +0200

    CustomNametagRendererFeature: Add ability to render used gear

--- common/src/main/java/com/wynntils/features/user/CustomNametagRendererFeature.java
+++ common/src/main/java/com/wynntils/features/user/CustomNametagRendererFeature.java
@@ -7,18 +7,37 @@ package com.wynntils.features.user;
 import com.wynntils.core.config.Config;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.managers.Model;
+import com.wynntils.core.webapi.WebManager;
+import com.wynntils.core.webapi.profiles.item.ItemProfile;
 import com.wynntils.mc.event.NametagRenderEvent;
+import com.wynntils.mc.utils.ComponentUtils;
+import com.wynntils.mc.utils.McUtils;
 import com.wynntils.wynn.model.UserInfoModel;
 import com.wynntils.wynn.objects.account.AccountType;
+import com.wynntils.wynn.utils.WynnPlayerUtils;
 import java.util.List;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class CustomNametagRendererFeature extends UserFeature {
+    private static final float RAYCAST_RANGE = 5f;
+
     @Config
     public boolean hideAllNametags = false;
 
     @Config
     public boolean showAccountType = true;
+
+    @Config
+    public boolean showGearOnHover = true;
 
     @SubscribeEvent
     public void onNameTagRender(NametagRenderEvent event) {
@@ -27,13 +46,67 @@ public class CustomNametagRendererFeature extends UserFeature {
             return;
         }
 
-        if (showAccountType) {
-            for (AccountType type : AccountType.values()) {
-                if (type.getComponent() == null) continue;
+        if (showGearOnHover) {
+            addGearNametag(event);
+        }
 
-                if (UserInfoModel.isAccountType(event.getEntity().getUUID(), type)) {
-                    event.addInjectedLine(type.getComponent());
-                }
+        if (showAccountType) {
+            addAccountTypeNametag(event);
+        }
+    }
+
+    private static void addGearNametag(NametagRenderEvent event) {
+        LocalPlayer player = McUtils.player();
+
+        Vec3 start = player.getEyePosition(1f);
+        Vec3 look = player.getLookAngle();
+        Vec3 direction = start.add(look.x * RAYCAST_RANGE, look.y * RAYCAST_RANGE, look.z * RAYCAST_RANGE);
+        AABB bb = player.getBoundingBox()
+                .expandTowards(look.x * RAYCAST_RANGE, look.y * RAYCAST_RANGE, look.z * RAYCAST_RANGE)
+                .expandTowards(1, 1, 1);
+
+        EntityHitResult hitResult = ProjectileUtil.getEntityHitResult(
+                McUtils.mc().level, player, start, direction, bb, (e) -> e instanceof Player);
+
+        if (hitResult == null) return;
+        if (hitResult.getEntity() != event.getEntity()) return;
+
+        Player hitPlayer = (Player) hitResult.getEntity();
+
+        if (hitPlayer.getScoreboardName().contains("§")) return; // npc
+        if (WynnPlayerUtils.isPlayerGhost(hitPlayer)) return;
+
+        ItemStack heldItem = hitPlayer.getMainHandItem();
+        if (heldItem != null) {
+            getItemComponent(event, heldItem);
+        }
+
+        for (ItemStack armorStack : hitPlayer.getArmorSlots()) {
+            getItemComponent(event, armorStack);
+        }
+    }
+
+    private static void getItemComponent(NametagRenderEvent event, ItemStack armorStack) {
+        String itemName = ComponentUtils.getUnformatted(armorStack.getHoverName());
+
+        if (itemName.contains("Crafted")) {
+            event.addInjectedLine(new TextComponent(itemName).withStyle(ChatFormatting.DARK_AQUA));
+            return;
+        }
+
+        ItemProfile itemProfile = WebManager.getItemsMap().get(itemName);
+        if (itemProfile == null) return;
+
+        event.addInjectedLine(new TextComponent(itemProfile.getDisplayName())
+                .withStyle(itemProfile.getTier().getChatFormatting()));
+    }
+
+    private static void addAccountTypeNametag(NametagRenderEvent event) {
+        for (AccountType type : AccountType.values()) {
+            if (type.getComponent() == null) continue;
+
+            if (UserInfoModel.isAccountType(event.getEntity().getUUID(), type)) {
+                event.addInjectedLine(type.getComponent());
             }
         }
     }
