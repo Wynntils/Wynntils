47dea8498894c1cc771338362ee439c5f7b403af
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2022-10-11 21:20:04 +0200

    CustomNametagRendererFeature: Add ability to render used gear (#538)
    * CustomNametagRendererFeature: Add ability to render used gear
    
    * CustomNametagRendererFeature: Fix exploit
    
    * Disable by default
    
    * Extract logic
    
    * Change back to default on
    
    * Add better name parser

--- common/src/main/java/com/wynntils/features/user/CustomNametagRendererFeature.java
+++ common/src/main/java/com/wynntils/features/user/CustomNametagRendererFeature.java
@@ -7,10 +7,23 @@ package com.wynntils.features.user;
 import com.wynntils.core.config.Config;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.managers.Model;
+import com.wynntils.core.webapi.WebManager;
+import com.wynntils.core.webapi.profiles.item.ItemProfile;
 import com.wynntils.mc.event.NametagRenderEvent;
+import com.wynntils.mc.utils.McUtils;
 import com.wynntils.wynn.model.UserInfoModel;
 import com.wynntils.wynn.objects.account.AccountType;
+import com.wynntils.wynn.utils.RaycastUtils;
+import com.wynntils.wynn.utils.WynnItemUtils;
+import com.wynntils.wynn.utils.WynnPlayerUtils;
 import java.util.List;
+import java.util.Optional;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class CustomNametagRendererFeature extends UserFeature {
@@ -20,6 +33,9 @@ public class CustomNametagRendererFeature extends UserFeature {
     @Config
     public boolean showAccountType = true;
 
+    @Config
+    public boolean showGearOnHover = true;
+
     @SubscribeEvent
     public void onNameTagRender(NametagRenderEvent event) {
         if (hideAllNametags) {
@@ -27,13 +43,65 @@ public class CustomNametagRendererFeature extends UserFeature {
             return;
         }
 
-        if (showAccountType) {
-            for (AccountType type : AccountType.values()) {
-                if (type.getComponent() == null) continue;
+        if (showGearOnHover) {
+            addGearNametag(event);
+        }
 
-                if (UserInfoModel.isAccountType(event.getEntity().getUUID(), type)) {
-                    event.addInjectedLine(type.getComponent());
-                }
+        if (showAccountType) {
+            addAccountTypeNametag(event);
+        }
+    }
+
+    private static void addGearNametag(NametagRenderEvent event) {
+        LocalPlayer player = McUtils.player();
+
+        Optional<Player> hitPlayer = RaycastUtils.getHoveredPlayer();
+        if (hitPlayer.isEmpty()) return;
+        if (hitPlayer.get() != event.getEntity()) return;
+
+        if (!WynnPlayerUtils.isLocalPlayer(player)) return;
+
+        ItemStack heldItem = hitPlayer.get().getMainHandItem();
+        if (heldItem != null) {
+            getItemComponent(event, heldItem);
+        }
+
+        for (ItemStack armorStack : hitPlayer.get().getArmorSlots()) {
+            getItemComponent(event, armorStack);
+        }
+    }
+
+    private static void getItemComponent(NametagRenderEvent event, ItemStack itemStack) {
+        String itemName = WynnItemUtils.getTranslatedName(itemStack);
+
+        if (itemName.contains("Crafted")) {
+            event.addInjectedLine(new TextComponent(itemName).withStyle(ChatFormatting.DARK_AQUA));
+            return;
+        }
+
+        ItemProfile itemProfile = WebManager.getItemsMap().get(itemName);
+        if (itemProfile == null) return;
+
+        // this solves an unidentified item showcase exploit
+        // boxes items are STONE_SHOVEL, 1 represents UNIQUE boxes and 6 MYTHIC boxes
+        if (itemStack.getItem() == Items.STONE_SHOVEL
+                && itemStack.getDamageValue() >= 1
+                && itemStack.getDamageValue() <= 6) {
+            event.addInjectedLine(new TextComponent("Unidentified Item")
+                    .withStyle(itemProfile.getTier().getChatFormatting()));
+            return;
+        }
+
+        event.addInjectedLine(new TextComponent(itemProfile.getDisplayName())
+                .withStyle(itemProfile.getTier().getChatFormatting()));
+    }
+
+    private static void addAccountTypeNametag(NametagRenderEvent event) {
+        for (AccountType type : AccountType.values()) {
+            if (type.getComponent() == null) continue;
+
+            if (UserInfoModel.isAccountType(event.getEntity().getUUID(), type)) {
+                event.addInjectedLine(type.getComponent());
             }
         }
     }
