2c04f72fc277492a02ac59a7d5314330a2e6cc66
Dylan P <dylan.m.priebe@gmail.com>
2022-11-02 03:17:23 -0500

    Added transparent ghost armor feature

---
+++ common/src/main/java/com/wynntils/mc/event/PlayerGhostArmorRenderEvent.java
@@ -0,0 +1,29 @@
+package com.wynntils.mc.event;
+
+import com.wynntils.core.WynntilsMod;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.eventbus.api.Event;
+
+public class PlayerGhostArmorRenderEvent extends Event {
+
+    private LivingEntity entity;
+    private boolean renderGhostArmor;
+
+    public PlayerGhostArmorRenderEvent(LivingEntity entity) {
+        this.entity = entity;
+        this.renderGhostArmor = true;
+    }
+
+    public LivingEntity getEntity() {
+        return entity;
+    }
+
+    public boolean shouldRenderGhostArmor() {
+        return renderGhostArmor;
+    }
+
+    public void setRenderGhostArmor(boolean renderGhostArmor) {
+        this.renderGhostArmor = renderGhostArmor;
+    }
+
+}
