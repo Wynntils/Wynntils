4de5907e93cd67402afc300d18a550fa4109c009
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-11-05 23:49:20 +0100

    fix: Do a bit more calculation for much less memory usage

--- common/src/main/java/com/wynntils/wynn/utils/ContainerUtils.java
+++ common/src/main/java/com/wynntils/wynn/utils/ContainerUtils.java
@@ -23,6 +23,7 @@ import net.minecraft.network.protocol.game.ServerboundUseItemPacket;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ClickType;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
@@ -95,17 +96,23 @@ public final class ContainerUtils {
         for (ItemStack itemStack : containerMenu.getItems()) {
             if (itemStack.isEmpty()) continue;
 
-            String displayName = ComponentUtils.getCoded(itemStack.getHoverName());
-            if (itemStack.getItem() == Items.EMERALD && displayName.equals(ChatFormatting.GREEN + "Emerald")) {
-                emeralds += itemStack.getCount();
-            } else if (itemStack.getItem() == Items.EMERALD_BLOCK
-                    && displayName.equals(ChatFormatting.GREEN + "Emerald Block")) {
-                emeralds += itemStack.getCount() * 64;
-            } else if (itemStack.getItem() == Items.EXPERIENCE_BOTTLE
-                    && displayName.equals(ChatFormatting.GREEN + "Liquid Emerald")) {
-                emeralds += itemStack.getCount() * (64 * 64);
-            } else if (WynnItemMatchers.isEmeraldPouch(itemStack)) {
+            if (WynnItemMatchers.isEmeraldPouch(itemStack)) {
                 emeralds += EmeraldPouchParser.getPouchUsage(itemStack);
+                continue;
+            }
+
+            Item item = itemStack.getItem();
+            if (item != Items.EMERALD && item != Items.EMERALD_BLOCK && item != Items.EXPERIENCE_BOTTLE) {
+                continue;
+            }
+
+            String displayName = ComponentUtils.getCoded(itemStack.getHoverName());
+            if (item == Items.EMERALD && displayName.equals(ChatFormatting.GREEN + "Emerald")) {
+                emeralds += itemStack.getCount();
+            } else if (item == Items.EMERALD_BLOCK && displayName.equals(ChatFormatting.GREEN + "Emerald Block")) {
+                emeralds += itemStack.getCount() * 64;
+            } else if (item == Items.EXPERIENCE_BOTTLE && displayName.equals(ChatFormatting.GREEN + "Liquid Emerald")) {
+                emeralds += itemStack.getCount() * (64 * 64);
             }
         }
 
