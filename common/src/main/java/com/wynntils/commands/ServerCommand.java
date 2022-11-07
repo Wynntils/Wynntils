bf556d1742dfb9c98552e5fd9f5202b16447ff1b
PhoenixOrigin <PhoenixOrigin@users.noreply.github.com>
2022-11-07 21:27:33 +0000

    ci: spotless formatting [skip ci]

--- common/src/main/java/com/wynntils/commands/ServerCommand.java
+++ common/src/main/java/com/wynntils/commands/ServerCommand.java
@@ -31,8 +31,9 @@ import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextComponent;
+
 public class ServerCommand extends CommandBase {
     @Override
     public void register(CommandDispatcher<CommandSourceStack> dispatcher) {
