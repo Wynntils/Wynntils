8b55b014b4558061a02dbcde529b8b55ee15ddd3
P0ke <alextodaro@rocketmailcom>
2022-11-08 11:14:44 -0800

    use number formatting tools we already have

--- common/src/main/java/com/wynntils/features/user/AbbreviateMobHealthFeature.java
+++ common/src/main/java/com/wynntils/features/user/AbbreviateMobHealthFeature.java
@@ -4,12 +4,11 @@
  */
 package com.wynntils.features.user;
 
-import com.wynntils.core.config.Config;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.mc.event.BossHealthUpdateEvent;
 import com.wynntils.mc.mixin.accessors.ClientboundBossEventPacketAccessor;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import com.wynntils.utils.StringUtils;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import net.minecraft.network.chat.TextComponent;
@@ -21,9 +20,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class AbbreviateMobHealthFeature extends UserFeature {
 
     private static final Pattern MOB_HEALTH_PATTERN = Pattern.compile("(.*§c)(\\d+)(§4❤.*)");
-
-    @Config
-    public static int precision = 1;
 
     @SubscribeEvent
     public void onHealthBarEvent(BossHealthUpdateEvent event) {
@@ -37,33 +33,9 @@ public class AbbreviateMobHealthFeature extends UserFeature {
         if (!healthMatcher.matches()) return;
 
         int rawHealth = Integer.parseInt(healthMatcher.group(2));
-        String formattedHealth = Abbreviation.formatNumber(rawHealth);
+        String formattedHealth = StringUtils.integerToShortString(rawHealth).toUpperCase(Locale.ROOT);
 
         String formattedName = healthMatcher.replaceAll("$1" + formattedHealth + "$3");
         addOperation.name = new TextComponent(formattedName);
-    }
-
-    private enum Abbreviation {
-        B(1_000_000_000),
-        M(1_000_000),
-        K(1_000);
-
-        final float value;
-
-        Abbreviation(float value) {
-            this.value = value;
-        }
-
-        static String formatNumber(int num) {
-            for (Abbreviation abbr : Abbreviation.values()) {
-                if (num >= abbr.value) {
-                    BigDecimal shortened = new BigDecimal(num / abbr.value).setScale(precision, RoundingMode.HALF_UP);
-                    return shortened.toPlainString() + abbr.name();
-                }
-            }
-
-            // no abbreviation necessary
-            return Integer.toString(num);
-        }
     }
 }
