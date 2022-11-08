416ab0118e59bfde409acbc4405c4ca63ccef166
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2022-11-08 17:42:43 +0100

    feat: Add GuildMapScreen (#657)
    * feat: Add AdvancementUpdateEvent
    
    * feat: Parse territories from advancements
    
    * feat: Add GuildMapScreen
    
    * fix: Use CustomColor
    
    * fix: Add comment

---
+++ common/src/main/java/com/wynntils/features/user/map/GuildMapFeature.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © Wynntils 2022.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.features.user.map;
+
+import com.wynntils.core.config.Config;
+import com.wynntils.core.features.UserFeature;
+import com.wynntils.core.features.properties.FeatureCategory;
+import com.wynntils.core.features.properties.FeatureInfo;
+import com.wynntils.core.features.properties.RegisterKeyBind;
+import com.wynntils.core.keybinds.KeyBind;
+import com.wynntils.core.managers.Model;
+import com.wynntils.gui.screens.maps.GuildMapScreen;
+import com.wynntils.mc.utils.McUtils;
+import com.wynntils.wynn.model.territory.GuildTerritoryModel;
+import java.util.List;
+import org.lwjgl.glfw.GLFW;
+
+@FeatureInfo(category = FeatureCategory.MAP)
+public class GuildMapFeature extends UserFeature {
+    public static GuildMapFeature INSTANCE;
+
+    @Config
+    public boolean renderUsingLinear = true;
+
+    @RegisterKeyBind
+    public final KeyBind openGuildMapKeybind = new KeyBind("Open Guild Map", GLFW.GLFW_KEY_J, false, () -> {
+        // If the current screen is already the map, and we get this event, this means we are holding the keybind
+        // and should signal that we should close when the key is not held anymore.
+        if (McUtils.mc().screen instanceof GuildMapScreen guildMapScreen) {
+            guildMapScreen.setHoldingMapKey(true);
+            return;
+        }
+
+        McUtils.mc().setScreen(new GuildMapScreen());
+    });
+
+    @Override
+    public List<Class<? extends Model>> getModelDependencies() {
+        return List.of(GuildTerritoryModel.class);
+    }
+}
