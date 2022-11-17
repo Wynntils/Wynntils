d1a1c2e2511b6a268987b3cd72408de1c5471683
Zatzou <12207527+Zatzou@users.noreply.github.com>
2022-11-17 14:52:52 +0200

    fix issues

--- common/src/main/java/com/wynntils/features/user/ChatTimestampFeature.java
+++ common/src/main/java/com/wynntils/features/user/ChatTimestampFeature.java
@@ -4,37 +4,57 @@
  */
 package com.wynntils.features.user;
 
+import com.wynntils.core.chat.ChatModel;
 import com.wynntils.core.config.Config;
+import com.wynntils.core.config.ConfigHolder;
 import com.wynntils.core.features.UserFeature;
-import com.wynntils.core.features.properties.StartDisabled;
+import com.wynntils.core.managers.Model;
+import com.wynntils.mc.utils.McUtils;
 import com.wynntils.wynn.event.ChatMessageReceivedEvent;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-@StartDisabled
 public class ChatTimestampFeature extends UserFeature {
     @Config
     public String formatPattern = "HH:mm:ss";
 
+    @Override
+    public List<Class<? extends Model>> getModelDependencies() {
+        return List.of(ChatModel.class);
+    }
+
+    @Override
+    protected void onConfigUpdate(ConfigHolder configHolder) {
+        // Try to set the new format string and if it fails revert to the default
+        try {
+            DateTimeFormatter fmt = DateTimeFormatter.ofPattern(formatPattern);
+
+            formatter = fmt;
+        } catch (Exception e) {
+            formatter = null;
+
+            McUtils.sendMessageToClient(new TranslatableComponent("feature.wynntils.chatTimestamp.invalidFormatMsg")
+                    .withStyle(ChatFormatting.DARK_RED));
+        }
+    }
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formatPattern);
+
     @SubscribeEvent
     public void onChat(ChatMessageReceivedEvent event) {
+        if (formatter == null) return;
+
         Component message = event.getMessage();
 
-        // use the user specified formatter and if that formatter fails for whatever reason handle it as gracefully as
-        // we can
-        MutableComponent timestamp;
-        try {
-            LocalDateTime date = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formatPattern);
-
-            timestamp = new TextComponent("§8[§7" + date.format(formatter) + "§8]§r ");
-        } catch (Exception e) {
-            timestamp = new TextComponent("§8[§7<Invalid time format string>§8]§r ");
-        }
+        LocalDateTime date = LocalDateTime.now();
+        MutableComponent timestamp = new TextComponent("§8[§7" + date.format(formatter) + "§8]§r ");
 
         timestamp.append(message);
 
