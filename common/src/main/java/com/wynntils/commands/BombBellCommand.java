d2430856ed1205577083abb0235cda15f9adf547
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-11-19 19:40:01 +0100

    feat: Finish commands

--- common/src/main/java/com/wynntils/commands/BombBellCommand.java
+++ common/src/main/java/com/wynntils/commands/BombBellCommand.java
@@ -13,6 +13,9 @@ import com.wynntils.wynn.model.BombBellModel;
 import com.wynntils.wynn.objects.BombInfo;
 import com.wynntils.wynn.objects.BombType;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.stream.Collectors;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -35,14 +38,55 @@ public class BombBellCommand extends CommandBase {
     }
 
     private int getBombTypeList(CommandContext<CommandSourceStack> context) {
+        BombType bombType;
+
+        try {
+            bombType = BombType.valueOf(context.getArgument("bombType", String.class));
+        } catch (IllegalArgumentException e) {
+            context.getSource().sendFailure(new TextComponent("Invalid bomb type").withStyle(ChatFormatting.RED));
+            return 0;
+        }
+
+        Set<BombInfo> bombBells = BombBellModel.getBombBells().stream()
+                .filter(bombInfo -> bombInfo.bomb() == bombType)
+                .collect(Collectors.toSet());
+
+        MutableComponent component = getBombListComponent(bombBells);
+
+        context.getSource().sendSuccess(component, false);
 
         return 1;
     }
 
     private int listBombs(CommandContext<CommandSourceStack> context) {
+        Set<BombInfo> bombBells = BombBellModel.getBombBells();
+
+        MutableComponent component = getBombListComponent(bombBells);
+
+        context.getSource().sendSuccess(component, false);
+
+        return 1;
+    }
+
+    private static MutableComponent getBombListComponent(Set<BombInfo> bombBells) {
         MutableComponent response = new TextComponent("Bomb Bells: ").withStyle(ChatFormatting.GOLD);
 
-        for (BombInfo bomb : BombBellModel.getBombBells()) {
+        if (bombBells.isEmpty()) {
+            response.append(new TextComponent(
+                                    "There are no active bombs at the moment! This might be because you do not have the ")
+                            .withStyle(ChatFormatting.RED))
+                    .append(new TextComponent("CHAMPION").withStyle(ChatFormatting.YELLOW))
+                    .append(new TextComponent(" rank on Wynncraft, which is necessary to use this feature.")
+                            .withStyle(ChatFormatting.RED));
+            return response;
+        }
+
+        for (BombInfo bomb : bombBells.stream()
+                .sorted(Comparator.comparing(BombInfo::bomb)
+                        .reversed()
+                        .thenComparing(BombInfo::startTime)
+                        .reversed())
+                .toList()) {
             response.append(new TextComponent("\n" + bomb.bomb().getName())
                             .withStyle(ChatFormatting.WHITE)
                             .append(new TextComponent(" on ").withStyle(ChatFormatting.GRAY))
@@ -52,8 +96,6 @@ public class BombBellCommand extends CommandBase {
                             .append(new TextComponent(bomb.getRemainingString()).withStyle(ChatFormatting.WHITE)));
         }
 
-        context.getSource().sendSuccess(response, false);
-
-        return 1;
+        return response;
     }
 }
