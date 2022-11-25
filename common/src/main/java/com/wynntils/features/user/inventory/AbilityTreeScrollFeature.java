c930e1b1a9284f7848becfcc884a154d65bf87de
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-11-26 00:14:05 +0100

    fix: Allow clicks on locked emerald pouch and ability tree slots

--- common/src/main/java/com/wynntils/features/user/inventory/AbilityTreeScrollFeature.java
+++ common/src/main/java/com/wynntils/features/user/inventory/AbilityTreeScrollFeature.java
@@ -10,8 +10,8 @@ import com.wynntils.core.features.properties.FeatureCategory;
 import com.wynntils.core.features.properties.FeatureInfo;
 import com.wynntils.mc.event.MouseScrollEvent;
 import com.wynntils.mc.utils.McUtils;
+import com.wynntils.wynn.screens.WynnScreenMatchers;
 import com.wynntils.wynn.utils.ContainerUtils;
-import java.util.regex.Pattern;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,9 +19,6 @@ import org.lwjgl.glfw.GLFW;
 
 @FeatureInfo(stability = FeatureInfo.Stability.STABLE, category = FeatureCategory.INVENTORY)
 public class AbilityTreeScrollFeature extends UserFeature {
-
-    private static final Pattern ABILITY_TREE_PATTERN =
-            Pattern.compile("(?:Warrior|Shaman|Mage|Assassin|Archer) Abilities");
     private static final int abilityTreePreviousSlot = 57;
     private static final int abilityTreeNextSlot = 59;
 
@@ -33,7 +30,7 @@ public class AbilityTreeScrollFeature extends UserFeature {
         Screen screen = McUtils.mc().screen;
 
         if (!(screen instanceof AbstractContainerScreen<?> gui)) return;
-        if (!ABILITY_TREE_PATTERN.matcher(gui.getTitle().getString()).matches()) return;
+        if (WynnScreenMatchers.isAbilityTreeScreen(gui)) return;
 
         boolean up = event.isScrollingUp() ^ invertScroll;
         int slot = up ? abilityTreePreviousSlot : abilityTreeNextSlot;
