9184386655db61674b969b27f0c11d488a694694
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2022-11-26 03:35:18 +0100

    feat: Redirect "not enough mana", healing and speed effect messages (#722)
    * feat: Redirect "not enough mana" messages
    
    * feat: Redirect healing messages
    
    * feat: Redirect speed boost messages
    
    * fix: Use .matches
    
    Co-authored-by: Alex Todaro <3767283+P0keDev@users.noreply.github.com>

--- common/src/main/java/com/wynntils/features/user/InfoMessageFilterFeature.java
+++ common/src/main/java/com/wynntils/features/user/InfoMessageFilterFeature.java
@@ -56,6 +56,14 @@ public class InfoMessageFilterFeature extends UserFeature {
             "^Your tool has 0 durability left! You will not receive any new resources until you repair it at a Blacksmith.$");
     private static final Pattern NO_CRAFTED_DURABILITY_PATTERN = Pattern.compile(
             "^Your items are damaged and have become less effective. Bring them to a Blacksmith to repair them.$");
+    private static final Pattern NO_MANA_LEFT_TO_CAST_PATTERN =
+            Pattern.compile("^§4You don't have enough mana to cast that spell!$");
+
+    private static final Pattern HEALED_PATTERN = Pattern.compile("^.+ gave you §r§c\\[\\+(\\d+) ❤\\]$");
+
+    private static final Pattern HEAL_PATTERN = Pattern.compile("^§r§c\\[\\+(\\d+) ❤\\]$");
+
+    private static final Pattern SPEED_PATTERN = Pattern.compile("^\\+3 minutes speed boost.$");
 
     private static final Pattern BACKGROUND_WELCOME_1 = Pattern.compile("^ +§6§lWelcome to Wynncraft!$");
     private static final Pattern BACKGROUND_WELCOME_2 =
@@ -78,6 +86,8 @@ public class InfoMessageFilterFeature extends UserFeature {
     private static final Pattern BACKGROUND_FRIEND_JOIN_PATTERN = Pattern.compile(
             "§r§7(§o)?(?<name>.+)§r§8(§o)? has logged into server §r§7(§o)?(?<server>.+)§r§8(§o)? as §r§7(§o)?an? (?<class>.+)");
     private static final Pattern BACKGROUND_FRIEND_LEAVE_PATTERN = Pattern.compile("§r§7(?<name>.+) left the game.");
+
+    private static final Pattern BACKGROUND_HEALED_PATTERN = Pattern.compile("^.+ gave you §r§7§o\\[\\+(\\d+) ❤\\]$");
 
     @Config
     private boolean hideWelcome = true;
@@ -105,6 +115,15 @@ public class InfoMessageFilterFeature extends UserFeature {
 
     @Config
     private FilterType craftedDurability = FilterType.REDIRECT;
+
+    @Config
+    private FilterType notEnoughMana = FilterType.REDIRECT;
+
+    @Config
+    private FilterType heal = FilterType.REDIRECT;
+
+    @Config
+    private FilterType speed = FilterType.REDIRECT;
 
     @SubscribeEvent
     public void onInfoMessage(ChatMessageReceivedEvent e) {
@@ -158,6 +177,49 @@ public class InfoMessageFilterFeature extends UserFeature {
                     String playerClass = matcher.group("class");
 
                     sendFriendJoinMessage(playerName, server, playerClass);
+                    return;
+                }
+            }
+
+            if (heal != FilterType.KEEP) {
+                Matcher matcher = HEAL_PATTERN.matcher(msg);
+                if (matcher.matches()) {
+                    e.setCanceled(true);
+                    if (heal == FilterType.HIDE) {
+                        return;
+                    }
+
+                    String amount = matcher.group(1);
+
+                    sendHealMessage(amount);
+                    return;
+                }
+
+                matcher = HEALED_PATTERN.matcher(msg);
+                if (matcher.matches()) {
+                    e.setCanceled(true);
+                    if (heal == FilterType.HIDE) {
+                        return;
+                    }
+
+                    String amount = matcher.group(1);
+
+                    sendHealMessage(amount);
+                    return;
+                }
+            }
+
+            if (speed != FilterType.KEEP) {
+                Matcher matcher = SPEED_PATTERN.matcher(uncoloredMsg);
+                if (matcher.matches()) {
+                    e.setCanceled(true);
+                    if (speed == FilterType.HIDE) {
+                        return;
+                    }
+
+                    NotificationManager.queueMessage(new TextComponent("+3 minutes")
+                            .withStyle(ChatFormatting.AQUA)
+                            .append(new TextComponent(" speed boost").withStyle(ChatFormatting.GRAY)));
                     return;
                 }
             }
@@ -288,6 +350,21 @@ public class InfoMessageFilterFeature extends UserFeature {
                     return;
                 }
             }
+
+            if (notEnoughMana != FilterType.KEEP) {
+                Matcher matcher = NO_MANA_LEFT_TO_CAST_PATTERN.matcher(msg);
+                if (matcher.matches()) {
+                    e.setCanceled(true);
+                    if (notEnoughMana == FilterType.HIDE) {
+                        return;
+                    }
+
+                    NotificationManager.queueMessage(
+                            new TextComponent("Not enough mana to do that spell!").withStyle(ChatFormatting.DARK_RED));
+
+                    return;
+                }
+            }
         } else if (messageType == MessageType.BACKGROUND) {
             if (hideSystemInfo) {
                 if (BACKGROUND_SYSTEM_INFO.matcher(msg).find()) {
@@ -376,7 +453,27 @@ public class InfoMessageFilterFeature extends UserFeature {
                     return;
                 }
             }
+
+            if (heal != FilterType.KEEP) {
+                Matcher matcher = BACKGROUND_HEALED_PATTERN.matcher(msg);
+                if (matcher.matches()) {
+                    e.setCanceled(true);
+                    if (heal == FilterType.HIDE) {
+                        return;
+                    }
+
+                    String amount = matcher.group(1);
+
+                    sendHealMessage(amount);
+                    return;
+                }
+            }
         }
+    }
+
+    private void sendHealMessage(String amount) {
+        NotificationManager.queueMessage(
+                new TextComponent("[+%s ❤]".formatted(amount)).withStyle(ChatFormatting.DARK_RED));
     }
 
     private void sendFriendLeaveMessage(String playerName) {
