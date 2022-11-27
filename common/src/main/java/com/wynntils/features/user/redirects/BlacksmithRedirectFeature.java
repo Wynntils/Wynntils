90fc8849dcd0f72b1c871a94ef07572109587856
san7890 <the@san7890.com>
2022-11-27 15:55:16 -0700

    Refactor to cut down on unused code
    Keeps selling and scrapping in two separate "lanes" to reduce wasted resources.
    
    Cuts down on stringbuilder excess.

--- common/src/main/java/com/wynntils/features/user/redirects/BlacksmithRedirectFeature.java
+++ common/src/main/java/com/wynntils/features/user/redirects/BlacksmithRedirectFeature.java
@@ -39,61 +39,57 @@ public class BlacksmithRedirectFeature extends UserFeature {
         event.setCanceled(true);
 
         EnumMap<ItemTier, Integer> totalItems = new EnumMap<>(ItemTier.class);
-        // If we get emeralds, this will be the number of emeralds we get. If we get scrap, this will be the number of
-        // scrap we get.
-        String paymentString = "";
+        // How many emeralds/scrap we got from the transaction.
+        String paymentString = messageMatcher.group(3);
         // Tracks count of sold or scrapped items
         int totalItemInteger = 0;
+        // Full message to send to the user.
+        String sendableMessage = "";
 
-        // Retrieve the color code of the item, and then match it to the item tier.
-        Matcher itemMatcher = ITEM_PATTERN.matcher(messageMatcher.group(2)); // Second group contains all of the items.
-        while (itemMatcher.find()) {
-            ChatFormatting itemColor = ChatFormatting.getByCode(itemMatcher
-                    .group(1)
-                    .charAt(0)); // find the color code, then get the ChatFormatting of the item to match it to a tier.
-            ItemTier tierToIncrease = ItemTier.fromChatFormatting(itemColor);
-            if (tierToIncrease == null) continue;
-            totalItems.put(tierToIncrease, totalItems.getOrDefault(tierToIncrease, 0) + 1);
-            totalItemInteger++;
+        // This is for selling items for emeralds.
+        if (messageMatcher.group(1).equals("sold me")) {
+
+            // Retrieve the color code of the item, and then match it to the item tier.
+            Matcher itemMatcher = ITEM_PATTERN.matcher(messageMatcher.group(2)); // Second group contains all of the items.
+            // Tally up the items that we sold.
+            while (itemMatcher.find()) {
+                ChatFormatting itemColor = ChatFormatting.getByCode(itemMatcher
+                        .group(1)
+                        .charAt(0)); // find the color code, then get the ChatFormatting of the item to match it to a tier.
+                ItemTier tierToIncrease = ItemTier.fromChatFormatting(itemColor);
+                if (tierToIncrease == null) continue;
+                totalItems.put(tierToIncrease, totalItems.getOrDefault(tierToIncrease, 0) + 1);
+                totalItemInteger++;
+            }
+            String itemPluralizer = totalItemInteger == 1 ? "item" : "items";
+
+                // Build up the string that outlines how many items were sold in what tier (0/0/0/0/0/0/0/0).
+                StringBuilder countByTier = new StringBuilder();
+                for (ItemTier tier : ItemTier.values()) {
+                    countByTier.append('/' + tier.getChatFormatting().toString() + totalItems.getOrDefault(tier, 0));
+                    countByTier.append(ChatFormatting.LIGHT_PURPLE);
+                }
+                countByTier.append(")");
+                countByTier.setCharAt(0, '(');
+
+                String countByTierString = countByTier.toString();
+                
+                // Sold 1 (1/0/0/0/0/0/0/0) item for 4e.
+                sendableMessage = String.format("%sSold %d %s %s for %s%s%s%s.", ChatFormatting.LIGHT_PURPLE, totalItemInteger, itemPluralizer, countByTierString, ChatFormatting.GREEN, paymentString, EmeraldSymbols.EMERALDS, ChatFormatting.LIGHT_PURPLE);
+            }
+        // Scrapping items for scrap.
+        else
+        {
+            Matcher itemMatcher = ITEM_PATTERN.matcher(messageMatcher.group(2)); // Second group contains all of the items.
+            while (itemMatcher.find()) {
+                totalItemInteger++;
+            }
+            String itemPluralizer = totalItemInteger == 1 ? "" : "s";
+
+            sendableMessage = String.format("%sScrapped %d %s for %s%s scrap%s.", ChatFormatting.LIGHT_PURPLE, totalItemInteger, itemPluralizer, ChatFormatting.YELLOW, paymentString, ChatFormatting.LIGHT_PURPLE);
         }
-            // Let's tally up the total number of items sold.
-            StringBuilder messageCounts = new StringBuilder();
-            for (ItemTier tier : ItemTier.values()) {
-                messageCounts.append('/' + tier.getChatFormatting().toString() + totalItems.getOrDefault(tier, 0));
-                messageCounts.append(ChatFormatting.LIGHT_PURPLE);
-            }
 
-            String pluralizer = totalItemInteger == 1 ? "" : "s";
-
-            messageCounts.append(") item" + pluralizer + " for ");
-
-            messageCounts.setCharAt(0, '(');
-
-            // How many emeralds/scrap we got from the blacksmith.
-            paymentString = messageMatcher.group(3);
-
-            String messageCountsString = messageCounts.toString();
-
-            // Now, we create the full message.
-            StringBuilder sendableMessage = new StringBuilder();
-            // Normal sale of items for emeralds.
-            if (messageMatcher.group(1).equals("sold me")) {
-                sendableMessage.append(ChatFormatting.LIGHT_PURPLE + "Sold " + totalItemInteger + " ");
-                sendableMessage.append(messageCountsString);
-                sendableMessage.append(ChatFormatting.GREEN + paymentString + EmeraldSymbols.EMERALDS
-                        + ChatFormatting.LIGHT_PURPLE + ".");
-            }
-            // Scrapping items for scrap
-            else {
-                sendableMessage.append(ChatFormatting.LIGHT_PURPLE + "Scrapped " + totalItemInteger);
-                sendableMessage.append(" item" + pluralizer + " for ");
-                sendableMessage.append(
-                        ChatFormatting.YELLOW + paymentString + " scrap" + ChatFormatting.LIGHT_PURPLE + ".");
-            }
-
-            String sendableMessageString = sendableMessage.toString();
-
-            // Finally, we send the message.
-            NotificationManager.queueMessage(sendableMessageString);
+        // Finally, we send the message.
+        NotificationManager.queueMessage(sendableMessage);
     }
 }
