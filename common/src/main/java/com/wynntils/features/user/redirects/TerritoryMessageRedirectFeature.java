733884872f20ecd6e73e060c7824948aee9f756b
san7890 <the@san7890.com>
2022-11-29 15:26:41 -0700

    Cleanout
    Removes Config, Priority, makes it just getOriginalCodedMessage, linebreaks, l18n cleanup.

--- common/src/main/java/com/wynntils/features/user/redirects/TerritoryMessageRedirectFeature.java
+++ common/src/main/java/com/wynntils/features/user/redirects/TerritoryMessageRedirectFeature.java
@@ -4,7 +4,6 @@
  */
 package com.wynntils.features.user.redirects;
 
-import com.wynntils.core.config.Config;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.features.properties.FeatureCategory;
 import com.wynntils.core.features.properties.FeatureInfo;
@@ -14,27 +13,23 @@ import com.wynntils.mc.utils.ComponentUtils;
 import com.wynntils.wynn.event.ChatMessageReceivedEvent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @FeatureInfo(category = FeatureCategory.REDIRECTS)
 public class TerritoryMessageRedirectFeature extends UserFeature {
     private static final Pattern TERRITORY_MESSAGE_PATTERN = Pattern.compile("§7\\[You are now (\\S+) (.+)\\]");
 
-    @Config
-    public boolean redirectTerritoryMessages = true;
-
     // Handles the subtitle text event.
     @SubscribeEvent
     public void onSubtitleSetText(SubtitleSetTextEvent event) {
-        if (!redirectTerritoryMessages) return;
-
         String codedString = ComponentUtils.getCoded(event.getComponent());
         Matcher matcher = TERRITORY_MESSAGE_PATTERN.matcher(codedString);
         if (matcher.matches()) event.setCanceled(true);
+
         String rawDirection = matcher.group(1);
         String rawTerritoryName = matcher.group(2);
         String directionalArrow = "";
+
         switch (rawDirection) {
             case "entering":
                 directionalArrow = "→";
@@ -43,6 +38,7 @@ public class TerritoryMessageRedirectFeature extends UserFeature {
                 directionalArrow = "←";
                 break;
         }
+
         // Want to account for weird stuff like "the Forgery" and make it "The Forgery" for the sake of our brief
         // message (looks odd otherwise). Same for the direction as well (e.g. "leaving" -> "Leaving").
         String territoryName = rawTerritoryName.substring(0, 1).toUpperCase() + rawTerritoryName.substring(1);
@@ -53,12 +49,9 @@ public class TerritoryMessageRedirectFeature extends UserFeature {
 
     // Handles the chat log message event, we don't want a duplicate so just cancel the event and rely on the subtitle
     // text event.
-    @SubscribeEvent(priority = EventPriority.LOW)
+    @SubscribeEvent
     public void onChat(ChatMessageReceivedEvent event) {
-        if (!redirectTerritoryMessages) {
-            return;
-        }
-        String codedString = ComponentUtils.getCoded(event.getMessage());
+        String codedString = event.getOriginalCodedMessage();
         Matcher matcher = TERRITORY_MESSAGE_PATTERN.matcher(codedString);
         if (matcher.matches()) event.setCanceled(true);
     }
