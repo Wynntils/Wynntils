14e0fbbde4b1b3632a714e91ae1f2cdfef78a1af
nHexanol <nHexanol@gmail.com>
2022-12-03 20:51:00 +0000

    Finally fixed this bs
    Took 1 hour 31 minutes

--- common/src/main/java/com/wynntils/features/user/GuildStarsToRankFeature.java
+++ common/src/main/java/com/wynntils/features/user/GuildStarsToRankFeature.java
@@ -5,33 +5,28 @@
 package com.wynntils.features.user;
 
 import com.wynntils.core.features.UserFeature;
-import com.wynntils.core.notifications.NotificationManager;
-import com.wynntils.mc.utils.McUtils;
 import com.wynntils.wynn.event.ChatMessageReceivedEvent;
 import com.wynntils.wynn.objects.GuildRank;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.minecraft.network.chat.*;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-
 public class GuildStarsToRankFeature extends UserFeature {
 
-    private static final Pattern guildMessage = Pattern.compile("(?:§r)?§3\\[(?:§b)?(★{0,5})?(?:§3)?(.*)§3\\](?:§b)? (.*)");
+    private static final Pattern guildMessage = Pattern.compile("(?:§r)?§3\\[(?:§b)?(★{0,5})?(?:§3)?(.*)§3\\](?:§b)? .*");
 
     @SubscribeEvent
     public void onChat(ChatMessageReceivedEvent event) {
         String message = event.getOriginalCodedMessage();
-        System.out.println(message);
         Matcher match = guildMessage.matcher(message);
-        if (!(match.matches())) return;
+        if (!(match.matches()) || match.group(2).equals("INFO")) return;
         MutableComponent newMessage = new TextComponent(String.format(
-                "§3b§b%s §3%s§b] §b%s",
-                GuildRank.fromStars(match.group(1).length()).getRank(), match.group(2), match.group(3)));
-
+                "§b[§b%s §3%s§b] §b",
+                GuildRank.fromStars(match.group(1).length()).getRank(), match.group(2)));
+        event.getMessage().getSiblings().forEach(newMessage::append);
         event.setMessage(newMessage);
     }
 
