7f1c1df49c4a45fce3ff7893c39142e21dd4f3ad
Magnus Ihse Bursie <mag@icus.se>
2022-12-08 23:43:13 +0100

    Use new Downloader for SplashManager

--- common/src/main/java/com/wynntils/wynn/model/SplashManager.java
+++ common/src/main/java/com/wynntils/wynn/model/SplashManager.java
@@ -8,8 +8,9 @@ import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.wynntils.core.managers.CoreManager;
+import com.wynntils.core.net.downloader.DownloadableResource;
+import com.wynntils.core.net.downloader.Downloader;
 import com.wynntils.utils.Utils;
-import java.io.File;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,30 +25,26 @@ public class SplashManager extends CoreManager {
     private static List<String> allSplashes = new ArrayList<>();
     private static String currentSplash = DEFAULT_SPLASH;
 
+    public static String getCurrentSplash() {
+        return currentSplash;
+    }
+
     public static void init() {
         updateCurrentSplash();
     }
 
     private static void updateCurrentSplash() {
-        RequestHandler handler = WebManager.getHandler();
-        handler.addAndDispatch(new RequestBuilder(SPLASHES_URL, "splashes")
-                .cacheTo(new File(WebManager.API_CACHE_ROOT, "splashes.json"))
-                .useCacheAsBackup()
-                .handleJsonArray(json -> {
-                    Type type = new TypeToken<List<String>>() {}.getType();
-                    allSplashes = GSON.fromJson(json, type);
-                    if (allSplashes.isEmpty()) {
-                        // Use fallback in case of failure
-                        currentSplash = DEFAULT_SPLASH;
-                    } else {
-                        currentSplash = allSplashes.get(Utils.getRandom().nextInt(allSplashes.size()));
-                    }
-                    return true;
-                })
-                .build());
-    }
-
-    public static String getCurrentSplash() {
-        return currentSplash;
+        DownloadableResource dl = Downloader.download(SPLASHES_URL, "splashes.json", "splashes");
+        dl.handleJsonObject(json -> {
+            Type type = new TypeToken<List<String>>() {}.getType();
+            allSplashes = GSON.fromJson(json, type);
+            if (allSplashes.isEmpty()) {
+                // Use fallback in case of failure
+                currentSplash = DEFAULT_SPLASH;
+            } else {
+                currentSplash = allSplashes.get(Utils.getRandom().nextInt(allSplashes.size()));
+            }
+            return true;
+        });
     }
 }
