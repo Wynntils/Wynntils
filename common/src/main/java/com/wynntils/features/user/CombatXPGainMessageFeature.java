285528d2f7877b80a509aa53596464b95c4e3130
san7890 <the@san7890.com>
2022-12-11 00:02:56 -0700

    whoops

--- common/src/main/java/com/wynntils/features/user/CombatXPGainMessageFeature.java
+++ common/src/main/java/com/wynntils/features/user/CombatXPGainMessageFeature.java
@@ -1,5 +1,6 @@
 package com.wynntils.features.user;
 
+import com.wynntils.core.WynntilsMod;
 import com.wynntils.core.config.Config;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.features.properties.FeatureInfo;
@@ -17,24 +18,28 @@ public class CombatXPGainMessageFeature extends UserFeature {
     @Config
     public boolean getCombatXPGainMessages = true;
 
-    private static final DecimalFormat percentFormat = new DecimalFormat("##.##0.00'%'");
+    private static final DecimalFormat percentFormat = new DecimalFormat("##.##'%'");
     private static float startTickXP = 0;
     private static float endTickXP = 0;
 
     @SubscribeEvent
-    public void onTick (ClientTickEvent.Start event) {
+    public void onTickStart (ClientTickEvent.Start event) {
         if(!getCombatXPGainMessages) { return; }
         CharacterManager.CharacterInfo data = WynnUtils.getCharacterInfo();
 
         startTickXP = data.getCurrentXp();
+
+        WynntilsMod.info("START TICK XP IS " + startTickXP); //FIXME DEBUG
     }
 
     @SubscribeEvent
-    public void onTick(ClientTickEvent.End event) {
+    public void onTickEnd (ClientTickEvent.End event) {
         if(!getCombatXPGainMessages) { return; }
         CharacterManager.CharacterInfo data = WynnUtils.getCharacterInfo();
     
         endTickXP = data.getCurrentXp();
+        WynntilsMod.info("END TICK XP IS " + endTickXP); //FIXME DEBUG
+
         if (endTickXP == startTickXP) { return; }
 
         int gainedXP = Math.round(endTickXP) - Math.round(startTickXP);
