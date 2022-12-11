f3ebc0a286c767e00c620cd2a8d2c5597422a39f
san7890 <the@san7890.com>
2022-12-10 21:30:31 -0700

    oh it's apparently a system pattern that's cool

--- common/src/main/java/com/wynntils/features/user/redirects/ChatRedirectFeature.java
+++ common/src/main/java/com/wynntils/features/user/redirects/ChatRedirectFeature.java
@@ -261,17 +261,11 @@ public class ChatRedirectFeature extends UserFeature {
     }
 
     private class HealRedirector extends SimpleRedirector {
-        private static final Pattern NORMAL_PATTERN = Pattern.compile("^§r§c\\[\\+(\\d+) ❤\\]$");
-        private static final Pattern BACKGROUND_PATTERN = Pattern.compile("^§r§7§o\\[\\+(\\d+) ❤\\]$");
+        private static final Pattern SYSTEM_PATTERN = Pattern.compile("^§c\\[\\+(\\d+) ❤\\]$");
 
         @Override
-        protected Pattern getNormalPattern() {
-            return NORMAL_PATTERN;
-        }
-
-        @Override
-        protected Pattern getBackgroundPattern() {
-            return BACKGROUND_PATTERN;
+        protected Pattern getSystemPattern() {
+            return SYSTEM_PATTERN;
         }
 
         @Override
