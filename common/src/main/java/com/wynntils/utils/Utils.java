9da1d4fc57b401a5e03adb5e9c0337f3b84d8520
Magnus Ihse Bursie <mag@icus.se>
2022-12-14 08:30:50 +0100

    Use NetManager.openLink to open links in browser

--- common/src/main/java/com/wynntils/utils/Utils.java
+++ common/src/main/java/com/wynntils/utils/Utils.java
@@ -4,11 +4,8 @@
  */
 package com.wynntils.utils;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Random;
-import net.minecraft.Util;
 
 /**
  * This is a "high-quality misc" class. Helper methods that are commonly used throughout the project
@@ -24,18 +21,5 @@ public final class Utils {
 
     public static Random getRandom() {
         return random;
-    }
-
-    /**
-     * Open the specified URL in the user's browser if possible, otherwise copy it to the clipboard
-     * and send it to chat.
-     * @param url The url to open
-     */
-    public static void openUrl(String url) {
-        Util.getPlatform().openUri(url);
-    }
-
-    public static String encodeUrl(String url) {
-        return URLEncoder.encode(url, StandardCharsets.UTF_8);
     }
 }
