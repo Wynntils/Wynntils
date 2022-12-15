64e93015aada4522c3712362cd60ac0dcbf07f37
Magnus Ihse Bursie <mag@icus.se>
2022-12-15 15:47:51 +0100

    Fix merge error

--- common/src/main/java/com/wynntils/core/managers/UpdateManager.java
+++ common/src/main/java/com/wynntils/core/managers/UpdateManager.java
@@ -6,6 +6,7 @@ package com.wynntils.core.managers;
 
 import com.wynntils.core.WynntilsMod;
 import com.wynntils.core.net.ApiResponse;
+import com.wynntils.core.net.NetManager;
 import com.wynntils.core.net.UrlId;
 import com.wynntils.utils.FileUtils;
 import com.wynntils.utils.MD5Verification;
