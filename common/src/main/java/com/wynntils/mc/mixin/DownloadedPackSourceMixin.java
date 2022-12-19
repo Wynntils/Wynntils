32911f2b6d4e2ffd98d696ca8d674822151e598e
Magnus Ihse Bursie <mag@icus.se>
2022-12-19 18:18:27 +0100

    Remove extra newline

--- common/src/main/java/com/wynntils/mc/mixin/DownloadedPackSourceMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/DownloadedPackSourceMixin.java
@@ -46,7 +46,6 @@ public abstract class DownloadedPackSourceMixin {
             } catch (IOException e) {
                 // ignored
             }
-
         } else {
             EventFactory.onResourcePackClearEvent(null);
         }
