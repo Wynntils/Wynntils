b9d2cb93c484789a1eebbb88f837f311d5644811
Magnus Ihse Bursie <mag@icus.se>
2022-12-21 19:02:40 +0100

    refactor: Move hades stuff to core.net.hades. (#910)

--- common/src/main/java/com/wynntils/core/net/hades/objects/PlayerStatus.java
+++ common/src/main/java/com/wynntils/core/net/hades/objects/PlayerStatus.java
@@ -2,7 +2,7 @@
  * Copyright © Wynntils 2022.
  * This file is released under AGPLv3. See LICENSE for full license details.
  */
-package com.wynntils.sockets.objects;
+package com.wynntils.core.net.hades.objects;
 
 public record PlayerStatus(float x, float y, float z, int health, int maxHealth, int mana, int maxMana) {
     public boolean equals(float x, float y, float z, int health, int mana, int maxHealth, int maxMana) {
