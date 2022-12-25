Wynntils/Wynntils

commit 43b2fa78275934e814e38eec5aeb3202af0e4eee

fix: Fix compatibility with other mods' client commands (#925)

common/src/main/java/com/wynntils/mc/mixin/ClientPacketListenerMixin.java
@@ -5,11 +5,9 @@
 package com.wynntils.mc.mixin;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.tree.RootCommandNode;
 import com.wynntils.mc.EventFactory;
 import com.wynntils.mc.event.ChatPacketReceivedEvent;
 import com.wynntils.mc.event.ChatSentEvent;
-import com.wynntils.mc.event.CommandsPacketEvent;
 import com.wynntils.mc.event.ContainerSetContentEvent;
 import com.wynntils.mc.utils.McUtils;
 import java.util.UUID;
@@ -18,7 +16,6 @@ import net.minecraft.client.gui.screens.ReceivingLevelScreen;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.multiplayer.ClientRegistryLayer;
 import net.minecraft.client.multiplayer.PlayerInfo;
-import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.core.LayeredRegistryAccess;
 import net.minecraft.network.chat.ChatType;
@@ -112,18 +109,10 @@ public abstract class ClientPacketListenerMixin {
 
     @Inject(
             method = "handleCommands(Lnet/minecraft/network/protocol/game/ClientboundCommandsPacket;)V",
-            at = @At("HEAD"),
-            cancellable = true)
-    private void handleCommandsPre(ClientboundCommandsPacket packet, CallbackInfo ci) {
+            at = @At("RETURN"))
+    private void handleCommandsPost(ClientboundCommandsPacket packet, CallbackInfo ci) {
         if (!isRenderThread()) return;
-        RootCommandNode<SharedSuggestionProvider> root = packet.getRoot(
-                CommandBuildContext.simple(this.registryAccess.compositeAccess(), this.enabledFeatures()));
-        CommandsPacketEvent event = EventFactory.onCommandsPacket(root);
-        if (!event.getRoot().equals(root)) {
-            // We modified command root, so inject it
-            this.commands = new CommandDispatcher<>(event.getRoot());
-            ci.cancel();
-        }
+        EventFactory.onCommandsPacket(this.commands.getRoot());
     }
 
     @Inject(method = "handlePlayerInfoUpdate", at = @At("RETURN"))

commit 55a90bb2ba3b099ff01b887193a857f9c69bad0a

fix: Our command manipulation was broken by #925

common/src/main/java/com/wynntils/mc/mixin/ClientPacketListenerMixin.java
@@ -5,9 +5,11 @@
 package com.wynntils.mc.mixin;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.tree.RootCommandNode;
 import com.wynntils.mc.EventFactory;
 import com.wynntils.mc.event.ChatPacketReceivedEvent;
 import com.wynntils.mc.event.ChatSentEvent;
+import com.wynntils.mc.event.CommandsPacketEvent;
 import com.wynntils.mc.event.ContainerSetContentEvent;
 import com.wynntils.mc.utils.McUtils;
 import java.util.UUID;
@@ -113,7 +115,15 @@ public abstract class ClientPacketListenerMixin {
             at = @At("RETURN"))
     private void handleCommandsPost(ClientboundCommandsPacket packet, CallbackInfo ci) {
         if (!isRenderThread()) return;
-        EventFactory.onCommandsPacket(this.commands.getRoot());
+        // We need to read the root from the CommandDispatcher, not the packet,
+        // due to interop with other mods
+        RootCommandNode<SharedSuggestionProvider> root = this.commands.getRoot();
+        CommandsPacketEvent event = EventFactory.onCommandsPacket(root);
+
+        if (event.getRoot() != root) {
+            // If we changed the root, replace the CommandDispatcher
+            this.commands = new CommandDispatcher(event.getRoot());
+        }
     }
 
     @Inject(method = "handlePlayerInfoUpdate", at = @At("RETURN"))