2bbb4bdaff5b3fc756c834767191722ceba0a011
Magnus Ihse Bursie <mag@icus.se>
2022-12-28 21:35:40 +0100

    Create ItemHandler

---
+++ common/src/main/java/com/wynntils/handlers/item/ItemHandler.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright © Wynntils 2022.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.handlers.item;
+
+import com.wynntils.core.components.Handler;
+
+public class ItemHandler extends Handler {}
