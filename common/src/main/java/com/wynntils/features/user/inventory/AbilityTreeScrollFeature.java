10e8e08d051f5ec86eda0e043ae7b83b8ed98362
Magnus Ihse Bursie <mag@icus.se>
2022-12-29 03:22:18 +0100

    refactor: Create ContainerManager from WynnScreenMatchers (#946)

--- common/src/main/java/com/wynntils/features/user/inventory/AbilityTreeScrollFeature.java
+++ common/src/main/java/com/wynntils/features/user/inventory/AbilityTreeScrollFeature.java
@@ -4,13 +4,13 @@
  */
 package com.wynntils.features.user.inventory;
 
+import com.wynntils.core.components.Managers;
 import com.wynntils.core.config.Config;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.features.properties.FeatureCategory;
 import com.wynntils.core.features.properties.FeatureInfo;
 import com.wynntils.mc.event.MouseScrollEvent;
 import com.wynntils.mc.utils.McUtils;
-import com.wynntils.wynn.screens.WynnScreenMatchers;
 import com.wynntils.wynn.utils.ContainerUtils;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -30,7 +30,7 @@ public class AbilityTreeScrollFeature extends UserFeature {
         Screen screen = McUtils.mc().screen;
 
         if (!(screen instanceof AbstractContainerScreen<?> gui)) return;
-        if (!WynnScreenMatchers.isAbilityTreeScreen(gui)) return;
+        if (!Managers.Container.isAbilityTreeScreen(gui)) return;
 
         boolean up = event.isScrollingUp() ^ invertScroll;
         int slot = up ? abilityTreePreviousSlot : abilityTreeNextSlot;
