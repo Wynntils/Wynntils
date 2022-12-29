bd03fe8434cd8685b16840972c3aeb52fdbbf2dc
Magnus Ihse Bursie <mag@icus.se>
2022-12-29 22:25:22 +0100

    Update durability arc feature

--- common/src/main/java/com/wynntils/features/user/inventory/DurabilityArcFeature.java
+++ common/src/main/java/com/wynntils/features/user/inventory/DurabilityArcFeature.java
@@ -12,13 +12,15 @@ import com.wynntils.core.features.properties.FeatureCategory;
 import com.wynntils.core.features.properties.FeatureInfo;
 import com.wynntils.core.features.properties.FeatureInfo.Stability;
 import com.wynntils.gui.render.RenderUtils;
+import com.wynntils.handlers.item.ItemAnnotation;
+import com.wynntils.handlers.item.ItemHandler;
 import com.wynntils.mc.event.HotbarSlotRenderEvent;
 import com.wynntils.mc.event.SlotRenderEvent;
 import com.wynntils.mc.objects.CustomColor;
-import com.wynntils.wynn.item.WynnItemStack;
-import com.wynntils.wynn.item.properties.DurabilityProperty;
-import com.wynntils.wynn.item.properties.ItemProperty;
+import com.wynntils.model.item.properties.DurableItemProperty;
+import com.wynntils.utils.CappedValue;
 import java.util.List;
+import java.util.Optional;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -49,17 +51,18 @@ public class DurabilityArcFeature extends UserFeature {
     }
 
     private void drawDurabilityArc(ItemStack item, int slotX, int slotY, boolean hotbar) {
-        if (!(item instanceof WynnItemStack wynnItem)) return;
+        Optional<ItemAnnotation> annotationOpt = ItemHandler.getItemStackAnnotation(item);
+        if (annotationOpt.isEmpty()) return;
+        if (!(annotationOpt.get() instanceof DurableItemProperty wynnItem)) return;
 
-        if (!wynnItem.hasProperty(ItemProperty.DURABILITY)) return; // no durability info
-        DurabilityProperty durability = wynnItem.getProperty(ItemProperty.DURABILITY);
+        CappedValue durability = wynnItem.getDurability();
 
         // calculate color of arc
-        float durabilityPercent = durability.getDurabilityPercent();
-        int colorInt = Mth.hsvToRgb(Math.max(0f, durabilityPercent) / 3f, 1f, 1f);
+        float durabilityFraction = (float) durability.getCurrent() / durability.getMax();
+        int colorInt = Mth.hsvToRgb(Math.max(0f, durabilityFraction) / 3f, 1f, 1f);
         CustomColor color = CustomColor.fromInt(colorInt).withAlpha(160);
 
         // draw
-        RenderUtils.drawArc(color, slotX, slotY, hotbar ? 0 : 200, durabilityPercent, 6, 8);
+        RenderUtils.drawArc(color, slotX, slotY, hotbar ? 0 : 200, durabilityFraction, 6, 8);
     }
 }
