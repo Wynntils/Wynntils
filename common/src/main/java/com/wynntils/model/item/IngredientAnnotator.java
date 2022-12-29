64a3ef3e450d9c4ad032b10e535fb137ba41a5e6
Magnus Ihse Bursie <mag@icus.se>
2022-12-29 17:14:21 +0100

    Improve IngredientAnnotator

--- common/src/main/java/com/wynntils/model/item/IngredientAnnotator.java
+++ common/src/main/java/com/wynntils/model/item/IngredientAnnotator.java
@@ -4,25 +4,51 @@
  */
 package com.wynntils.model.item;
 
+import com.wynntils.core.WynntilsMod;
 import com.wynntils.core.components.Managers;
 import com.wynntils.handlers.item.ItemAnnotation;
 import com.wynntils.handlers.item.ItemAnnotator;
+import com.wynntils.mc.utils.ComponentUtils;
 import com.wynntils.model.item.game.IngredientItem;
 import com.wynntils.wynn.item.parsers.WynnItemMatchers;
 import com.wynntils.wynn.objects.profiles.ingredient.IngredientProfile;
+import java.util.Map;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import net.minecraft.ChatFormatting;
 import net.minecraft.world.item.ItemStack;
 
 public final class IngredientAnnotator implements ItemAnnotator {
+    private static final Pattern INGREDIENT_PATTERN = Pattern.compile("^§7(.*)§[3567] \\[§([8bde])✫(§8)?✫(§8)?✫§[3567]\\]$");
+    private static final Map<ChatFormatting, Integer> LEVEL_COLORS = Map.of(
+            ChatFormatting.DARK_GRAY, 0,
+            ChatFormatting.YELLOW, 1,
+            ChatFormatting.LIGHT_PURPLE, 2,
+            ChatFormatting.AQUA, 3
+    );
+
     @Override
     public ItemAnnotation getAnnotation(ItemStack itemStack) {
-        Matcher matcher = WynnItemMatchers.ingredientOrMaterialMatcher(itemStack.getHoverName());
-        if (!matcher.matches()) {
+        String name = ComponentUtils.getCoded(itemStack.getHoverName());
+        Matcher matcher = INGREDIENT_PATTERN.matcher(name);
+        if (!matcher.matches()) return null;
+
+        String ingredientName = matcher.group(1);
+        String tierColor = matcher.group(2);
+        int tier = getTierFromColorCode(tierColor);
+
+
+        IngredientProfile ingredientProfile = Managers.ItemProfiles.getIngredient(ingredientName);
+        if (ingredientProfile == null) return null;
+        if (ingredientProfile.getTier().getTierInt() != tier) {
+            WynntilsMod.warn("Incorrect tier in ingredient database: " + ingredientName + " is " + tier);
             return null;
         }
-        IngredientProfile ingredientProfile = Managers.ItemProfiles.getIngredient(matcher.group(1));
-        if (ingredientProfile == null) return null;
 
         return new IngredientItem(ingredientProfile);
+    }
+
+    private int getTierFromColorCode(String tierColor) {
+        return LEVEL_COLORS.getOrDefault(ChatFormatting.getByCode(tierColor.charAt(0)), 0);
     }
 }
