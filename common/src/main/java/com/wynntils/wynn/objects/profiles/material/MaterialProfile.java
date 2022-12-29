57dd895546f3b91e107823966bd04db477a37ce4
Magnus Ihse Bursie <mag@icus.se>
2022-12-29 13:49:14 +0100

    Fix up MaterialProfile

--- common/src/main/java/com/wynntils/wynn/objects/profiles/material/MaterialProfile.java
+++ common/src/main/java/com/wynntils/wynn/objects/profiles/material/MaterialProfile.java
@@ -9,6 +9,64 @@ import java.util.List;
 import java.util.Map;
 
 public class MaterialProfile {
+    public static final Map<MaterialType, List<LeveledMaterial>> LEVELED_MATERIALS = Map.of(
+            MaterialType.ORE,
+            List.of(
+                    new LeveledMaterial("Copper", 1),
+                    new LeveledMaterial("Granite", 10),
+                    new LeveledMaterial("Gold", 20),
+                    new LeveledMaterial("Sandstone", 30),
+                    new LeveledMaterial("Iron", 40),
+                    new LeveledMaterial("Silver", 50),
+                    new LeveledMaterial("Cobalt", 60),
+                    new LeveledMaterial("Kanderstone", 70),
+                    new LeveledMaterial("Diamond", 80),
+                    new LeveledMaterial("Molten", 90),
+                    new LeveledMaterial("Voidstone", 100),
+                    new LeveledMaterial("Dernic", 110)),
+            MaterialType.LOG,
+            List.of(
+                    new LeveledMaterial("Oak", 1),
+                    new LeveledMaterial("Birch", 10),
+                    new LeveledMaterial("Willow", 20),
+                    new LeveledMaterial("Acacia", 30),
+                    new LeveledMaterial("Spruce", 40),
+                    new LeveledMaterial("Jungle", 50),
+                    new LeveledMaterial("Dark", 60),
+                    new LeveledMaterial("Light", 70),
+                    new LeveledMaterial("Pine", 80),
+                    new LeveledMaterial("Avo", 90),
+                    new LeveledMaterial("Sky", 100),
+                    new LeveledMaterial("Dernic", 110)),
+            MaterialType.CROP,
+            List.of(
+                    new LeveledMaterial("Wheat", 1),
+                    new LeveledMaterial("Barley", 10),
+                    new LeveledMaterial("Oat", 20),
+                    new LeveledMaterial("Malt", 30),
+                    new LeveledMaterial("Hops", 40),
+                    new LeveledMaterial("Rye", 50),
+                    new LeveledMaterial("Millet", 60),
+                    new LeveledMaterial("Decay_Roots", 70),
+                    new LeveledMaterial("Rice", 80),
+                    new LeveledMaterial("Sorghum", 90),
+                    new LeveledMaterial("Hemp", 100),
+                    new LeveledMaterial("Dernic_Seed", 110)),
+            MaterialType.FISH,
+            List.of(
+                    new LeveledMaterial("Gudgeon", 1),
+                    new LeveledMaterial("Trout", 10),
+                    new LeveledMaterial("Salmon", 20),
+                    new LeveledMaterial("Carp", 30),
+                    new LeveledMaterial("Icefish", 40),
+                    new LeveledMaterial("Piranha", 50),
+                    new LeveledMaterial("Koi", 60),
+                    new LeveledMaterial("Gylia_Fish", 70),
+                    new LeveledMaterial("Bass", 80),
+                    new LeveledMaterial("Molten_Eel", 90),
+                    new LeveledMaterial("Starfish", 100),
+                    new LeveledMaterial("Dernic_Fish", 110)));
+
     private final ResourceType resourceType;
     private final LeveledMaterial leveledMaterial;
     private final int tier;
@@ -17,6 +75,25 @@ public class MaterialProfile {
         this.resourceType = resourceType;
         this.leveledMaterial = leveledMaterial;
         this.tier = tier;
+    }
+
+    public static MaterialProfile fromCodedString(String name) {
+        // FIXME
+        // §fWheat String§6 [§e✫§8✫✫§6]
+        // §fSalmon Oil§6 [§e✫§8✫✫§6]
+        return null;
+    }
+
+    public ResourceType getResourceType() {
+        return resourceType;
+    }
+
+    public LeveledMaterial getLeveledMaterial() {
+        return leveledMaterial;
+    }
+
+    public int getTier() {
+        return tier;
     }
 
     public enum MaterialType {
@@ -50,63 +127,4 @@ public class MaterialProfile {
     }
 
     public record LeveledMaterial(String name, int level) {}
-
-    Map<MaterialType, List<LeveledMaterial>> leveledMaterials = Map.of(
-            MaterialType.ORE,
-                    List.of(
-                            new LeveledMaterial("Copper", 1),
-                            new LeveledMaterial("Granite", 10),
-                            new LeveledMaterial("Gold", 20),
-                            new LeveledMaterial("Sandstone", 30),
-                            new LeveledMaterial("Iron", 40),
-                            new LeveledMaterial("Silver", 50),
-                            new LeveledMaterial("Cobalt", 60),
-                            new LeveledMaterial("Kanderstone", 70),
-                            new LeveledMaterial("Diamond", 80),
-                            new LeveledMaterial("Molten", 90),
-                            new LeveledMaterial("Voidstone", 100),
-                            new LeveledMaterial("Dernic", 110)),
-            MaterialType.LOG,
-                    List.of(
-                            new LeveledMaterial("Oak", 1),
-                            new LeveledMaterial("Birch", 10),
-                            new LeveledMaterial("Willow", 20),
-                            new LeveledMaterial("Acacia", 30),
-                            new LeveledMaterial("Spruce", 40),
-                            new LeveledMaterial("Jungle", 50),
-                            new LeveledMaterial("Dark", 60),
-                            new LeveledMaterial("Light", 70),
-                            new LeveledMaterial("Pine", 80),
-                            new LeveledMaterial("Avo", 90),
-                            new LeveledMaterial("Sky", 100),
-                            new LeveledMaterial("Dernic", 110)),
-            MaterialType.CROP,
-                    List.of(
-                            new LeveledMaterial("Wheat", 1),
-                            new LeveledMaterial("Barley", 10),
-                            new LeveledMaterial("Oat", 20),
-                            new LeveledMaterial("Malt", 30),
-                            new LeveledMaterial("Hops", 40),
-                            new LeveledMaterial("Rye", 50),
-                            new LeveledMaterial("Millet", 60),
-                            new LeveledMaterial("Decay_Roots", 70),
-                            new LeveledMaterial("Rice", 80),
-                            new LeveledMaterial("Sorghum", 90),
-                            new LeveledMaterial("Hemp", 100),
-                            new LeveledMaterial("Dernic_Seed", 110)),
-            MaterialType.FISH,
-                    List.of(
-                            new LeveledMaterial("Gudgeon", 1),
-                            new LeveledMaterial("Trout", 10),
-                            new LeveledMaterial("Salmon", 20),
-                            new LeveledMaterial("Carp", 30),
-                            new LeveledMaterial("Icefish", 40),
-                            new LeveledMaterial("Piranha", 50),
-                            new LeveledMaterial("Koi", 60),
-                            new LeveledMaterial("Gylia_Fish", 70),
-                            new LeveledMaterial("Bass", 80),
-                            new LeveledMaterial("Molten_Eel", 90),
-                            new LeveledMaterial("Starfish", 100),
-                            new LeveledMaterial("Dernic_Fish", 110)));
-
 }
