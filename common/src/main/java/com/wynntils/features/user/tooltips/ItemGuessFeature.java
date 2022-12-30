ad8a4695b3b5f8b09c8e9ee0845d12d129330507
Magnus Ihse Bursie <mag@icus.se>
2022-12-30 17:28:48 +0100

    Item guess tooltips

--- common/src/main/java/com/wynntils/features/user/tooltips/ItemGuessFeature.java
+++ common/src/main/java/com/wynntils/features/user/tooltips/ItemGuessFeature.java
@@ -4,16 +4,98 @@
  */
 package com.wynntils.features.user.tooltips;
 
+import com.wynntils.core.components.Managers;
 import com.wynntils.core.config.Config;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.features.properties.FeatureCategory;
 import com.wynntils.core.features.properties.FeatureInfo;
 import com.wynntils.core.features.properties.FeatureInfo.Stability;
+import com.wynntils.features.user.ItemFavoriteFeature;
+import com.wynntils.mc.event.ItemTooltipRenderEvent;
+import com.wynntils.model.item.ItemModel;
+import com.wynntils.model.item.game.GearBoxItem;
+import com.wynntils.wynn.objects.EmeraldSymbols;
+import com.wynntils.wynn.objects.profiles.item.ItemProfile;
+import com.wynntils.wynn.objects.profiles.item.ItemTier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @FeatureInfo(stability = Stability.STABLE, category = FeatureCategory.TOOLTIPS)
 public class ItemGuessFeature extends UserFeature {
-    public static ItemGuessFeature INSTANCE;
-
     @Config
     public boolean showGuessesPrice = true;
+
+    @SubscribeEvent
+    public void onTooltipPre(ItemTooltipRenderEvent.Pre event) {
+        var wynnItemOpt = ItemModel.getWynnItem(event.getItemStack());
+        if (wynnItemOpt.isEmpty()) return;
+        if (!(wynnItemOpt.get() instanceof GearBoxItem gearBoxItem)) return;
+
+        List<Component> tooltips = new ArrayList<>(event.getTooltips());
+        tooltips.addAll(getTooltipAddon(gearBoxItem));
+        event.setTooltips(tooltips);
+    }
+
+    private List<Component> getTooltipAddon(GearBoxItem gearBoxItem) {
+        List<Component> addon = new ArrayList<>();
+        List<String> itemPossibilities = gearBoxItem.getItemPossibilities();
+        ItemTier itemTier = gearBoxItem.getItemTier();
+
+        addon.add(Component.translatable("feature.wynntils.itemGuess.possibilities"));
+
+        Map<Integer, List<MutableComponent>> levelToItems = new TreeMap<>();
+
+        for (String item : itemPossibilities) {
+            ItemProfile profile = Managers.ItemProfiles.getItemsProfile(item);
+
+            int level = (profile != null) ? profile.getLevelRequirement() : -1;
+
+            MutableComponent itemDesc = Component.literal(item).withStyle(itemTier.getChatFormatting());
+
+            if (ItemFavoriteFeature.INSTANCE.favoriteItems.contains(item)) {
+                itemDesc.withStyle(ChatFormatting.UNDERLINE);
+            }
+
+            levelToItems.computeIfAbsent(level, i -> new ArrayList<>()).add(itemDesc);
+        }
+
+        for (Map.Entry<Integer, List<MutableComponent>> entry : levelToItems.entrySet()) {
+            int level = entry.getKey();
+            List<MutableComponent> itemsForLevel = entry.getValue();
+
+            MutableComponent guesses = Component.literal("    ");
+
+            guesses.append(Component.translatable("feature.wynntils.itemGuess.levelLine", level == -1 ? "?" : level));
+
+            if (showGuessesPrice && level != -1) {
+                guesses.append(Component.literal(" [")
+                        .append(Component.literal(
+                                        (itemTier.getItemIdentificationCost(level) + " " + EmeraldSymbols.E_STRING))
+                                .withStyle(ChatFormatting.GREEN))
+                        .append(Component.literal("]"))
+                        .withStyle(ChatFormatting.GRAY));
+            }
+
+            guesses.append("§7: ");
+
+            Optional<MutableComponent> itemsComponent = itemsForLevel.stream()
+                    .reduce((i, j) -> i.append(Component.literal(", ").withStyle(ChatFormatting.GRAY))
+                            .append(j));
+
+            if (itemsComponent.isPresent()) {
+                guesses.append(itemsComponent.get());
+
+                addon.add(guesses);
+            }
+        }
+
+        return addon;
+    }
 }
