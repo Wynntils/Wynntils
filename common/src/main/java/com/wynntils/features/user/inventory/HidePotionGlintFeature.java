e98f4662bf2ae79801230a23b9999aa449d73b6c
magicus <magicus@users.noreply.github.com>
2022-12-30 23:42:22 +0000

    ci: spotless formatting

--- common/src/main/java/com/wynntils/features/user/inventory/HidePotionGlintFeature.java
+++ common/src/main/java/com/wynntils/features/user/inventory/HidePotionGlintFeature.java
@@ -4,13 +4,10 @@
  */
 package com.wynntils.features.user.inventory;
 
-import com.wynntils.core.components.Model;
-import com.wynntils.core.components.Models;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.features.properties.FeatureCategory;
 import com.wynntils.core.features.properties.FeatureInfo;
 import com.wynntils.mc.event.DrawPotionGlintEvent;
-import java.util.List;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @FeatureInfo(category = FeatureCategory.INVENTORY)
