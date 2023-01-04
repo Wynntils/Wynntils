4b82d7688548986fe59e10a965e85cd6bb6e43d5
Magnus Ihse Bursie <mag@icus.se>
2023-01-04 12:14:34 +0100

    Restore temporary workaround

--- common/src/main/java/com/wynntils/wynn/handleditems/annotators/game/GearAnnotator.java
+++ common/src/main/java/com/wynntils/wynn/handleditems/annotators/game/GearAnnotator.java
@@ -7,14 +7,23 @@ package com.wynntils.wynn.handleditems.annotators.game;
 import com.wynntils.core.components.Managers;
 import com.wynntils.handlers.item.ItemAnnotation;
 import com.wynntils.handlers.item.ItemAnnotator;
+import com.wynntils.mc.utils.ComponentUtils;
+import com.wynntils.wynn.item.GearItemStack;
 import com.wynntils.wynn.objects.profiles.item.ItemProfile;
+import com.wynntils.wynn.utils.WynnUtils;
 import net.minecraft.world.item.ItemStack;
 
 public final class GearAnnotator implements ItemAnnotator {
     @Override
     public ItemAnnotation getAnnotation(ItemStack itemStack, String name) {
         // Lookup Gear Profile
-        ItemProfile itemProfile = Managers.ItemProfiles.getItemsProfile(name);
+
+        // FIXME: Temporary workaround awaiting full merge
+        if (!(itemStack instanceof GearItemStack gearItemStack)) return null;
+        String strippedName = WynnUtils.normalizeBadString(ComponentUtils.stripFormatting(
+                gearItemStack.getOriginalHoverName().getString()));
+
+        ItemProfile itemProfile = Managers.ItemProfiles.getItemsProfile(strippedName);
         if (itemProfile == null) return null;
 
         // Verify that rarity matches
