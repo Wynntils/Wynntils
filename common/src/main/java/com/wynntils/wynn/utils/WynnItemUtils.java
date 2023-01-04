a155596c4b4a6da604f33c11846b032ec2806654
Magnus Ihse Bursie <mag@icus.se>
2023-01-04 21:17:51 +0100

    Remove dead code

--- common/src/main/java/com/wynntils/wynn/utils/WynnItemUtils.java
+++ common/src/main/java/com/wynntils/wynn/utils/WynnItemUtils.java
@@ -23,48 +23,6 @@ import net.minecraft.world.item.ItemStack;
 import org.lwjgl.glfw.GLFW;
 
 public final class WynnItemUtils {
-    /**
-     * Create a list of ItemIdentificationContainer corresponding to the given ItemProfile, formatted for item guide items
-     *
-     * @param item the profile of the item
-     * @return a list of appropriately formatted ItemIdentificationContainer
-     */
-    public static List<ItemIdentificationContainer> identificationsFromProfile(ItemProfile item) {
-        List<ItemIdentificationContainer> ids = new ArrayList<>();
-
-        for (Map.Entry<String, IdentificationProfile> entry : item.getStatuses().entrySet()) {
-            IdentificationProfile idProfile = entry.getValue();
-            IdentificationModifier type = idProfile.getType();
-            String idName = entry.getKey();
-            MutableComponent line;
-
-            boolean inverted = idProfile.isInverted();
-            if (idProfile.hasConstantValue()) {
-                int value = idProfile.getBaseValue();
-                line = Component.literal((value > 0 ? "+" : "") + value + type.getInGame(idName));
-                line.setStyle(
-                        Style.EMPTY.withColor(inverted ^ (value > 0) ? ChatFormatting.GREEN : ChatFormatting.RED));
-            } else {
-                int min = idProfile.getMin();
-                int max = idProfile.getMax();
-                ChatFormatting mainColor = inverted ^ (min > 0) ? ChatFormatting.GREEN : ChatFormatting.RED;
-                ChatFormatting textColor = inverted ^ (min > 0) ? ChatFormatting.DARK_GREEN : ChatFormatting.DARK_RED;
-                line = Component.literal((min > 0 ? "+" : "") + min).withStyle(mainColor);
-                line.append(Component.literal(" to ").withStyle(textColor));
-                line.append(Component.literal((max > 0 ? "+" : "") + max + type.getInGame(idName))
-                        .withStyle(mainColor));
-            }
-
-            line.append(Component.literal(" " + IdentificationProfile.getAsLongName(idName))
-                    .withStyle(ChatFormatting.GRAY));
-
-            ItemIdentificationContainer id =
-                    new ItemIdentificationContainer(item, idProfile, type, idName, 0, 0, -1, line, line, line, line);
-            ids.add(id);
-        }
-
-        return ids;
-    }
 
     public static void removeLoreTooltipLines(List<Component> tooltip) {
         List<Component> toRemove = new ArrayList<>();
