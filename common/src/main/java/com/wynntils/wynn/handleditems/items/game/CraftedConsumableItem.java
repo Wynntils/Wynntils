ce89d780aa8bf6b93c90a6b4c5a90c19d42af932
Alex <alextodaro@rocketmail.com>
2023-01-06 13:33:26 -0800

    fix crafted highlights & add pouch highlight

--- common/src/main/java/com/wynntils/wynn/handleditems/items/game/CraftedConsumableItem.java
+++ common/src/main/java/com/wynntils/wynn/handleditems/items/game/CraftedConsumableItem.java
@@ -5,9 +5,11 @@
 package com.wynntils.wynn.handleditems.items.game;
 
 import com.wynntils.utils.CappedValue;
+import com.wynntils.wynn.handleditems.properties.GearTierItemProperty;
 import com.wynntils.wynn.handleditems.properties.UsesItemPropery;
+import com.wynntils.wynn.objects.profiles.item.ItemTier;
 
-public class CraftedConsumableItem extends GameItem implements UsesItemPropery {
+public class CraftedConsumableItem extends GameItem implements UsesItemPropery, GearTierItemProperty {
     private final String name;
     private final CappedValue uses;
 
@@ -22,6 +24,10 @@ public class CraftedConsumableItem extends GameItem implements UsesItemPropery {
 
     public CappedValue getUses() {
         return uses;
+    }
+
+    public ItemTier getGearTier() {
+        return ItemTier.CRAFTED;
     }
 
     @Override
