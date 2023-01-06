d2cc8c6fc89321c43641d06de5d5a3f2a64af6d3
Magnus Ihse Bursie <mag@icus.se>
2023-01-06 02:53:55 +0100

    Hide irrelevant Vanilla stuff in tooltips, PoC

--- common/src/main/java/com/wynntils/mc/mixin/ItemStackMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/ItemStackMixin.java
@@ -10,10 +10,14 @@ import com.wynntils.mc.EventFactory;
 import com.wynntils.mc.event.ItemTooltipHoveredNameEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin implements AnnotatedItemStack {
@@ -31,6 +35,22 @@ public abstract class ItemStackMixin implements AnnotatedItemStack {
     private Component redirectGetHoveredName(ItemStack instance) {
         ItemTooltipHoveredNameEvent event = EventFactory.onGetHoverName(instance.getHoverName(), instance);
         return event.getHoveredName();
+    }
+
+    @Inject(method = "getHideFlags", at = @At("RETURN"), cancellable = true)
+    private int onGetHideFlags(CallbackInfoReturnable cir) {
+        cir.setReturnValue(127);
+        return 127;
+    }
+
+    @ModifyVariable(
+            method =
+                    "getTooltipLines(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/TooltipFlag;)Ljava/util/List;",
+            at = @At("HEAD"),
+            ordinal = 0,
+            argsOnly = true)
+    private TooltipFlag onGetTooltipLines(TooltipFlag flags) {
+        return TooltipFlag.NORMAL;
     }
 
     @Override
