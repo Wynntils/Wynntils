82dda970868077980ca354a7e132a7dec47510ce
Magnus Ihse Bursie <mag@icus.se>
2023-01-14 00:55:41 +0100

    add isVariable to rosetta stone

--- common/src/main/java/com/wynntils/wynn/model/gear/IdStatTypes.java
+++ common/src/main/java/com/wynntils/wynn/model/gear/IdStatTypes.java
@@ -5,119 +5,195 @@
 package com.wynntils.wynn.model.gear;
 
 public enum IdStatTypes implements IdType {
-    SKILL_AGILITY("Agility", null, "rawAgility", "AGILITYPOINTS", "agilityPoints"),
-    SKILL_DEFENCE("Defence", null, "rawDefence", "DEFENSEPOINTS", "defensePoints"),
-    SKILL_DEXTERITY("Dexterity", null, "rawDexterity", "DEXTERITYPOINTS", "dexterityPoints"),
-    SKILL_INTELLIGENCE("Intelligence", null, "rawIntelligence", "INTELLIGENCEPOINTS", "intelligencePoints"),
-    SKILL_STRENGTH("Strength", null, "rawStrength", "STRENGTHPOINTS", "strengthPoints"),
+    SKILL_AGILITY(IsVariable.NO, "Agility", null, "rawAgility", "AGILITYPOINTS", "agilityPoints"),
+    SKILL_DEFENCE(IsVariable.NO, "Defence", null, "rawDefence", "DEFENSEPOINTS", "defensePoints"),
+    SKILL_DEXTERITY(IsVariable.NO, "Dexterity", null, "rawDexterity", "DEXTERITYPOINTS", "dexterityPoints"),
+    SKILL_INTELLIGENCE(
+            IsVariable.NO, "Intelligence", null, "rawIntelligence", "INTELLIGENCEPOINTS", "intelligencePoints"),
+    SKILL_STRENGTH(IsVariable.NO, "Strength", null, "rawStrength", "STRENGTHPOINTS", "strengthPoints"),
 
     // FIXME: check discrepancy!
-    DEFENCE_AIR("Air Defence", "%", "airDefence", "AIRDEFENSE", "bonusAirDefense"),
-    DEFENCE_EARTH("Earth Defence", "%", "earthDefence", "EARTHDEFENSE", "bonusEarthDefense"),
-    DEFENCE_FIRE("Fire Defence", "%", "fireDefence", "FIREDEFENSE", "bonusFireDefense"),
-    DEFENCE_THUNDER("Thunder Defence", "%", "thunderDefence", "THUNDERDEFENSE", "bonusThunderDefense"),
-    DEFENCE_WATER("Water Defence", "%", "waterDefence", "WATERDEFENSE", "bonusWaterDefense"),
+    DEFENCE_AIR(IsVariable.YES, "Air Defence", "%", "airDefence", "AIRDEFENSE", "bonusAirDefense"),
+    DEFENCE_EARTH(IsVariable.YES, "Earth Defence", "%", "earthDefence", "EARTHDEFENSE", "bonusEarthDefense"),
+    DEFENCE_FIRE(IsVariable.YES, "Fire Defence", "%", "fireDefence", "FIREDEFENSE", "bonusFireDefense"),
+    DEFENCE_THUNDER(IsVariable.YES, "Thunder Defence", "%", "thunderDefence", "THUNDERDEFENSE", "bonusThunderDefense"),
+    DEFENCE_WATER(IsVariable.YES, "Water Defence", "%", "waterDefence", "WATERDEFENSE", "bonusWaterDefense"),
 
     // FIXME: check discrepancy!
-    DAMAGE_AIR("Air Damage", "%", "airDamage", "AIRDAMAGEBONUS", "bonusAirDamage"),
-    DAMAGE_EARTH("Earth Damage", "%", "earthDamage", "EARTHDAMAGEBONUS", "bonusEarthDamage"),
-    DAMAGE_FIRE("Fire Damage", "%", "fireDamage", "FIREDAMAGEBONUS", "bonusFireDamage"),
-    DAMAGE_THUNDER("Thunder Damage", "%", "thunderDamage", "THUNDERDAMAGEBONUS", "bonusThunderDamage"),
-    DAMAGE_WATER("Water Damage", "%", "waterDamage", "WATERDAMAGEBONUS", "bonusWaterDamage"),
+    DAMAGE_AIR(IsVariable.YES, "Air Damage", "%", "airDamage", "AIRDAMAGEBONUS", "bonusAirDamage"),
+    DAMAGE_EARTH(IsVariable.YES, "Earth Damage", "%", "earthDamage", "EARTHDAMAGEBONUS", "bonusEarthDamage"),
+    DAMAGE_FIRE(IsVariable.YES, "Fire Damage", "%", "fireDamage", "FIREDAMAGEBONUS", "bonusFireDamage"),
+    DAMAGE_THUNDER(IsVariable.YES, "Thunder Damage", "%", "thunderDamage", "THUNDERDAMAGEBONUS", "bonusThunderDamage"),
+    DAMAGE_WATER(IsVariable.YES, "Water Damage", "%", "waterDamage", "WATERDAMAGEBONUS", "bonusWaterDamage"),
 
     // FIXME: missing from Athena. Check lore name, e.g. Aleph null.
-    // This is variable. How do I know???
-    DAMAGE_ELEMANTAL("Elemental Damage", "%", "FIXME:MISSING", "FIXME:UNKNWON", "elementalDamageBonus"),
+    DAMAGE_ELEMANTAL(IsVariable.YES, "Elemental Damage", "%", "FIXME:MISSING", "FIXME:UNKNWON", "elementalDamageBonus"),
 
     // FIXME
-    DAMAGE_SPELL_ELEMENTAL_PERCENT("Elemental Spell Damage", "%", "elementalSpellDamage", "SPELLELEMENTALDAMAGEBONUS", "spellElementalDamageBonus"),
+    DAMAGE_SPELL_ELEMENTAL_PERCENT(
+            IsVariable.YES,
+            "Elemental Spell Damage",
+            "%",
+            "elementalSpellDamage",
+            "SPELLELEMENTALDAMAGEBONUS",
+            "spellElementalDamageBonus"),
     // FIXME, check with only available item: Forest Aconite, untradable
-    DAMAGE_SPELL_ELEMENTAL_RAW("Elemental Spell Damage", null, "rawElementalSpellDamage", "FIXME:UNKNOWN", "spellElementalDamageBonusRaw"),
+    DAMAGE_SPELL_ELEMENTAL_RAW(
+            IsVariable.YES,
+            "Elemental Spell Damage",
+            null,
+            "rawElementalSpellDamage",
+            "SPELLELEMENTALDAMAGEBONUSRAW",
+            "spellElementalDamageBonusRaw"),
 
     // FIXME: this is e.g. on Soul Ink. Afaict, Athena is missing this. Check lore name!
-    DAMAGE_SPELL_AIR_PERCENT("Air Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellAirDamageBonus"),
-    DAMAGE_SPELL_WATER_RAW("Water Spell Damage", null, "FIXME:MISSING", "FIXME:UNKNOWN", "spellWaterDamageBonusRaw"),
+    DAMAGE_SPELL_AIR_PERCENT(
+            IsVariable.YES, "Air Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellAirDamageBonus"),
 
     // Note: these are untested, but assumed...
-    DAMAGE_SPELL_EARTH_PERCENT("Earth Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellEarthDamageBonus"),
-    DAMAGE_SPELL_FIRE_PERCENT("Fire Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellFireDamageBonus"),
-    DAMAGE_SPELL_THUNDER_PERCENT("Thunder Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellThunderDamageBonus"),
-    DAMAGE_SPELL_WATER_PERCENT("Water Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellWaterDamageBonus"),
+    DAMAGE_SPELL_EARTH_PERCENT(
+            IsVariable.YES, "Earth Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellEarthDamageBonus"),
+    DAMAGE_SPELL_FIRE_PERCENT(
+            IsVariable.YES, "Fire Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellFireDamageBonus"),
+    DAMAGE_SPELL_THUNDER_PERCENT(
+            IsVariable.YES, "Thunder Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellThunderDamageBonus"),
+    DAMAGE_SPELL_WATER_PERCENT(
+            IsVariable.YES, "Water Spell Damage", "%", "FIXME:MISSING", "FIXME:UNKNOWN", "spellWaterDamageBonus"),
 
-    DAMAGE_SPELL_EARTH_RAW("Earth Spell Damage", null, "FIXME:MISSING", "FIXME:UNKNOWN", "spellEarthDamageBonusRaw"),
-    DAMAGE_SPELL_FIRE_RAW("Fire Spell Damage", null, "FIXME:MISSING", "FIXME:UNKNOWN", "spellFireDamageBonusRaw"),
-    DAMAGE_SPELL_THUNDER_RAW("Thunder Spell Damage", null, "FIXME:MISSING", "FIXME:UNKNOWN", "spellThunderDamageBonusRaw"),
-    DAMAGE_SPELL_AIR_RAW("Air Spell Damage", null, "FIXME:MISSING", "FIXME:UNKNOWN", "spellAirDamageBonusRaw"),
+    // FIXME: this is e.g. on Soul Ink.
+    DAMAGE_SPELL_WATER_RAW(
+            IsVariable.YES,
+            "Water Spell Damage",
+            null,
+            "rawWaterSpellDamage",
+            "SPELLWATERDAMAGEBONUSRAW",
+            "spellWaterDamageBonusRaw"),
+    DAMAGE_SPELL_EARTH_RAW(
+            IsVariable.YES,
+            "Earth Spell Damage",
+            null,
+            "rawEarthSpellDamage",
+            "SPELLEARTHDAMAGEBONUSRAW",
+            "spellEarthDamageBonusRaw"),
+    DAMAGE_SPELL_FIRE_RAW(
+            IsVariable.YES,
+            "Fire Spell Damage",
+            null,
+            "rawFireSpellDamage",
+            "SPELLFIREDAMAGEBONUSRAW",
+            "spellFireDamageBonusRaw"),
+    DAMAGE_SPELL_THUNDER_RAW(
+            IsVariable.YES,
+            "Thunder Spell Damage",
+            null,
+            "rawThunderSpellDamage",
+            "SPELLTHUNDERDAMAGEBONUSRAW",
+            "spellThunderDamageBonusRaw"),
+    DAMAGE_SPELL_AIR_RAW(
+            IsVariable.YES,
+            "Air Spell Damage",
+            null,
+            "rawAirSpellDamage",
+            "SPELLAIRDAMAGEBONUSRAW",
+            "spellAirDamageBonusRaw"),
 
     // FIXME: should perhaps be named _RAW? Do we have percent? And is the LORE name from old Athena really correct???
     // Check lore with e.g. Spearmint
-    DAMAGE_MAIN_AIR("Air Main Attack Damage", null, "rawAirSpellDamage", "SPELLAIRDAMAGEBONUSRAW", "mainAttackAirDamageBonusRaw"),
-    DAMAGE_MAIN_EARTH("Earth Main Attack Damage", null, "rawEarthSpellDamage", "SPELLEARTHDAMAGEBONUSRAW", "mainAttackEarthDamageBonusRaw"),
-    DAMAGE_MAIN_FIRE("Fire Main Attack Damage", null, "rawFireSpellDamage", "SPELLFIREDAMAGEBONUSRAW", "mainAttackFireDamageBonusRaw"),
-    DAMAGE_MAIN_THUNDER("Thunder Main Attack Damage", null, "rawThunderSpellDamage", "SPELLTHUNDERDAMAGEBONUSRAW", "mainAttackThunderDamageBonusRaw"),
-    DAMAGE_MAIN_WATER("Water Main Attack Damage", null, "rawWaterSpellDamage", "SPELLWATERDAMAGEBONUSRAW", "mainAttackWaterDamageBonusRaw"),
+    DAMAGE_MAIN_AIR_RAW(
+            IsVariable.YES, "Air Main Attack Damage", null, "FIXME", "FIXME", "mainAttackAirDamageBonusRaw"),
+    DAMAGE_MAIN_EARTH_RAW(
+            IsVariable.YES, "Earth Main Attack Damage", null, "FIXME", "FIXME", "mainAttackEarthDamageBonusRaw"),
+    DAMAGE_MAIN_FIRE_RAW(
+            IsVariable.YES, "Fire Main Attack Damage", null, "FIXME", "FIXME", "mainAttackFireDamageBonusRaw"),
+    DAMAGE_MAIN_THUNDER_RAW(
+            IsVariable.YES, "Thunder Main Attack Damage", null, "FIXME", "FIXME", "mainAttackThunderDamageBonusRaw"),
+    DAMAGE_MAIN_WATER_RAW(
+            IsVariable.YES, "Water Main Attack Damage", null, "FIXME", "FIXME", "mainAttackWaterDamageBonusRaw"),
+
+    DAMAGE_MAIN_AIR_PERCENT(
+            IsVariable.YES, "Air Main Attack Damage", "%", "airDamage", "FIXME:UNKNOWN", "mainAttackAirDamageBonus"),
+    DAMAGE_MAIN_EARTH_PERCENT(
+            IsVariable.YES,
+            "Earth Main Attack Damage",
+            "%",
+            "earthDamage",
+            "FIXME:UNKNOWN",
+            "mainAttackEarthDamageBonus"),
+    DAMAGE_MAIN_FIRE_PERCENT(
+            IsVariable.YES, "Fire Main Attack Damage", "%", "fireDamage", "FIXME:UNKNOWN", "mainAttackFireDamageBonus"),
+    DAMAGE_MAIN_THUNDER_PERCENT(
+            IsVariable.YES,
+            "Thunder Main Attack Damage",
+            "%",
+            "thunderDamage",
+            "FIXME:UNKNOWN",
+            "mainAttackThunderDamageBonus"),
+    DAMAGE_MAIN_WATER_PERCENT(
+            IsVariable.YES,
+            "Water Main Attack Damage",
+            "%",
+            "waterDamage",
+            "FIXME:UNKNOWN",
+            "mainAttackWaterDamageBonus"),
 
     // FIXME
-    DAMAGE_SPELL_NEUTRAL("Neutral Spell Damage", null, "rawNeutralSpellDamage", "SPELLNEUTRALDAMAGEBONUSRAW", "FIXME"),
-    DAMAGE_MAIN_ATTACK_PERCENT("Main Attack Damage", "%", "mainAttackDamage", "DAMAGEBONUS", "damageBonus"),
-    DAMAGE_MAIN_ATTACK_RAW("Main Attack Damage", null, "rawMainAttackDamage", "DAMAGEBONUSRAW", "damageBonusRaw"),
-    DAMAGE_SPELL_PERCENT("Spell Damage", "%", "spellDamage", "SPELLDAMAGE", "spellDamage"),
-    DAMAGE_SPELL_RAW("Spell Damage", null, "rawSpellDamage", "SPELLDAMAGERAW", "spellDamageRaw"),
+    DAMAGE_SPELL_NEUTRAL(
+            IsVariable.YES,
+            "Neutral Spell Damage",
+            null,
+            "rawNeutralSpellDamage",
+            "SPELLNEUTRALDAMAGEBONUSRAW",
+            "FIXME"),
+    DAMAGE_MAIN_ATTACK_PERCENT(
+            IsVariable.YES, "Main Attack Damage", "%", "mainAttackDamage", "DAMAGEBONUS", "damageBonus"),
+    DAMAGE_MAIN_ATTACK_RAW(
+            IsVariable.YES, "Main Attack Damage", null, "rawMainAttackDamage", "DAMAGEBONUSRAW", "damageBonusRaw"),
+    DAMAGE_SPELL_PERCENT(IsVariable.YES, "Spell Damage", "%", "spellDamage", "SPELLDAMAGE", "spellDamage"),
+    DAMAGE_SPELL_RAW(IsVariable.YES, "Spell Damage", null, "rawSpellDamage", "SPELLDAMAGERAW", "spellDamageRaw"),
 
-    HEALTH("Health", null, "rawHealth", "HEALTHBONUS", "healthBonus"),
-    HEALTH_REGEN_PERCENT("Health Regen", "%", "healthRegen", "HEALTHREGEN", "healthRegen"),
-    HEALTH_REGEN_RAW("Health Regen", null, "rawHealthRegen", "HEALTHREGENRAW", "healthRegenRaw"),
-    LIFE_STEAL("Life Steal", "/3s", "lifeSteal", "LIFESTEAL", "lifeSteal"),
+    HEALTH(IsVariable.YES, "Health", null, "rawHealth", "HEALTHBONUS", "healthBonus"),
+    HEALTH_REGEN_PERCENT(IsVariable.YES, "Health Regen", "%", "healthRegen", "HEALTHREGEN", "healthRegen"),
+    HEALTH_REGEN_RAW(IsVariable.YES, "Health Regen", null, "rawHealthRegen", "HEALTHREGENRAW", "healthRegenRaw"),
+    LIFE_STEAL(IsVariable.YES, "Life Steal", "/3s", "lifeSteal", "LIFESTEAL", "lifeSteal"),
 
-    MANA_REGEN("Mana Regen", "/5s", "manaRegen", "MANAREGEN", "manaRegen"),
-    MANA_STEAL("Mana Steal", "/3s", "manaSteal", "MANASTEAL", "manaSteal"),
+    MANA_REGEN(IsVariable.YES, "Mana Regen", "/5s", "manaRegen", "MANAREGEN", "manaRegen"),
+    MANA_STEAL(IsVariable.YES, "Mana Steal", "/3s", "manaSteal", "MANASTEAL", "manaSteal"),
 
-    WALK_SPEED("Walk Speed", "%", "walkSpeed", "SPEED", "speed"),
+    WALK_SPEED(IsVariable.YES, "Walk Speed", "%", "walkSpeed", "SPEED", "speed"),
     // FIXME
-    SPRINT("Sprint", "%", "sprint", "STAMINA", "FIXME"),
+    SPRINT(IsVariable.YES, "Sprint", "%", "sprint", "STAMINA", "FIXME"),
     // FIXME
-    SPRINT_REGEN("Sprint Regen", "%", "sprintRegen", "STAMINA_REGEN", "FIXME"),
+    SPRINT_REGEN(IsVariable.YES, "Sprint Regen", "%", "sprintRegen", "STAMINA_REGEN", "FIXME"),
 
-    THORNS("Thorns", "%", "thorns", "THORNS", "thorns"),
-    EXPLODING("Exploding", "%", "exploding", "EXPLODING", "exploding"),
-    POISON("Poison", "/3s", "poison", "POISON", "poison"),
-    REFLECTION("Reflection", "%", "reflection", "REFLECTION", "reflection"),
+    THORNS(IsVariable.YES, "Thorns", "%", "thorns", "THORNS", "thorns"),
+    EXPLODING(IsVariable.YES, "Exploding", "%", "exploding", "EXPLODING", "exploding"),
+    POISON(IsVariable.YES, "Poison", "/3s", "poison", "POISON", "poison"),
+    REFLECTION(IsVariable.YES, "Reflection", "%", "reflection", "REFLECTION", "reflection"),
 
-    STEALING("Stealing", "%", "stealing", "EMERALDSTEALING", "emeraldStealing"),
+    STEALING(IsVariable.YES, "Stealing", "%", "stealing", "EMERALDSTEALING", "emeraldStealing"),
     // FIXME: Check discrepancy between LORE and api
-    ATTACK_SPEED("Attack Speed", " tier", "attackSpeed", "ATTACKSPEED", "attackSpeedBonus"),
+    ATTACK_SPEED(IsVariable.YES, "Attack Speed", " tier", "attackSpeed", "ATTACKSPEED", "attackSpeedBonus"),
     // FIXME
-    JUMP_HEIGHT("Jump Height", null, "rawJumpHeight", "JUMP_HEIGHT", "FIXME"),
-    LOOT_BONUS("Loot Bonus", "%", "lootBonus", "LOOTBONUS", "lootBonus"),
-    SOUL_POINT_REGEN("Soul Point Regen", "%", "soulPointRegen", "SOULPOINTS", "soulPoints"),
-    XP_BONUS("XP Bonus", "%", "xpBonus", "XPBONUS", "xpBonus");
+    JUMP_HEIGHT(IsVariable.YES, "Jump Height", null, "rawJumpHeight", "JUMP_HEIGHT", "FIXME"),
+    LOOT_BONUS(IsVariable.YES, "Loot Bonus", "%", "lootBonus", "LOOTBONUS", "lootBonus"),
+    SOUL_POINT_REGEN(IsVariable.YES, "Soul Point Regen", "%", "soulPointRegen", "SOULPOINTS", "soulPoints"),
+    XP_BONUS(IsVariable.YES, "XP Bonus", "%", "xpBonus", "XPBONUS", "xpBonus");
 
-    /*
-    "SPELLWATERDAMAGEBONUSRAW" -> "rawWaterSpellDamage"
-    "SPELLFIREDAMAGEBONUSRAW" -> "rawFireSpellDamage"
-    "SPELLTHUNDERDAMAGEBONUSRAW" -> "rawThunderSpellDamage"
-    "SPELLAIRDAMAGEBONUSRAW" -> "rawAirSpellDamage"
-    "SPELLEARTHDAMAGEBONUSRAW" -> "rawEarthSpellDamage"
-
-    "SPELLELEMENTALDAMAGEBONUSRAW" -> "rawElementalSpellDamage"
-
-    "SPELL_COST_RAW_1" -> "raw1stSpellCost"
-    "SPELL_COST_RAW_2" -> "raw2ndSpellCost"
-    "SPELL_COST_RAW_3" -> "raw3rdSpellCost"
-    "SPELL_COST_RAW_4" -> "raw4thSpellCost"
-    "SPELL_COST_PCT_1" -> "1stSpellCost"
-    "SPELL_COST_PCT_2" -> "2ndSpellCost"
-    "SPELL_COST_PCT_3" -> "3rdSpellCost"
-    "SPELL_COST_PCT_4" -> "4thSpellCost"
-     */
-
+    private final IsVariable isVariable;
     private final String displayName;
     private final String unit;
     private final String athenaName;
     private final String loreName;
     private final String apiName;
 
-    IdStatTypes(String displayName, String unit, String athenaName, String loreName, String apiName) {
+    IdStatTypes(
+            IsVariable isVariable,
+            String displayName,
+            String unit,
+            String athenaName,
+            String loreName,
+            String apiName) {
+        this.isVariable = isVariable;
         this.displayName = displayName;
         this.unit = unit;
         this.athenaName = athenaName;
@@ -128,6 +204,10 @@ public enum IdStatTypes implements IdType {
     @Override
     public String getKey() {
         return this.name();
+    }
+
+    public IsVariable getIsVariable() {
+        return isVariable;
     }
 
     @Override
@@ -154,5 +234,4 @@ public enum IdStatTypes implements IdType {
     public String getApiName() {
         return apiName;
     }
-
 }
