ddd9d064a061f485b798e80f2dcfc77d7484f898
Magnus Ihse Bursie <mag@icus.se>
2023-01-16 11:48:48 +0100

    fix: Two micro-opts, and prohibit infinite recursion on equals

--- common/src/main/java/com/wynntils/handlers/item/ItemHandler.java
+++ common/src/main/java/com/wynntils/handlers/item/ItemHandler.java
@@ -29,6 +29,8 @@ public class ItemHandler extends Handler {
     private final List<ItemAnnotator> annotators = new ArrayList<>();
     private Map<Class<?>, Integer> profilingTimes = new HashMap<>();
     private Map<Class<?>, Integer> profilingCounts = new HashMap<>();
+    // Keep this as a field just of performance reasons to skip a new allocation in annotate()
+    private List<ItemAnnotator> crashedAnnotators = new ArrayList<>();
 
     public static Optional<ItemAnnotation> getItemStackAnnotation(ItemStack item) {
         if (item == null) return Optional.empty();
@@ -91,7 +93,6 @@ public class ItemHandler extends Handler {
         ItemAnnotation annotation = null;
 
         long startTime = System.currentTimeMillis();
-        List<ItemAnnotator> crashedAnnotators = new LinkedList<>();
         String name = WynnUtils.normalizeBadString(ComponentUtils.getCoded(item.getHoverName()));
 
         for (ItemAnnotator annotator : annotators) {
@@ -119,6 +120,7 @@ public class ItemHandler extends Handler {
         for (ItemAnnotator annotator : crashedAnnotators) {
             annotators.remove(annotator);
         }
+        crashedAnnotators.clear();
 
         if (annotation == null) return;
 
