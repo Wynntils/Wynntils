d3ac076aa6155c219e17cb76b87219f7990f3a27
DonkeyBlaster <DonkeyBlaster@users.noreply.github.com>
2023-01-19 20:41:41 +0000

    ci: spotless formatting

--- common/src/main/java/com/wynntils/wynn/event/SpellProgressEvent.java
+++ common/src/main/java/com/wynntils/wynn/event/SpellProgressEvent.java
@@ -1,5 +1,7 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
 package com.wynntils.wynn.event;
 
-public class SpellProgressEvent extends SpellEvent {
-    
-}
+public class SpellProgressEvent extends SpellEvent {}
