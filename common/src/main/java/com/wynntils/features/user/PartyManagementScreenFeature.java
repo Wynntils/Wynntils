dcac2bad6f92886d4ae4312c45c4a7da3bce1ac3
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-01-20 00:51:58 -0800

    add dependencies

--- common/src/main/java/com/wynntils/features/user/PartyManagementScreenFeature.java
+++ common/src/main/java/com/wynntils/features/user/PartyManagementScreenFeature.java
@@ -1,19 +1,24 @@
 package com.wynntils.features.user;
 
-import com.wynntils.core.config.ConfigHolder;
+import com.wynntils.core.components.Model;
+import com.wynntils.core.components.Models;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.features.properties.FeatureInfo;
 import com.wynntils.core.features.properties.RegisterKeyBind;
 import com.wynntils.core.keybinds.KeyBind;
-import com.wynntils.gui.screens.ChatTabEditingScreen;
 import com.wynntils.gui.screens.PartyManagementScreen;
 import com.wynntils.mc.utils.McUtils;
 import org.lwjgl.glfw.GLFW;
+
+import java.util.List;
 
 @FeatureInfo(stability = FeatureInfo.Stability.STABLE)
 public class PartyManagementScreenFeature extends UserFeature {
     @RegisterKeyBind
     private final KeyBind openPartyManagementScreen = new KeyBind("Open Party Management Screen", GLFW.GLFW_KEY_RIGHT_BRACKET, true, () -> McUtils.mc().setScreen(PartyManagementScreen.create()));
 
-
+    @Override
+    public List<Model> getModelDependencies() {
+        return List.of(Models.PlayerRelations);
+    }
 }
