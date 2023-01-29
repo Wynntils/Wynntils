0b6c3d880b597b692110cbe3d85791e51c78e184
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2023-01-29 19:58:38 +0100

    fix: Fix bug

--- common/src/main/java/com/wynntils/features/user/ChangelogFeature.java
+++ common/src/main/java/com/wynntils/features/user/ChangelogFeature.java
@@ -17,30 +17,32 @@ import java.util.Map;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ChangelogFeature extends UserFeature {
+    // "v0.0.2-alpha.2" is the first version with a changelog on GitHub
     @Config(visible = false)
-    public String lastShownVersion = "v0.0.2-alpha.1";
+    public String lastShownVersion = "v0.0.2-alpha.2";
 
     @SubscribeEvent
     public void onWorldStateChange(WorldStateEvent event) {
-        if (event.isFirstJoinWorld()) {
-            ApiResponse response = Managers.Net.callApi(
-                    UrlId.API_ATHENA_UPDATE_CHANGELOG,
-                    Map.of("old_version", lastShownVersion, "new_version", WynntilsMod.getVersion()));
+        if (!event.isFirstJoinWorld()) return;
+        if (WynntilsMod.getVersion().equals(lastShownVersion)) return;
 
-            response.handleJsonObject(
-                    jsonObject -> {
-                        if (!jsonObject.has("changelog")) return;
+        ApiResponse response = Managers.Net.callApi(
+                UrlId.API_ATHENA_UPDATE_CHANGELOG,
+                Map.of("old_version", lastShownVersion, "new_version", WynntilsMod.getVersion()));
 
-                        String changelog = jsonObject.get("changelog").getAsString();
+        response.handleJsonObject(
+                jsonObject -> {
+                    if (!jsonObject.has("changelog")) return;
 
-                        lastShownVersion = WynntilsMod.getVersion();
+                    String changelog = jsonObject.get("changelog").getAsString();
 
-                        Managers.TickScheduler.scheduleNextTick(
-                                () -> McUtils.mc().setScreen(ChangelogScreen.create(changelog)));
-                    },
-                    throwable -> {
-                        WynntilsMod.warn("Could not get update changelog: ", throwable);
-                    });
-        }
+                    lastShownVersion = WynntilsMod.getVersion();
+
+                    Managers.TickScheduler.scheduleNextTick(
+                            () -> McUtils.mc().setScreen(ChangelogScreen.create(changelog)));
+                },
+                throwable -> {
+                    WynntilsMod.warn("Could not get update changelog: ", throwable);
+                });
     }
 }
