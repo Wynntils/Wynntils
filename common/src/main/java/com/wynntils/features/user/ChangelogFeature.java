522221a49ffd803652300613dace720bbc2a9081
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2023-01-29 17:53:43 +0100

    feat: Use new API

--- common/src/main/java/com/wynntils/features/user/ChangelogFeature.java
+++ common/src/main/java/com/wynntils/features/user/ChangelogFeature.java
@@ -6,6 +6,7 @@ package com.wynntils.features.user;
 
 import com.wynntils.core.WynntilsMod;
 import com.wynntils.core.components.Managers;
+import com.wynntils.core.config.Config;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.net.ApiResponse;
 import com.wynntils.core.net.UrlId;
@@ -16,15 +17,23 @@ import java.util.Map;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ChangelogFeature extends UserFeature {
+    @Config(visible = false)
+    public String lastShownVersion = "v0.0.2-alpha.1";
+
     @SubscribeEvent
     public void onWorldStateChange(WorldStateEvent event) {
         if (event.isFirstJoinWorld()) {
             ApiResponse response = Managers.Net.callApi(
-                    UrlId.API_ATHENA_UPDATE_CHANGELOG, Map.of("version", WynntilsMod.getVersion()));
+                    UrlId.API_ATHENA_UPDATE_CHANGELOG,
+                    Map.of("old_version", lastShownVersion, "new_version", WynntilsMod.getVersion()));
 
             response.handleJsonObject(
                     jsonObject -> {
+                        if (!jsonObject.has("changelog")) return;
+
                         String changelog = jsonObject.get("changelog").getAsString();
+
+                        lastShownVersion = WynntilsMod.getVersion();
 
                         Managers.TickScheduler.scheduleNextTick(
                                 () -> McUtils.mc().setScreen(ChangelogScreen.create(changelog)));
