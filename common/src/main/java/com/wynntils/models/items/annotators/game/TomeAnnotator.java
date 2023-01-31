2e21eac56a90fe2aed2a35601948e698c8a4c051
Magnus Ihse Bursie <mag@icus.se>
2023-01-31 18:58:07 +0100

    PARSING DONE!!!!

--- common/src/main/java/com/wynntils/models/items/annotators/game/TomeAnnotator.java
+++ common/src/main/java/com/wynntils/models/items/annotators/game/TomeAnnotator.java
@@ -7,34 +7,17 @@ package com.wynntils.models.items.annotators.game;
 import com.wynntils.core.components.Models;
 import com.wynntils.handlers.item.ItemAnnotation;
 import com.wynntils.handlers.item.ItemAnnotator;
-import com.wynntils.models.gearinfo.type.GearTier;
-import com.wynntils.models.rewards.type.TomeInfo;
-import com.wynntils.models.rewards.type.TomeType;
-import java.util.Optional;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
 public final class TomeAnnotator implements ItemAnnotator {
-    private static final Pattern TOME_PATTERN = Pattern.compile(
-            "^§[5abcdef]((?<Variant>[\\w']+)? ?Tome of (?<Type>\\w+)" + "(?:| Mastery (?<Tier>[IVX]{1,4})))$");
-
     @Override
     public ItemAnnotation getAnnotation(ItemStack itemStack, String name) {
         if (!(itemStack.getItem() == Items.ENCHANTED_BOOK)) return null;
-        Matcher matcher = TOME_PATTERN.matcher(name);
+        Matcher matcher = Models.Rewards.TOME_PATTERN.matcher(name);
         if (!matcher.matches()) return null;
 
-        Optional<TomeType> tomeTypeOpt = TomeType.fromString(matcher.group("Type"));
-        if (tomeTypeOpt.isEmpty()) return null;
-
-        TomeType tomeType = tomeTypeOpt.get();
-        GearTier gearTier = GearTier.fromFormattedString(name);
-        String variant = tomeType.hasVariants() ? matcher.group("Variant") : null;
-        String tier = tomeType.isTiered() ? matcher.group("Tier") : null;
-
-        TomeInfo tomeInfo = Models.Rewards.getTomeInfo(matcher, tomeType, gearTier, variant, tier);
-        return Models.Rewards.fromTomeItemStack(itemStack, tomeInfo);
+        return Models.Rewards.fromTomeItemStack(itemStack, name, matcher);
     }
 }
--- common/src/main/java/com/wynntils/models/rewards/RewardsModel.java
+++ common/src/main/java/com/wynntils/models/rewards/RewardsModel.java
@@ -16,21 +16,18 @@ import com.wynntils.models.rewards.type.CharmInfo;
 import com.wynntils.models.rewards.type.TomeInfo;
 import com.wynntils.models.rewards.type.TomeType;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import net.minecraft.world.item.ItemStack;
 
 public class RewardsModel extends Model {
     public static final Pattern CHARM_PATTERN = Pattern.compile("^§[5abcdef](Charm of the (?<Type>\\w+))$");
+    public static final Pattern TOME_PATTERN = Pattern.compile(
+            "^§[5abcdef]((?<Variant>[\\w']+)? ?Tome of (?<Type>\\w+)" + "(?:| Mastery (?<Tier>[IVX]{1,4})))$");
 
     public RewardsModel() {
         super(List.of());
-    }
-
-    public TomeInfo getTomeInfo(Matcher matcher, TomeType tomeType, GearTier gearTier, String variant, String tier) {
-        // TODO: replace with API lookup
-        TomeInfo tomeInfo = new TomeInfo(matcher.group(1), gearTier, variant, tomeType, tier);
-        return tomeInfo;
     }
 
     public ItemAnnotation fromCharmItemStack(ItemStack itemStack, String name, Matcher matcher) {
@@ -49,7 +46,18 @@ public class RewardsModel extends Model {
         return new CharmItem(charmInfo, result.identifications(), result.tierCount());
     }
 
-    public TomeItem fromTomeItemStack(ItemStack itemStack, TomeInfo tomeInfo) {
+    public static TomeItem fromTomeItemStack(ItemStack itemStack, String name, Matcher matcher) {
+        Optional<TomeType> tomeTypeOpt = TomeType.fromString(matcher.group("Type"));
+        if (tomeTypeOpt.isEmpty()) return null;
+
+        TomeType tomeType = tomeTypeOpt.get();
+        GearTier gearTier = GearTier.fromFormattedString(name);
+        String variant = tomeType.hasVariants() ? matcher.group("Variant") : null;
+        String tier = tomeType.isTiered() ? matcher.group("Tier") : null;
+
+        // TODO: replace with API lookup
+        TomeInfo tomeInfo = new TomeInfo(matcher.group(1), gearTier, variant, tomeType, tier);
+
         GearParseResult result = GearParser.parseItemStack(itemStack);
         if (result.tier() != tomeInfo.gearTier()) {
             WynntilsMod.warn("Tier for " + tomeInfo.displayName() + " is reported as " + result.tier());
