316fd56224637d9af77cfc405af85890c91f65cf
Alex Todaro <3767283+P0keDev@users.noreply.github.com>
2023-02-09 03:37:12 -0800

    feat: Rework gear viewer (#1141)

---
+++ common/src/main/java/com/wynntils/screens/base/WynntilsContainerScreen.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.screens.base;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.wynntils.core.WynntilsMod;
+import com.wynntils.utils.mc.McUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+
+public abstract class WynntilsContainerScreen<T extends AbstractContainerMenu> extends AbstractContainerScreen<T> {
+    protected WynntilsContainerScreen(T abstractContainerMenu, Inventory inventory, Component component) {
+        super(abstractContainerMenu, inventory, component);
+    }
+
+    private void failure(String method, Throwable e) {
+        WynntilsMod.error("Failure in " + this.getClass().getSimpleName() + "." + method + "()", e);
+        McUtils.sendMessageToClient(Component.literal("Wynntils: Failure in "
+                        + this.getClass().getSimpleName() + " during " + method + ". Screen forcefully closed.")
+                .withStyle(ChatFormatting.RED));
+        McUtils.mc().setScreen(null);
+    }
+
+    @Override
+    protected final void init() {
+        try {
+            doInit();
+        } catch (Throwable t) {
+            failure("init", t);
+        }
+    }
+
+    protected void doInit() {
+        super.init();
+    }
+
+    @Override
+    public final void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+        try {
+            doRender(poseStack, mouseX, mouseY, partialTick);
+        } catch (Throwable t) {
+            failure("render", t);
+        }
+    }
+
+    public void doRender(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+        super.render(poseStack, mouseX, mouseY, partialTick);
+    }
+}
