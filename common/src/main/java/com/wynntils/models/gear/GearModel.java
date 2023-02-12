0e34f2e3b191afe320071fb28e0820d6ee6b8b4c
Magnus Ihse Bursie <mag@icus.se>
2023-02-12 15:54:13 +0100

    feat: Add "how to obtain item" info

--- common/src/main/java/com/wynntils/models/gear/GearModel.java
+++ common/src/main/java/com/wynntils/models/gear/GearModel.java
@@ -10,7 +10,6 @@ import com.wynntils.core.components.Model;
 import com.wynntils.models.elements.ElementModel;
 import com.wynntils.models.gear.parsing.GearParseResult;
 import com.wynntils.models.gear.parsing.GearParser;
-import com.wynntils.models.gear.type.GearDropType;
 import com.wynntils.models.gear.type.GearInfo;
 import com.wynntils.models.gear.type.GearInstance;
 import com.wynntils.models.items.items.game.CraftedGearItem;
@@ -54,13 +53,10 @@ public final class GearModel extends Model {
         List<GearInfo> possibilities = possibilitiesCache.get(gearBoxItem);
         if (possibilities != null) return possibilities;
 
-        // FIXME: This is not entirely correct. Some drop type "NEVER" items can still be
-        // in boxes, e.g. as quest rewards. We need "htoi" (how to obtain item) data to solve
-        // that properly.
         List<GearInfo> possibleGear = getAllGearInfos()
                 .filter(gear -> gear.type() == gearBoxItem.getGearType()
                         && gear.tier() == gearBoxItem.getGearTier()
-                        && gear.metaInfo().dropType() != GearDropType.NEVER
+                        && canBeGearBox(gear)
                         && gearBoxItem
                                 .getLevelRange()
                                 .inRange(gear.requirements().level()))
@@ -68,6 +64,12 @@ public final class GearModel extends Model {
         possibilitiesCache.put(gearBoxItem, possibleGear);
 
         return possibleGear;
+    }
+
+    public boolean canBeGearBox(GearInfo gear) {
+        // If all the ways we can obtain this is by merchants, it cannot be in a gear box
+        return gear.metaInfo().obtainInfo().stream()
+                .anyMatch(o -> !o.sourceType().isMerchant());
     }
 
     public void reloadData() {
