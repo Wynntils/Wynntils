f4a44ddb39a0a8db248174d95d68ea46022802c8
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2023-02-17 14:58:37 +0100

    fix: Fix guild map time held order

--- common/src/main/java/com/wynntils/utils/SimpleDateFormatter.java
+++ common/src/main/java/com/wynntils/utils/SimpleDateFormatter.java
@@ -9,17 +9,19 @@ import java.text.FieldPosition;
 import java.text.ParsePosition;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TimeZone;
 
 public class SimpleDateFormatter extends DateFormat {
-    private static final Map<Integer, String> MAPPINGS = Map.of(
-            Calendar.DAY_OF_YEAR, "d",
-            Calendar.HOUR_OF_DAY, "h",
-            Calendar.MINUTE, "m",
-            Calendar.SECOND, "s");
+    private static final Map<Integer, String> MAPPINGS = new LinkedHashMap<>();
 
     public SimpleDateFormatter() {
+        MAPPINGS.put(Calendar.DAY_OF_YEAR, "d");
+        MAPPINGS.put(Calendar.HOUR_OF_DAY, "h");
+        MAPPINGS.put(Calendar.MINUTE, "m");
+        MAPPINGS.put(Calendar.SECOND, "s");
+
         setCalendar(Calendar.getInstance());
         setTimeZone(TimeZone.getTimeZone("UTC"));
     }
