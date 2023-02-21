86a74d726d00f99f766e39bda017a9456203b846
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-02-20 19:11:09 -0800

    last few tweaks for alignment

--- common/src/main/java/com/wynntils/screens/partymanagement/PartyManagementScreen.java
+++ common/src/main/java/com/wynntils/screens/partymanagement/PartyManagementScreen.java
@@ -90,8 +90,8 @@ public final class PartyManagementScreen extends Screen implements TextboxScreen
         // region Invite input and button
         inviteInput = new TextInputBoxWidget(
                 (int) (dividedWidth * 36),
-                (int) (dividedHeight * 8) + 1,
-                (int) ((dividedWidth * 57) - (dividedWidth * 36)),
+                (int) (dividedHeight * PARTY_LIST_DIV_HEIGHT) + 1,
+                (int) ((dividedWidth * 57) - (dividedWidth * 36)) - 1,
                 20, // height is static 20 to match the button below
                 null, this, inviteInput);
         this.addRenderableWidget(inviteInput);
@@ -99,8 +99,8 @@ public final class PartyManagementScreen extends Screen implements TextboxScreen
         inviteButton = new Button.Builder(
                 Component.translatable("screens.wynntils.partyManagementGui.invite"),
                 (button) -> inviteFromField())
-                .pos((int) (dividedWidth * 57), (int) (dividedHeight * 8) + 1)
-                .size((int) (dividedWidth * 3), 20) // height must be static 20 or the texture will break
+                .pos((int) (dividedWidth * 57), (int) (dividedHeight * PARTY_LIST_DIV_HEIGHT) + 1)
+                .size((int) (dividedWidth * 3) - 2, 20) // height must be static 20 or the texture will break
                 .build();
         this.addRenderableWidget(inviteButton);
         // endregion
@@ -147,15 +147,16 @@ public final class PartyManagementScreen extends Screen implements TextboxScreen
 
         // region debug dividers
         // to make positioning easier, we will split the screen into GRID_DIVISIONS parts horizontally and vertically
-        for (int i = 1; i <= GRID_DIVISIONS - 1; i++) {
-            double x = dividedWidth * i;
-            double y = dividedHeight * i;
-            RenderUtils.drawRect(poseStack, CommonColors.GRAY, (float) x, 0, 0, 1, this.height);
-            RenderUtils.drawRect(poseStack, CommonColors.GRAY, 0, (float) y, 0, this.width, 1);
-            if (i % 2 == 0) continue; // reduce clutter
-            FontRenderer.getInstance().renderText(poseStack, String.valueOf(i), (float) x, this.height / 2, CommonColors.RED, HorizontalAlignment.Center, VerticalAlignment.Middle, TextShadow.NORMAL);
-            FontRenderer.getInstance().renderText(poseStack, String.valueOf(i), this.width / 2, (float) y, CommonColors.CYAN, HorizontalAlignment.Center, VerticalAlignment.Middle, TextShadow.NORMAL);
-        }
+        // uncomment when fixing gui alignment
+//        for (int i = 1; i <= GRID_DIVISIONS - 1; i++) {
+//            double x = dividedWidth * i;
+//            double y = dividedHeight * i;
+//            RenderUtils.drawRect(poseStack, CommonColors.GRAY, (float) x, 0, 0, 1, this.height);
+//            RenderUtils.drawRect(poseStack, CommonColors.GRAY, 0, (float) y, 0, this.width, 1);
+//            if (i % 2 == 0) continue; // reduce clutter
+//            FontRenderer.getInstance().renderText(poseStack, String.valueOf(i), (float) x, this.height / 2, CommonColors.RED, HorizontalAlignment.Center, VerticalAlignment.Middle, TextShadow.NORMAL);
+//            FontRenderer.getInstance().renderText(poseStack, String.valueOf(i), this.width / 2, (float) y, CommonColors.CYAN, HorizontalAlignment.Center, VerticalAlignment.Middle, TextShadow.NORMAL);
+//        }
         // endregion
 
         // region Invite field header
@@ -169,7 +170,7 @@ public final class PartyManagementScreen extends Screen implements TextboxScreen
                                 ChatFormatting.GRAY +
                                 I18n.get("screens.wynntils.partyManagementGui.inviteFieldHeader2"),
                         dividedWidth * 36,
-                        dividedHeight * 8,
+                        dividedHeight * PARTY_LIST_DIV_HEIGHT, // should be lined up with the party list header
                         CommonColors.WHITE,
                         HorizontalAlignment.Left,
                         VerticalAlignment.Bottom,
