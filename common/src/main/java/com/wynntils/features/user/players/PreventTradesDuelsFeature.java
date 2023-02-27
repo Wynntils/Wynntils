63f195bdac874df815b9ba3ea342fba04cd44e5b
Magnus Ihse Bursie <mag@icus.se>
2023-02-27 17:05:44 +0100

    feat: Option to only prevent trade/duel while fighting

--- common/src/main/java/com/wynntils/features/user/players/PreventTradesDuelsFeature.java
+++ common/src/main/java/com/wynntils/features/user/players/PreventTradesDuelsFeature.java
@@ -4,7 +4,9 @@
  */
 package com.wynntils.features.user.players;
 
+import com.wynntils.core.components.Managers;
 import com.wynntils.core.components.Models;
+import com.wynntils.core.config.Config;
 import com.wynntils.core.features.UserFeature;
 import com.wynntils.core.features.properties.FeatureCategory;
 import com.wynntils.core.features.properties.FeatureInfo;
@@ -14,25 +16,38 @@ import com.wynntils.utils.wynn.WynnItemMatchers;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @FeatureInfo(category = FeatureCategory.PLAYERS)
 public class PreventTradesDuelsFeature extends UserFeature {
+    private static final int FIGHTING_CUTOFF = 30; // 30 seconds
+
+    @Config
+    public boolean onlyWhileFighting = true;
 
     @SubscribeEvent
     public void onPlayerRightClick(PlayerInteractEvent.Interact event) {
-        if (!shouldBlockClick(event.getPlayer(), event.getItemStack(), event.getTarget())) return;
-
-        // stops interact packet from going out
-        event.setCanceled(true);
+        handlePlayerClick(event, event.getPlayer(), event.getItemStack(), event.getTarget());
     }
 
     @SubscribeEvent
     public void onPlayerLeftClick(PlayerAttackEvent event) {
-        if (!shouldBlockClick(event.getPlayer(), event.getPlayer().getMainHandItem(), event.getTarget())) return;
+        handlePlayerClick(event, event.getPlayer(), event.getPlayer().getMainHandItem(), event.getTarget());
+    }
 
-        // stops attack packet from going out
+    private void handlePlayerClick(Event event, Player player, ItemStack itemStack, Entity target) {
+        int timeSinceLastFight = (int) ((System.currentTimeMillis() - Models.CombatXp.getLastXpGainTimestamp()) / 1000);
+        if (onlyWhileFighting && timeSinceLastFight >= FIGHTING_CUTOFF) return;
+
+        if (!shouldBlockClick(player, itemStack, target)) return;
+
+        // stops interact packet from going out
         event.setCanceled(true);
+
+        if (onlyWhileFighting) {
+            Managers.Notification.queueMessage("Trade/Duel blocked for " + (FIGHTING_CUTOFF - timeSinceLastFight) + " s");
+        }
     }
 
     private boolean shouldBlockClick(Player player, ItemStack item, Entity target) {
