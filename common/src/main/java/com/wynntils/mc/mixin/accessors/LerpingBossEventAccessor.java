7198cca1c644f9093e7cd61be8713ab4bbcc848a
Magnus Ihse Bursie <mag@icus.se>
2023-03-01 19:11:21 +0100

    refactor: Move LerpingBossEventAccessor to the right place (#1230)

--- common/src/main/java/com/wynntils/mc/mixin/accessors/LerpingBossEventAccessor.java
+++ common/src/main/java/com/wynntils/mc/mixin/accessors/LerpingBossEventAccessor.java
@@ -2,7 +2,7 @@
  * Copyright © Wynntils 2022.
  * This file is released under AGPLv3. See LICENSE for full license details.
  */
-package com.wynntils.mc.mixin;
+package com.wynntils.mc.mixin.accessors;
 
 import net.minecraft.client.gui.components.LerpingBossEvent;
 import org.spongepowered.asm.mixin.Mixin;
