fb53b0ef1d6e81533e0783be291f1808c49b4549
Magnus Ihse Bursie <mag@icus.se>
2023-03-01 20:54:06 +0100

    Add documentation to events.

--- common/src/main/java/com/wynntils/models/damage/type/DamageDealtEvent.java
+++ common/src/main/java/com/wynntils/models/damage/type/DamageDealtEvent.java
@@ -8,6 +8,11 @@ import com.wynntils.models.stats.type.DamageType;
 import java.util.Map;
 import net.minecraftforge.eventbus.api.Event;
 
+/**
+ * This event is sent out whenever Wynntils register that a mob has received damage.
+ * We cannot tell which mob it is, but we know the damage is caused by melee or
+ * spell attack from the current player.
+ */
 public final class DamageDealtEvent extends Event {
     private final Map<DamageType, Integer> damages;
 
--- common/src/main/java/com/wynntils/models/damage/type/FocusedDamageEvent.java
+++ common/src/main/java/com/wynntils/models/damage/type/FocusedDamageEvent.java
@@ -6,6 +6,15 @@ package com.wynntils.models.damage.type;
 
 import net.minecraftforge.eventbus.api.Event;
 
+/**
+ * These events are sent when Wynncraft sets up a boss bar for the mob which
+ * is the target of the player's attacks. MobFocused is sent when a new mob
+ * is tracked by the boss bar, and MobDamaged when the same mob just loses health.
+ *
+ * Note that other players or environmental damage can also cause the mob to lose
+ * health, so the difference in health for MobDamaged might not be due to the current
+ * player.
+ */
 public abstract class FocusedDamageEvent extends Event {
     private final String mobName;
     private final String mobElementals;
