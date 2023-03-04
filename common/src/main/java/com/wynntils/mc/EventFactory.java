a760e610b06ff0e60297be1ff7e805d926f5607d
kristofbolyai <kristofbolyai@users.noreply.github.com>
2023-03-04 19:09:36 +0000

    ci: spotless formatting

--- common/src/main/java/com/wynntils/mc/EventFactory.java
+++ common/src/main/java/com/wynntils/mc/EventFactory.java
@@ -95,7 +95,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 import net.minecraft.client.Camera;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiComponent;
