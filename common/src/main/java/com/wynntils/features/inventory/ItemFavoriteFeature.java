35d4cafda4c5f6d7572a00807bc32d498c0e3cff
Magnus Ihse Bursie <mag@icus.se>
2023-03-09 23:13:52 +0100

    Revert changes to actual Storage

--- common/src/main/java/com/wynntils/features/inventory/ItemFavoriteFeature.java
+++ common/src/main/java/com/wynntils/features/inventory/ItemFavoriteFeature.java
@@ -4,10 +4,13 @@
  */
 package com.wynntils.features.inventory;
 
+import com.google.common.reflect.TypeToken;
 import com.wynntils.core.components.Models;
 import com.wynntils.core.config.Category;
+import com.wynntils.core.config.Config;
 import com.wynntils.core.config.ConfigCategory;
 import com.wynntils.core.features.UserFeature;
+import com.wynntils.core.json.TypeOverride;
 import com.wynntils.mc.event.ContainerCloseEvent;
 import com.wynntils.mc.event.SlotRenderEvent;
 import com.wynntils.models.items.WynnItem;
@@ -16,7 +19,10 @@ import com.wynntils.utils.mc.McUtils;
 import com.wynntils.utils.render.RenderUtils;
 import com.wynntils.utils.render.Texture;
 import com.wynntils.utils.wynn.ContainerUtils;
+import java.lang.reflect.Type;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
@@ -25,6 +31,15 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @ConfigCategory(Category.INVENTORY)
 public class ItemFavoriteFeature extends UserFeature {
+    public static ItemFavoriteFeature INSTANCE;
+
+    // This should really move to FavoritesModel, but for now, models cannot have configs
+    @Config(visible = false)
+    public Set<String> favoriteItems = new HashSet<>();
+
+    @TypeOverride
+    private final Type favoriteItemsType = new TypeToken<Set<String>>() {}.getType();
+
     @SubscribeEvent
     public void onChestCloseAttempt(ContainerCloseEvent.Pre e) {
         if (!Models.WorldState.onWorld()) return;
