04bd241373bd7073261d41df4176bd53144f0e67
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-03-16 00:15:55 +0100

    feat: Add CombatExperienceOverlay (#1339)

---
+++ common/src/main/java/com/wynntils/features/overlays/CombatExperienceOverlayFeature.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.features.overlays;
+
+import com.wynntils.core.config.Category;
+import com.wynntils.core.config.ConfigCategory;
+import com.wynntils.core.features.Feature;
+import com.wynntils.core.features.overlays.Overlay;
+import com.wynntils.core.features.overlays.OverlayPosition;
+import com.wynntils.core.features.overlays.RenderState;
+import com.wynntils.core.features.overlays.TextOverlay;
+import com.wynntils.core.features.overlays.annotations.OverlayInfo;
+import com.wynntils.core.features.overlays.sizes.GuiScaledOverlaySize;
+import com.wynntils.mc.event.RenderEvent;
+import com.wynntils.utils.render.type.HorizontalAlignment;
+import com.wynntils.utils.render.type.VerticalAlignment;
+
+@ConfigCategory(Category.OVERLAYS)
+public class CombatExperienceOverlayFeature extends Feature {
+    @OverlayInfo(renderAt = RenderState.Pre, renderType = RenderEvent.ElementType.GUI)
+    private final Overlay combatExperienceOverlay = new CombatExperienceOverlay();
+
+    public static class CombatExperienceOverlay extends TextOverlay {
+        protected CombatExperienceOverlay() {
+            super(
+                    new OverlayPosition(
+                            -73,
+                            0,
+                            VerticalAlignment.Bottom,
+                            HorizontalAlignment.Center,
+                            OverlayPosition.AnchorSection.BottomMiddle),
+                    new GuiScaledOverlaySize(200, 20),
+                    HorizontalAlignment.Center,
+                    VerticalAlignment.Middle);
+        }
+
+        @Override
+        public String getTemplate() {
+            return "&2[&a{capped_xp} &6({xp_pct:1}%)&2]";
+        }
+
+        @Override
+        public String getPreviewTemplate() {
+            return "&2[&a54321/2340232&2 &6(2.3%)]";
+        }
+    }
+}
