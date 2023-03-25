2d751933bdef17fc1e5e9603025d4d490f9defff
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-03-25 05:03:59 -0700

    add commands

--- common/src/main/java/com/wynntils/features/overlays/StopwatchFeature.java
+++ common/src/main/java/com/wynntils/features/overlays/StopwatchFeature.java
@@ -1,5 +1,6 @@
 package com.wynntils.features.overlays;
 
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.wynntils.core.components.Models;
 import com.wynntils.core.config.Category;
 import com.wynntils.core.config.ConfigCategory;
@@ -9,11 +10,14 @@ import com.wynntils.core.features.overlays.OverlayPosition;
 import com.wynntils.core.features.overlays.OverlaySize;
 import com.wynntils.core.features.overlays.TextOverlay;
 import com.wynntils.core.features.overlays.annotations.OverlayInfo;
+import com.wynntils.core.features.properties.RegisterCommand;
 import com.wynntils.core.features.properties.RegisterKeyBind;
 import com.wynntils.core.keybinds.KeyBind;
 import com.wynntils.mc.event.RenderEvent;
 import com.wynntils.utils.render.type.HorizontalAlignment;
 import com.wynntils.utils.render.type.VerticalAlignment;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
 import org.lwjgl.glfw.GLFW;
 
 @ConfigCategory(Category.OVERLAYS)
@@ -25,6 +29,35 @@ public class StopwatchFeature extends Feature {
     @RegisterKeyBind
     private final KeyBind resetStopwatchKeybind =
             new KeyBind("Reset Stopwatch", GLFW.GLFW_KEY_KP_DECIMAL, true, Models.Stopwatch::reset);
+
+    @RegisterCommand
+    private final LiteralCommandNode<CommandSourceStack> startCommand =
+            Commands.literal("start")
+                    .executes(ctx -> {
+                        Models.Stopwatch.start();
+                        return 0;
+                    })
+                    .build();
+
+    @RegisterCommand
+    private final LiteralCommandNode<CommandSourceStack> stopCommand =
+            Commands.literal("stop")
+                    .executes(ctx -> {
+                        if (Models.Stopwatch.isRunning()) {
+                            Models.Stopwatch.stop();
+                        }
+                        return 0;
+                    })
+                    .build();
+
+    @RegisterCommand
+    private final LiteralCommandNode<CommandSourceStack> resetCommand =
+            Commands.literal("reset")
+                    .executes(ctx -> {
+                        Models.Stopwatch.reset();
+                        return 0;
+                    })
+                    .build();
 
     @OverlayInfo(renderType = RenderEvent.ElementType.GUI)
     private final Overlay stopwatchOverlay = new StopwatchOverlay();
