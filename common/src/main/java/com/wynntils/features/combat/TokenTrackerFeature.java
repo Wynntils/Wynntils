e417a027e3d63c4ee58fd269b0430fa8bc23dfd3
Magnus Ihse Bursie <mag@icus.se>
2023-03-25 22:29:41 +0100

    feat: Stop playing token bell sound after a while (#1376)
    feat: Stop playing token bell sound after you have passed a few tokens above the needed amount
    
    As requested by @POkeDev

--- common/src/main/java/com/wynntils/features/combat/TokenTrackerFeature.java
+++ common/src/main/java/com/wynntils/features/combat/TokenTrackerFeature.java
@@ -18,6 +18,7 @@ import com.wynntils.models.token.event.TokenGatekeeperEvent;
 import com.wynntils.utils.colors.CustomColor;
 import com.wynntils.utils.mc.McUtils;
 import com.wynntils.utils.render.Texture;
+import com.wynntils.utils.type.CappedValue;
 import java.util.ArrayList;
 import java.util.List;
 import net.minecraft.ChatFormatting;
@@ -40,9 +41,14 @@ public class TokenTrackerFeature extends Feature {
         // Do not play sound when depositing from the inventory
         if (event.getCount() < event.getOldCount()) return;
 
-        if (Models.Token.getCollected(event.getGatekeeper()).isAtCap()) {
-            McUtils.mc().getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.BELL_BLOCK, 0.7f, 0.75f));
-        }
+        CappedValue deposited = event.getGatekeeper().getDeposited();
+        int collected = Models.Token.inInventory(event.getGatekeeper()) + deposited.current();
+
+        if (collected < deposited.max()) return;
+        // Do not keep playing the sound if we go too far over the needed amount
+        if (collected > deposited.max() + 5) return;
+
+        McUtils.mc().getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.BELL_BLOCK, 0.7f, 0.75f));
     }
 
     protected static final class TokenBarOverlay extends BarOverlay {
