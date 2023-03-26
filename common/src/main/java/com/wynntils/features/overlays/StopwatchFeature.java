dc0605d1f30e6bf9ee528d5abacd24f5ce4ab5e2
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-03-26 01:46:30 -0700

    fix: hide timer when 0 by default

--- common/src/main/java/com/wynntils/features/overlays/StopwatchFeature.java
+++ common/src/main/java/com/wynntils/features/overlays/StopwatchFeature.java
@@ -73,7 +73,7 @@ public class StopwatchFeature extends Feature {
 
     public static class StopwatchOverlay extends TextOverlay {
         private static final String TEMPLATE =
-                "{if_str(stopwatch_running;\"\";if_str(stopwatch_zero;\"&7\";\"&e\"))}{stopwatch_hours}:{stopwatch_minutes}:{stopwatch_seconds}.{stopwatch_milliseconds}";
+                "{if_str(stopwatch_zero;\"\";concat(if_str(stopwatch_running;\"\";\"&e\");stopwatch_hours;\":\";stopwatch_minutes;\":\";stopwatch_seconds;\".\";stopwatch_milliseconds))}";
 
         protected StopwatchOverlay() {
             super(
