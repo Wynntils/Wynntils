40493c0c13ed95f9baac1bc154e972e4cf9c3416
Magnus Ihse Bursie <mag@icus.se>
2023-03-28 20:26:05 +0200

    feat: Embellishment feature: Wybel sounds
    Are you tired of just *seeing* your cute wybels purr in chat? Fret no more! Now you can actually hear them purr and squeak, for real.

---
+++ common/src/main/java/com/wynntils/features/embellishments/WybelSoundFeature.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.features.embellishments;
+
+import com.wynntils.core.config.Category;
+import com.wynntils.core.config.Config;
+import com.wynntils.core.config.ConfigCategory;
+import com.wynntils.core.config.RegisterConfig;
+import com.wynntils.core.features.Feature;
+import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
+import com.wynntils.handlers.chat.type.RecipientType;
+import com.wynntils.utils.mc.McUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+@ConfigCategory(Category.EMBELLISHMENTS)
+public class WybelSoundFeature extends Feature {
+    private static final ResourceLocation WYBEL_SQUEAK_ID = new ResourceLocation("wynntils:wybel.squeak");
+    private static SoundEvent WYBEL_SQUEAK_SOUND = SoundEvent.createVariableRangeEvent(WYBEL_SQUEAK_ID);
+
+    private static final ResourceLocation WYBEL_PURR_ID = new ResourceLocation("wynntils:wybel.purr");
+    private static SoundEvent WYBEL_PURR_SOUND = SoundEvent.createVariableRangeEvent(WYBEL_PURR_ID);
+
+    @RegisterConfig
+    public final Config<Boolean> hideText = new Config<>(false);
+
+    @SubscribeEvent
+    public void onChat(ChatMessageReceivedEvent event) {
+        if (event.getRecipientType() != RecipientType.PETS) return;
+
+        String msg = event.getCodedMessage();
+        if (msg.contains("squeak")) {
+            McUtils.playSound(WYBEL_SQUEAK_SOUND);
+            if (hideText.get()) {
+                event.setCanceled(true);
+            }
+        }
+        if (msg.contains("purr")) {
+            McUtils.playSound(WYBEL_PURR_SOUND);
+            if (hideText.get()) {
+                event.setCanceled(true);
+            }
+        }
+    }
+}
