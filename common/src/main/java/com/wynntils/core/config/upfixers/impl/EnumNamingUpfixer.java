697b5dea9b0397f6271cc01909804202d60d5536
Magnus Ihse Bursie <mag@icus.se>
2023-03-29 22:44:33 +0200

    Fix enum upfixer to work with embedded enums as well

--- common/src/main/java/com/wynntils/core/config/upfixers/impl/EnumNamingUpfixer.java
+++ common/src/main/java/com/wynntils/core/config/upfixers/impl/EnumNamingUpfixer.java
@@ -4,35 +4,103 @@
  */
 package com.wynntils.core.config.upfixers.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import com.wynntils.core.WynntilsMod;
+import com.wynntils.core.components.Managers;
 import com.wynntils.core.config.ConfigHolder;
 import com.wynntils.core.config.upfixers.ConfigUpfixer;
 import com.wynntils.utils.EnumUtils;
+import com.wynntils.utils.colors.CustomColor;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Set;
 
 public class EnumNamingUpfixer implements ConfigUpfixer {
+    public static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(CustomColor.class, new CustomColor.CustomColorSerializer())
+            .registerTypeAdapterFactory(new EnumConverterFactory())
+            .setPrettyPrinting()
+            .serializeNulls()
+            .create();
+
     @Override
     public boolean apply(JsonObject configObject, Set<ConfigHolder> configHolders) {
-        for (ConfigHolder configHolder : configHolders) {
-            String name = configHolder.getJsonName();
+        for (ConfigHolder config : configHolders) {
+            String jsonName = config.getJsonName();
+            if (!configObject.has(jsonName)) continue;
 
-            Type type = configHolder.getType();
-            if (type instanceof Class clazz && clazz.isEnum()) {
-                if (!configObject.has(name)) continue;
+            JsonElement origJson = configObject.get(jsonName);
+            Object value = GSON.fromJson(origJson, config.getType());
+            JsonElement newJson = Managers.Json.GSON.toJsonTree(value, config.getType());
 
-                JsonPrimitive configValue = configObject.getAsJsonPrimitive(name);
-                if (!configValue.isString()) continue;
-
-                String originalValue = configValue.getAsString();
-                Enum<?> enumValue = Enum.valueOf(clazz, originalValue);
-                String newValue = EnumUtils.toJsonFormat(enumValue);
-
-                configObject.addProperty(name, newValue);
+            if (!(newJson.equals(origJson))) {
+                configObject.add(jsonName, newJson);
             }
         }
 
         return true;
+    }
+
+    private static final class EnumConverterFactory implements TypeAdapterFactory {
+        @Override
+        public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+            if (!type.getRawType().isEnum()) return null;
+
+            Class<? extends Enum<?>> enumClazz = (Class<? extends Enum<?>>) type.getRawType();
+            return new EnumConverter<>(enumClazz);
+        }
+    }
+
+    private static final class EnumConverter<T> extends TypeAdapter<T> {
+        private final Class<? extends Enum<?>> enumClazz;
+
+        private EnumConverter(Class<? extends Enum<?>> enumClazz) {
+            this.enumClazz = enumClazz;
+        }
+
+        @Override
+        public void write(JsonWriter out, T value) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public T read(JsonReader in) throws IOException {
+            if (in.peek() != JsonToken.STRING) {
+                in.nextNull();
+                return null;
+            }
+
+            String jsonString = in.nextString();
+
+            // The double casting is needed, or javac will complain...
+            Enum<?> value;
+            try {
+                value = Enum.valueOf((Class<Enum>) (Type) enumClazz, jsonString);
+            } catch (IllegalArgumentException e) {
+                // Maybe it is already converted?
+                value = EnumUtils.fromJsonFormat(enumClazz, jsonString);
+            }
+            if (value == null) {
+                WynntilsMod.warn("Illegal enum value: " + jsonString + " for type " + enumClazz.getName());
+                return replacement();
+            }
+
+            return (T) value;
+        }
+
+        private T replacement() {
+            Enum<?> firstValue = enumClazz.getEnumConstants()[0];
+            WynntilsMod.warn("Will replace with first enum value: " + firstValue.name());
+            return (T) firstValue;
+        }
     }
 }
