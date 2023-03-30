33f0596b67470c3777e10c055bdab5723fa08b15
Jamie <JamCal117@gmail.com>
2023-03-30 17:43:33 +0100

    Removed delete all button

--- common/src/main/java/com/wynntils/screens/maps/PoiManagementScreen.java
+++ common/src/main/java/com/wynntils/screens/maps/PoiManagementScreen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2022.
+ * Copyright © Wynntils 2023.
  * This file is released under AGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.screens.maps;
@@ -30,8 +30,6 @@ public class PoiManagementScreen extends WynntilsScreen {
     private final MainMapScreen oldMapScreen;
     private Button nextButton;
     private Button previousButton;
-    private Button deleteAllButton;
-    private int deleteCount = 3;
     private List<CustomPoi> waypoints;
     private int pageHeight;
     private int page;
@@ -78,20 +76,6 @@ public class PoiManagementScreen extends WynntilsScreen {
                         .pos(this.width / 2 - 22, this.height - 45)
                         .size(20, 20)
                         .build());
-
-        this.addRenderableWidget(
-                deleteAllButton = new Button.Builder(
-                                Component.literal("DELETE ALL (Press " + deleteCount + " times)"), (button) -> {
-                                    clearAllButton();
-                                })
-                        .pos(25, this.height - 45)
-                        .size(font.width("DELETE ALL (Press " + deleteCount + " times)") + 15, 20)
-                        .build());
-
-        // In case doInit called again, won't say press 0 times
-        if (deleteCount <= 0) {
-            deleteAllButton.setMessage(Component.literal("DELETED!"));
-        }
 
         waypoints =
                 Managers.Feature.getFeatureInstance(MapFeature.class).customPois.get();
@@ -222,22 +206,5 @@ public class PoiManagementScreen extends WynntilsScreen {
     private void checkAvailablePages() {
         nextButton.active = waypoints.size() - page * pageHeight > pageHeight;
         previousButton.active = page > 0;
-    }
-
-    private void clearAllButton() {
-        deleteCount--;
-
-        if (deleteCount == 0) {
-            Managers.Feature.getFeatureInstance(MapFeature.class)
-                    .customPois
-                    .get()
-                    .clear();
-
-            Managers.Config.saveConfig();
-            deleteAllButton.setMessage(Component.literal("DELETED!"));
-            populatePois();
-        } else if (deleteCount > 0) {
-            deleteAllButton.setMessage(Component.literal("DELETE ALL (Press " + deleteCount + " times)"));
-        }
     }
 }
