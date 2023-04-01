f2182aefb4f34d0b1943607d9a28041084d4240c
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-04-01 22:36:11 +0200

    feat: Add import button for PoiManagementScreen (works with legacy's Artemis Export button)

--- common/src/main/java/com/wynntils/screens/maps/PoiManagementScreen.java
+++ common/src/main/java/com/wynntils/screens/maps/PoiManagementScreen.java
@@ -4,6 +4,7 @@
  */
 package com.wynntils.screens.maps;
 
+import com.google.gson.JsonSyntaxException;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.wynntils.core.components.Managers;
 import com.wynntils.features.map.MapFeature;
@@ -22,15 +23,17 @@ import java.util.List;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
 
-public class PoiManagementScreen extends WynntilsScreen {
+public final class PoiManagementScreen extends WynntilsScreen {
     private final MainMapScreen oldMapScreen;
     private Button nextButton;
     private Button previousButton;
     private Button undoDeleteButton;
+    private Button importButton;
     private List<CustomPoi> waypoints;
     private int pageHeight;
     private int currentPage;
@@ -92,6 +95,21 @@ public class PoiManagementScreen extends WynntilsScreen {
                         .size(font.width(Component.translatable("screens.wynntils.poiManagementGui.undo")) + 15, 20)
                         .build());
 
+        this.addRenderableWidget(
+                importButton = new Button.Builder(
+                                Component.translatable("screens.wynntils.poiManagementGui.import"),
+                                (button) -> importFromClipboard())
+                        .pos(
+                                this.width / 2
+                                        + 75
+                                        - font.width(
+                                                Component.translatable("screens.wynntils.poiManagementGui.import")),
+                                this.height - 45)
+                        .size(font.width(Component.translatable("screens.wynntils.poiManagementGui.import")) + 15, 20)
+                        .tooltip(Tooltip.create(
+                                Component.translatable("screens.wynntils.poiManagementGui.import.tooltip")))
+                        .build());
+
         if (deletedIndexes.isEmpty()) {
             undoDeleteButton.active = false;
         }
@@ -106,6 +124,27 @@ public class PoiManagementScreen extends WynntilsScreen {
         checkAvailablePages();
 
         populatePois();
+    }
+
+    private void importFromClipboard() {
+        String clipboard = McUtils.mc().keyboardHandler.getClipboard();
+
+        CustomPoi[] customPois;
+        try {
+            customPois = Managers.Json.GSON.fromJson(clipboard, CustomPoi[].class);
+        } catch (JsonSyntaxException e) {
+            importButton.setTooltip(
+                    Tooltip.create(Component.translatable("screens.wynntils.poiManagementGui.import.error")));
+            return;
+        }
+
+        Managers.Feature.getFeatureInstance(MapFeature.class).customPois.get().addAll(List.of(customPois));
+        Managers.Config.saveConfig();
+
+        populatePois();
+
+        importButton.setTooltip(Tooltip.create(
+                Component.translatable("screens.wynntils.poiManagementGui.import.success", customPois.length)));
     }
 
     @Override
@@ -205,6 +244,8 @@ public class PoiManagementScreen extends WynntilsScreen {
 
             this.addRenderableWidget(newWidget);
         }
+
+        checkAvailablePages();
     }
 
     private void nextPage() {
