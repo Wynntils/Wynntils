89eb240bdf1b1e75e18b5b84cc6a345b106f747f
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-04-03 14:04:13 +0200

    feat: Introduce JUnit tests

---
+++ common/src/test/java/TestComponentUtils.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+import com.wynntils.utils.mc.ComponentUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestComponentUtils {
+    @Test
+    void getCoded_ShouldWork() {
+        final Component component = Component.literal("Hello,")
+                .withStyle(ChatFormatting.RED)
+                .append(Component.literal(" World!")
+                        .withStyle(ChatFormatting.BLUE)
+                        .withStyle(ChatFormatting.BOLD));
+        final String expected = "§cHello,§r§9§l World!";
+
+        String actual = ComponentUtils.getCoded(component);
+
+        Assertions.assertEquals(expected, actual, "ComponentUtils.getCoded() returned an unexpected value.");
+    }
+}
