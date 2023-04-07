ad5103981a39f96999b6295dceb691b40c81e493
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-04-07 13:37:23 +0200

    chore: Port ChatItemFeature to use StyledText

--- common/src/main/java/com/wynntils/features/chat/ChatItemFeature.java
+++ common/src/main/java/com/wynntils/features/chat/ChatItemFeature.java
@@ -8,26 +8,24 @@ import com.wynntils.core.components.Models;
 import com.wynntils.core.config.Category;
 import com.wynntils.core.config.ConfigCategory;
 import com.wynntils.core.features.Feature;
-import com.wynntils.core.text.CodedString;
+import com.wynntils.core.text.PartStyle;
+import com.wynntils.core.text.StyledText;
+import com.wynntils.core.text.StyledTextPart;
 import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
 import com.wynntils.mc.event.KeyInputEvent;
 import com.wynntils.mc.mixin.accessors.ChatScreenAccessor;
 import com.wynntils.mc.mixin.accessors.ItemStackInfoAccessor;
 import com.wynntils.models.items.FakeItemStack;
 import com.wynntils.models.items.items.game.GearItem;
-import com.wynntils.utils.mc.ComponentUtils;
 import com.wynntils.utils.mc.McUtils;
+import com.wynntils.utils.type.IterationDecision;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
-import java.util.stream.Collectors;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.ChatScreen;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -55,7 +53,7 @@ public class ChatItemFeature extends Feature {
         }
 
         // replace encoded strings with placeholders for less confusion
-        Matcher m = Models.Gear.gearChatEncodingMatcher(CodedString.fromString(chatInput.getValue()));
+        Matcher m = Models.Gear.gearChatEncodingMatcher(chatInput.getValue());
         while (m.find()) {
             String encodedItem = m.group();
             StringBuilder name = new StringBuilder(m.group("Name"));
@@ -72,83 +70,54 @@ public class ChatItemFeature extends Feature {
     public void onChatReceived(ChatMessageReceivedEvent e) {
         if (!Models.WorldState.onWorld()) return;
 
-        Component message = e.getMessage();
+        StyledText styledText = e.getStyledText();
 
-        if (!Models.Gear.gearChatEncodingMatcher(ComponentUtils.getCoded(message))
-                .find()) return;
+        StyledText modified = styledText.iterate((part, changes) -> {
+            StyledTextPart partToReplace = part;
+            Matcher matcher = Models.Gear.gearChatEncodingMatcher(partToReplace.getUnformattedString());
 
-        e.setMessage(decodeMessage(message));
-    }
+            while (matcher.find()) {
+                GearItem decodedItem = Models.Gear.fromEncodedString(matcher.group());
+                if (decodedItem == null) continue;
 
-    private Component decodeMessage(Component message) {
-        // no item tooltips to insert
-        List<MutableComponent> components =
-                message.getSiblings().stream().map(Component::copy).collect(Collectors.toList());
-        components.add(0, message.plainCopy().withStyle(message.getStyle()));
+                String unformattedString = partToReplace.getUnformattedString();
 
-        MutableComponent temp = Component.literal("");
+                String firstPart = unformattedString.substring(0, matcher.start());
+                String lastPart = unformattedString.substring(matcher.end());
 
-        for (Component comp : components) {
-            Matcher m = Models.Gear.gearChatEncodingMatcher(ComponentUtils.getCoded(comp));
-            if (!m.find()) {
-                Component newComponent = comp.copy();
-                temp.append(newComponent);
-                continue;
+                PartStyle partStyle = partToReplace.getPartStyle();
+
+                StyledTextPart first = new StyledTextPart(firstPart, partStyle.getStyle(), null, Style.EMPTY);
+                StyledTextPart item = createItemPart(decodedItem);
+                StyledTextPart last = new StyledTextPart(lastPart, partStyle.getStyle(), null, Style.EMPTY);
+
+                changes.remove(partToReplace);
+                changes.add(first);
+                changes.add(item);
+                changes.add(last);
+
+                partToReplace = last;
+                matcher = Models.Gear.gearChatEncodingMatcher(lastPart);
             }
 
-            boolean lastComponentIsItem = false;
+            return IterationDecision.CONTINUE;
+        });
 
-            do {
-                CodedString text = ComponentUtils.getCoded(comp);
-                Style style = comp.getStyle();
+        if (modified.equals(styledText)) return;
 
-                GearItem item = Models.Gear.fromEncodedString(m.group());
-                if (item == null) { // couldn't decode, skip
-                    comp = comp.copy();
-                    lastComponentIsItem = false;
-                    continue;
-                }
-
-                MutableComponent preText = Component.literal(
-                        text.getInternalCodedStringRepresentation().substring(0, m.start()));
-                preText.withStyle(style);
-                temp.append(preText);
-
-                // If the component is a chat item too, add a space
-                // to prevent the hover event from "merging" with this one
-                if (lastComponentIsItem) {
-                    temp.append(" ");
-                }
-
-                // create hover-able text component for the item
-                Component itemComponent = createItemComponent(item);
-                temp.append(itemComponent);
-
-                comp = Component.literal(ComponentUtils.getLastPartCodes(ComponentUtils.getCoded(preText))
-                                + text.getInternalCodedStringRepresentation().substring(m.end()))
-                        .withStyle(style);
-                m = Models.Gear.gearChatEncodingMatcher(
-                        ComponentUtils.getCoded(comp)); // recreate matcher for new substring
-                lastComponentIsItem = true;
-            } while (m.find()); // search for multiple items in the same message
-
-            temp.append(comp); // leftover text after item(s)
-        }
-
-        return temp;
+        e.setMessage(modified.getComponent());
     }
 
-    private Component createItemComponent(GearItem gearItem) {
-        MutableComponent itemComponent = Component.literal(
-                        gearItem.getGearInfo().name())
-                .withStyle(ChatFormatting.UNDERLINE)
-                .withStyle(gearItem.getGearInfo().tier().getChatFormatting());
+    private StyledTextPart createItemPart(GearItem gearItem) {
+        Style style = Style.EMPTY
+                .applyFormat(ChatFormatting.UNDERLINE)
+                .withColor(gearItem.getGearInfo().tier().getChatFormatting());
 
         ItemStack itemStack = new FakeItemStack(gearItem, "From chat");
         HoverEvent.ItemStackInfo itemHoverEvent = new HoverEvent.ItemStackInfo(itemStack);
         ((ItemStackInfoAccessor) itemHoverEvent).setItemStack(itemStack);
-        itemComponent.withStyle(s -> s.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, itemHoverEvent)));
+        style = style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, itemHoverEvent));
 
-        return itemComponent;
+        return new StyledTextPart(gearItem.getGearInfo().name(), style, null, Style.EMPTY);
     }
 }
