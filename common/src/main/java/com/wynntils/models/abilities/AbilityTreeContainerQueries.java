4063cc36a3af1c213b9a6796d853d0e4dd525d88
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-04-10 20:59:27 +0200

    feat: Add AbilityTreeDataDumpFeature

--- common/src/main/java/com/wynntils/models/abilities/AbilityTreeContainerQueries.java
+++ common/src/main/java/com/wynntils/models/abilities/AbilityTreeContainerQueries.java
@@ -9,7 +9,7 @@ import com.wynntils.core.components.Models;
 import com.wynntils.core.text.StyledText;
 import com.wynntils.handlers.container.ScriptedContainerQuery;
 import com.wynntils.handlers.container.type.ContainerContent;
-import com.wynntils.models.abilities.type.AbilityTreeInfo;
+import com.wynntils.models.abilities.type.AbilityTreeDump;
 import com.wynntils.utils.mc.McUtils;
 import com.wynntils.utils.wynn.InventoryUtils;
 import java.util.List;
@@ -22,11 +22,10 @@ public class AbilityTreeContainerQueries {
     private static final int ABILITY_TREE_PAGES = 7;
     private static final int ABILITY_TREE_SLOT = 9;
     private static final int NEXT_PAGE_SLOT = 59;
-    private static final int DUMMY_SLOT = 89;
     private static final StyledText NEXT_PAGE_ITEM_NAME = StyledText.fromString("§7Next Page");
 
     public void dumpAbilityTreeData() {
-        AbilityTreeInfo dump = new AbilityTreeInfo();
+        AbilityTreeDump dump = new AbilityTreeDump();
 
         ScriptedContainerQuery.QueryBuilder queryBuilder = ScriptedContainerQuery.builder("Ability Tree Dump Query")
                 .onError(msg -> {
@@ -50,7 +49,7 @@ public class AbilityTreeContainerQueries {
                 .matchTitle(Models.Container.ABILITY_TREE_PATTERN.pattern())
                 .processContainer(c -> {});
         queryBuilder
-                .clickOnSlot(DUMMY_SLOT)
+                .clickOnSlot(89)
                 .matchTitle(Models.Container.ABILITY_TREE_PATTERN.pattern())
                 .processContainer(c -> dumpAbilityTreePage(c, 2, dump));
 
@@ -67,18 +66,22 @@ public class AbilityTreeContainerQueries {
         queryBuilder.build().executeQuery();
     }
 
-    private void dumpAbilityTreePage(ContainerContent content, int page, AbilityTreeInfo dump) {
+    private void dumpAbilityTreePage(ContainerContent content, int page, AbilityTreeDump dump) {
         List<ItemStack> items = content.items();
 
         for (int slot = 0; slot < items.size(); slot++) {
             ItemStack item = items.get(slot);
-            dump.processItem(item, slot, page);
+            if (Models.AbilityTree.isNodeItem(item, slot)) {
+                dump.addNodeFromItem(item, page, slot);
+            } else if (Models.AbilityTree.isConnectionItem(item)) {
+                dump.addConnectionFromItem(page, slot);
+            }
         }
 
         dump.processConnections(page, page == ABILITY_TREE_PAGES);
 
         if (page == ABILITY_TREE_PAGES) {
-            dump.saveToDisk();
+            dump.finalizeDump();
         }
     }
 }
