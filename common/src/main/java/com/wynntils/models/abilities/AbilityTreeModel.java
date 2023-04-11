753d2d679bdba3cf9c38549da6124c584de4fb53
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-04-11 13:02:10 +0200

    feat: Load ability tree data from URLs

--- common/src/main/java/com/wynntils/models/abilities/AbilityTreeModel.java
+++ common/src/main/java/com/wynntils/models/abilities/AbilityTreeModel.java
@@ -4,11 +4,43 @@
  */
 package com.wynntils.models.abilities;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.wynntils.core.components.Managers;
 import com.wynntils.core.components.Model;
+import com.wynntils.core.net.Download;
+import com.wynntils.core.net.UrlId;
+import com.wynntils.models.abilities.type.AbilityTreeInfo;
+import com.wynntils.models.character.type.ClassType;
+import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AbilityTreeModel extends Model {
+    private Map<ClassType, AbilityTreeInfo> ABILIIY_TREE_MAP = new HashMap<>();
+
     public AbilityTreeModel() {
         super(List.of());
+
+        reloadData();
+    }
+
+    @Override
+    public void reloadData() {
+        Download dl = Managers.Net.download(UrlId.DATA_STATIC_ABILITIES);
+        dl.handleReader(reader -> {
+            Type type = new TypeToken<HashMap<String, AbilityTreeInfo>>() {}.getType();
+            Gson gson = new GsonBuilder().create();
+
+            Map<String, AbilityTreeInfo> abilityMap = gson.fromJson(reader, type);
+
+            Map<ClassType, AbilityTreeInfo> tempMap = new HashMap<>();
+
+            abilityMap.forEach((key, value) -> tempMap.put(ClassType.fromName(key), value));
+
+            ABILIIY_TREE_MAP = tempMap;
+        });
     }
 }
