09221f44362ef598f61099098d24709228a96495
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-04-13 16:07:05 +0200

    refactor: Other small refactors

--- common/src/main/java/com/wynntils/models/cosmetics/CosmeticsModel.java
+++ common/src/main/java/com/wynntils/models/cosmetics/CosmeticsModel.java
@@ -4,21 +4,35 @@
  */
 package com.wynntils.models.cosmetics;
 
+import com.mojang.blaze3d.platform.NativeImage;
+import com.wynntils.core.WynntilsMod;
 import com.wynntils.core.components.Model;
 import com.wynntils.core.components.Models;
 import com.wynntils.models.cosmetics.type.WynntilsCapeLayer;
 import com.wynntils.models.cosmetics.type.WynntilsElytraLayer;
 import com.wynntils.models.cosmetics.type.WynntilsLayer;
+import com.wynntils.models.players.WynntilsUser;
 import com.wynntils.models.players.type.CosmeticInfo;
+import com.wynntils.models.worlds.event.WorldStateEvent;
+import com.wynntils.models.worlds.type.WorldState;
+import com.wynntils.utils.mc.McUtils;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiFunction;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.player.PlayerModelPart;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class CosmeticsModel extends Model {
     private static final BiFunction<
@@ -39,12 +53,14 @@ public class CosmeticsModel extends Model {
                             WynntilsLayer>>
             REGISTERED_LAYERS = List.of(CAPE_LAYER, ELYTRA_LAYER);
 
+    private final Map<UUID, ResourceLocation[]> cosmeticTextures = new ConcurrentHashMap<>();
+
     public CosmeticsModel() {
         super(List.of());
     }
 
-    // Note: This method is static deliberately. We don't want to reference Models in the mixin this is called, due to
-    // it's unpredictable class loading.
+    // Note: This method is static deliberately.
+    // We don't want to reference Models in the mixin this is called, due to it's unpredictable class loading.
     public static List<
                     BiFunction<
                             RenderLayerParent<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>>,
@@ -54,11 +70,18 @@ public class CosmeticsModel extends Model {
         return REGISTERED_LAYERS;
     }
 
+    @SubscribeEvent
+    public void onWorldStateChange(WorldStateEvent event) {
+        if (event.getNewState() == WorldState.NOT_CONNECTED) {
+            cosmeticTextures.clear();
+        }
+    }
+
     public boolean shouldRenderCape(Player player, boolean elytra) {
         if (player.isInvisible() || !player.isModelPartShown(PlayerModelPart.CAPE)) return false;
 
-        if (Models.Player.getUser(player.getUUID()) == null
-                || Models.Player.getUserCosmeticTexture(player.getUUID()) == null) return false;
+        if (Models.Player.getUser(player.getUUID()) == null || getUserCosmeticTexture(player.getUUID()) == null)
+            return false;
 
         CosmeticInfo cosmetics = Models.Player.getUser(player.getUUID()).cosmetics();
         return (elytra ? cosmetics.hasElytra() : cosmetics.hasCape());
@@ -74,7 +97,7 @@ public class CosmeticsModel extends Model {
     }
 
     public ResourceLocation getCapeTexture(Player player) {
-        ResourceLocation[] textures = Models.Player.getUserCosmeticTexture(player.getUUID());
+        ResourceLocation[] textures = getUserCosmeticTexture(player.getUUID());
         if (textures == null) return null;
 
         int frames = textures.length;
@@ -85,5 +108,43 @@ public class CosmeticsModel extends Model {
         double percentage = ((System.currentTimeMillis() % 2000) / 2000d);
         int currentFrame = (int) (frames * percentage);
         return textures[currentFrame];
+    }
+
+    public void loadCosmeticTextures(UUID uuid, WynntilsUser user) {
+        try {
+            if (user.cosmetics().texture() == null || user.cosmetics().texture().isEmpty()) return;
+            if (cosmeticTextures.containsKey(uuid)) return;
+
+            byte[] textureBytes = Base64.getDecoder().decode(user.cosmetics().texture());
+            ByteArrayInputStream byteStream = new ByteArrayInputStream(textureBytes);
+            NativeImage image = NativeImage.read(byteStream);
+
+            int frames = (image.getHeight() * 2) / image.getWidth();
+            int frameHeight = image.getHeight() / frames;
+
+            ResourceLocation[] locations = new ResourceLocation[frames];
+            String baseLocation = "wynntils:capes/" + uuid.toString().replace("-", "");
+
+            if (frames == 1) { // not animated
+                locations[0] = new ResourceLocation(baseLocation);
+                McUtils.mc().getTextureManager().register(locations[0], new DynamicTexture(image));
+            } else { // animated
+                for (int i = 0; i < frames; i++) {
+                    NativeImage frame = new NativeImage(frameHeight * 2, frameHeight, false);
+                    image.copyRect(frame, 0, frameHeight * i, 0, 0, frameHeight * 2, frameHeight, false, false);
+
+                    locations[i] = new ResourceLocation(baseLocation + "/" + i);
+                    McUtils.mc().getTextureManager().register(locations[i], new DynamicTexture(frame));
+                }
+            }
+
+            cosmeticTextures.put(uuid, locations);
+        } catch (IOException e) {
+            WynntilsMod.warn("IOException occurred while loading cosmetics for user " + uuid, e);
+        }
+    }
+
+    public ResourceLocation[] getUserCosmeticTexture(UUID uuid) {
+        return cosmeticTextures.getOrDefault(uuid, null);
     }
 }
