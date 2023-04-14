5532d1440d26aa8262037a7bf587873762e448b0
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-04-14 14:18:10 +0200

    Fix states not appearing

--- common/src/main/java/com/wynntils/models/abilitytree/type/AbilityTreeSkillNode.java
+++ common/src/main/java/com/wynntils/models/abilitytree/type/AbilityTreeSkillNode.java
@@ -5,6 +5,7 @@
 package com.wynntils.models.abilitytree.type;
 
 import java.util.List;
+import java.util.Objects;
 
 public record AbilityTreeSkillNode(
         String name,
@@ -17,4 +18,23 @@ public record AbilityTreeSkillNode(
         ArchetypeRequirement requiredArchetype,
         String archetype,
         AbilityTreeLocation location,
-        List<String> connections) {}
+        List<String> connections) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbilityTreeSkillNode that = (AbilityTreeSkillNode) o;
+        return cost == that.cost
+                && Objects.equals(formattedName, that.formattedName)
+                && Objects.equals(requiredAbility, that.requiredAbility)
+                && Objects.equals(requiredArchetype, that.requiredArchetype)
+                && Objects.equals(archetype, that.archetype)
+                && Objects.equals(location, that.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(formattedName, cost, requiredAbility, requiredArchetype, archetype, location);
+    }
+}
