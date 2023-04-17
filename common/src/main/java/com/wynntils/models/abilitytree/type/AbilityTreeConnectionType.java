37f88a97a2873c22457df33072e7fd5c7c41a40c
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-04-17 16:18:25 +0200

    Fixing up some merges

--- common/src/main/java/com/wynntils/models/abilitytree/type/AbilityTreeConnectionType.java
+++ common/src/main/java/com/wynntils/models/abilitytree/type/AbilityTreeConnectionType.java
@@ -4,11 +4,14 @@
  */
 package com.wynntils.models.abilitytree.type;
 
+import com.wynntils.core.WynntilsMod;
 import com.wynntils.utils.type.Pair;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -36,7 +39,10 @@ public enum AbilityTreeConnectionType {
                     new boolean[] {true, false, false, true}, 15,
                     new boolean[] {true, true, false, false}, 16,
                     new boolean[] {false, true, false, true}, 17),
-            List.of()),
+            List.of(
+                    Pair.of(HORIZONTAL, UP_LEFT_TURN),
+                    Pair.of(HORIZONTAL, UP_RIGHT_TURN),
+                    Pair.of(UP_LEFT_TURN, UP_RIGHT_TURN))),
 
     THREE_WAY_RIGHT(
             18,
@@ -45,7 +51,10 @@ public enum AbilityTreeConnectionType {
                     new boolean[] {true, true, false, false}, 20,
                     new boolean[] {false, true, true, false}, 21,
                     new boolean[] {true, false, true, false}, 22),
-            List.of()),
+            List.of(
+                    Pair.of(VERTICAL, DOWN_RIGHT_TURN),
+                    Pair.of(VERTICAL, UP_RIGHT_TURN),
+                    Pair.of(DOWN_LEFT_TURN, UP_LEFT_TURN))),
     THREE_WAY_DOWN(
             23,
             Map.of(
@@ -54,9 +63,9 @@ public enum AbilityTreeConnectionType {
                     new boolean[] {false, true, true, false}, 26,
                     new boolean[] {false, true, false, true}, 27),
             List.of(
-                    Pair.of(HORIZONTAL, UP_LEFT_TURN),
-                    Pair.of(HORIZONTAL, UP_RIGHT_TURN),
-                    Pair.of(UP_LEFT_TURN, UP_RIGHT_TURN))),
+                    Pair.of(HORIZONTAL, DOWN_LEFT_TURN),
+                    Pair.of(HORIZONTAL, DOWN_RIGHT_TURN),
+                    Pair.of(DOWN_LEFT_TURN, DOWN_RIGHT_TURN))),
     THREE_WAY_LEFT(
             28,
             Map.of(
@@ -67,7 +76,7 @@ public enum AbilityTreeConnectionType {
             List.of(
                     Pair.of(VERTICAL, DOWN_LEFT_TURN),
                     Pair.of(VERTICAL, UP_LEFT_TURN),
-                    Pair.of(DOWN_LEFT_TURN, UP_LEFT_TURN))),
+                    Pair.of(DOWN_RIGHT_TURN, UP_RIGHT_TURN))),
 
     FOUR_WAY(
             1,
@@ -88,13 +97,26 @@ public enum AbilityTreeConnectionType {
                     Pair.of(DOWN_LEFT_TURN, UP_RIGHT_TURN),
                     Pair.of(DOWN_RIGHT_TURN, UP_LEFT_TURN),
                     Pair.of(VERTICAL, THREE_WAY_UP),
+                    Pair.of(DOWN_LEFT_TURN, THREE_WAY_UP),
+                    Pair.of(DOWN_RIGHT_TURN, THREE_WAY_UP),
                     Pair.of(HORIZONTAL, THREE_WAY_RIGHT),
+                    Pair.of(DOWN_LEFT_TURN, THREE_WAY_RIGHT),
+                    Pair.of(UP_LEFT_TURN, THREE_WAY_RIGHT),
                     Pair.of(VERTICAL, THREE_WAY_DOWN),
-                    Pair.of(HORIZONTAL, THREE_WAY_LEFT)));
+                    Pair.of(UP_LEFT_TURN, THREE_WAY_DOWN),
+                    Pair.of(UP_RIGHT_TURN, THREE_WAY_DOWN),
+                    Pair.of(HORIZONTAL, THREE_WAY_LEFT),
+                    Pair.of(DOWN_RIGHT_TURN, THREE_WAY_LEFT),
+                    Pair.of(UP_RIGHT_TURN, THREE_WAY_LEFT)));
 
     private final int baseDamage;
     private final Map<boolean[], Integer> activeDamageMap; // boolean[] is {up, right, down, left}
+
+    // This is a list of all possible merges between two AbilityTreeConnectionTypes. Self merges (this type + other
+    // type) are not included.
     private final List<Pair<AbilityTreeConnectionType, AbilityTreeConnectionType>> possibleMerges;
+    // A set of compatible types. If this type is merged with a type in this set, the result will be this type.
+    private final Set<AbilityTreeConnectionType> selfMergeTypes;
 
     private final ItemStack baseItemStack;
     private final Map<Integer, ItemStack> itemStackMap;
@@ -112,6 +134,13 @@ public enum AbilityTreeConnectionType {
         this.baseItemStack = generateItemStack(baseDamage);
         for (boolean[] active : activeDamageMap.keySet()) {
             this.itemStackMap.put(Arrays.hashCode(active), generateItemStack(activeDamageMap.get(active)));
+        }
+
+        this.selfMergeTypes = new HashSet<>();
+
+        for (Pair<AbilityTreeConnectionType, AbilityTreeConnectionType> merge : possibleMerges) {
+            selfMergeTypes.add(merge.a());
+            selfMergeTypes.add(merge.b());
         }
     }
 
@@ -136,6 +165,16 @@ public enum AbilityTreeConnectionType {
             return first;
         }
 
+        // If the second type is compatible, the result is the first type
+        if (first.selfMergeTypes.contains(second)) {
+            return first;
+        }
+
+        // If the first type is compatible, the result is the second type
+        if (second.selfMergeTypes.contains(first)) {
+            return second;
+        }
+
         // Swap the two variables so they are in order
         if (second.ordinal() < first.ordinal()) {
             AbilityTreeConnectionType temp = first;
@@ -152,7 +191,9 @@ public enum AbilityTreeConnectionType {
             }
         }
 
-        throw new IllegalStateException(
+        WynntilsMod.error(
                 "Tried to merge two incompatbilty AbilityTreeConnectionTypes: " + first + " and " + second + ".");
+
+        return first;
     }
 }
