e1c5d36cc4a886e241afa3e431dcfff28e42bcb9
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-05-21 22:29:12 -0700

    use styledtext for replacements

--- common/src/main/java/com/wynntils/features/chat/GuildRankReplacementFeature.java
+++ common/src/main/java/com/wynntils/features/chat/GuildRankReplacementFeature.java
@@ -9,11 +9,15 @@ import com.wynntils.core.config.Config;
 import com.wynntils.core.config.ConfigCategory;
 import com.wynntils.core.config.RegisterConfig;
 import com.wynntils.core.features.Feature;
+import com.wynntils.core.text.PartStyle;
 import com.wynntils.core.text.StyledText;
+import com.wynntils.core.text.StyledTextPart;
 import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
+import com.wynntils.utils.type.IterationDecision;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Style;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -29,29 +33,61 @@ public class GuildRankReplacementFeature extends Feature {
     public void onChatMessageReceived(ChatMessageReceivedEvent e) {
         if (rankType.get() == RankType.STARS) return; // default wynn behavior is stars
 
-        Matcher m = RANK_STARS_PATTERN.matcher(
-                StyledText.fromComponent(e.getMessage()).getString());
+        StyledText styledText = e.getStyledText();
+
+        Matcher m = styledText.getMatcher(RANK_STARS_PATTERN);
         if (!m.find()) return;
 
         if (rankType.get() == RankType.NONE) {
-            e.setMessage(StyledText.fromString(m.replaceFirst("$1")).getComponent());
+
+            StyledText modified = styledText.iterate((part, changes) -> {
+                if (part.getString(null, PartStyle.StyleType.NONE).contains("★")) {
+                    changes.remove(part);
+                }
+                return IterationDecision.CONTINUE;
+            });
+            e.setMessage(modified.getComponent());
             return;
         }
 
-        String rank = m.group(2);
-        int stars = (int) rank.chars().filter(c -> c == '★').count();
-        String rankName = "";
-        switch (stars) {
-            case 0 -> rankName = ChatFormatting.AQUA + "Recruit "
-                    + ChatFormatting.DARK_AQUA; // Recruit doesn't come with color codes
-            case 1 -> rankName = "Recruiter ";
-            case 2 -> rankName = "Captain ";
-            case 3 -> rankName = "Strategist ";
-            case 4 -> rankName = "Chief ";
-            case 5 -> rankName = "Owner ";
-        }
-        e.setMessage(
-                StyledText.fromString(m.replaceFirst("$1" + rankName + "$3")).getComponent());
+        // rankType == RankType.NAME
+        StyledText modified = styledText.iterateBackwards((part, changes) -> {
+            int stars = (int) part.getString(null, PartStyle.StyleType.NONE)
+                    .chars()
+                    .filter(c -> c == '★')
+                    .count();
+            if (stars > 0) { // This is the part to replace
+                changes.remove(part);
+
+                String rankName = "";
+                switch (stars) {
+                    case 1 -> rankName = "Recruiter ";
+                    case 2 -> rankName = "Captain ";
+                    case 3 -> rankName = "Strategist ";
+                    case 4 -> rankName = "Chief ";
+                    case 5 -> rankName = "Owner ";
+                }
+                changes.add(new StyledTextPart(rankName, part.getPartStyle().getStyle(), null, Style.EMPTY));
+                return IterationDecision.BREAK;
+            }
+
+            if (part.getString(null, PartStyle.StyleType.NONE).contains("[")) {
+                // We've already gone through the rest of the string and did not find any stars, this must be a recruit
+                // Since the [ and the username are the same part, we need to do some surgery:
+                // Find username from the original part, then remove it, and add our new reconstructed one
+                String username = part.getString(null, PartStyle.StyleType.NONE).substring(1);
+                changes.remove(part);
+                changes.add(new StyledTextPart(
+                        "[" + ChatFormatting.AQUA + "Recruit " + ChatFormatting.DARK_AQUA + username,
+                        part.getPartStyle().getStyle(),
+                        null,
+                        Style.EMPTY));
+                return IterationDecision.BREAK;
+            }
+
+            return IterationDecision.CONTINUE;
+        });
+        e.setMessage(modified.getComponent());
     }
 
     private enum RankType {
