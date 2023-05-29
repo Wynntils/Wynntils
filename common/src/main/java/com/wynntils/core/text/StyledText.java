4cecf5d62fbee51131d27d3991514f6032490ca6
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-05-29 19:31:00 +0200

    fix: fix typo in comment

--- common/src/main/java/com/wynntils/core/text/StyledText.java
+++ common/src/main/java/com/wynntils/core/text/StyledText.java
@@ -457,7 +457,7 @@ public final class StyledText implements Iterable<StyledTextPart> {
 
     /**
      * Replaces all occurrences of the given regex with the given replacement.
-     * <p> Note that {@link PartStyle.StyleType.NONE} is used when matching replacing.
+     * <p> Note that {@link PartStyle.StyleType.NONE} is used when matching and replacing.
      * @param regex the regex to replace
      * @param replacement the replacement
      * @return the new {@link StyledText}
