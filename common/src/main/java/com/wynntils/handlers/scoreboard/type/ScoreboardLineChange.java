2c7a0bc8f1696f0316b3ee004f5484ce34c5a31c
Mcrtin <66840021+Mcrtin@users.noreply.github.com>
2023-06-04 13:15:21 +0200

    refactor: Port ScoreboardLineChange

--- common/src/main/java/com/wynntils/handlers/scoreboard/type/ScoreboardLineChange.java
+++ common/src/main/java/com/wynntils/handlers/scoreboard/type/ScoreboardLineChange.java
@@ -4,7 +4,7 @@
  */
 package com.wynntils.handlers.scoreboard.type;
 
-import com.wynntils.core.text.CodedString;
+import com.wynntils.core.text.StyledText;
 import net.minecraft.server.ServerScoreboard;
 
-public record ScoreboardLineChange(CodedString lineText, ServerScoreboard.Method method, int lineIndex) {}
+public record ScoreboardLineChange(StyledText lineText, ServerScoreboard.Method method, int lineIndex) {}
