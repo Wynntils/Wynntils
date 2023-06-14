d520b9ddbca96401a068622a9b3c57faa9a025e6
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-06-14 19:51:28 +0200

    fix: add feature category

--- common/src/main/java/com/wynntils/features/map/GatheringNodeCooldownFeature.java
+++ common/src/main/java/com/wynntils/features/map/GatheringNodeCooldownFeature.java
@@ -4,10 +4,13 @@
  */
 package com.wynntils.features.map;
 
+import com.wynntils.core.config.Category;
+import com.wynntils.core.config.ConfigCategory;
 import com.wynntils.core.features.Feature;
 import com.wynntils.models.profession.event.ProfessionNodeGatheredEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+@ConfigCategory(Category.MAP)
 public class GatheringNodeCooldownFeature extends Feature {
     @SubscribeEvent
     public void onNodeGathered(ProfessionNodeGatheredEvent.LabelShown event) {
