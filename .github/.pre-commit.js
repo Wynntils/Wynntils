25f5cded8a0989f6a78c9089f6f73fd8274a08e8
Scyu_ <scyu@scyu.dev>
2023-06-30 22:44:21 +0100

    fix: fix snapshot jar name

--- .github/.pre-commit.js
+++ .github/.pre-commit.js
@@ -12,6 +12,6 @@ exports.preCommit = (props) => {
     // replace only the version string example:
     // version = "0.0.3-alpha.2"
     // version = "0.0.3-alpha.103+MC-1.19.4"
-    replace("./build.gradle", /(?<=version = ")\d+\.\d+\.\d+(-\w+\.\d+)?(\+MC-\d\.\d+\.\d+)?(?=")/g, props.version);
+    replace("./build.gradle", /(?<=version = ")\d+\.\d+\.\d+(-\w+\.\d+)?(\+MC-\d\.\d+\.\d+)?(-SNAPSHOT)?(?=")/g, props.version);
     // Regex provided by Github Copilot
 };
