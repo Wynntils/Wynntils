20835b31bf35807697e93cfc8e9eef842e84a26a
Mcrtin <66840021+Mcrtin@users.noreply.github.com>
2023-06-30 11:24:36 +0200

    expand wildcard import and apply spotless

--- common/src/main/java/com/wynntils/mc/mixin/PlayerTabOverlayMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/PlayerTabOverlayMixin.java
@@ -1,9 +1,11 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
 package com.wynntils.mc.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.wynntils.core.components.Manager;
 import com.wynntils.core.components.Managers;
-import com.wynntils.core.features.FeatureManager;
 import com.wynntils.features.ui.PlayerInfoFeature;
 import net.minecraft.client.gui.components.PlayerTabOverlay;
 import net.minecraft.world.scores.Objective;
@@ -18,7 +20,6 @@ public class PlayerTabOverlayMixin {
 
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private void render(PoseStack poseStack, int width, Scoreboard scoreboard, Objective objective, CallbackInfo ci) {
-        if (Managers.Feature.getFeatureInstance(PlayerInfoFeature.class).isEnabled())
-            ci.cancel();
+        if (Managers.Feature.getFeatureInstance(PlayerInfoFeature.class).isEnabled()) ci.cancel();
     }
 }
