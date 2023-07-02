28289d2f37fd072bcb271514c2467317b93ba4e3
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-07-02 11:35:09 +0200

    chore: remove empty line

--- common/src/main/java/com/wynntils/features/inventory/InventoryEmeraldCountFeature.java
+++ common/src/main/java/com/wynntils/features/inventory/InventoryEmeraldCountFeature.java
@@ -112,7 +112,6 @@ public class InventoryEmeraldCountFeature extends Feature {
     }
 
     private void renderTextCount(PoseStack poseStack, int x, int y, int emeralds) {
-
         final HorizontalAlignment emeraldTextAlignment =
                 textDisplaySide.get() == TextDisplaySide.LEFT ? HorizontalAlignment.LEFT : HorizontalAlignment.RIGHT;
         final int emeraldTextOffsetX = textDisplaySide.get() == TextDisplaySide.LEFT ? 1 : -1;
