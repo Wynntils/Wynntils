23a110e0914bbbbc1b2bf3b907e23516d9540f03
Magnus Ihse Bursie <mag@icus.se>
2023-07-05 23:51:14 +0200

    fix: i18n problems with content tracker title etc (#1686)
    * fix: i18n problems with content tracker title etc
    
    * ci: spotless formatting
    
    * Fix merge error
    
    ---------
    
    Co-authored-by: magicus <magicus@users.noreply.github.com>

--- common/src/main/java/com/wynntils/features/overlays/ContentTrackerOverlayFeature.java
+++ common/src/main/java/com/wynntils/features/overlays/ContentTrackerOverlayFeature.java
@@ -78,10 +78,12 @@ public class ContentTrackerOverlayFeature extends Feature {
 
             toRenderPreview
                     .get(0)
-                    .setText(I18n.get("feature.wynntils.contentTrackerOverlay.overlay.tracker.title") + " Quest:");
+                    .setText(I18n.get("feature.wynntils.contentTrackerOverlay.overlay.contentTracker.title")
+                            + " Quest:");
             toRenderPreview
                     .get(1)
-                    .setText(I18n.get("feature.wynntils.contentTrackerOverlay.overlay.tracker.testQuestName") + ":");
+                    .setText(I18n.get("feature.wynntils.contentTrackerOverlay.overlay.contentTracker.testQuestName")
+                            + ":");
             toRenderPreview
                     .get(2)
                     .setText(
@@ -130,7 +132,7 @@ public class ContentTrackerOverlayFeature extends Feature {
             }
 
             toRender.get(0)
-                    .setText(I18n.get("feature.wynntils.contentTrackerOverlay.overlay.tracker.title") + " "
+                    .setText(I18n.get("feature.wynntils.contentTrackerOverlay.overlay.contentTracker.title") + " "
                             + Models.Content.getTrackedType() + ":");
             toRender.get(1).setText(Models.Content.getTrackedName());
             toRender.get(2).setText(Models.Content.getTrackedTask());
