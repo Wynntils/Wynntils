2509e238b26c409d77247fef33be1d09a0709793
Halcyon <@>
2023-07-06 04:06:18 +0800

    feat: render lootrun path on map

--- common/src/main/java/com/wynntils/utils/render/MapRenderer.java
+++ common/src/main/java/com/wynntils/utils/render/MapRenderer.java
@@ -384,7 +384,6 @@ public final class MapRenderer {
     }
 
     private static float signedArea(Vector3f p0, Vector3f p1, Vector3f p2) {
-
         return (p1.x() - p0.x()) * (p2.y() - p0.y()) - (p2.x() - p0.x()) * (p1.y() - p0.y());
     }
 
