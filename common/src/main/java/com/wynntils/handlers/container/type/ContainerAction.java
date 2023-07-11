8fb171e34e3487b5a555b40b102593d09977b66f
Magnus Ihse Bursie <mag@icus.se>
2023-07-11 22:07:48 +0200

    Extract simple types

---
+++ common/src/main/java/com/wynntils/handlers/container/type/ContainerAction.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under AGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.handlers.container.type;
+
+@FunctionalInterface
+public interface ContainerAction {
+    void processContainer(ContainerContent container);
+}
