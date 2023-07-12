3fef50aa9e93054dc2572b1cbc1d111fb964fda8
Magnus Ihse Bursie <mag@icus.se>
2023-07-13 00:49:20 +0200

    refactor: Use named colors instead of hard-coded color codes (#1723)

--- common/src/main/java/com/wynntils/models/content/type/ContentType.java
+++ common/src/main/java/com/wynntils/models/content/type/ContentType.java
@@ -4,23 +4,25 @@
  */
 package com.wynntils.models.content.type;
 
+import net.minecraft.ChatFormatting;
+
 public enum ContentType {
-    QUEST("Quest", "b"),
-    STORYLINE_QUEST("Quest", "a"),
-    MINI_QUEST("Mini-Quest", "5"),
-    CAVE("Cave", "6"),
-    SECRET_DISCOVERY("Secret Discovery", "b"),
-    WORLD_DISCOVERY("World Discovery", "e"),
-    TERRITORIAL_DISCOVERY("Territorial Discovery", "f"),
-    DUNGEON("Dungeon", "c"),
-    RAID("Raid", "e"),
-    BOSS_ALTAR("Boss Altar", "d"),
-    LOOTRUN_CAMP("Lootrun Camp", "9");
+    QUEST("Quest", ChatFormatting.AQUA.getChar()),
+    STORYLINE_QUEST("Quest", ChatFormatting.GREEN.getChar()),
+    MINI_QUEST("Mini-Quest", ChatFormatting.DARK_PURPLE.getChar()),
+    CAVE("Cave", ChatFormatting.GOLD.getChar()),
+    SECRET_DISCOVERY("Secret Discovery", ChatFormatting.AQUA.getChar()),
+    WORLD_DISCOVERY("World Discovery", ChatFormatting.YELLOW.getChar()),
+    TERRITORIAL_DISCOVERY("Territorial Discovery", ChatFormatting.WHITE.getChar()),
+    DUNGEON("Dungeon", ChatFormatting.RED.getChar()),
+    RAID("Raid", ChatFormatting.YELLOW.getChar()),
+    BOSS_ALTAR("Boss Altar", ChatFormatting.LIGHT_PURPLE.getChar()),
+    LOOTRUN_CAMP("Lootrun Camp", ChatFormatting.BLUE.getChar());
 
     private final String displayName;
-    private final String colorCode;
+    private final char colorCode;
 
-    ContentType(String displayName, String colorCode) {
+    ContentType(String displayName, char colorCode) {
         this.displayName = displayName;
         this.colorCode = colorCode;
     }
@@ -38,6 +40,6 @@ public enum ContentType {
     }
 
     public String getColorCode() {
-        return colorCode;
+        return String.valueOf(colorCode);
     }
 }
