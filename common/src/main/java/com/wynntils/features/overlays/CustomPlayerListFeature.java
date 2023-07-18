b895a83182b4dd1fa29bcb3d475ce286a53c846a
Mcrtin <66840021+Mcrtin@users.noreply.github.com>
2023-07-18 15:38:31 +0200

    Use Instant and Duration instead of System.currentTimeMillis(), extract animation progress to an inner class, extract execution limiting behavior to an inner class, fix double roll on the right side of the overlay

--- common/src/main/java/com/wynntils/features/overlays/CustomPlayerListFeature.java
+++ common/src/main/java/com/wynntils/features/overlays/CustomPlayerListFeature.java
@@ -27,9 +27,12 @@ import com.wynntils.utils.render.Texture;
 import com.wynntils.utils.render.type.HorizontalAlignment;
 import com.wynntils.utils.render.type.TextShadow;
 import com.wynntils.utils.render.type.VerticalAlignment;
-import java.util.ArrayList;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Supplier;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.PlayerTabOverlay;
 import net.minecraft.client.multiplayer.PlayerInfo;
@@ -63,8 +66,10 @@ public class CustomPlayerListFeature extends Feature {
         @RegisterConfig
         public final Config<Integer> openingDuration = new Config<>(125);
 
-        private double animationProgress = 0;
-        private long lastTime = -1;
+        private final AnimationPercentage animationPercentage = new AnimationPercentage(
+                McUtils.options().keyPlayerList::isDown, Duration.of(openingDuration.get(), ChronoUnit.MILLIS));
+        private final TimelimitedExecutioner<List<StyledText>> availablePlayers =
+                new TimelimitedExecutioner<>(CustomPlayerListOverlay::getAvailablePlayers, Duration.ofMillis(250));
 
         protected CustomPlayerListOverlay() {
             super(
@@ -78,17 +83,10 @@ public class CustomPlayerListFeature extends Feature {
                     Texture.PLAYER_INFO_OVERLAY.height());
         }
 
-        private List<StyledText> lastPlayers = new ArrayList<>();
-        private long nextExecution = 0;
-
-        private List<StyledText> getAvailablePlayers() {
-            if (System.currentTimeMillis() < nextExecution) return lastPlayers;
-
-            nextExecution = System.currentTimeMillis() + 250;
-
+        private static List<StyledText> getAvailablePlayers() {
             PlayerTabOverlay defaultTabList = McUtils.mc().gui.getTabList();
 
-            lastPlayers = McUtils.player().connection.getListedOnlinePlayers().stream()
+            return McUtils.player().connection.getListedOnlinePlayers().stream()
                     .sorted(PLAYER_INFO_COMPARATOR)
                     .limit(80)
                     .map(defaultTabList::getNameForDisplay)
@@ -100,16 +98,18 @@ public class CustomPlayerListFeature extends Feature {
                             styledText.replace(ChatFormatting.GRAY.toString(), ChatFormatting.BLACK.toString()))
                     .map(StyledText::fromString)
                     .toList();
-
-            return lastPlayers;
         }
 
         @Override
         public void render(PoseStack poseStack, MultiBufferSource bufferSource, float partialTicks, Window window) {
-            if (!McUtils.options().keyPlayerList.isDown() && animationProgress <= 0.0) return;
-            double animation = getAnimation();
+            if (!McUtils.options().keyPlayerList.isDown() && animationPercentage.finishedClosingAnimation()) return;
+            renderPlayerList(poseStack, animationPercentage.getAnimation());
+        }
 
-            renderPlayerList(poseStack, animation);
+        @Override
+        public void renderPreview(
+                PoseStack poseStack, MultiBufferSource bufferSource, float partialTicks, Window window) {
+            renderPlayerList(poseStack, 1);
         }
 
         private void renderPlayerList(PoseStack poseStack, double animation) {
@@ -133,7 +133,7 @@ public class CustomPlayerListFeature extends Feature {
             currentDist += DISTANCE_BETWEEN_CATEGORIES;
             renderCategoryTitle(poseStack, "Guild", currentDist, categoryStart);
 
-            renderPlayerNames(poseStack, categoryStart, getAvailablePlayers());
+            renderPlayerNames(poseStack, categoryStart, availablePlayers.get());
 
             if (animation < 1) {
                 RenderUtils.disableScissor();
@@ -141,13 +141,7 @@ public class CustomPlayerListFeature extends Feature {
 
             float middle = getRenderX() + HALF_WIDTH + ROLL_WIDTH;
             renderRoll(poseStack, (float) (middle - ROLL_WIDTH + 2 - HALF_WIDTH * animation));
-            renderRoll(poseStack, (float) (middle + HALF_WIDTH * animation));
-        }
-
-        @Override
-        public void renderPreview(
-                PoseStack poseStack, MultiBufferSource bufferSource, float partialTicks, Window window) {
-            renderPlayerList(poseStack, 1);
+            renderRoll(poseStack, (float) (middle - 2 + HALF_WIDTH * animation));
         }
 
         private void renderRoll(PoseStack poseStack, float xPos) {
@@ -207,34 +201,89 @@ public class CustomPlayerListFeature extends Feature {
                     getRenderX() + ROLL_WIDTH,
                     getRenderY(),
                     0,
-                    Texture.PLAYER_INFO_OVERLAY.width(),
+                    Texture.PLAYER_INFO_OVERLAY.width() - ROLL_WIDTH,
                     Texture.PLAYER_INFO_OVERLAY.height(),
                     ROLL_WIDTH,
                     0,
-                    Texture.PLAYER_INFO_OVERLAY.width(),
+                    Texture.PLAYER_INFO_OVERLAY.width() - ROLL_WIDTH,
                     Texture.PLAYER_INFO_OVERLAY.height(),
                     Texture.PLAYER_INFO_OVERLAY.width(),
                     Texture.PLAYER_INFO_OVERLAY.height());
         }
 
-        private double getAnimation() {
-            if (openingDuration.get() <= 0) return 1;
-            if (lastTime == -1) {
-                lastTime = System.currentTimeMillis() - 1;
-            }
+        @Override
+        protected void onConfigUpdate(ConfigHolder configHolder) {
+            animationPercentage.setOpeningDuration(Duration.of(openingDuration.get(), ChronoUnit.MILLIS));
+        }
+    }
 
-            if (McUtils.options().keyPlayerList.isDown()) {
-                animationProgress += (System.currentTimeMillis() - lastTime) / (double) openingDuration.get();
-                animationProgress = Math.min(1, animationProgress);
-            } else if (animationProgress > 0.0) {
-                animationProgress -= (System.currentTimeMillis() - lastTime) / (double) openingDuration.get();
-                animationProgress = Math.max(0, animationProgress);
-            }
-            lastTime = animationProgress <= 0.0 ? -1 : System.currentTimeMillis();
-            return Math.sin((float) (animationProgress * 1f / 2f * Math.PI));
+    private static final class AnimationPercentage {
+        private final Supplier<Boolean> openingContitions;
+        private Duration openingDuration;
+        private Instant lastTime = Instant.EPOCH;
+
+        private double openingProgress;
+
+        private AnimationPercentage(Supplier<Boolean> openingContitions, Duration openingDuration) {
+            this.openingContitions = openingContitions;
+            this.openingDuration = openingDuration;
         }
 
-        @Override
-        protected void onConfigUpdate(ConfigHolder configHolder) {}
+        private double getAnimation() {
+            if (openingDuration.isZero() || openingDuration.isNegative()) return 1;
+            if (openingProgress == 0) {
+                lastTime = Instant.now().minus(1, ChronoUnit.MILLIS);
+            }
+
+            if (openingContitions.get()) {
+                addOpeningProgress(
+                        Duration.between(lastTime, Instant.now()).toMillis() / ((double) openingDuration.toMillis()));
+            } else if (openingProgress > 0) {
+                addOpeningProgress(
+                        -Duration.between(lastTime, Instant.now()).toMillis() / ((double) openingDuration.toMillis()));
+            }
+            lastTime = Instant.now();
+            return applyTransformation(openingProgress);
+        }
+
+        public void addOpeningProgress(double openingProgress) {
+            setOpeningProgress(this.openingProgress + openingProgress);
+        }
+
+        public void setOpeningProgress(double openingProgress) {
+            this.openingProgress = Math.clamp(0, 1, openingProgress);
+        }
+
+        private static double applyTransformation(double openingProgress) {
+            return java.lang.Math.sin((float) (openingProgress / 2f * java.lang.Math.PI));
+        }
+
+        public void setOpeningDuration(Duration openingDuration) {
+            this.openingDuration = openingDuration;
+        }
+
+        public boolean finishedClosingAnimation() {
+            return openingProgress == 0;
+        }
+    }
+
+    private static final class TimelimitedExecutioner<T> {
+
+        private final Supplier<T> method;
+        private final Duration cooldown;
+        private T current;
+        private Instant lastExecutionTime = Instant.EPOCH;
+
+        private TimelimitedExecutioner(Supplier<T> method, Duration cooldown) {
+            this.method = method;
+            this.cooldown = cooldown;
+        }
+
+        public T get() {
+            Duration difference = Duration.between(lastExecutionTime, Instant.now());
+            if (difference.minus(cooldown).isNegative()) return current;
+            lastExecutionTime = Instant.now();
+            return current = method.get();
+        }
     }
 }
