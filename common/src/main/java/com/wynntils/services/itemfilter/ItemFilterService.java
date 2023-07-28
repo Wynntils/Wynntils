cb18ce0287b7f7091234dd0ff4336ef569709697
Etrenak <Etrenak@users.noreply.github.com>
2023-07-28 18:27:08 +0000

    ci: spotless formatting

--- common/src/main/java/com/wynntils/services/itemfilter/ItemFilterService.java
+++ common/src/main/java/com/wynntils/services/itemfilter/ItemFilterService.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import net.minecraft.client.resources.language.I18n;
 
 public class ItemFilterService extends Service {
     private final Map<String, Function<String, ? extends ItemFilter>> filterSuppliers = new HashMap<>();
