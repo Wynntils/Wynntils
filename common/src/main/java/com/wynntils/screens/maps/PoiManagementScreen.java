c9c665cafac6ab477b0cfe5fb4dedc7a61cbe33d
Jamie <JamCal117@gmail.com>
2023-07-31 18:22:01 +0100

    feat: Export waypoints

--- common/src/main/java/com/wynntils/screens/maps/PoiManagementScreen.java
+++ common/src/main/java/com/wynntils/screens/maps/PoiManagementScreen.java
@@ -21,6 +21,7 @@ import com.wynntils.utils.render.type.TextShadow;
 import com.wynntils.utils.render.type.VerticalAlignment;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
@@ -31,10 +32,11 @@ import net.minecraft.network.chat.Component;
 
 public final class PoiManagementScreen extends WynntilsScreen {
     private final MainMapScreen oldMapScreen;
+
     private Button nextButton;
     private Button previousButton;
     private Button undoDeleteButton;
-    private Button importButton;
+
     private List<CustomPoi> waypoints;
     private int pageHeight;
     private int currentPage;
@@ -73,13 +75,13 @@ public final class PoiManagementScreen extends WynntilsScreen {
 
         this.addRenderableWidget(
                 nextButton = new Button.Builder(Component.literal(">"), (button) -> nextPage())
-                        .pos(this.width / 2 + 2, this.height - 45)
+                        .pos(this.width / 2, this.height - 45)
                         .size(20, 20)
                         .build());
 
         this.addRenderableWidget(
                 previousButton = new Button.Builder(Component.literal("<"), (button) -> previousPage())
-                        .pos(this.width / 2 - 22, this.height - 45)
+                        .pos(this.width / 2 - 20, this.height - 45)
                         .size(20, 20)
                         .build());
 
@@ -95,20 +97,29 @@ public final class PoiManagementScreen extends WynntilsScreen {
                         .size(font.width(Component.translatable("screens.wynntils.poiManagementGui.undo")) + 15, 20)
                         .build());
 
-        this.addRenderableWidget(
-                importButton = new Button.Builder(
-                                Component.translatable("screens.wynntils.poiManagementGui.import"),
-                                (button) -> importFromClipboard())
-                        .pos(
-                                this.width / 2
-                                        + 75
-                                        - font.width(
-                                                Component.translatable("screens.wynntils.poiManagementGui.import")),
-                                this.height - 45)
-                        .size(font.width(Component.translatable("screens.wynntils.poiManagementGui.import")) + 15, 20)
-                        .tooltip(Tooltip.create(
-                                Component.translatable("screens.wynntils.poiManagementGui.import.tooltip")))
-                        .build());
+        this.addRenderableWidget(new Button.Builder(
+                        Component.translatable("screens.wynntils.poiManagementGui.import"),
+                        (button) -> importFromClipboard())
+                .pos(
+                        this.width / 2
+                                + 75
+                                - font.width(Component.translatable("screens.wynntils.poiManagementGui.import")),
+                        this.height - 45)
+                .size(font.width(Component.translatable("screens.wynntils.poiManagementGui.import")) + 15, 20)
+                .tooltip(Tooltip.create(Component.translatable("screens.wynntils.poiManagementGui.import.tooltip")))
+                .build());
+
+        this.addRenderableWidget(new Button.Builder(
+                        Component.translatable("screens.wynntils.poiManagementGui.export"),
+                        (button) -> exportToClipboard())
+                .pos(
+                        this.width / 2
+                                - 75
+                                - font.width(Component.translatable("screens.wynntils.poiManagementGui.export")),
+                        this.height - 45)
+                .size(font.width(Component.translatable("screens.wynntils.poiManagementGui.export")) + 15, 20)
+                .tooltip(Tooltip.create(Component.translatable("screens.wynntils.poiManagementGui.export.tooltip")))
+                .build());
 
         if (deletedIndexes.isEmpty()) {
             undoDeleteButton.active = false;
@@ -134,27 +145,48 @@ public final class PoiManagementScreen extends WynntilsScreen {
         try {
             customPois = Managers.Json.GSON.fromJson(clipboard, CustomPoi[].class);
         } catch (JsonSyntaxException e) {
-            importButton.setTooltip(
-                    Tooltip.create(Component.translatable("screens.wynntils.poiManagementGui.import.error")));
+            McUtils.sendErrorToClient(I18n.get("screens.wynntils.poiManagementGui.import.error"));
             return;
         }
 
         if (customPois == null) {
-            importButton.setTooltip(
-                    Tooltip.create(Component.translatable("screens.wynntils.poiManagementGui.import.error")));
+            McUtils.sendErrorToClient(I18n.get("screens.wynntils.poiManagementGui.import.error"));
             return;
         }
 
-        Managers.Feature.getFeatureInstance(MainMapFeature.class)
+        List<CustomPoi> existingPois = Managers.Feature.getFeatureInstance(MainMapFeature.class)
                 .customPois
-                .get()
-                .addAll(List.of(customPois));
+                .get();
+
+        List<CustomPoi> poisToAdd = Stream.of(customPois)
+                .filter(newPoi -> !existingPois.contains(newPoi))
+                .toList();
+
+        existingPois.addAll(poisToAdd);
         Managers.Config.saveConfig();
 
         populatePois();
 
-        importButton.setTooltip(Tooltip.create(
-                Component.translatable("screens.wynntils.poiManagementGui.import.success", customPois.length)));
+        McUtils.sendMessageToClient(
+                Component.translatable("screens.wynntils.poiManagementGui.import.success", poisToAdd.size())
+                        .withStyle(ChatFormatting.GREEN));
+    }
+
+    private void exportToClipboard() {
+        McUtils.mc()
+                .keyboardHandler
+                .setClipboard(Managers.Feature.getFeatureInstance(MainMapFeature.class).customPois.get().stream()
+                        .map(Managers.Json.GSON::toJson)
+                        .toList()
+                        .toString());
+
+        McUtils.sendMessageToClient(Component.translatable(
+                        "screens.wynntils.poiManagementGui.exportedWaypoints",
+                        Managers.Feature.getFeatureInstance(MainMapFeature.class)
+                                .customPois
+                                .get()
+                                .size())
+                .withStyle(ChatFormatting.GREEN));
     }
 
     @Override
