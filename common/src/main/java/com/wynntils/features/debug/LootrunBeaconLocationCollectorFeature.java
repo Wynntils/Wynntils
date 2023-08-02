30a19a1005d60389ec82587d1df13d7e69aaeb2e
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-08-02 15:16:41 +0200

    fix: fix merge

--- common/src/main/java/com/wynntils/features/debug/LootrunBeaconLocationCollectorFeature.java
+++ common/src/main/java/com/wynntils/features/debug/LootrunBeaconLocationCollectorFeature.java
@@ -4,7 +4,6 @@
  */
 package com.wynntils.features.debug;
 
-import com.google.common.collect.ComparisonChain;
 import com.wynntils.core.components.Models;
 import com.wynntils.core.config.Category;
 import com.wynntils.core.config.ConfigCategory;
@@ -16,7 +15,7 @@ import com.wynntils.models.beacons.type.Beacon;
 import com.wynntils.models.lootrun.event.LootrunBeaconSelectedEvent;
 import com.wynntils.models.lootrun.type.LootrunLocation;
 import com.wynntils.models.lootrun.type.LootrunTaskType;
-import com.wynntils.utils.mc.type.Location;
+import com.wynntils.models.lootrun.type.TaskLocation;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -47,17 +46,5 @@ public class LootrunBeaconLocationCollectorFeature extends Feature {
         tasks.get().putIfAbsent(currentLocationOpt.get(), new TreeSet<>());
         tasks.get().get(currentLocationOpt.get()).add(new TaskLocation(beacon.location(), currentTaskTypeOpt.get()));
         tasks.touched();
-    }
-
-    private record TaskLocation(Location location, LootrunTaskType taskType) implements Comparable<TaskLocation> {
-        @Override
-        public int compareTo(LootrunBeaconLocationCollectorFeature.TaskLocation taskLocation) {
-            return ComparisonChain.start()
-                    .compare(location.x(), taskLocation.location.x())
-                    .compare(location.y(), taskLocation.location.y())
-                    .compare(location.z(), taskLocation.location.z())
-                    .compare(taskType, taskLocation.taskType)
-                    .result();
-        }
     }
 }
--- common/src/main/java/com/wynntils/models/lootrun/type/TaskLocation.java
+++ common/src/main/java/com/wynntils/models/lootrun/type/TaskLocation.java
@@ -7,7 +7,6 @@ package com.wynntils.models.lootrun.type;
 import com.google.common.collect.ComparisonChain;
 import com.wynntils.utils.mc.type.Location;
 
-// This location has to be a Location because Position doesn't have proper mapping, so GSON can't serialize it.
 public record TaskLocation(Location location, LootrunTaskType taskType) implements Comparable<TaskLocation> {
     @Override
     public int compareTo(TaskLocation taskLocation) {
