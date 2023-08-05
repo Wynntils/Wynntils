201b5b6abeef741f6a4fdd340c97118d0b868bf2
Etrenak <Etrenak@users.noreply.github.com>
2023-08-05 16:47:14 +0000

    ci: spotless formatting

--- common/src/main/java/com/wynntils/features/inventory/ContainerSearchFeature.java
+++ common/src/main/java/com/wynntils/features/inventory/ContainerSearchFeature.java
@@ -242,7 +242,8 @@ public class ContainerSearchFeature extends Feature {
             helpTooltip.add(Component.empty());
             helpTooltip.add(Component.literal(factory.getKeyword() + ":")
                     .withStyle(ChatFormatting.YELLOW)
-                    .append(Component.translatable(factory.getI18nKey() + ".usage").withStyle(ChatFormatting.GRAY)));
+                    .append(Component.translatable(factory.getI18nKey() + ".usage")
+                            .withStyle(ChatFormatting.GRAY)));
         });
 
         WynntilsButton infoButton = new BasicTexturedButton(
