a76b1abac28074b2073bcd7a26bb4e79befab502
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-08-05 17:49:34 +0200

    feat: add lr task function

--- common/src/main/java/com/wynntils/core/consumers/functions/FunctionManager.java
+++ common/src/main/java/com/wynntils/core/consumers/functions/FunctionManager.java
@@ -470,6 +470,7 @@ public final class FunctionManager extends Manager {
         registerFunction(new LootrunFunctions.LootrunBeaconCountFunction());
         registerFunction(new LootrunFunctions.LootrunStateFunction());
         registerFunction(new LootrunFunctions.LootrunTaskNameFunction());
+        registerFunction(new LootrunFunctions.LootrunTaskTypeFunction());
 
         registerFunction(new MinecraftFunctions.DirFunction());
         registerFunction(new MinecraftFunctions.FpsFunction());
