45ddb28fd711986c0d0e2060f08dd406e90f4e41
Rafii2198 <rafii2198@gmail.com>
2023-08-08 20:27:43 +0200

    feat: Lootrun Time and Challenges functions (#1949)
    * feat: Lootrun time functions
    
    * apply fixes
    
    * remove debug
    
    * feat: Add lootrun challenges functions
    
    * Persisting attempt
    
    * feat: Working persistance
    
    * fix: Stray semicolon
    
    * Attempt at different solution
    
    * more fixes
    
    * storage order
    
    * remove newlines
    
    * fix: fix stuff
    
    * fix: reset on worldstatechange
    
    ---------
    
    Co-authored-by: Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>

--- common/src/main/java/com/wynntils/models/lootrun/scoreboard/LootrunScoreboardPart.java
+++ common/src/main/java/com/wynntils/models/lootrun/scoreboard/LootrunScoreboardPart.java
@@ -13,7 +13,9 @@ import com.wynntils.handlers.scoreboard.ScoreboardSegment;
 import com.wynntils.handlers.scoreboard.type.SegmentMatcher;
 import com.wynntils.models.lootrun.type.LootrunTaskType;
 import com.wynntils.models.lootrun.type.LootrunningState;
+import com.wynntils.utils.type.CappedValue;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LootrunScoreboardPart extends ScoreboardPart {
@@ -26,6 +28,10 @@ public class LootrunScoreboardPart extends ScoreboardPart {
     private static final Pattern SLAY_PATTERN = Pattern.compile("^Slay! Wave (\\d) [-—] (\\d) (Target|Mob)s? Left!$");
     private static final Pattern DESTROY_PATTERN = Pattern.compile("^Destroy the objective!$");
     private static final Pattern DEFEND_PATTERN = Pattern.compile("^Defend for (\\d+)s!$");
+
+    private static final Pattern TIMER_PATTERN =
+            Pattern.compile("^[-—] Time Left: (\\d+):(\\d+)(?: \\[\\+\\d+[msMS]\\])?$");
+    private static final Pattern CHALLENGES_PATTERN = Pattern.compile("^[-—] Challenges: (\\d+)/(\\d+)$");
 
     @Override
     public SegmentMatcher getSegmentMatcher() {
@@ -55,6 +61,31 @@ public class LootrunScoreboardPart extends ScoreboardPart {
             Models.Lootrun.setState(LootrunningState.IN_TASK, LootrunTaskType.DESTROY);
         } else if (currentStateLine.matches(DEFEND_PATTERN, PartStyle.StyleType.NONE)) {
             Models.Lootrun.setState(LootrunningState.IN_TASK, LootrunTaskType.DEFEND);
+        }
+
+        if (content.size() < 2) {
+            WynntilsMod.warn(
+                    "Lootrun scoreboard segment content is too short; less than 2; we might be in a bad state");
+            return;
+        }
+
+        StyledText timeRemainingLine = content.get(1);
+        Matcher matcher = timeRemainingLine.getMatcher(TIMER_PATTERN, PartStyle.StyleType.NONE);
+        if (matcher.matches()) {
+            Models.Lootrun.setTimeLeft(Integer.parseInt(matcher.group(1)) * 60 + Integer.parseInt(matcher.group(2)));
+        }
+
+        if (content.size() < 3) {
+            WynntilsMod.warn(
+                    "Lootrun scoreboard segment content is too short; less than 3; we might be in a bad state");
+            return;
+        }
+
+        StyledText challengesLine = content.get(2);
+        matcher = challengesLine.getMatcher(CHALLENGES_PATTERN, PartStyle.StyleType.NONE);
+        if (matcher.matches()) {
+            Models.Lootrun.setChallenges(
+                    new CappedValue(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2))));
         }
     }
 
