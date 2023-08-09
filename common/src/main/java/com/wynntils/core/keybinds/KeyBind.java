7c3348d749c44f5f987784b284110a7211f56ac9
Alex <avtodaro@gmail.com>
2023-08-08 18:18:00 -0700

    Fix copyright year changes in file headers

--- common/src/main/java/com/wynntils/core/keybinds/KeyBind.java
+++ common/src/main/java/com/wynntils/core/keybinds/KeyBind.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2022-2023.
+ * Copyright © Wynntils 2022.
  * This file is released under AGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.core.keybinds;
