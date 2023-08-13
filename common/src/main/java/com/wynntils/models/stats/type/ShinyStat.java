99c6da97fd9229041f11c1050aafab45a87360a3
Magnus Ihse Bursie <mag@icus.se>
2023-08-13 13:22:50 +0200

    Make ShinyStat a record

--- common/src/main/java/com/wynntils/models/stats/type/ShinyStat.java
+++ common/src/main/java/com/wynntils/models/stats/type/ShinyStat.java
@@ -4,12 +4,4 @@
  */
 package com.wynntils.models.stats.type;
 
-public class ShinyStat {
-    private final String name;
-    private final int value;
-
-    public ShinyStat(String name, int value) {
-        this.name = name;
-        this.value = value;
-    }
-}
+public record ShinyStat(String name, int value) {}
