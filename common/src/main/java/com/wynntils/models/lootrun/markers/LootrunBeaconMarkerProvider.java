7a68730b359642009d2b81ceb2b6f44f1371b568
Magnus Ihse Bursie <mag@icus.se>
2023-08-13 11:13:11 +0200

    chore: Change license to LGPL3 (#1988)
    * chore: Update LICENSE text
    
    * chore: Update header in all source code files
    
    * chore: Update license metadata for mod loaders using SPDX identifier
    
    * chore: Update README

--- common/src/main/java/com/wynntils/models/lootrun/markers/LootrunBeaconMarkerProvider.java
+++ common/src/main/java/com/wynntils/models/lootrun/markers/LootrunBeaconMarkerProvider.java
@@ -1,6 +1,6 @@
 /*
  * Copyright © Wynntils 2023.
- * This file is released under AGPLv3. See LICENSE for full license details.
+ * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.models.lootrun.markers;
 
