e0e0fe0375d2f0b30932877115e1bd7e05c52fbb
JamieCallan117 <JamieCallan117@users.noreply.github.com>
2023-08-14 22:19:17 +0000

    ci: spotless formatting

--- common/src/main/java/com/wynntils/core/consumers/functions/FunctionManager.java
+++ common/src/main/java/com/wynntils/core/consumers/functions/FunctionManager.java
@@ -461,7 +461,7 @@ public final class FunctionManager extends Manager {
         registerFunction(new EnvironmentFunctions.MemMaxFunction());
         registerFunction(new EnvironmentFunctions.MemPctFunction());
         registerFunction(new EnvironmentFunctions.MemUsedFunction());
-        
+
         registerFunction(new InventoryFunctions.CappedAccessoryDurabilityFunction());
         registerFunction(new InventoryFunctions.CappedArmorDurabilityFunction());
         registerFunction(new InventoryFunctions.CappedHeldItemDurabilityFunction());
