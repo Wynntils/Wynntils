d88b8c1df657d40b86a657d4b77f5ef3dca2aba0
Jamie <JamCal117@gmail.com>
2023-08-16 11:46:57 +0100

    Define regions

--- common/src/main/java/com/wynntils/utils/render/Texture.java
+++ common/src/main/java/com/wynntils/utils/render/Texture.java
@@ -9,7 +9,7 @@ import net.minecraft.resources.ResourceLocation;
 // If a texture is currently in a specific category but you want to use
 // it elsewhere, please move it to a more appropriate location
 public enum Texture {
-    // Character Selection
+    // region Character Selection
     CHANGE_WORLD_BUTTON("character_selection/change_world_button.png", 26, 52),
     CHARACTER_BUTTON("character_selection/character_button.png", 104, 64),
     CHARACTER_INFO("character_selection/character_info.png", 123, 38),
@@ -18,23 +18,27 @@ public enum Texture {
     PLAY_BUTTON("character_selection/play_button.png", 79, 76),
     SCROLL_BUTTON("character_selection/scroll_button.png", 7, 17),
     XP_BAR("character_selection/xp_bar.png", 100, 12),
+    // endregion
 
-    // Content Book
+    // region Content Book
     CONFIG_BOOK_BACKGROUND("content_book/config_book.png", 400, 220),
     CONFIG_BOOK_SCROLL_AREA("content_book/config_book_scroll_area.png", 168, 175),
     CONFIG_BOOK_SCROLL_BUTTON("content_book/config_book_scroll_button.png", 5, 18),
     CONTENT_BOOK_BACKGROUND("content_book/content_book.png", 339, 220),
     CONTENT_BOOK_SEARCH("content_book/content_book_search.png", 133, 23),
     CONTENT_BOOK_TITLE("content_book/content_book_title.png", 168, 33),
+    // endregion
 
-    // Icons
+    // region Icons
     WYNNCRAFT_ICON("icons/wynncraft_icon.png", 64, 64),
+
     // Activities
     ACTIVITY_CAN_START("icons/activities/activity_can_start_icon.png", 11, 7),
     ACTIVITY_CANNOT_START("icons/activities/activity_cannot_start_icon.png", 7, 7),
     ACTIVITY_FINISHED("icons/activities/activity_finished_icon.png", 11, 7),
     ACTIVITY_STARTED("icons/activities/activity_started_icon.png", 7, 7),
     CAVE_AVALIABLE_ICON("icons/activities/cave_avaliable_icon.png", 7, 7),
+
     // Content Book
     DIALOGUE_ICON("icons/content_book/dialogue_icon.png", 14, 11),
     DISCOVERIES_ICON("icons/content_book/discoveries_icon.png", 16, 32),
@@ -45,6 +49,7 @@ public enum Texture {
     QUEST_BOOK_ICON("icons/content_book/quests_icon.png", 24, 34),
     SECRET_DISCOVERIES_ICON("icons/content_book/secret_discoveries_icon.png", 16, 26),
     SETTINGS_ICON("icons/content_book/settings_icon.png", 17, 34),
+
     // Discoveries
     DISCOVERED_SECRET("icons/discoveries/discovered_secret.png", 19, 36),
     DISCOVERED_SECRET_ICON("icons/discoveries/discovered_secret_icon.png", 5, 7),
@@ -58,6 +63,7 @@ public enum Texture {
     UNDISCOVERED_TERRITORY_ICON("icons/discoveries/undiscovered_territory_icon.png", 6, 7),
     UNDISCOVERED_WORLD("icons/discoveries/undiscovered_world.png", 21, 34),
     UNDISCOVERED_WORLD_ICON("icons/discoveries/undiscovered_world_icon.png", 11, 7),
+
     // Generic
     ADD_ICON("icons/generic/add_icon.png", 14, 14),
     BOAT_ICON("icons/generic/boat_icon.png", 15, 14),
@@ -74,6 +80,7 @@ public enum Texture {
     SOUL_POINT_ICON("icons/generic/soul_point_icon.png", 10, 16),
     WAYPOINT_FOCUS_ICON("icons/generic/waypoint_focus_icon.png", 12, 16),
     WAYPOINT_MANAGER_ICON("icons/generic/waypoint_manager_icon.png", 12, 16),
+
     // Generic Offset
     ADD_ICON_OFFSET("icons/generic_offset/add_icon_offset.png", 14, 28),
     BACK_ARROW_OFFSET("icons/generic_offset/back_arrow_icon_offset.png", 32, 9),
@@ -84,6 +91,7 @@ public enum Texture {
     SORT_ALPHABETICALLY_OFFSET("icons/generic_offset/sort_alphabetical_icon_offset.png", 23, 46),
     SORT_DISTANCE_OFFSET("icons/generic_offset/sort_distance_icon_offset.png", 23, 46),
     SORT_LEVEL_OFFSET("icons/generic_offset/sort_level_icon_offset.png", 23, 46),
+
     // Map
     ALCHEMIST_STATION("icons/map/alchemist_station.png", 16, 19),
     ARMOR_MERCHANT("icons/map/armor_merchant.png", 17, 17),
@@ -133,21 +141,25 @@ public enum Texture {
     WEAPONSMITHING_STATION("icons/map/weaponsmithing_station.png", 18, 15),
     WOODCUTTING("icons/map/woodcutting.png", 16, 17),
     WOODWORKING_STATION("icons/map/woodworking_station.png", 17, 15),
+    // endregion
 
-    // Lootrun
+    // region Lootrun
     LOOTRUN_LINE("lootrun/path_arrow.png", 16, 16),
+
     // Challenge Icons
     DEFEND("lootrun/challenge_icons/defend.png", 20, 23),
     DESTROY("lootrun/challenge_icons/destroy.png", 12, 12),
     SLAY("lootrun/challenge_icons/slay.png", 20, 20),
     SPELUNK("lootrun/challenge_icons/spelunk.png", 18, 16),
+    // endregion
 
-    // Map
+    // region Map
     // Map Borders
     FULLSCREEN_MAP_BORDER("map/map_borders/full_map_border.png", 510, 254),
     GILDED_MAP_TEXTURES("map/map_borders/gilded_map_textures.png", 262, 524),
     PAPER_MAP_TEXTURES("map/map_borders/paper_map_textures.png", 256, 512),
     WYNN_MAP_TEXTURES("map/map_borders/wynn_map_textures.png", 126, 256),
+
     // Map Components
     CIRCLE_MASK("map/map_components/circle_mask.png", 256, 256),
     MAP_BUTTONS_BACKGROUND("map/map_components/map_buttons_background.png", 144, 26),
@@ -155,9 +167,11 @@ public enum Texture {
     MAP_INFO_TOOLTIP_CENTER("map/map_components/map_info_tooltip_center.png", 200, 5),
     MAP_INFO_TOOLTIP_TOP("map/map_components/map_info_tooltip_top.png", 200, 10),
     MAP_POINTERS("map/map_components/map_pointers.png", 256, 256),
+    // endregion
 
-    // Overlays
+    // region Overlays
     PLAYER_LIST_OVERLAY("overlays/player_list_overlay.png", 385, 229),
+
     // Bars
     BUBBLE_BAR("overlays/bars/bars_bubbles.png", 256, 256),
     EXPERIENCE_BAR("overlays/bars/bars_exp.png", 256, 256),
@@ -166,14 +180,16 @@ public enum Texture {
     MANA_BAR("overlays/bars/bars_mana.png", 256, 256),
     MANA_BAR_OVERFLOW("overlays/bars/bars_mana_overflow.png", 256, 256),
     UNIVERSAL_BAR("overlays/bars/universal_bar.png", 81, 16),
+    // endregion
 
-    // Seaskipper
+    // region Seaskipper
     BOAT_BUTTON("seaskipper/boat_button.png", 39, 76),
     DESTINATION_BUTTON("seaskipper/destination_button.png", 66, 40),
     DESTINATION_LIST("seaskipper/destination_list_background.png", 143, 205),
     TRAVEL_BUTTON("seaskipper/travel_button.png", 100, 76),
+    // endregion
 
-    // UI Components
+    // region UI Components
     BACKGROUND_SPLASH("ui_components/background_splash.png", 1920, 1027),
     COSMETIC_VIEWER_BACKGROUND("ui_components/cosmetics_viewer_background.png", 72, 92),
     EMERALD_COUNT_BACKGROUND("ui_components/emerald_count_background.png", 24, 24),
@@ -183,6 +199,7 @@ public enum Texture {
     OVERLAY_SELECTION_GUI("ui_components/overlay_selection_gui.png", 195, 256),
     SCROLL_BACKGROUND("ui_components/scroll_background.png", 294, 198),
     VIGNETTE("ui_components/vignette.png", 512, 512);
+    // endregion
 
     private final ResourceLocation resource;
     private final int width;
