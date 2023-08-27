6d26f52d11389053b5a7937601d202cadc61aea6
JamieCallan117 <JamieCallan117@users.noreply.github.com>
2023-08-27 10:44:39 +0000

    ci: spotless formatting

--- common/src/main/java/com/wynntils/features/utilities/PerCharacterGuildContributionFeature.java
+++ common/src/main/java/com/wynntils/features/utilities/PerCharacterGuildContributionFeature.java
@@ -97,7 +97,8 @@ public class PerCharacterGuildContributionFeature extends Feature {
                 .withStyle(ChatFormatting.AQUA)
                 .withStyle(ChatFormatting.UNDERLINE)
                 .withStyle(style -> style.withClickEvent(new ClickEvent(
-                        ClickEvent.Action.RUN_COMMAND, "/wynntils feature disable PerCharacterGuildContributionFeature"))));
+                        ClickEvent.Action.RUN_COMMAND,
+                        "/wynntils feature disable PerCharacterGuildContributionFeature"))));
 
         contributionMessage.append(
                 Component.literal(" to disable this functionality.").withStyle(ChatFormatting.DARK_AQUA));
