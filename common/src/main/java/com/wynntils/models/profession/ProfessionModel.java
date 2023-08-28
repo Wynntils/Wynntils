3116d77e7b1adbcf274871e7999957062b6b137f
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-08-28 15:11:12 +0200

    chore: remove features not relevant anymore

--- common/src/main/java/com/wynntils/models/profession/ProfessionModel.java
+++ common/src/main/java/com/wynntils/models/profession/ProfessionModel.java
@@ -13,7 +13,6 @@ import com.wynntils.core.text.StyledText;
 import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
 import com.wynntils.handlers.labels.event.EntityLabelChangedEvent;
 import com.wynntils.mc.event.ContainerSetSlotEvent;
-import com.wynntils.mc.event.TickEvent;
 import com.wynntils.models.character.CharacterModel;
 import com.wynntils.models.items.items.game.MaterialItem;
 import com.wynntils.models.profession.event.ProfessionNodeGatheredEvent;
@@ -22,17 +21,11 @@ import com.wynntils.models.profession.type.ProfessionProgress;
 import com.wynntils.models.profession.type.ProfessionType;
 import com.wynntils.models.worlds.BombModel;
 import com.wynntils.models.worlds.WorldStateModel;
-import com.wynntils.models.worlds.event.WorldStateEvent;
-import com.wynntils.models.worlds.type.BombType;
 import com.wynntils.utils.mc.LoreUtils;
-import com.wynntils.utils.mc.McUtils;
-import com.wynntils.utils.mc.RenderedStringUtils;
 import com.wynntils.utils.type.Pair;
 import com.wynntils.utils.type.TimedSet;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -40,8 +33,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -78,9 +69,6 @@ public class ProfessionModel extends Model {
     private Map<ProfessionType, ProfessionProgress> professionProgressMap = new ConcurrentHashMap<>();
     private final Map<ProfessionType, TimedSet<Float>> rawXpGainInLastMinute = new HashMap<>();
 
-    private final List<ProfessionTimerArmorStand> professionTimerArmorStands = new LinkedList<>();
-    private int tickTimer = 0;
-
     public ProfessionModel(CharacterModel characterModel, WorldStateModel worldStateModel, BombModel bombModel) {
         super(List.of(characterModel, worldStateModel, bombModel));
         for (ProfessionType pt : ProfessionType.values()) {
@@ -109,12 +97,6 @@ public class ProfessionModel extends Model {
 
             ProfessionNodeGatheredEvent.LabelShown gatherEvent = new ProfessionNodeGatheredEvent.LabelShown();
             WynntilsMod.postEvent(gatherEvent);
-            if (gatherEvent.shouldAddCooldownArmorstand()) {
-                boolean professionSpeed = Models.Bomb.isBombActive(BombType.PROFESSION_SPEED);
-                professionTimerArmorStands.add(new ProfessionTimerArmorStand(
-                        event.getEntity(),
-                        professionSpeed ? PROFESSION_NODE_RESPAWN_TIME / 2 : PROFESSION_NODE_RESPAWN_TIME));
-            }
 
             return;
         }
@@ -155,37 +137,6 @@ public class ProfessionModel extends Model {
         if (matcher.matches()) {
             updateLevel(ProfessionType.fromString(matcher.group("name")), Integer.parseInt(matcher.group("level")));
         }
-    }
-
-    @SubscribeEvent
-    public void onWorldStateChange(WorldStateEvent event) {
-        professionTimerArmorStands.forEach(armorStand ->
-                McUtils.mc().level.removeEntity(armorStand.entity.getId(), Entity.RemovalReason.DISCARDED));
-
-        professionTimerArmorStands.clear();
-        tickTimer = 0;
-    }
-
-    @SubscribeEvent
-    public void onTick(TickEvent event) {
-        if (tickTimer % TICKS_PER_TIMER_UPDATE != 0) {
-            tickTimer++;
-            return;
-        }
-
-        tickTimer = 0;
-
-        List<ProfessionTimerArmorStand> removedElements = new ArrayList<>();
-
-        for (ProfessionTimerArmorStand armorStand : professionTimerArmorStands) {
-            boolean toBeRemoved = armorStand.tick();
-
-            if (toBeRemoved) {
-                removedElements.add(armorStand);
-            }
-        }
-
-        professionTimerArmorStands.removeAll(removedElements);
     }
 
     public void resetValueFromItem(ItemStack professionInfoItem) {
@@ -252,50 +203,5 @@ public class ProfessionModel extends Model {
 
     public int getProfessionDryStreak() {
         return professionDryStreak.get();
-    }
-
-    public int getGatherCooldownTime() {
-        return Models.Bomb.isBombActive(BombType.PROFESSION_SPEED) ? GATHER_COOLDOWN_TIME / 2 : GATHER_COOLDOWN_TIME;
-    }
-
-    private static final class ProfessionTimerArmorStand {
-        private final Entity entity;
-        private final int length;
-
-        private long startTime;
-        private long endTime;
-
-        private ProfessionTimerArmorStand(Entity positionBaseEntity, int length) {
-            this.entity = createArmorStandAt(positionBaseEntity);
-            this.length = length;
-            this.startTime = System.currentTimeMillis();
-            this.endTime = startTime + length * 1000L;
-
-            McUtils.mc().level.putNonPlayerEntity(entity.getId(), entity);
-        }
-
-        public boolean tick() {
-            int remaining = Math.round((endTime - System.currentTimeMillis()) / 1000L);
-            entity.setCustomName(RenderedStringUtils.getPercentageComponent(length - remaining, length, 8, true, "s"));
-
-            boolean toBeRemoved = remaining <= 0;
-
-            if (toBeRemoved) {
-                McUtils.mc().level.removeEntity(entity.getId(), Entity.RemovalReason.DISCARDED);
-            }
-
-            return toBeRemoved;
-        }
-
-        private static Entity createArmorStandAt(Entity copiedEntity) {
-            Entity entity = EntityType.ARMOR_STAND.create(McUtils.mc().level);
-            entity.copyPosition(copiedEntity);
-            entity.setPos(entity.position().add(0, -1, 0));
-            entity.setCustomNameVisible(true);
-            entity.setInvisible(true);
-            entity.setInvulnerable(true);
-
-            return entity;
-        }
     }
 }
