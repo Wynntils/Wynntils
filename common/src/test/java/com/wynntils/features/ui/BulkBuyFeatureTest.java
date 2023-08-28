8d850ae3781af9b64d9fb6e7ca3e49c309fec7dc
geekazodium <geekazodium@users.noreply.github.com>
2023-08-28 08:03:01 +0000

    ci: spotless formatting

--- common/src/test/java/com/wynntils/features/ui/BulkBuyFeatureTest.java
+++ common/src/test/java/com/wynntils/features/ui/BulkBuyFeatureTest.java
@@ -1,15 +1,18 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
 package com.wynntils.features.ui;
 
 import com.wynntils.testutils.RegexTest;
 import com.wynntils.testutils.TestPrivateFieldsUtil;
-import org.junit.jupiter.api.Test;
-
 import java.util.regex.Pattern;
+import org.junit.jupiter.api.Test;
 
 public class BulkBuyFeatureTest extends RegexTest {
     @Test
-    public void testPricePattern(){
-        Pattern pattern = TestPrivateFieldsUtil.getPrivateStaticRegexPattern(BulkBuyFeature.class,"PRICE_PATTERN");
+    public void testPricePattern() {
+        Pattern pattern = TestPrivateFieldsUtil.getPrivateStaticRegexPattern(BulkBuyFeature.class, "PRICE_PATTERN");
         assertMatchesAny(pattern, "§6 - §a✔ §f10§7²");
         assertMatchesAny(pattern, "§6 - §a✔ §f1483§7²");
         assertMatchesAny(pattern, "§6 - §c✖ §f244§7²");
--- common/src/test/java/com/wynntils/testutils/RegexTest.java
+++ common/src/test/java/com/wynntils/testutils/RegexTest.java
@@ -1,14 +1,20 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
 package com.wynntils.testutils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class RegexTest {
-    public Matcher assertMatchesAny(Pattern pattern, String string){
+    public Matcher assertMatchesAny(Pattern pattern, String string) {
         Matcher matcher = pattern.matcher(string);
         boolean matches = matcher.find();
-        if(!matches){
-            throw new AssertionError(String.format("asserted regex matches string, however it did not match string. \nregex:\t%s\nstring:\t%s",pattern,string.replaceAll("\n","\\\\n")));
+        if (!matches) {
+            throw new AssertionError(String.format(
+                    "asserted regex matches string, however it did not match string. \nregex:\t%s\nstring:\t%s",
+                    pattern, string.replaceAll("\n", "\\\\n")));
         }
         return matcher;
     }
