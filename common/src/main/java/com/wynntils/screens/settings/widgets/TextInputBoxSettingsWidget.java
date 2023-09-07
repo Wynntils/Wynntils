1615310f9ef2fd73dfa35f241e9a71ff5260032d
Mcrtin <66840021+Mcrtin@users.noreply.github.com>
2023-09-07 20:04:33 +0200

    fix: Fix descriptions of text-based settings not displaying

--- common/src/main/java/com/wynntils/screens/settings/widgets/TextInputBoxSettingsWidget.java
+++ common/src/main/java/com/wynntils/screens/settings/widgets/TextInputBoxSettingsWidget.java
@@ -8,7 +8,11 @@ import com.wynntils.core.persisted.config.Config;
 import com.wynntils.screens.base.TextboxScreen;
 import com.wynntils.screens.base.widgets.TextInputBoxWidget;
 import com.wynntils.utils.colors.CommonColors;
+import com.wynntils.utils.mc.ComponentUtils;
 import com.wynntils.utils.render.FontRenderer;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
 
 public class TextInputBoxSettingsWidget<T> extends TextInputBoxWidget {
     protected final Config<T> config;
@@ -17,6 +21,7 @@ public class TextInputBoxSettingsWidget<T> extends TextInputBoxWidget {
         super(0, 6, width, FontRenderer.getInstance().getFont().lineHeight + 8, null, textboxScreen);
         this.config = config;
         setTextBoxInput(config.get().toString());
+        tooltip = ComponentUtils.wrapTooltips(List.of(Component.literal(config.getDescription())), 150);
     }
 
     public TextInputBoxSettingsWidget(Config<T> config, TextboxScreen textboxScreen) {
