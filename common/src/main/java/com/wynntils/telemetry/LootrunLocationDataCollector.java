007b8d6877da6ebb94d78a137666e4b4aaaa53eb
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-09-29 19:32:12 +0200

    feat: Add CrowdSourcedDataManager (disabled, in preparation for opt-in crowd-sourced data) (#2030)
    * feat: Add TelemetryManager (and base for porting lootrun task telemetry)
    
    * feat: Move LootrunBeaconLocationCollectorFeature to LootrunLocationTelemetryCollector
    
    * chore: add back configs
    
    * chore: rebrand to CrowdSource-ing
    
    * chore: CrowdSource"d"
    
    * fix: implement comparable
    
    * chore: fix telemetry -> crowdsourced renames

---
+++ common/src/main/java/com/wynntils/telemetry/LootrunLocationDataCollector.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.telemetry;
+
+import com.wynntils.core.components.Models;
+import com.wynntils.core.crowdsource.CrowdSourcedDataCollector;
+import com.wynntils.core.crowdsource.datatype.LootrunTaskLocation;
+import com.wynntils.core.crowdsource.type.CrowdSourcedDataType;
+import com.wynntils.models.lootrun.type.LootrunLocation;
+import com.wynntils.models.particle.event.ParticleVerifiedEvent;
+import com.wynntils.models.particle.type.ParticleType;
+import com.wynntils.utils.mc.type.Location;
+import java.util.Optional;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class LootrunLocationDataCollector extends CrowdSourcedDataCollector<LootrunTaskLocation> {
+    @SubscribeEvent
+    public void onLootrunParticle(ParticleVerifiedEvent event) {
+        if (event.getParticle().particleType() != ParticleType.LOOTRUN_TASK) return;
+
+        Optional<LootrunLocation> lootrunLocationOpt = Models.Lootrun.getLocation();
+        if (lootrunLocationOpt.isEmpty()) return;
+
+        LootrunLocation lootrunLocation = lootrunLocationOpt.get();
+
+        collect(new LootrunTaskLocation(
+                lootrunLocation, Location.containing(event.getParticle().position())));
+    }
+
+    @Override
+    protected CrowdSourcedDataType getDataType() {
+        return CrowdSourcedDataType.LOOTRUN_TASK_LOCATIONS;
+    }
+}
