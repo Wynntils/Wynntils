33f7bc77628ea388d967433a485d8e8f49346dc6
Rafii2198 <rafii2198@gmail.com>
2023-10-03 16:00:40 +0200

    feat: Lootrun Beacon Count now shows Red Challenges left (#2163)
    * feat: Lootrun Beacon Count now shows Red Challenges left
    
    * fix: Add missing preview change

--- common/src/main/java/com/wynntils/overlays/lootrun/LootrunBeaconCountOverlay.java
+++ common/src/main/java/com/wynntils/overlays/lootrun/LootrunBeaconCountOverlay.java
@@ -11,7 +11,7 @@ import com.wynntils.utils.render.type.VerticalAlignment;
 
 public class LootrunBeaconCountOverlay extends TextOverlay {
     private static final String TEMPLATE =
-            "{if_string(string_equals(lootrun_state; \"NOT_RUNNING\"); \"\"; \\nCONCAT(\"§eYellow: \"; STRING(lootrun_beacon_count(\"YELLOW\"));\"\\n§9Blue: \"; STRING(lootrun_beacon_count(\"BLUE\"));\"\\n§5Purple: \"; STRING(lootrun_beacon_count(\"PURPLE\"));\"\\n§7Gray: \"; STRING(lootrun_beacon_count(\"GRAY\"));\"/3\";\"\\n§6Orange: \"; STRING(lootrun_beacon_count(\"ORANGE\"));\"\";\"\\n§bAqua: \"; STRING(lootrun_beacon_count(\"AQUA\"));\"\";\"\\n§8Dark Gray: \"; STRING(lootrun_beacon_count(\"DARK_GRAY\"));\"/1\";\"\\n§aGreen: \"; STRING(lootrun_beacon_count(\"GREEN\"));\"\";\"\\n§cRed: \"; STRING(lootrun_beacon_count(\"RED\"));\"\";\"\\n§fWhite: \"; STRING(lootrun_beacon_count(\"WHITE\"));\"/1\";\"\\n§4R§ca§6i§en§ab§2o§bw§9: §f\"; STRING(lootrun_beacon_count(\"RAINBOW\"));\"/1\"))}\\n";
+            "{if_string(string_equals(lootrun_state; \"NOT_RUNNING\"); \"\"; \\nCONCAT(\"§eYellow: \"; STRING(lootrun_beacon_count(\"YELLOW\"));\"\\n§9Blue: \"; STRING(lootrun_beacon_count(\"BLUE\"));\"\\n§5Purple: \"; STRING(lootrun_beacon_count(\"PURPLE\"));\"\\n§7Gray: \"; STRING(lootrun_beacon_count(\"GRAY\"));\"/3\";\"\\n§6Orange: \"; STRING(lootrun_beacon_count(\"ORANGE\"));\"\";\"\\n§bAqua: \"; STRING(lootrun_beacon_count(\"AQUA\"));\"\";\"\\n§8Dark Gray: \"; STRING(lootrun_beacon_count(\"DARK_GRAY\"));\"/1\";\"\\n§aGreen: \"; STRING(lootrun_beacon_count(\"GREEN\"));\"\";\"\\n§cRed: \"; STRING(lootrun_beacon_count(\"RED\"));\" (\"; STRING(lootrun_red_beacon_challenge_count);\")\";\"\\n§fWhite: \"; STRING(lootrun_beacon_count(\"WHITE\"));\"/1\";\"\\n§4R§ca§6i§en§ab§2o§bw§9: §f\"; STRING(lootrun_beacon_count(\"RAINBOW\"));\"/1\"))}\\n";
 
     private static final String PREVIEW_TEMPLATE =
             """
@@ -23,7 +23,7 @@ public class LootrunBeaconCountOverlay extends TextOverlay {
             §bAqua: 0
             §8Dark Gray: 0/1
             §aGreen: 0
-            §cRed: 0
+            §cRed: 0 (0)
             §fWhite: 0/1
             §4R§ca§6i§en§ab§2o§bw§9: 0/1
             """;
