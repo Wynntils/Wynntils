1dfb3d4996121c7c284f3f464c2fd3e454492b5e
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-10-20 16:31:01 +0200

    chore: fix comment

--- common/src/main/java/com/wynntils/handlers/scoreboard/ScoreboardHandler.java
+++ common/src/main/java/com/wynntils/handlers/scoreboard/ScoreboardHandler.java
@@ -179,7 +179,7 @@ public final class ScoreboardHandler extends Handler {
         // 4. Check for segment correctness
         //    There are 2 error cases here:
         //       - Fatal error: We find info that makes the current scoreboard invalid
-        //       - "Valid" error: We find an error, but it only makes the current scoreboard invalid, not the whole
+        //       - "Valid" error: We find an error, but it only makes the current segment invalid, not the scoreboard
         //                        If we find a segment that is not valid,
         //                        we return the list of valid segments up to that point.
         //                        This is a valid case because the scoreboard cannot fit all segments,
