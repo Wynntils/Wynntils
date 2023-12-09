82f990a5d46fc989d375d9b9b075029e7e6344f8
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2023-12-09 22:13:10 +0100

    chore: remove DeprecatedCharmInfo

--- common/src/main/java/com/wynntils/models/rewards/type/DeprecatedCharmInfo.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright © Wynntils 2023.
- * This file is released under LGPLv3. See LICENSE for full license details.
- */
-package com.wynntils.models.rewards.type;
-
-import com.wynntils.models.gear.type.GearTier;
-
-public record DeprecatedCharmInfo(String displayName, GearTier tier, String type) {}
