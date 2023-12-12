71a41d3a4c7b8ff4b04fff50e4c0fbde9e6fe7c7
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2023-12-12 07:31:10 +0100

    chore: remove unused method

--- common/src/main/java/com/wynntils/models/rewards/CharmInfoRegistry.java
+++ common/src/main/java/com/wynntils/models/rewards/CharmInfoRegistry.java
@@ -12,7 +12,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.wynntils.core.WynntilsMod;
 import com.wynntils.core.components.Managers;
-import com.wynntils.core.components.Models;
 import com.wynntils.core.net.Download;
 import com.wynntils.core.net.UrlId;
 import com.wynntils.models.gear.type.GearDropRestrictions;
@@ -21,7 +20,6 @@ import com.wynntils.models.gear.type.GearRestrictions;
 import com.wynntils.models.gear.type.GearTier;
 import com.wynntils.models.rewards.type.CharmInfo;
 import com.wynntils.models.rewards.type.CharmRequirements;
-import com.wynntils.models.stats.type.SkillStatType;
 import com.wynntils.models.stats.type.StatPossibleValues;
 import com.wynntils.models.stats.type.StatType;
 import com.wynntils.models.wynnitem.AbstractItemInfoDeserializer;
@@ -112,48 +110,6 @@ public class CharmInfoRegistry {
             List<Pair<StatType, StatPossibleValues>> variableStats = parseVariableStats(json, "identifications");
 
             return new CharmInfo(displayName, tier, metaInfo, requirements, baseStats, variableStats);
-        }
-
-        private List<Pair<StatType, Integer>> parseBaseStats(JsonObject json) {
-            JsonObject baseJson = JsonUtils.getNullableJsonObject(json, "base");
-
-            List<Pair<StatType, Integer>> list = new ArrayList<>();
-            for (Map.Entry<String, JsonElement> entry : baseJson.entrySet()) {
-                StatType statType = Models.Stat.fromApiRollId(entry.getKey());
-
-                if (statType == null) {
-                    WynntilsMod.warn("Item DB contains invalid stat type " + entry.getKey());
-                    continue;
-                }
-
-                if (statType instanceof SkillStatType) {
-                    // Skill stats are not variable for gear
-                    continue;
-                }
-
-                int baseValue;
-
-                // Base ids are a pre-identified, so there is no range
-                if (baseJson.get(statType.getApiName()).isJsonPrimitive()) {
-                    baseValue = JsonUtils.getNullableJsonInt(baseJson, statType.getApiName());
-                } else {
-                    WynntilsMod.warn("Tome with a non-static base stat: " + statType.getApiName());
-                    continue;
-                }
-
-                // If the base value is 0, this stat is not present on the item
-                if (baseValue == 0) continue;
-
-                // "Inverted" stats (i.e. spell costs) will be stored as a positive value,
-                // and only converted to negative at display time.
-                if (statType.showAsInverted()) {
-                    baseValue = -baseValue;
-                }
-
-                list.add(Pair.of(statType, baseValue));
-            }
-
-            return list;
         }
 
         private GearMetaInfo parseMetaInfo(JsonObject json, String name, String apiName) {
