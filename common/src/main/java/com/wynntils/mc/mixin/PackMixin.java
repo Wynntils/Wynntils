5870a0ae2d1c426397c9364fde94f27314259de6
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-12-12 06:07:19 -0800

    feat: allows packs to be applied on top of wynn

---
+++ common/src/main/java/com/wynntils/mc/mixin/PackMixin.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © Wynntils 2023.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.mc.mixin;
+
+import com.wynntils.core.WynntilsMod;
+import com.wynntils.core.components.Managers;
+import com.wynntils.core.consumers.features.FeatureManager;
+import com.wynntils.core.events.MixinHelper;
+import com.wynntils.features.utilities.AutoApplyResourcePackFeature;
+import com.wynntils.utils.mc.McUtils;
+import net.minecraft.server.packs.repository.Pack;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Pack.class)
+public abstract class PackMixin {
+    @Shadow
+    public abstract String getId();
+
+    @Shadow @Final private boolean fixedPosition;
+
+    @Redirect(method = "*",at = @At(target="Lnet/minecraft/server/packs/repository/Pack;fixedPosition:Z",value = "FIELD",opcode = Opcodes.GETFIELD))
+    private boolean onGetFixedPosition(Pack pack){
+        boolean enabled = Managers.Feature.getFeatureInstance(AutoApplyResourcePackFeature.class).isEnabled();
+        if(enabled){
+            if(pack.getId().equals("server")){
+                return false;
+            }
+        }
+        return this.fixedPosition;
+    }
+}
