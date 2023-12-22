0ae9c84536aa832e9c1286f641e29b4dbf1742a3
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-12-22 08:14:36 +0100

    feat: Make ChatItemFeature support the new encoding

--- common/src/main/java/com/wynntils/features/chat/ChatItemFeature.java
+++ common/src/main/java/com/wynntils/features/chat/ChatItemFeature.java
@@ -16,15 +16,22 @@ import com.wynntils.mc.event.KeyInputEvent;
 import com.wynntils.mc.mixin.accessors.ChatScreenAccessor;
 import com.wynntils.mc.mixin.accessors.ItemStackInfoAccessor;
 import com.wynntils.models.items.FakeItemStack;
+import com.wynntils.models.items.WynnItem;
 import com.wynntils.models.items.items.game.GearItem;
+import com.wynntils.models.items.properties.GearTierItemProperty;
+import com.wynntils.models.items.properties.IdentifiableItemProperty;
+import com.wynntils.utils.EncodedByteBuffer;
 import com.wynntils.utils.mc.McUtils;
+import com.wynntils.utils.type.ErrorOr;
 import com.wynntils.utils.type.IterationDecision;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.ChatScreen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.ItemStack;
@@ -53,6 +60,8 @@ public class ChatItemFeature extends Feature {
         }
 
         // replace encoded strings with placeholders for less confusion
+
+        // check for old chat item encoding
         Matcher m = Models.Gear.gearChatEncodingMatcher(chatInput.getValue());
         while (m.find()) {
             String encodedItem = m.group();
@@ -64,6 +73,33 @@ public class ChatItemFeature extends Feature {
             chatInput.setValue(chatInput.getValue().replace(encodedItem, "<" + name + ">"));
             chatItems.put(name.toString(), encodedItem);
         }
+
+        // check for new chat item encoding
+        m = Models.ItemEncoding.getEncodedDataPattern().matcher(chatInput.getValue());
+        while (m.find()) {
+            EncodedByteBuffer encodedByteBuffer = EncodedByteBuffer.fromUtf16String(m.group());
+            ErrorOr<WynnItem> errorOrDecodedItem = Models.ItemEncoding.decodeItem(encodedByteBuffer);
+
+            String name;
+            if (errorOrDecodedItem.hasError()) {
+                name = "encoding_error";
+            } else {
+                WynnItem decodedItem = errorOrDecodedItem.getValue();
+
+                name = decodedItem.getClass().getSimpleName();
+
+                if (decodedItem instanceof IdentifiableItemProperty identifiableItemProperty) {
+                    name = identifiableItemProperty.getName();
+                }
+            }
+
+            while (chatItems.containsKey(name)) { // avoid overwriting entries
+                name += "_";
+            }
+
+            chatInput.setValue(chatInput.getValue().replace(m.group(), "<" + name + ">"));
+            chatItems.put(name, m.group());
+        }
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
@@ -72,34 +108,20 @@ public class ChatItemFeature extends Feature {
 
         StyledText styledText = e.getStyledText();
 
+        // Decode old chat item encoding
         StyledText modified = styledText.iterate((part, changes) -> {
             StyledTextPart partToReplace = part;
-            Matcher matcher =
-                    Models.Gear.gearChatEncodingMatcher(partToReplace.getString(null, PartStyle.StyleType.NONE));
 
-            while (matcher.find()) {
-                GearItem decodedItem = Models.Gear.fromEncodedString(matcher.group());
-                if (decodedItem == null) continue;
+            decoodeDeprecatedChatEncoding(changes, partToReplace);
 
-                String unformattedString = partToReplace.getString(null, PartStyle.StyleType.NONE);
+            return IterationDecision.CONTINUE;
+        });
 
-                String firstPart = unformattedString.substring(0, matcher.start());
-                String lastPart = unformattedString.substring(matcher.end());
+        // Decode new chat item encoding
+        modified = modified.iterate((part, changes) -> {
+            StyledTextPart partToReplace = part;
 
-                PartStyle partStyle = partToReplace.getPartStyle();
-
-                StyledTextPart first = new StyledTextPart(firstPart, partStyle.getStyle(), null, Style.EMPTY);
-                StyledTextPart item = createItemPart(decodedItem);
-                StyledTextPart last = new StyledTextPart(lastPart, partStyle.getStyle(), null, Style.EMPTY);
-
-                changes.remove(partToReplace);
-                changes.add(first);
-                changes.add(item);
-                changes.add(last);
-
-                partToReplace = last;
-                matcher = Models.Gear.gearChatEncodingMatcher(lastPart);
-            }
+            decoodeChatEncoding(changes, partToReplace);
 
             return IterationDecision.CONTINUE;
         });
@@ -109,16 +131,94 @@ public class ChatItemFeature extends Feature {
         e.setMessage(modified.getComponent());
     }
 
-    private StyledTextPart createItemPart(GearItem gearItem) {
-        Style style = Style.EMPTY
-                .applyFormat(ChatFormatting.UNDERLINE)
-                .withColor(gearItem.getItemInfo().tier().getChatFormatting());
+    private void decoodeChatEncoding(List<StyledTextPart> changes, StyledTextPart partToReplace) {
+        Matcher matcher = Models.ItemEncoding.getEncodedDataPattern()
+                .matcher(partToReplace.getString(null, PartStyle.StyleType.NONE));
 
-        ItemStack itemStack = new FakeItemStack(gearItem, "From chat");
+        while (matcher.find()) {
+            EncodedByteBuffer encodedByteBuffer = EncodedByteBuffer.fromUtf16String(matcher.group());
+            ErrorOr<WynnItem> errorOrDecodedItem = Models.ItemEncoding.decodeItem(encodedByteBuffer);
+
+            String unformattedString = partToReplace.getString(null, PartStyle.StyleType.NONE);
+
+            String firstPart = unformattedString.substring(0, matcher.start());
+            String lastPart = unformattedString.substring(matcher.end());
+
+            PartStyle partStyle = partToReplace.getPartStyle();
+
+            StyledTextPart first = new StyledTextPart(firstPart, partStyle.getStyle(), null, Style.EMPTY);
+            StyledTextPart replacedPart = errorOrDecodedItem.hasError()
+                    ? createErrorPart(matcher.group(), errorOrDecodedItem.getError())
+                    : createItemPart(errorOrDecodedItem.getValue());
+            StyledTextPart last = new StyledTextPart(lastPart, partStyle.getStyle(), null, Style.EMPTY);
+
+            changes.remove(partToReplace);
+            changes.add(first);
+            changes.add(replacedPart);
+            changes.add(last);
+
+            partToReplace = last;
+            matcher = Models.Gear.gearChatEncodingMatcher(lastPart);
+        }
+    }
+
+    private void decoodeDeprecatedChatEncoding(List<StyledTextPart> changes, StyledTextPart partToReplace) {
+        Matcher matcher = Models.Gear.gearChatEncodingMatcher(partToReplace.getString(null, PartStyle.StyleType.NONE));
+
+        while (matcher.find()) {
+            GearItem decodedItem = Models.Gear.fromEncodedString(matcher.group());
+            if (decodedItem == null) continue;
+
+            String unformattedString = partToReplace.getString(null, PartStyle.StyleType.NONE);
+
+            String firstPart = unformattedString.substring(0, matcher.start());
+            String lastPart = unformattedString.substring(matcher.end());
+
+            PartStyle partStyle = partToReplace.getPartStyle();
+
+            StyledTextPart first = new StyledTextPart(firstPart, partStyle.getStyle(), null, Style.EMPTY);
+            StyledTextPart item = createItemPart(decodedItem);
+            StyledTextPart last = new StyledTextPart(lastPart, partStyle.getStyle(), null, Style.EMPTY);
+
+            changes.remove(partToReplace);
+            changes.add(first);
+            changes.add(item);
+            changes.add(last);
+
+            partToReplace = last;
+            matcher = Models.Gear.gearChatEncodingMatcher(lastPart);
+        }
+    }
+
+    private StyledTextPart createErrorPart(String originalString, String error) {
+        // Display the original string with a red underline and a tooltip with the error message
+        Style style = Style.EMPTY.applyFormat(ChatFormatting.UNDERLINE).withColor(ChatFormatting.RED);
+
+        HoverEvent hoverEvent = new HoverEvent(
+                HoverEvent.Action.SHOW_TEXT, Component.literal(error).withStyle(ChatFormatting.RED));
+        style = style.withHoverEvent(hoverEvent);
+
+        return new StyledTextPart(originalString, style, null, Style.EMPTY);
+    }
+
+    private StyledTextPart createItemPart(WynnItem wynnItem) {
+        String name = wynnItem.getClass().getSimpleName();
+
+        if (wynnItem instanceof IdentifiableItemProperty identifiableItemProperty) {
+            name = identifiableItemProperty.getName();
+        }
+
+        Style style = Style.EMPTY.applyFormat(ChatFormatting.UNDERLINE).withColor(ChatFormatting.GOLD);
+
+        if (wynnItem instanceof GearTierItemProperty tierItemProperty) {
+            style = style.withColor(tierItemProperty.getGearTier().getChatFormatting());
+        }
+
+        ItemStack itemStack = new FakeItemStack(wynnItem, "From chat");
         HoverEvent.ItemStackInfo itemHoverEvent = new HoverEvent.ItemStackInfo(itemStack);
         ((ItemStackInfoAccessor) itemHoverEvent).setItemStack(itemStack);
         style = style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, itemHoverEvent));
 
-        return new StyledTextPart(gearItem.getItemInfo().name(), style, null, Style.EMPTY);
+        return new StyledTextPart(name, style, null, Style.EMPTY);
     }
 }
--- common/src/main/java/com/wynntils/models/items/FakeItemStack.java
+++ common/src/main/java/com/wynntils/models/items/FakeItemStack.java
@@ -7,26 +7,31 @@ package com.wynntils.models.items;
 import com.wynntils.core.components.Handlers;
 import com.wynntils.core.components.Models;
 import com.wynntils.core.text.StyledText;
+import com.wynntils.handlers.tooltip.TooltipBuilder;
 import com.wynntils.handlers.tooltip.impl.identifiable.IdentifiableTooltipBuilder;
 import com.wynntils.models.items.items.game.GearItem;
+import com.wynntils.models.items.properties.IdentifiableItemProperty;
 import java.util.List;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 
 public class FakeItemStack extends ItemStack {
-    private final GearItem gearItem;
+    private final WynnItem wynnItem;
     private final String source;
 
-    private FakeItemStack(GearItem gearItem, ItemStack itemStack, String source) {
+    private FakeItemStack(WynnItem wynnItem, ItemStack itemStack, String source) {
         super(itemStack.getItem(), 1);
         this.setTag(itemStack.getTag());
-        Handlers.Item.updateItem(
-                this, gearItem, StyledText.fromString(gearItem.getItemInfo().name()));
 
-        this.gearItem = gearItem;
+        if (wynnItem instanceof IdentifiableItemProperty identifiableItemProperty) {
+            Handlers.Item.updateItem(this, wynnItem, StyledText.fromString(identifiableItemProperty.getName()));
+        }
+
+        this.wynnItem = wynnItem;
         this.source = source;
     }
 
@@ -34,13 +39,25 @@ public class FakeItemStack extends ItemStack {
         this(gearItem, gearItem.getItemInfo().metaInfo().material().itemStack(), source);
     }
 
+    // This should be only used by chat items, so the item does not matter
+    public FakeItemStack(WynnItem wynnItem, String source) {
+        this(wynnItem, new ItemStack(Items.STONE), source);
+    }
+
     @Override
     public List<Component> getTooltipLines(Player player, TooltipFlag isAdvanced) {
-        IdentifiableTooltipBuilder tooltipBuilder = Handlers.Tooltip.buildNew(gearItem, false);
-        List<Component> tooltip = tooltipBuilder.getTooltipLines(Models.Character.getClassType());
-        // Add a line describing the source of this fake stack
-        tooltip.add(
-                1, Component.literal(source).withStyle(ChatFormatting.DARK_GRAY).withStyle(ChatFormatting.ITALIC));
-        return tooltip;
+        if (wynnItem instanceof GearItem gearItem) {
+            TooltipBuilder tooltipBuilder = Handlers.Tooltip.buildNew(gearItem, false);
+            List<Component> tooltip = tooltipBuilder.getTooltipLines(Models.Character.getClassType());
+            // Add a line describing the source of this fake stack
+            tooltip.add(
+                    1,
+                    Component.literal(source)
+                            .withStyle(ChatFormatting.DARK_GRAY)
+                            .withStyle(ChatFormatting.ITALIC));
+            return tooltip;
+        }
+
+        return List.of();
     }
 }
