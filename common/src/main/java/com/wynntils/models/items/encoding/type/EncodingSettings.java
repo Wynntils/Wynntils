48c56430357dcd0daaee4ec4661e3385c9cbfcbd
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2024-01-03 01:12:26 +0100

    chore: fix comment in EncodingSettings

--- common/src/main/java/com/wynntils/models/items/encoding/type/EncodingSettings.java
+++ common/src/main/java/com/wynntils/models/items/encoding/type/EncodingSettings.java
@@ -7,6 +7,6 @@ package com.wynntils.models.items.encoding.type;
 /**
  * Settings for encoding items.
  * @param extendedIdentificationEncoding Whether to use extended identification encoding. This only affects identifiable items.
- * @param shareItemName Whether to share the item name between the item name and the item lore. This only affects crafted and custom items.
+ * @param shareItemName Whether to share the item name. This only affects crafted and custom items.
  */
 public record EncodingSettings(boolean extendedIdentificationEncoding, boolean shareItemName) {}
