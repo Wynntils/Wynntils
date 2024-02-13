8ed2f45fdd6f82cb8bb050e3488d54ce3cde1e5c
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2024-02-13 11:05:32 -0800

    computeIfAbsent, temporary map

--- common/src/main/java/com/wynntils/services/destination/DestinationService.java
+++ common/src/main/java/com/wynntils/services/destination/DestinationService.java
@@ -34,30 +34,39 @@ public class DestinationService extends Service {
      * @return Abbreviation of the location (eg. "Ra"). Reads from destination json first, if not found returns first + next non-conflicting letter of the location name.
      */
     public String getAbbreviation(String location) {
-        if (!destinations.containsKey(location)) {
-            String abbreviation = location.substring(0, 2);
-            int i = 2;
-            while (destinations.containsValue(abbreviation)) {
-                if (location.charAt(i) == ' ') i++;
-                abbreviation = location.charAt(0) + "" + location.charAt(i);
-                i++;
-            }
-            WynntilsMod.warn("DestinationService: No destination found for " + location
-                    + ", using fallback abbreviation " + abbreviation);
-            return abbreviation;
-        }
+        destinations.computeIfAbsent(location, this::findNextAbbreviation);
         return destinations.get(location);
     }
 
+    /**
+     * Finds the next non-conflicting abbreviation for a location, excluding spaces
+     * @param location Full location name of the destination (eg. "Ragni")
+     */
+    private String findNextAbbreviation(String location) {
+        String abbreviation = location.substring(0, 2);
+        int i = 2;
+        while (destinations.containsValue(abbreviation)) {
+            if (location.charAt(i) == ' ') i++;
+            abbreviation = location.charAt(0) + "" + location.charAt(i);
+            i++;
+        }
+        WynntilsMod.warn("DestinationService: No destination found for " + location
+                + ", computed fallback abbreviation " + abbreviation);
+        return abbreviation;
+    }
+
     private void loadDestinations() {
-        destinations = new HashMap<>();
+        Map<String, String> newDestinations = new HashMap<>();
+
         Download dl = Managers.Net.download(UrlId.DATA_STATIC_DESTINATIONS);
         dl.handleReader(reader -> {
             Destination result = WynntilsMod.GSON.fromJson(reader, Destination.class);
             for (Destination.DestinationDetail destination : result.destinations) {
-                destinations.put(destination.location, destination.abbreviation);
+                newDestinations.put(destination.location, destination.abbreviation);
             }
         });
+
+        destinations = newDestinations;
     }
 
     private static class Destination {
