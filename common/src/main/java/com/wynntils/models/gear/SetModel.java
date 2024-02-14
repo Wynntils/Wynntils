d1aedb76cf5be059c22033bcc5068f16daf67b58
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2024-02-14 14:50:28 -0800

    start setmodel

---
+++ common/src/main/java/com/wynntils/models/gear/SetModel.java
@@ -0,0 +1,40 @@
+package com.wynntils.models.gear;
+
+import com.google.gson.reflect.TypeToken;
+import com.wynntils.core.WynntilsMod;
+import com.wynntils.core.components.Managers;
+import com.wynntils.core.components.Model;
+import com.wynntils.core.net.Download;
+import com.wynntils.core.net.UrlId;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SetModel extends Model {
+
+    public SetModel() {
+        super(List.of());
+        loadSetData();
+    }
+    private void loadSetData() {
+        Download dl = Managers.Net.download(UrlId.DATA_STATIC_ITEM_SETS);
+        dl.handleReader(reader -> {
+            Map<String, SetData> setData = WynntilsMod.GSON.fromJson(reader, new TypeToken<Map<String, SetData>>(){}.getType());
+        });
+    }
+
+    public static class SetData {
+        private List<Map<String, Integer>> bonuses;
+        private List<String> items;
+
+        public List<Map<String, Integer>> getBonuses() {
+            return Collections.unmodifiableList(bonuses);
+        }
+
+        public List<String> getItems() {
+            return Collections.unmodifiableList(items);
+        }
+    }
+}
