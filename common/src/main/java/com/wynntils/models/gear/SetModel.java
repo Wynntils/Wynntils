d3b5cedab6bac66dba9963c45b3bad4689f16189
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2024-02-15 12:06:15 -0800

    put into setData map

--- common/src/main/java/com/wynntils/models/gear/SetModel.java
+++ common/src/main/java/com/wynntils/models/gear/SetModel.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright © Wynntils 2024.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
 package com.wynntils.models.gear;
 
 import com.google.gson.reflect.TypeToken;
@@ -6,23 +10,33 @@ import com.wynntils.core.components.Managers;
 import com.wynntils.core.components.Model;
 import com.wynntils.core.net.Download;
 import com.wynntils.core.net.UrlId;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class SetModel extends Model {
+    private final Map<String, SetData> setData = new HashMap<>();
 
     public SetModel() {
         super(List.of());
         loadSetData();
     }
+
+    public SetData getSetData(String setId) {
+        return setData.get(setId);
+    }
+
     private void loadSetData() {
         Download dl = Managers.Net.download(UrlId.DATA_STATIC_ITEM_SETS);
+        AtomicReference<Map<String, SetData>> newSetData = new AtomicReference<>();
         dl.handleReader(reader -> {
-            Map<String, SetData> setData = WynntilsMod.GSON.fromJson(reader, new TypeToken<Map<String, SetData>>(){}.getType());
+            newSetData.set(WynntilsMod.GSON.fromJson(reader, new TypeToken<Map<String, SetData>>() {}.getType()));
         });
+
+        setData.clear();
+        setData.putAll(newSetData.get());
     }
 
     public static class SetData {
