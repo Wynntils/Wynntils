dd0360e195d33e26e9e00e8a803f4ceee1db42ba
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2024-03-12 18:51:55 +0100

    fix: Make translations work with NPC dialogues (#2362)
    * refactor: ChatHandler signals to NpcDialogueModel, merge NpcDialogue and ConfirmationlessDialogue types
    
    * feat: Add the new post-processing pipeline for NPC dialogues
    
    * fix: work on making TranscribeMessagesFeature work with translations correctly
    
    * fix: Fix translation cache not being able to load, add shutdown hook for saving, save on world state change
    
    * chore: update special character handling and add notes about characters
    
    * fix: Clear selection dialogues from chat when they disappear (with overlay on)
    
    * feat: Add hover/clickable event support for translations
    
    * fix: add missing return, fix saving logic
    
    * chore: add coment for confirmationlessDialogues

--- common/src/main/java/com/wynntils/models/npcdialogue/type/NpcDialogue.java
+++ common/src/main/java/com/wynntils/models/npcdialogue/type/NpcDialogue.java
@@ -7,12 +7,33 @@ package com.wynntils.models.npcdialogue.type;
 import com.wynntils.core.text.StyledText;
 import com.wynntils.handlers.chat.type.NpcDialogueType;
 import java.util.List;
+import net.minecraft.network.chat.Component;
 
+/**
+ * Represents the current dialogue of an NPC.
+ *
+ * @param currentDialogue The current dialogue of the NPC.
+ * @param dialogueType    The type of dialogue.
+ * @param isProtected     Whether the dialogue is protects the player from being attacked.
+ * @param addTime         The time the dialogue was added.
+ * @param removeTime      The time the dialogue will be removed. This is used for confirmationless dialogues.
+ */
 public record NpcDialogue(
-        List<StyledText> currentDialogue, NpcDialogueType dialogueType, boolean isProtected, long addTime) {
-    public static final NpcDialogue EMPTY = new NpcDialogue(List.of(), NpcDialogueType.NONE, false, 0);
+        List<StyledText> currentDialogue,
+        NpcDialogueType dialogueType,
+        boolean isProtected,
+        long addTime,
+        long removeTime) {
+    public static final NpcDialogue EMPTY = new NpcDialogue(List.of(), NpcDialogueType.NONE, false, 0, 0);
 
     public boolean isEmpty() {
         return currentDialogue.isEmpty();
+    }
+
+    public List<Component> dialogueComponent() {
+        return currentDialogue.stream()
+                .map(StyledText::getComponent)
+                .map(c -> (Component) c)
+                .toList();
     }
 }
