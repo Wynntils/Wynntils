6f9976f7ca749197a9c14bc1edfabf4dda4119ff
Jamie <JamCal117@gmail.com>
2024-05-04 18:34:16 +0100

    feat: Add option to not include intermission in total. Refactor overlay template creation. Update time format

--- common/src/main/java/com/wynntils/overlays/RaidProgressOverlay.java
+++ common/src/main/java/com/wynntils/overlays/RaidProgressOverlay.java
@@ -13,119 +13,20 @@ import com.wynntils.utils.render.type.HorizontalAlignment;
 import com.wynntils.utils.render.type.VerticalAlignment;
 
 public class RaidProgressOverlay extends TextOverlay {
-    private static final String TEMPLATE_MILLISECONDS =
-            """
-            {concat("§6§l§n";current_raid;"
+    private static final int MAX_CHALLENGES = 3;
 
-            §dChallenge 1: ";if_str(eq(raid_room_time("challenge_1");-1);"§7--:--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_1");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_1");1000);60));2);":";leading_zeros(int(mod(raid_room_time("challenge_1");1000));3)));"
-            §dChallenge 2: ";if_str(eq(raid_room_time("challenge_2");-1);"§7--:--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_2");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_2");1000);60));2);":";leading_zeros(int(mod(raid_room_time("challenge_2");1000));3)));"
-            §dChallenge 3: ";if_str(eq(raid_room_time("challenge_3");-1);"§7--:--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_3");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_3");1000);60));2);":";leading_zeros(int(mod(raid_room_time("challenge_3");1000));3)));"
+    private String template;
 
-            §4Boss: ";if_str(eq(raid_room_time("boss_fight");-1);"§7--:--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("boss_fight");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("boss_fight");1000);60));2);":";leading_zeros(int(mod(raid_room_time("boss_fight");1000));3)));"
-
-            §5Total: ";concat("§b";leading_zeros(int(div(div(raid_time;1000);60));2);":";leading_zeros(int(mod(div(raid_time;1000);60));2);":";leading_zeros(int(mod(raid_time;1000));3)))}
-            """;
-
-    private static final String TEMPLATE_MILLISECONDS_INTERMISSION =
-            """
-            {concat("§6§l§n";current_raid;"
-
-            §dChallenge 1: ";if_str(eq(raid_room_time("challenge_1");-1);"§7--:--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_1");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_1");1000);60));2);":";leading_zeros(int(mod(raid_room_time("challenge_1");1000));3)));"
-            §dChallenge 2: ";if_str(eq(raid_room_time("challenge_2");-1);"§7--:--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_2");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_2");1000);60));2);":";leading_zeros(int(mod(raid_room_time("challenge_2");1000));3)));"
-            §dChallenge 3: ";if_str(eq(raid_room_time("challenge_3");-1);"§7--:--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_3");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_3");1000);60));2);":";leading_zeros(int(mod(raid_room_time("challenge_3");1000));3)));"
-
-            §4Boss: ";if_str(eq(raid_room_time("boss_fight");-1);"§7--:--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("boss_fight");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("boss_fight");1000);60));2);":";leading_zeros(int(mod(raid_room_time("boss_fight");1000));3)));"
-
-            §8Intermission: ";concat("§b";leading_zeros(int(div(div(raid_intermission_time;1000);60));2);":";leading_zeros(int(mod(div(raid_intermission_time;1000);60));2);":";leading_zeros(int(mod(raid_intermission_time;1000));3));"
-            §5Total: ";concat("§b";leading_zeros(int(div(div(raid_time;1000);60));2);":";leading_zeros(int(mod(div(raid_time;1000);60));2);":";leading_zeros(int(mod(raid_time;1000));3)))}
-            """;
-
-    private static final String TEMPLATE_SECONDS =
-            """
-            {concat("§6§l§n";current_raid;"
-
-            §dChallenge 1: ";if_str(eq(raid_room_time("challenge_1");-1);"§7--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_1");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_1");1000);60));2)));"
-            §dChallenge 2: ";if_str(eq(raid_room_time("challenge_2");-1);"§7--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_2");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_2");1000);60));2)));"
-            §dChallenge 3: ";if_str(eq(raid_room_time("challenge_3");-1);"§7--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_3");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_3");1000);60));2)));"
-
-            §4Boss: ";if_str(eq(raid_room_time("boss_fight");-1);"§7--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("boss_fight");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("boss_fight");1000);60));2)));"
-
-            §5Total: ";concat("§b";leading_zeros(int(div(div(raid_time;1000);60));2);":";leading_zeros(int(mod(div(raid_time;1000);60));2)))}
-            """;
-
-    private static final String TEMPLATE_SECONDS_INTERMISSION =
-            """
-            {concat("§6§l§n";current_raid;"
-
-            §dChallenge 1: ";if_str(eq(raid_room_time("challenge_1");-1);"§7--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_1");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_1");1000);60));2)));"
-            §dChallenge 2: ";if_str(eq(raid_room_time("challenge_2");-1);"§7--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_2");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_2");1000);60));2)));"
-            §dChallenge 3: ";if_str(eq(raid_room_time("challenge_3");-1);"§7--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("challenge_3");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("challenge_3");1000);60));2)));"
-
-            §4Boss: ";if_str(eq(raid_room_time("boss_fight");-1);"§7--:--";concat("§b";leading_zeros(int(div(div(raid_room_time("boss_fight");1000);60));2);":";leading_zeros(int(mod(div(raid_room_time("boss_fight");1000);60));2)));"
-
-            §8Intermission: ";concat("§b";leading_zeros(int(div(div(raid_intermission_time;1000);60));2);":";leading_zeros(int(mod(div(raid_intermission_time;1000);60));2));"
-            §5Total: ";concat("§b";leading_zeros(int(div(div(raid_time;1000);60));2);":";leading_zeros(int(mod(div(raid_time;1000);60));2)))}
-            """;
-
-    private static final String PREVIEW_TEMPLATE_MILLISECONDS =
-            """
-            §6§l§nNest of the Grootslangs
-
-            §dChallenge 1: §b01:17:022
-            §dChallenge 2: §b02:04:185
-            §dChallenge 3: §7--:--:--
-
-            §4Boss: §7--:--:--
-
-            §5Total: §b03:36:279
-            """;
-
-    private static final String PREVIEW_TEMPLATE_MILLISECONDS_INTERMISSION =
-            """
-            §6§l§nNest of the Grootslangs
-
-            §dChallenge 1: §b01:17:022
-            §dChallenge 2: §b02:04:185
-            §dChallenge 3: §7--:--:--
-
-            §4Boss: §7--:--:--
-
-            §8Intermission: §700:15:072
-            §5Total: §b03:36:279
-            """;
-
-    private static final String PREVIEW_TEMPLATE_SECONDS =
-            """
-            §6§l§nNest of the Grootslangs
-
-            §dChallenge 1: §b01:17
-            §dChallenge 2: §b02:04
-            §dChallenge 3: §7--:--
-
-            §4Boss: §7--:--
-
-            §5Total: §b03:36
-            """;
-
-    private static final String PREVIEW_TEMPLATE_SECONDS_INTERMISSION =
-            """
-            §6§l§nNest of the Grootslangs
-
-            §dChallenge 1: §b01:17
-            §dChallenge 2: §b02:04
-            §dChallenge 3: §7--:--
-
-            §4Boss: §7--:--
-
-            §8Intermission: §700:15
-            §5Total: §b03:36
-            """;
+    private String previewTemplate;
 
     @Persisted
     private final Config<Boolean> showIntermission = new Config<>(true);
 
     @Persisted
     private final Config<Boolean> showMilliseconds = new Config<>(true);
+
+    @Persisted
+    private final Config<Boolean> totalIntermission = new Config<>(true);
 
     public RaidProgressOverlay() {
         super(
@@ -137,30 +38,141 @@ public class RaidProgressOverlay extends TextOverlay {
                         OverlayPosition.AnchorSection.MIDDLE_LEFT),
                 150,
                 120);
+
+        buildTemplates();
     }
 
     @Override
     protected String getTemplate() {
-        if (showIntermission.get()) {
-            return showMilliseconds.get() ? TEMPLATE_MILLISECONDS_INTERMISSION : TEMPLATE_SECONDS_INTERMISSION;
-        }
-
-        return showMilliseconds.get() ? TEMPLATE_MILLISECONDS : TEMPLATE_SECONDS;
+        return template;
     }
 
     @Override
     protected String getPreviewTemplate() {
-        if (showIntermission.get()) {
-            return showMilliseconds.get()
-                    ? PREVIEW_TEMPLATE_MILLISECONDS_INTERMISSION
-                    : PREVIEW_TEMPLATE_SECONDS_INTERMISSION;
-        }
+        return previewTemplate;
+    }
 
-        return showMilliseconds.get() ? PREVIEW_TEMPLATE_MILLISECONDS : PREVIEW_TEMPLATE_SECONDS;
+    @Override
+    protected void onConfigUpdate(Config<?> config) {
+        buildTemplates();
     }
 
     @Override
     public boolean isRenderedDefault() {
         return Models.Raid.getCurrentRaid() != null;
+    }
+
+    private void buildTemplates() {
+        StringBuilder templateBuilder = new StringBuilder("{concat(\"§6§l§n\";current_raid;\"\n\";");
+        StringBuilder previewBuilder = new StringBuilder("§6§l§nNest of the Grootslangs\n\n");
+
+        for (int i = 0; i < MAX_CHALLENGES; i++) {
+            templateBuilder.append(getChallengeTemplate(i + 1));
+        }
+
+        for (int i = 0; i < MAX_CHALLENGES; i++) {
+            previewBuilder.append(getChallengePreview(i + 1));
+        }
+
+        templateBuilder.append(getBossTemplate());
+        previewBuilder.append(getBossPreview());
+
+        if (showIntermission.get()) {
+            templateBuilder.append(getIntermissionTemplate());
+            previewBuilder.append(getIntermissionPreview());
+        }
+
+        templateBuilder.append(getTotalTemplate());
+        previewBuilder.append(getTotalPreview());
+
+        template = templateBuilder.toString();
+        previewTemplate = previewBuilder.toString();
+    }
+
+    private String getChallengeTemplate(int challengeNum) {
+        if (showMilliseconds.get()) {
+            return "\"\n§dChallenge " + challengeNum + ": \";if_str(eq(raid_room_time(\"challenge_" + challengeNum
+                    + "\");-1);\"§7--:--.---\";concat(\"§b\";leading_zeros(int(div(div(raid_room_time(\"challenge_"
+                    + challengeNum + "\");1000);60));2);\":\";leading_zeros(int(mod(div(raid_room_time(\"challenge_"
+                    + challengeNum + "\");1000);60));2);\".\";leading_zeros(int(mod(raid_room_time(\"challenge_"
+                    + challengeNum + "\");1000));3)));";
+        }
+
+        return "\"\n§dChallenge " + challengeNum + ": \";if_str(eq(raid_room_time(\"challenge_" + challengeNum
+                + "\");-1);\"§7--:--\";concat(\"§b\";leading_zeros(int(div(div(raid_room_time(\"challenge_"
+                + challengeNum + "\");1000);60));2);\":\";leading_zeros(int(mod(div(raid_room_time(\"challenge_"
+                + challengeNum + "\");1000);60));2)));";
+    }
+
+    private String getChallengePreview(int challengeNum) {
+        if (showMilliseconds.get()) {
+            return "§dChallenge " + challengeNum + ": §b01:17.022\n";
+        }
+
+        return "§dChallenge " + challengeNum + ": §b01:17\n";
+    }
+
+    private String getBossTemplate() {
+        if (showMilliseconds.get()) {
+            return "\"\n\n§4Boss: \";if_str(eq(raid_room_time(\"boss_fight\");-1);\"§7--:--.--\";concat(\"§b\";leading_zeros(int(div(div(raid_room_time(\"boss_fight\");1000);60));2);\":\";leading_zeros(int(mod(div(raid_room_time(\"boss_fight\");1000);60));2);\".\";leading_zeros(int(mod(raid_room_time(\"boss_fight\");1000));3)));\"\n\";";
+        }
+
+        return "\"\n\n§4Boss: \";if_str(eq(raid_room_time(\"boss_fight\");-1);\"§7--:--\";concat(\"§b\";leading_zeros(int(div(div(raid_room_time(\"boss_fight\");1000);60));2);\":\";leading_zeros(int(mod(div(raid_room_time(\"boss_fight\");1000);60));2)));\"\n\";";
+    }
+
+    private String getBossPreview() {
+        if (showMilliseconds.get()) {
+            return "\n§4Boss: §7--:--.---\n";
+        }
+
+        return "\n§4Boss: §7--:--\n";
+    }
+
+    private String getIntermissionTemplate() {
+        if (showMilliseconds.get()) {
+            return "\"\n§8Intermission: \";concat(\"§b\";leading_zeros(int(div(div(raid_intermission_time;1000);60));2);\":\";leading_zeros(int(mod(div(raid_intermission_time;1000);60));2);\".\";leading_zeros(int(mod(raid_intermission_time;1000));3));";
+        }
+
+        return "\"\n§8Intermission: \";concat(\"§b\";leading_zeros(int(div(div(raid_intermission_time;1000);60));2);\":\";leading_zeros(int(mod(div(raid_intermission_time;1000);60));2));";
+    }
+
+    private String getIntermissionPreview() {
+        if (showMilliseconds.get()) {
+            return "\n§8Intermission: §700:15.072";
+        }
+
+        return "\n§8Intermission: §700:15";
+    }
+
+    private String getTotalTemplate() {
+        if (totalIntermission.get()) {
+            if (showMilliseconds.get()) {
+                return "\"\n§5Total: \";concat(\"§b\";leading_zeros(int(div(div(raid_time;1000);60));2);\":\";leading_zeros(int(mod(div(raid_time;1000);60));2);\".\";leading_zeros(int(mod(raid_time;1000));3)))}";
+            }
+
+            return "\"\n§5Total: \";concat(\"§b\";leading_zeros(int(div(div(raid_time;1000);60));2);\":\";leading_zeros(int(mod(div(raid_time;1000);60));2)))}";
+        } else {
+            if (showMilliseconds.get()) {
+                return "\"\n§5Total: \";concat(\"§b\";leading_zeros(int(div(div(sub(raid_time;raid_intermission_time);1000);60));2);\":\";leading_zeros(int(mod(div(sub(raid_time;raid_intermission_time);1000);60));2);\".\";leading_zeros(int(mod(sub(raid_time;raid_intermission_time);1000));3)))}";
+            }
+
+            return "\"\n§5Total: \";concat(\"§b\";leading_zeros(int(div(div(sub(raid_time;raid_intermission_time);1000);60));2);\":\";leading_zeros(int(mod(div(sub(raid_time;raid_intermission_time);1000);60));2)))}";
+        }
+    }
+
+    private String getTotalPreview() {
+        if (totalIntermission.get()) {
+            if (showMilliseconds.get()) {
+                return "\n§5Total: §b03:36.279";
+            }
+
+            return "\n§5Total: §b03:36";
+        } else {
+            if (showMilliseconds.get()) {
+                return "\n§5Total: §b03:21.207";
+            }
+
+            return "\n§5Total: §b03:21";
+        }
     }
 }
