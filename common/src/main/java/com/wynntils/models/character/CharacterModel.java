813064d7733bf763b3d1a92403555acb4cc4de8c
Jamie <JamCal117@gmail.com>
2024-05-10 15:28:55 +0100

    fix: Don't send 2 death messages

--- common/src/main/java/com/wynntils/models/character/CharacterModel.java
+++ common/src/main/java/com/wynntils/models/character/CharacterModel.java
@@ -167,7 +167,12 @@ public final class CharacterModel extends Model {
     public void onChatReceived(ChatMessageReceivedEvent e) {
         if (!e.getStyledText().matches(WYNN_DEATH_MESSAGE)) return;
         lastDeathLocation = Location.containing(lastPositionBeforeTeleport);
-        WynntilsMod.postEvent(new CharacterDeathEvent(lastDeathLocation));
+        CharacterDeathEvent deathEvent = new CharacterDeathEvent(lastDeathLocation);
+        WynntilsMod.postEvent(deathEvent);
+
+        if (deathEvent.isCanceled()) {
+            e.setCanceled(true);
+        }
     }
 
     @SubscribeEvent
