258797d490ff49e9963b137f130938bb5d42ed84
Magnus Ihse Bursie <mag@icus.se>
2024-05-15 14:30:51 +0200

    chore: Fix review comments

--- common/src/main/java/com/wynntils/services/mapdata/providers/builtin/CategoriesProvider.java
+++ common/src/main/java/com/wynntils/services/mapdata/providers/builtin/CategoriesProvider.java
@@ -4,6 +4,7 @@
  */
 package com.wynntils.services.mapdata.providers.builtin;
 
+import com.wynntils.models.containers.type.LootChestType;
 import com.wynntils.services.map.Label;
 import com.wynntils.services.map.type.CombatKind;
 import com.wynntils.services.map.type.ServiceKind;
@@ -35,7 +36,7 @@ public class CategoriesProvider extends BuiltInProvider {
         for (Label.LabelLayer layer : Label.LabelLayer.values()) {
             PROVIDED_CATEGORIES.add(new PlaceCategory(layer));
         }
-        for (int tier = 1; tier <= 4; tier++) {
+        for (int tier = 1; tier <= LootChestType.values().length; tier++) {
             PROVIDED_CATEGORIES.add(new FoundChestCategory(tier));
         }
         PROVIDED_CATEGORIES.add(new WaypointCategory());
@@ -107,7 +108,7 @@ public class CategoriesProvider extends BuiltInProvider {
             return new AbstractMapAttributes() {
                 @Override
                 public int getPriority() {
-                    return 500;
+                    return 1000;
                 }
 
                 @Override
