wiji1/Artemis

port: Misc mixin fixes

common/src/main/java/com/wynntils/features/tooltips/TooltipVanillaHideFeature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2022-2023.
+ * Copyright © Wynntils 2022-2024.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.features.tooltips;
@@ -29,9 +29,9 @@ public class TooltipVanillaHideFeature extends Feature {
     }
 
     @SubscribeEvent
-    public void onTooltipFlagsMask(ItemTooltipFlagsEvent.Mask event) {
+    public void onTooltipFlagsMask(ItemTooltipFlagsEvent.HideTooltip event) {
         if (!hideAdditionalInfo.get()) return;
 
-        event.setMask(-1);
+        event.setHideTooltip(true);
     }
 }

common/src/main/java/com/wynntils/mc/event/ItemTooltipFlagsEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2023.
+ * Copyright © Wynntils 2023-2024.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.mc.event;
@@ -36,20 +36,20 @@ public abstract class ItemTooltipFlagsEvent extends Event {
         }
     }
 
-    public static final class Mask extends ItemTooltipFlagsEvent {
-        private int mask;
+    public static final class HideTooltip extends ItemTooltipFlagsEvent {
+        private boolean hideTooltip;
 
-        public Mask(ItemStack itemStack, int mask) {
+        public HideTooltip(ItemStack itemStack, boolean hideTooltip) {
             super(itemStack);
-            this.mask = mask;
+            this.hideTooltip = hideTooltip;
         }
 
-        public int getMask() {
-            return mask;
+        public boolean getHideTooltip() {
+            return hideTooltip;
         }
 
-        public void setMask(int mask) {
-            this.mask = mask;
+        public void setHideTooltip(boolean hideTooltip) {
+            this.hideTooltip = hideTooltip;
         }
     }
 }

Wynntils/Wynntils

common/src/main/java/com/wynntils/features/tooltips/TooltipVanillaHideFeature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2022-2023.
+ * Copyright © Wynntils 2022-2024.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.features.tooltips;
@@ -29,9 +29,9 @@ public class TooltipVanillaHideFeature extends Feature {
     }
 
     @SubscribeEvent
-    public void onTooltipFlagsMask(ItemTooltipFlagsEvent.Mask event) {
+    public void onTooltipFlagsMask(ItemTooltipFlagsEvent.HideAdditionalTooltip event) {
         if (!hideAdditionalInfo.get()) return;
 
-        event.setMask(-1);
+        event.setHideAdditionalTooltip(true);
     }
 }

common/src/main/java/com/wynntils/mc/event/ItemTooltipFlagsEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2023.
+ * Copyright © Wynntils 2023-2024.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.mc.event;
@@ -36,20 +36,20 @@ public abstract class ItemTooltipFlagsEvent extends Event {
         }
     }
 
-    public static final class Mask extends ItemTooltipFlagsEvent {
-        private int mask;
+    public static final class HideAdditionalTooltip extends ItemTooltipFlagsEvent {
+        private boolean hideTooltip;
 
-        public Mask(ItemStack itemStack, int mask) {
+        public HideAdditionalTooltip(ItemStack itemStack, boolean hideTooltip) {
             super(itemStack);
-            this.mask = mask;
+            this.hideTooltip = hideTooltip;
         }
 
-        public int getMask() {
-            return mask;
+        public boolean getHideAdditionalTooltip() {
+            return hideTooltip;
         }
 
-        public void setMask(int mask) {
-            this.mask = mask;
+        public void setHideAdditionalTooltip(boolean hideTooltip) {
+            this.hideTooltip = hideTooltip;
         }
     }
 }