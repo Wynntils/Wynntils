0c863977128fb9305a4894e460aa8450e7066574
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2024-07-02 21:41:31 -0700

    undo i18n

--- common/src/main/java/com/wynntils/features/TerritoryDefenseMessageFeature.java
+++ common/src/main/java/com/wynntils/features/TerritoryDefenseMessageFeature.java
@@ -44,9 +44,7 @@ public class TerritoryDefenseMessageFeature extends Feature {
             Matcher matcher = StyledText.fromComponent(tooltipLine)
                     .getMatcher(TERRITORY_DEFENSE_PATTERN, PartStyle.StyleType.NONE);
             if (matcher.matches()) {
-                String message = I18n.get(
-                        "feature.wynntils.territoryDefenseMessage.message", titleMatcher.group(1), matcher.group(1));
-                Handlers.Command.sendCommandImmediately("g " + message);
+                Handlers.Command.sendCommandImmediately("g %s defense is %s".formatted(titleMatcher.group(1), matcher.group(1)));
                 return;
             }
         }
