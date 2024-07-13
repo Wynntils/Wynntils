4bc14106e9a52303803df903f300bcb5d889a9c3
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2024-07-13 09:50:38 +0200

    feat: Port LabelHandler to TextDisplay (without porting usages) [skip ci] (#2653)
    feat: Port LabelHandler to TextDisplay (without porting usages)
    
    This commit ports LabelHandler to TextDisplays, which are used instead of hidden armor stands in newer MC versions.

--- common/src/main/java/com/wynntils/handlers/labels/event/EntityLabelChangedEvent.java
+++ common/src/main/java/com/wynntils/handlers/labels/event/EntityLabelChangedEvent.java
@@ -11,6 +11,8 @@ import net.minecraft.world.entity.Entity;
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.ICancellableEvent;
 
+// Use TextDisplayChangedEvent.Component instead, Wynn does not use armor stands for most of its labels
+@Deprecated
 public class EntityLabelChangedEvent extends Event implements ICancellableEvent {
     private final Entity entity;
     private final StyledText oldName;
