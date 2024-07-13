937c18a2ced5a5981950ca46a8b316ca70c98e4a
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2024-07-13 10:18:09 +0200

    feat: Port ExtendedSeasonLeaderboardFeature to TextDisplay (+ pass LabelInfo in TextDisplayChangedEvent) [skip ci] (#2655)
    feat: Port ExtendedSeasonLeaderboardFeature to TextDisplay (+ pass LabelInfo in TextDisplayChangedEvent)

--- common/src/main/java/com/wynntils/handlers/labels/event/TextDisplayChangedEvent.java
+++ common/src/main/java/com/wynntils/handlers/labels/event/TextDisplayChangedEvent.java
@@ -5,6 +5,8 @@
 package com.wynntils.handlers.labels.event;
 
 import com.wynntils.core.text.StyledText;
+import com.wynntils.handlers.labels.type.LabelInfo;
+import java.util.Optional;
 import net.minecraft.world.entity.Display;
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.ICancellableEvent;
@@ -16,20 +18,26 @@ import net.neoforged.bus.api.ICancellableEvent;
  */
 public abstract class TextDisplayChangedEvent extends Event implements ICancellableEvent {
     private final Display.TextDisplay textDisplay;
+    private final LabelInfo labelInfo;
 
-    protected TextDisplayChangedEvent(Display.TextDisplay textDisplay) {
+    protected TextDisplayChangedEvent(Display.TextDisplay textDisplay, LabelInfo labelInfo) {
         this.textDisplay = textDisplay;
+        this.labelInfo = labelInfo;
     }
 
     public Display.TextDisplay getTextDisplay() {
         return textDisplay;
     }
 
+    public Optional<LabelInfo> getLabelInfo() {
+        return Optional.ofNullable(labelInfo);
+    }
+
     public static class Text extends TextDisplayChangedEvent {
         private StyledText text;
 
-        public Text(Display.TextDisplay textDisplay, StyledText text) {
-            super(textDisplay);
+        public Text(Display.TextDisplay textDisplay, LabelInfo labelInfo, StyledText text) {
+            super(textDisplay, labelInfo);
             this.text = text;
         }
 
