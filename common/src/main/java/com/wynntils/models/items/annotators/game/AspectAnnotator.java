6504126cace5ec5f12b3aa822f9268a777f69393
Jamie <JamCal117@gmail.com>
2024-08-03 12:09:04 +0100

    fix: Annotate mythic aspects

--- common/src/main/java/com/wynntils/models/items/annotators/game/AspectAnnotator.java
+++ common/src/main/java/com/wynntils/models/items/annotators/game/AspectAnnotator.java
@@ -20,6 +20,7 @@ import net.minecraft.world.item.ItemStack;
 
 public class AspectAnnotator implements GameItemAnnotator {
     private static final Pattern ASPECT_PATTERN = Pattern.compile("^§(.)Aspect of .*$");
+    private static final Pattern MYTHIC_ASPECT_PATTERN = Pattern.compile("^§(.)(.*) Embodiment of .*$");
     private static final Pattern CLASS_PATTERN = Pattern.compile("^§(?:c✖|a✔) §7Class Req: §f(?<name>.+)$");
     // TODO: Check tier 4 aspects as this pattern may not match those
     private static final Pattern TIER_PATTERN = Pattern.compile("§7Tier ([IV]+).*");
@@ -27,7 +28,11 @@ public class AspectAnnotator implements GameItemAnnotator {
     @Override
     public ItemAnnotation getAnnotation(ItemStack itemStack, StyledText name) {
         Matcher matcher = name.getMatcher(ASPECT_PATTERN);
-        if (!matcher.matches()) return null;
+        if (!matcher.matches()) {
+            matcher = name.getMatcher(MYTHIC_ASPECT_PATTERN);
+
+            if (!matcher.matches()) return null;
+        }
 
         ClassType classType = null;
         int tier = 0;
