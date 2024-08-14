95c458d3c3702d5daa61c99db23a82c90865e731
Rafii2198 <rafii2198@gmail.com>
2024-08-14 13:55:56 +0200

    fix: Move emeralds higher to avoid clipping

--- common/src/main/java/com/wynntils/features/inventory/InventoryEmeraldCountFeature.java
+++ common/src/main/java/com/wynntils/features/inventory/InventoryEmeraldCountFeature.java
@@ -97,7 +97,7 @@ public class InventoryEmeraldCountFeature extends Feature {
                 && showInventoryEmeraldCount.get()
                 && bottomEmeralds != 0;
         if (topEmeralds != 0) {
-            int y = containerScreen.topPos;
+            int y = containerScreen.topPos - 9;
             switch (emeraldCountType.get()) {
                 case TEXT -> renderTextCount(event.getPoseStack(), textX, y, topEmeralds);
                 case TEXTURE -> {
