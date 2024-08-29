75d1249a2e4af6c6dd2c2e03a7a5a1970d40320c
Wasinthorn Suksri <mccommanderminecraft@gmail.com>
2024-08-29 15:12:43 +0700

    fix: fix transparent cape for ghost players [skip ci] (#2790)
    * fix: fix transparent cape for ghost players
    
    * fix: do not use core component directly, renaming method
    
    * ci: spotless formatting
    
    * fix: use fully-qualified mixin target
    
    * feat: add PlayerFeatureRenderTranslucentCheckEvent.Cape event to set translucence only for cape
    
    * fix: remove abstract from new event
    
    * chore: apply requested changes
    
    * fix: rename event class, apply requested changes
    
    ---------
    
    Co-authored-by: SteveKunG <SteveKunG@users.noreply.github.com>

---
+++ common/src/main/java/com/wynntils/mc/extension/PlayerModelExtension.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright © Wynntils 2022-2024.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.mc.extension;
+
+public interface PlayerModelExtension {
+    void setTranslucenceCape(float translucence);
+}
