bac643b9c43c1e29ccee7e25d2e9c2d405652839
Essentuan <51426319+Essentuan@users.noreply.github.com>
2024-09-10 12:35:31 -0500

    Remove unused timestampWidth

--- common/src/main/java/com/wynntils/mc/mixin/ChatComponentMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/ChatComponentMixin.java
@@ -21,7 +21,6 @@ import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -46,9 +45,6 @@ public abstract class ChatComponentMixin implements ChatComponentExtension {
     @Shadow
     @Final
     private Minecraft minecraft;
-
-    @Unique
-    private int timestampWidth = 0;
 
     @WrapOperation(
             method = "addMessageToDisplayQueue",
