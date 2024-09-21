1c55f5517b7cac1dd577b29de3c85e778c876237
Eve <phanta@phanta.pl>
2024-09-22 00:30:23 +0200

    Add option for showing gear icons on unboxed unidentified items
    Also, use an enum map for the gear icon texture coordinate table

--- common/src/main/java/com/wynntils/features/inventory/UnidentifiedItemIconFeature.java
+++ common/src/main/java/com/wynntils/features/inventory/UnidentifiedItemIconFeature.java
@@ -14,10 +14,13 @@ import com.wynntils.core.persisted.config.ConfigCategory;
 import com.wynntils.mc.event.HotbarSlotRenderEvent;
 import com.wynntils.mc.event.SlotRenderEvent;
 import com.wynntils.models.gear.type.GearType;
+import com.wynntils.models.items.WynnItem;
 import com.wynntils.models.items.items.game.GearBoxItem;
+import com.wynntils.models.items.items.game.GearItem;
 import com.wynntils.utils.render.RenderUtils;
 import com.wynntils.utils.render.Texture;
 import com.wynntils.utils.type.Pair;
+import java.util.EnumMap;
 import java.util.Map;
 import java.util.Optional;
 import net.minecraft.world.item.ItemStack;
@@ -25,24 +28,30 @@ import net.neoforged.bus.api.SubscribeEvent;
 
 @ConfigCategory(Category.INVENTORY)
 public class UnidentifiedItemIconFeature extends Feature {
-    private static final Map<GearType, Pair<Integer, Integer>> TEXTURE_COORDS = Map.ofEntries(
-            Map.entry(GearType.SPEAR, Pair.of(16 * 1, 16 * 1)),
-            Map.entry(GearType.WAND, Pair.of(16 * 0, 16 * 1)),
-            Map.entry(GearType.DAGGER, Pair.of(16 * 2, 16 * 1)),
-            Map.entry(GearType.BOW, Pair.of(16 * 3, 16 * 1)),
-            Map.entry(GearType.RELIK, Pair.of(16 * 0, 16 * 2)),
-            Map.entry(GearType.RING, Pair.of(16 * 1, 16 * 2)),
-            Map.entry(GearType.BRACELET, Pair.of(16 * 2, 16 * 2)),
-            Map.entry(GearType.NECKLACE, Pair.of(16 * 3, 16 * 2)),
-            Map.entry(GearType.HELMET, Pair.of(16 * 0, 16 * 0)),
-            Map.entry(GearType.CHESTPLATE, Pair.of(16 * 1, 16 * 0)),
-            Map.entry(GearType.LEGGINGS, Pair.of(16 * 2, 16 * 0)),
-            Map.entry(GearType.BOOTS, Pair.of(16 * 3, 16 * 0)),
-            Map.entry(GearType.MASTERY_TOME, Pair.of(16 * 0, 16 * 3)),
-            Map.entry(GearType.CHARM, Pair.of(16 * 1, 16 * 3)));
+    private static final Map<GearType, Pair<Integer, Integer>> TEXTURE_COORDS = new EnumMap<>(GearType.class);
+
+    static {
+        TEXTURE_COORDS.put(GearType.SPEAR, Pair.of(16 * 1, 16 * 1));
+        TEXTURE_COORDS.put(GearType.WAND, Pair.of(16 * 0, 16 * 1));
+        TEXTURE_COORDS.put(GearType.DAGGER, Pair.of(16 * 2, 16 * 1));
+        TEXTURE_COORDS.put(GearType.BOW, Pair.of(16 * 3, 16 * 1));
+        TEXTURE_COORDS.put(GearType.RELIK, Pair.of(16 * 0, 16 * 2));
+        TEXTURE_COORDS.put(GearType.RING, Pair.of(16 * 1, 16 * 2));
+        TEXTURE_COORDS.put(GearType.BRACELET, Pair.of(16 * 2, 16 * 2));
+        TEXTURE_COORDS.put(GearType.NECKLACE, Pair.of(16 * 3, 16 * 2));
+        TEXTURE_COORDS.put(GearType.HELMET, Pair.of(16 * 0, 16 * 0));
+        TEXTURE_COORDS.put(GearType.CHESTPLATE, Pair.of(16 * 1, 16 * 0));
+        TEXTURE_COORDS.put(GearType.LEGGINGS, Pair.of(16 * 2, 16 * 0));
+        TEXTURE_COORDS.put(GearType.BOOTS, Pair.of(16 * 3, 16 * 0));
+        TEXTURE_COORDS.put(GearType.MASTERY_TOME, Pair.of(16 * 0, 16 * 3));
+        TEXTURE_COORDS.put(GearType.CHARM, Pair.of(16 * 1, 16 * 3));
+    }
 
     @Persisted
     public final Config<UnidentifiedItemTextures> texture = new Config<>(UnidentifiedItemTextures.WYNN);
+
+    @Persisted
+    public final Config<Boolean> showOnUnboxed = new Config<>(true);
 
     @SubscribeEvent
     public void onSlotRender(SlotRenderEvent.CountPre e) {
@@ -55,10 +64,8 @@ public class UnidentifiedItemIconFeature extends Feature {
     }
 
     private void drawIcon(PoseStack poseStack, ItemStack itemStack, int slotX, int slotY, int z) {
-        Optional<GearBoxItem> gearBoxItemOpt = Models.Item.asWynnItem(itemStack, GearBoxItem.class);
-        if (gearBoxItemOpt.isEmpty()) return;
-
-        GearType gearType = gearBoxItemOpt.get().getGearType();
+        GearType gearType = getUnidentifiedGearType(itemStack);
+        if (gearType == null) return;
 
         int textureX = TEXTURE_COORDS.get(gearType).a();
         int textureY = TEXTURE_COORDS.get(gearType).b();
@@ -76,6 +83,22 @@ public class UnidentifiedItemIconFeature extends Feature {
                 16,
                 Texture.GEAR_ICONS.width(),
                 Texture.GEAR_ICONS.height());
+    }
+
+    private GearType getUnidentifiedGearType(ItemStack itemStack) {
+        Optional<WynnItem> wynnItemOpt = Models.Item.getWynnItem(itemStack);
+        if (wynnItemOpt.isEmpty()) return null;
+        WynnItem wynnItem = wynnItemOpt.get();
+
+        if (wynnItem instanceof GearBoxItem box) {
+            return box.getGearType();
+        }
+
+        if (showOnUnboxed.get() && wynnItem instanceof GearItem gear && gear.isUnidentified()) {
+            return gear.getGearType();
+        }
+
+        return null;
     }
 
     public enum UnidentifiedItemTextures {
