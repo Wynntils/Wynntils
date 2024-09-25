f92f1dfe6f910eab96398cd21efec82a2d5897b9
Jamie <JamCal117@gmail.com>
2024-09-25 15:29:54 +0100

    refactor: Use Supplier

--- common/src/main/java/com/wynntils/features/wynntils/BetaWarningFeature.java
+++ common/src/main/java/com/wynntils/features/wynntils/BetaWarningFeature.java
@@ -5,14 +5,15 @@
 package com.wynntils.features.wynntils;
 
 import com.wynntils.core.WynntilsMod;
-import com.wynntils.core.components.Handlers;
 import com.wynntils.core.components.Managers;
 import com.wynntils.core.consumers.features.Feature;
 import com.wynntils.core.mod.event.WynncraftConnectionEvent;
+import com.wynntils.core.net.UrlId;
 import com.wynntils.core.persisted.config.Category;
 import com.wynntils.core.persisted.config.ConfigCategory;
 import com.wynntils.mc.event.PlayerInfoEvent;
 import com.wynntils.utils.mc.McUtils;
+import java.util.function.Supplier;
 import net.minecraft.network.chat.Component;
 import net.neoforged.bus.api.SubscribeEvent;
 
@@ -39,28 +40,26 @@ public class BetaWarningFeature extends Feature {
         if (warnType == WarnType.NONE) return;
 
         McUtils.sendMessageToClient(warnType.getWarning());
-
-        // Send a link to our Discord server for beta builds, we can't include the link in the message as Urls have
-        // not been loaded by the time the enum is initialised
-        if (warnType == WarnType.RELEASE) {
-            Handlers.Command.sendCommandImmediately("wynntils discord");
-        }
         warnType = WarnType.NONE;
     }
 
+    // As urls may not be initialised yet, we have to use a supplier so that it will be loaded when we need
+    // to access the Component which will only be after joining the server and urls should be loaded by then.
     private enum WarnType {
-        NONE(Component.empty()),
-        BETA(Component.translatable("feature.wynntils.betaWarning.usingBetaOnNormalServer")),
-        RELEASE(Component.translatable("feature.wynntils.betaWarning.usingReleaseOnBetaServer"));
+        NONE(Component::empty),
+        BETA(() -> Component.translatable("feature.wynntils.betaWarning.usingBetaOnNormalServer")),
+        RELEASE(() -> Component.translatable(
+                "feature.wynntils.betaWarning.usingReleaseOnBetaServer",
+                Managers.Url.getUrl(UrlId.LINK_WYNNTILS_DISCORD_INVITE)));
 
-        private final Component warning;
+        private final Supplier<Component> warningSupplier;
 
-        WarnType(Component warning) {
-            this.warning = warning;
+        WarnType(Supplier<Component> warningSupplier) {
+            this.warningSupplier = warningSupplier;
         }
 
         public Component getWarning() {
-            return warning;
+            return warningSupplier.get();
         }
     }
 }
