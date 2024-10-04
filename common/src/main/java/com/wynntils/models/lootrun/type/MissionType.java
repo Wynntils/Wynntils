77e5e264e5aba93799fd423534ecae7e14016dde
MakiilGrosnach <157435355+MakiilGrosnach@users.noreply.github.com>
2024-10-04 16:33:29 -0400

    Updated MissionType.java | 10/04/2024 - 4:31PM EST
    Added 2.1 Mission choices:
    
    HOARDER
    JESTER'S TRICK
    TREASURE HUNTING
    GOURMAND
    PORPHYROPHOBIA
    CHRONOKINESIS
    SAFETY SEEKER
    WARMTH DEVOURER
    COMPLETE CHAOS

--- common/src/main/java/com/wynntils/models/lootrun/type/MissionType.java
+++ common/src/main/java/com/wynntils/models/lootrun/type/MissionType.java
@@ -13,17 +13,26 @@ public enum MissionType {
     FAILED("Failed", ChatFormatting.DARK_RED),
 
     CLEANSING_GREED("Cleansing Greed", ChatFormatting.YELLOW),
+    HOARDER("Hoarder", ChatFormatting.YELLOW),
+    JESTERS_TRICK("Jester's Trick", ChatFormatting.YELLOW),
+    TREASURE_HUNTING("Treasure Hunting", ChatFormatting.YELLOW),
     HIGH_ROLLER("High Roller", ChatFormatting.YELLOW),
     MATERIALISM("Materialism", ChatFormatting.YELLOW),
     ORPHIONS_GRACE("Orphion's Grace", ChatFormatting.BLUE),
+    GOURMAND("Gourmand", ChatFormatting.BLUE),
+    PORPHYROPHOBIA("Porphyrophobia", ChatFormatting.BLUE),
     CLEANSING_RITUAL("Cleansing Ritual", ChatFormatting.DARK_PURPLE),
     EQUILIBRIUM("Equilibrium", ChatFormatting.DARK_PURPLE),
     INNER_PEACE("Inner Peace", ChatFormatting.DARK_PURPLE),
     BACKUP_BEAT("Backup Beat", ChatFormatting.GREEN),
     STASIS("Stasis", ChatFormatting.GREEN),
+    CHRONOKINESIS("Chronokinesis", ChatFormatting.GREEN),
     GAMBLING_BEAST("Gambling Beast", ChatFormatting.RED),
     REDEMPTION("Redemption", ChatFormatting.RED),
-    ULTIMATE_SACRIFICE("Ultimate Sacrifice", ChatFormatting.RED);
+    ULTIMATE_SACRIFICE("Ultimate Sacrifice", ChatFormatting.RED),
+    SAFETY_SEEKER("Safety Seeker", ChatFormatting.RED),
+    WARMTH_DEVOURER("Warmth Devourer", ChatFormatting.RED),
+    COMPLETE_CHAOS("Complete Chaos", ChatFormatting.WHITE);
 
     private final String name;
     private final ChatFormatting color;
