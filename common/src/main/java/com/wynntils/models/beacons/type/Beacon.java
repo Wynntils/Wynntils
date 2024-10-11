873a1daae19e826fad19ab56296d9840796309bf
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2024-10-11 11:34:41 +0200

    fix: fix lootrun beacon serialization crashing

--- common/src/main/java/com/wynntils/models/beacons/type/Beacon.java
+++ common/src/main/java/com/wynntils/models/beacons/type/Beacon.java
@@ -4,6 +4,41 @@
  */
 package com.wynntils.models.beacons.type;
 
+import java.util.Objects;
 import net.minecraft.world.phys.Vec3;
 
-public record Beacon(Vec3 position, BeaconKind beaconKind) {}
+public final class Beacon<T extends BeaconKind> {
+    private final Vec3 position;
+    private final T beaconKind;
+
+    public Beacon(Vec3 position, T beaconKind) {
+        this.position = position;
+        this.beaconKind = beaconKind;
+    }
+
+    public Vec3 position() {
+        return position;
+    }
+
+    public T beaconKind() {
+        return beaconKind;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Beacon) obj;
+        return Objects.equals(this.position, that.position) && Objects.equals(this.beaconKind, that.beaconKind);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, beaconKind);
+    }
+
+    @Override
+    public String toString() {
+        return "Beacon[" + "position=" + position + ", " + "beaconKind=" + beaconKind + ']';
+    }
+}
