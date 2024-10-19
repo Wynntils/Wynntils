e7a7adadd0b450714a88691f394fdac6d3a96623
magicus <magicus@users.noreply.github.com>
2024-10-19 10:26:10 +0000

    ci: spotless formatting

--- common/src/main/java/com/wynntils/features/map/MainMapFeature.java
+++ common/src/main/java/com/wynntils/features/map/MainMapFeature.java
@@ -18,7 +18,6 @@ import com.wynntils.services.map.pois.CustomPoi;
 import com.wynntils.utils.colors.CustomColor;
 import com.wynntils.utils.mc.McUtils;
 import com.wynntils.utils.mc.type.Location;
-import com.wynntils.utils.mc.type.PoiLocation;
 import com.wynntils.utils.render.type.HealthTexture;
 import com.wynntils.utils.render.type.PointerType;
 import java.util.ArrayList;
