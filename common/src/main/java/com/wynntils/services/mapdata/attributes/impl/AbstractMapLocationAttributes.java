c5b8f2df571149f505273aa220f299b6e1a6ecc1
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2024-10-25 13:00:29 +0200

    fix: Block marker options from paths and areas, refactor how "blocking" works (#2950)

--- common/src/main/java/com/wynntils/services/mapdata/attributes/impl/AbstractMapLocationAttributes.java
+++ common/src/main/java/com/wynntils/services/mapdata/attributes/impl/AbstractMapLocationAttributes.java
@@ -5,5 +5,22 @@
 package com.wynntils.services.mapdata.attributes.impl;
 
 import com.wynntils.services.mapdata.attributes.type.MapLocationAttributes;
+import com.wynntils.utils.colors.CustomColor;
+import java.util.Optional;
 
-public abstract class AbstractMapLocationAttributes extends AbstractMapAttributes implements MapLocationAttributes {}
+public abstract class AbstractMapLocationAttributes extends AbstractMapAttributes implements MapLocationAttributes {
+    @Override
+    public final Optional<CustomColor> getFillColor() {
+        return Optional.empty();
+    }
+
+    @Override
+    public final Optional<CustomColor> getBorderColor() {
+        return Optional.empty();
+    }
+
+    @Override
+    public final Optional<Float> getBorderWidth() {
+        return Optional.empty();
+    }
+}
