2efba4da747daea764bbc0be17d573ec06a9c171
Jamie <JamCal117@gmail.com>
2024-10-29 14:08:20 +0000

    fix: Don't clear mantle shield when on cooldown [skip ci] (#2962)

--- common/src/main/java/com/wynntils/models/abilities/type/ShieldType.java
+++ common/src/main/java/com/wynntils/models/abilities/type/ShieldType.java
@@ -23,13 +23,20 @@ public abstract class ShieldType {
     }
 
     public boolean validSpell(SpellType spellType) {
-        return spellType == validSpell;
+        return spellType == validSpell && shouldClearOnSpellCast();
     }
 
     public boolean verifyShield(ArmorStand armorStand) {
         if (!validClass()) return false;
 
         return verifyArmorStand(armorStand);
+    }
+
+    // Arrow shield is part of the spell, so it will always be reset on the spell being cast
+    // but shields such as Mantle shield is an ability added to the war scream spell, so it
+    // is based on a cooldown rather than the spell itself
+    protected boolean shouldClearOnSpellCast() {
+        return true;
     }
 
     protected abstract boolean verifyArmorStand(ArmorStand armorStand);
