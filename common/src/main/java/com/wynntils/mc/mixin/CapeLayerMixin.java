68770094d6dfd145ddfef204d52244686042ea5e
Jamie <JamCal117@gmail.com>
2024-11-03 15:42:30 +0000

    wip: Port PlayerGhostTransparencyFeature (cape transparency needs fixing)

--- common/src/main/java/com/wynntils/mc/mixin/CapeLayerMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/CapeLayerMixin.java
@@ -4,22 +4,15 @@
  */
 package com.wynntils.mc.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.wynntils.core.events.MixinHelper;
 import com.wynntils.mc.event.PlayerRenderLayerEvent;
-import com.wynntils.mc.event.RenderTranslucentCheckEvent;
-import com.wynntils.mc.extension.PlayerModelExtension;
-import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.layers.CapeLayer;
 import net.minecraft.client.renderer.entity.state.PlayerRenderState;
-import net.minecraft.client.resources.PlayerSkin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CapeLayer.class)
@@ -49,27 +42,30 @@ public abstract class CapeLayerMixin {
      * because {@link net.minecraft.client.model.PlayerModel#renderCloak(PoseStack, VertexConsumer, int, int)} method does not accept {@code color}
      * argument
      */
-    @ModifyArg(
-            method =
-                    "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/player/AbstractClientPlayer;FFFFFF)V",
-            at =
-                    @At(
-                            value = "INVOKE",
-                            target =
-                                    "net/minecraft/client/renderer/MultiBufferSource.getBuffer(Lnet/minecraft/client/renderer/RenderType;)Lcom/mojang/blaze3d/vertex/VertexConsumer;"))
-    private RenderType setTranslucenceCapeRenderType(
-            RenderType original,
-            @Local(argsOnly = true) AbstractClientPlayer livingEntity,
-            @Local PlayerSkin playerSkin) {
-        // Always set default translucence value to 1.0f, because cape layer doesn't rendered same as ghost player.
-        // It hidden by checking if player is invisible or cape model part is turned off
-        RenderTranslucentCheckEvent.Cape event = new RenderTranslucentCheckEvent.Cape(false, livingEntity, 1.0f);
-        MixinHelper.post(event);
-
-        float translucence = event.getTranslucence();
-
-        ((PlayerModelExtension) ((CapeLayer) (Object) this).getParentModel()).setTranslucenceCape(translucence);
-
-        return event.isTranslucent() ? RenderType.entityTranslucent(playerSkin.capeTexture()) : original;
-    }
+    //    @ModifyArg(
+    //            method =
+    //
+    // "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/player/AbstractClientPlayer;FFFFFF)V",
+    //            at =
+    //                    @At(
+    //                            value = "INVOKE",
+    //                            target =
+    //
+    // "net/minecraft/client/renderer/MultiBufferSource.getBuffer(Lnet/minecraft/client/renderer/RenderType;)Lcom/mojang/blaze3d/vertex/VertexConsumer;"))
+    //    private RenderType setTranslucenceCapeRenderType(
+    //            RenderType original,
+    //            @Local(argsOnly = true) AbstractClientPlayer livingEntity,
+    //            @Local PlayerSkin playerSkin) {
+    //        // Always set default translucence value to 1.0f, because cape layer doesn't rendered same as ghost
+    // player.
+    //        // It hidden by checking if player is invisible or cape model part is turned off
+    //        RenderTranslucentCheckEvent.Cape event = new RenderTranslucentCheckEvent.Cape(false, livingEntity, 1.0f);
+    //        MixinHelper.post(event);
+    //
+    //        float translucence = event.getTranslucence();
+    //
+    //        ((PlayerModelExtension) ((CapeLayer) (Object) this).getParentModel()).setTranslucenceCape(translucence);
+    //
+    //        return event.isTranslucent() ? RenderType.entityTranslucent(playerSkin.capeTexture()) : original;
+    //    }
 }
