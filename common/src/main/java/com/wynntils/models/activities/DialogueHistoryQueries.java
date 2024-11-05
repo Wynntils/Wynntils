9264245f987df3b8922000a00d9a5bee073c89f3
Jamie <JamCal117@gmail.com>
2024-11-05 14:14:50 +0000

    fix: Fix dialogue history parsing

--- common/src/main/java/com/wynntils/models/activities/DialogueHistoryQueries.java
+++ common/src/main/java/com/wynntils/models/activities/DialogueHistoryQueries.java
@@ -23,7 +23,7 @@ import net.minecraft.world.item.ItemStack;
 public class DialogueHistoryQueries {
     private static final int DIALOGUE_HISTORY_SLOT = 58;
     private static final Pattern DIALOGUE_HISTORY_PAGE_PATTERN = Pattern.compile("§8Page (\\d+) of (\\d+)");
-    private static final StyledText DIALOGUE_HISTORY = StyledText.fromString("§2Dialogue History");
+    private static final StyledText DIALOGUE_HISTORY = StyledText.fromString("Dialogue History");
 
     private List<List<StyledText>> newDialogueHistory;
 
@@ -37,6 +37,7 @@ public class DialogueHistoryQueries {
                 .onError(msg -> {
                     WynntilsMod.warn("Problem getting dialogue history: " + msg);
                     McUtils.sendErrorToClient("Dumping Dialogue History failed");
+                    newDialogueHistory = null;
                 })
 
                 // Open content book
