eaacac5928ac07a706424d1494db678d66652cf1
Jamie <JamCal117@gmail.com>
2024-11-10 10:50:54 +0000

    feat: Block third person in character selection

--- common/src/main/java/com/wynntils/features/utilities/CharacterSelectionUtilitiesFeature.java
+++ common/src/main/java/com/wynntils/features/utilities/CharacterSelectionUtilitiesFeature.java
@@ -6,17 +6,26 @@ package com.wynntils.features.utilities;
 
 import com.wynntils.core.components.Models;
 import com.wynntils.core.consumers.features.Feature;
+import com.wynntils.core.persisted.Persisted;
 import com.wynntils.core.persisted.config.Category;
+import com.wynntils.core.persisted.config.Config;
 import com.wynntils.core.persisted.config.ConfigCategory;
 import com.wynntils.mc.event.InventoryKeyPressEvent;
+import com.wynntils.mc.event.KeyInputEvent;
 import com.wynntils.models.containers.containers.CharacterSelectionContainer;
+import com.wynntils.models.worlds.event.WorldStateEvent;
+import com.wynntils.models.worlds.type.WorldState;
 import com.wynntils.utils.mc.McUtils;
 import java.util.List;
+import net.minecraft.client.CameraType;
 import net.minecraft.client.KeyMapping;
 import net.neoforged.bus.api.SubscribeEvent;
 
 @ConfigCategory(Category.UTILITIES)
 public class CharacterSelectionUtilitiesFeature extends Feature {
+    @Persisted
+    public final Config<Boolean> blockThirdPerson = new Config<>(true);
+
     @SubscribeEvent
     public void onInventoryKeyPress(InventoryKeyPressEvent e) {
         if (!(Models.Container.getCurrentContainer() instanceof CharacterSelectionContainer)) return;
@@ -31,5 +40,24 @@ public class CharacterSelectionUtilitiesFeature extends Feature {
             Models.CharacterSelection.playWithCharacter(slot);
             break;
         }
+    }
+
+    @SubscribeEvent
+    public void onKeyPress(KeyInputEvent e) {
+        if (!blockThirdPerson.get()) return;
+        if (Models.WorldState.getCurrentState() != WorldState.CHARACTER_SELECTION) return;
+
+        KeyMapping perspectiveKey = McUtils.options().keyTogglePerspective;
+        if (perspectiveKey.matches(e.getKey(), e.getScanCode())) {
+            e.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onWorldStateChange(WorldStateEvent e) {
+        if (!blockThirdPerson.get()) return;
+        if (e.getNewState() != WorldState.CHARACTER_SELECTION) return;
+
+        McUtils.options().setCameraType(CameraType.FIRST_PERSON);
     }
 }
