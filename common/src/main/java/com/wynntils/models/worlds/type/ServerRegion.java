ec860167320d02df347c2d87b56a2a4cad100c20
Jamie <JamCal117@gmail.com>
2024-12-21 13:16:51 +0000

    feat: Add some basic support for regional servers

---
+++ common/src/main/java/com/wynntils/models/worlds/type/ServerRegion.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © Wynntils 2024.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.models.worlds.type;
+
+public enum ServerRegion {
+    WC,
+    NA,
+    SA,
+    AF,
+    EU,
+    AS,
+    AU
+}
