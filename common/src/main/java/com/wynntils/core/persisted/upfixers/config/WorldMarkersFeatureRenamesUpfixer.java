4bd279d53b362db7394c6149534d13aa16964fb7
kristofbolyai <kristofbolyai@users.noreply.github.com>
2025-01-03 09:16:40 +0000

    ci: spotless formatting

--- common/src/main/java/com/wynntils/core/persisted/upfixers/config/WorldMarkersFeatureRenamesUpfixer.java
+++ common/src/main/java/com/wynntils/core/persisted/upfixers/config/WorldMarkersFeatureRenamesUpfixer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2024.
+ * Copyright © Wynntils 2024-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.core.persisted.upfixers.config;
