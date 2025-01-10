bfb7eb1b893906ed0922a5fd3230b19388718f1c
ryanzhoudev <57310593+ryanzhoudev@users.noreply.github.com>
2025-01-10 11:41:49 -0800

    same for party

--- common/src/main/java/com/wynntils/screens/gearviewer/widgets/PartyButton.java
+++ common/src/main/java/com/wynntils/screens/gearviewer/widgets/PartyButton.java
@@ -1,10 +1,13 @@
 package com.wynntils.screens.gearviewer.widgets;
 
 import com.wynntils.core.components.Handlers;
+import com.wynntils.models.players.event.PartyEvent;
 import net.minecraft.network.chat.Component;
+import net.neoforged.bus.api.SubscribeEvent;
 
 public class PartyButton extends PlayerInteractionButton {
     private final String playerName;
+    private boolean isAlreadyParty = false;
 
     public PartyButton(int x, int y, String playerName) {
         super(x, y, Component.translatable("screens.wynntils.gearViewer.inviteParty"), Component.literal("P"));
@@ -14,6 +17,18 @@ public class PartyButton extends PlayerInteractionButton {
     @Override
     public void onPress() {
         super.onPress();
-        Handlers.Command.queueCommand("party " + playerName);
+        Handlers.Command.queueCommand("party " + (isAlreadyParty ? "kick " : "") + playerName);
+    }
+
+    @SubscribeEvent
+    public void onPartyInvite(PartyEvent.Invited e) {
+        isAlreadyParty = e.getPlayerName().equals(playerName);
+    }
+
+    @SubscribeEvent
+    public void onPartyKick(PartyEvent.OtherLeft e) {
+        if (e.getPlayerName().equals(playerName)) {
+            isAlreadyParty = false;
+        }
     }
 }
