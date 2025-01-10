9e5eb763e9f7eb83d611c60e7bf2c5b3c12c47f4
ryanzhoudev <57310593+ryanzhoudev@users.noreply.github.com>
2025-01-10 11:34:48 -0800

    dynamically switch behaviour based on friend status

--- common/src/main/java/com/wynntils/screens/gearviewer/widgets/FriendButton.java
+++ common/src/main/java/com/wynntils/screens/gearviewer/widgets/FriendButton.java
@@ -1,11 +1,13 @@
 package com.wynntils.screens.gearviewer.widgets;
 
 import com.wynntils.core.components.Handlers;
+import com.wynntils.models.players.event.FriendsEvent;
 import net.minecraft.network.chat.Component;
 import net.neoforged.bus.api.SubscribeEvent;
 
 public class FriendButton extends PlayerInteractionButton {
     private final String playerName;
+    private boolean isAlreadyFriend = false;
 
     public FriendButton(int x, int y, String playerName) {
         super(x, y, Component.translatable("screens.wynntils.gearViewer.addFriend"), Component.literal("F"));
@@ -15,11 +17,18 @@ public class FriendButton extends PlayerInteractionButton {
     @Override
     public void onPress() {
         super.onPress();
-        Handlers.Command.queueCommand("friend add " + playerName);
+        Handlers.Command.queueCommand("friend " + (isAlreadyFriend ? "remove" : "add") + " " + playerName);
     }
 
     @SubscribeEvent
-    public void onFriendAdd(String playerName) {
-        Handlers.Command.queueCommand("friend add " + playerName);
+    public void onFriendAdd(FriendsEvent.Added e) {
+        isAlreadyFriend = e.getPlayerName().equals(playerName);
+    }
+
+    @SubscribeEvent
+    public void onFriendRemove(FriendsEvent.Removed e) {
+        if (e.getPlayerName().equals(playerName)) {
+            isAlreadyFriend = false;
+        }
     }
 }
