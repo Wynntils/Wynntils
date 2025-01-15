b4ce2965e9c5873b9f553ba4e33596e734575a28
Jamie <JamCal117@gmail.com>
2025-01-15 20:58:30 +0000

    feat: Add custom guild log screen

---
+++ common/src/main/java/com/wynntils/models/items/annotators/gui/GuildLogAnnotator.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © Wynntils 2025.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.models.items.annotators.gui;
+
+import com.wynntils.core.text.StyledText;
+import com.wynntils.handlers.item.GuiItemAnnotator;
+import com.wynntils.handlers.item.ItemAnnotation;
+import com.wynntils.models.items.items.gui.GuildLogItem;
+import com.wynntils.utils.mc.LoreUtils;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+public class GuildLogAnnotator implements GuiItemAnnotator {
+    private static final Pattern LOG_NAME_PATTERN = Pattern.compile(
+            "§3(\\d+) ((month|week|day|hour|minute|second)s?) ago §8\\((?<month>\\d+)\\/(?<day>\\d+)\\/(?<year>\\d+) (?<hour>\\d+):(?<minute>\\d+) (AM|PM) EST\\)");
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("MM/dd/yy hh:mm a", Locale.ROOT);
+    private static final ZoneId LOG_ZONE = ZoneId.of("America/New_York");
+
+    @Override
+    public ItemAnnotation getAnnotation(ItemStack itemStack, StyledText name) {
+        if (itemStack.getItem() != Items.PAPER) return null;
+
+        Matcher matcher = name.getMatcher(LOG_NAME_PATTERN);
+
+        if (matcher.matches()) {
+            String month = matcher.group("month");
+            String day = matcher.group("day");
+            String year = matcher.group("year");
+            String hour = matcher.group("hour");
+            String minute = matcher.group("minute");
+            String amPm = matcher.group(9);
+            StringBuilder dateTimeStr = new StringBuilder(month)
+                    .append("/")
+                    .append(day)
+                    .append("/")
+                    .append(year)
+                    .append(" ")
+                    .append(hour)
+                    .append(":")
+                    .append(minute)
+                    .append(" ")
+                    .append(amPm);
+
+            LocalDateTime localDateTime = LocalDateTime.parse(dateTimeStr, DATE_FORMATTER);
+
+            ZonedDateTime estZonedDateTime = localDateTime.atZone(LOG_ZONE);
+
+            LinkedList<StyledText> unpaddedLore = new LinkedList<>(LoreUtils.getLore(itemStack));
+
+            // Remove the first and last line which are always empty
+            unpaddedLore.removeFirst();
+            unpaddedLore.removeLast();
+
+            return new GuildLogItem(estZonedDateTime.toInstant(), unpaddedLore);
+        }
+
+        return null;
+    }
+}
---
+++ common/src/main/java/com/wynntils/screens/guildlog/GuildLogScreen.java
@@ -0,0 +1,256 @@
+/*
+ * Copyright © Wynntils 2025.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.screens.guildlog;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.wynntils.core.consumers.screens.WynntilsScreen;
+import com.wynntils.core.text.StyledText;
+import com.wynntils.handlers.wrappedscreen.WrappedScreen;
+import com.wynntils.handlers.wrappedscreen.type.WrappedScreenInfo;
+import com.wynntils.models.guild.type.GuildLogType;
+import com.wynntils.models.items.items.gui.GuildLogItem;
+import com.wynntils.screens.base.widgets.BasicTexturedButton;
+import com.wynntils.screens.guildlog.widgets.LogButton;
+import com.wynntils.screens.guildlog.widgets.LogDateWidget;
+import com.wynntils.screens.guildlog.widgets.LogEntryWidget;
+import com.wynntils.utils.MathUtils;
+import com.wynntils.utils.colors.CommonColors;
+import com.wynntils.utils.mc.ComponentUtils;
+import com.wynntils.utils.render.FontRenderer;
+import com.wynntils.utils.render.RenderUtils;
+import com.wynntils.utils.render.Texture;
+import com.wynntils.utils.render.type.HorizontalAlignment;
+import com.wynntils.utils.render.type.TextShadow;
+import com.wynntils.utils.render.type.VerticalAlignment;
+import com.wynntils.utils.wynn.ContainerUtils;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.network.chat.Component;
+
+public class GuildLogScreen extends WynntilsScreen implements WrappedScreen {
+    // Constants
+    private static final float SCROLL_FACTOR = 10f;
+    private static final int SCROLL_AREA_HEIGHT = 125;
+
+    // Collections
+    private List<AbstractWidget> logs = new ArrayList<>();
+
+    // UI size, positions, etc
+    private boolean draggingScroll = false;
+    private float scrollY;
+    private int scrollOffset = 0;
+    private int maxScrollOffset = 0;
+
+    // WrappedScreen fields
+    private final WrappedScreenInfo wrappedScreenInfo;
+    private final GuildLogHolder holder;
+
+    public GuildLogScreen(WrappedScreenInfo wrappedScreenInfo, GuildLogHolder holder) {
+        super(Component.literal("Guild Log"));
+        this.wrappedScreenInfo = wrappedScreenInfo;
+        this.holder = holder;
+    }
+
+    @Override
+    protected void doInit() {
+        this.addRenderableWidget(new BasicTexturedButton(
+                getTranslationX() + 4,
+                getTranslationY() + 2,
+                Texture.LOG_BACK.width(),
+                Texture.LOG_BACK.height(),
+                Texture.LOG_BACK,
+                (button) -> ContainerUtils.clickOnSlot(
+                        holder.BACK_BUTTON_SLOT,
+                        wrappedScreenInfo.containerId(),
+                        button,
+                        wrappedScreenInfo.containerMenu().getItems()),
+                List.of(Component.translatable("gui.back").withStyle(ChatFormatting.GRAY, ChatFormatting.BOLD))));
+
+        for (GuildLogType logType : GuildLogType.values()) {
+            this.addRenderableWidget(new LogButton(
+                    getTranslationX() + 9,
+                    getTranslationY() + 17 + (logType.ordinal() * 23),
+                    (button) -> ContainerUtils.clickOnSlot(
+                            holder.LOG_SLOTS_MAP.get(logType),
+                            wrappedScreenInfo.containerId(),
+                            button,
+                            wrappedScreenInfo.containerMenu().getItems()),
+                    ComponentUtils.wrapTooltips(
+                            List.of(
+                                    Component.literal(logType.getDisplayName())
+                                            .withStyle(logType.getColor(), ChatFormatting.BOLD),
+                                    Component.translatable(logType.getTooltipKey())
+                                            .withStyle(ChatFormatting.GRAY)),
+                            150),
+                    logType));
+        }
+    }
+
+    @Override
+    public void doRender(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        PoseStack poseStack = guiGraphics.pose();
+        renderBackground(guiGraphics, mouseX, mouseY, partialTick);
+
+        RenderUtils.drawTexturedRect(poseStack, Texture.GUILD_LOG_BACKGROUND, getTranslationX(), getTranslationY());
+        FontRenderer.getInstance()
+                .renderText(
+                        poseStack,
+                        StyledText.fromComponent(wrappedScreenInfo.screen().getTitle()),
+                        getTranslationX() + 20,
+                        getTranslationY() + 10,
+                        CommonColors.TITLE_GRAY,
+                        HorizontalAlignment.LEFT,
+                        VerticalAlignment.MIDDLE,
+                        TextShadow.NONE);
+
+        renderLogs(guiGraphics, mouseX, mouseY, partialTick);
+
+        renderScroll(poseStack);
+
+        renderables.forEach(widget -> widget.render(guiGraphics, mouseX, mouseY, partialTick));
+    }
+
+    @Override
+    public boolean doMouseClicked(double mouseX, double mouseY, int button) {
+        if (!draggingScroll) {
+            if (MathUtils.isInside(
+                    (int) mouseX,
+                    (int) mouseY,
+                    getTranslationX() + 393,
+                    getTranslationX() + 393 + Texture.SCROLL_BUTTON.width(),
+                    (int) scrollY,
+                    (int) (scrollY + Texture.SCROLL_BUTTON.height()))) {
+                draggingScroll = true;
+
+                return true;
+            }
+        }
+
+        return super.doMouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double dragX, double dragY) {
+        if (draggingScroll) {
+            int scrollAreaStartY = getTranslationY() + 15 + 17;
+            int scrollAreaHeight = SCROLL_AREA_HEIGHT - Texture.SCROLL_BUTTON.height();
+
+            int newOffset = Math.round(MathUtils.map(
+                    (float) mouseY, scrollAreaStartY, scrollAreaStartY + scrollAreaHeight, 0, maxScrollOffset));
+
+            newOffset = Math.max(0, Math.min(newOffset, maxScrollOffset));
+
+            scroll(newOffset);
+
+            return true;
+        }
+
+        return super.mouseDragged(mouseX, mouseY, button, dragX, dragY);
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        draggingScroll = false;
+
+        return super.mouseReleased(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double deltaX, double deltaY) {
+        int scrollAmount = (int) (-deltaY * SCROLL_FACTOR);
+        int newOffset = Math.max(0, Math.min(scrollOffset + scrollAmount, maxScrollOffset));
+        scroll(newOffset);
+
+        return super.mouseScrolled(mouseX, mouseY, deltaX, deltaY);
+    }
+
+    public void updateLogItems() {
+        logs = new ArrayList<>();
+        maxScrollOffset = 0;
+
+        LocalDate currentDate = null;
+
+        int renderX = getTranslationX() + 108;
+        int renderY = getTranslationY() + 17;
+
+        for (GuildLogItem logItem : holder.guildLogItems) {
+            Instant logInstant = logItem.getLogInstant();
+            LocalDate logDate = logInstant.atZone(ZoneId.systemDefault()).toLocalDate();
+
+            if (currentDate == null || !logDate.isEqual(currentDate)) {
+                currentDate = logDate;
+                LogDateWidget dateWidget = new LogDateWidget(renderX, renderY, logInstant);
+                logs.add(dateWidget);
+
+                dateWidget.visible = renderY <= getTranslationY() + 17 + 139;
+
+                renderY += 21;
+                maxScrollOffset += 21;
+            }
+
+            int widgetHeight = Texture.LOG_ENTRY_TOP.height()
+                    + Texture.LOG_ENTRY_BOTTOM.height()
+                    + ((logItem.getLogInfo().size() + 1) * Texture.LOG_ENTRY_MIDDLE.height());
+
+            LogEntryWidget entryWidget =
+                    new LogEntryWidget(renderX, renderY, widgetHeight, logItem.getLogInfo(), logInstant);
+            logs.add(entryWidget);
+
+            entryWidget.visible = renderY <= getTranslationY() + 17 + 139;
+
+            renderY += widgetHeight + 1;
+            maxScrollOffset += widgetHeight + 1;
+        }
+
+        maxScrollOffset -= SCROLL_AREA_HEIGHT - 4 + Texture.SCROLL_BUTTON.height();
+    }
+
+    private void scroll(int newOffset) {
+        scrollOffset = newOffset;
+        int currentY = getTranslationY() + 17;
+
+        for (AbstractWidget widget : logs) {
+            int newY = currentY - scrollOffset;
+            widget.setY(newY);
+            widget.visible =
+                    (newY <= getTranslationY() + 17 + 139) && (newY + widget.getHeight() >= getTranslationY() + 17);
+            currentY += widget.getHeight() + 1;
+        }
+    }
+
+    private void renderLogs(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        RenderUtils.enableScissor(
+                getTranslationX() + 108, getTranslationY() + 16, Texture.LOG_ENTRY_MIDDLE.width(), 139);
+        logs.forEach(log -> log.render(guiGraphics, mouseX, mouseY, partialTick));
+        RenderUtils.disableScissor();
+    }
+
+    private void renderScroll(PoseStack poseStack) {
+        scrollY = getTranslationY()
+                + 15
+                + MathUtils.map(scrollOffset, 0, maxScrollOffset, 0, 141 - Texture.SCROLL_BUTTON.height());
+
+        RenderUtils.drawTexturedRect(poseStack, Texture.SCROLL_BUTTON, getTranslationX() + 393, scrollY);
+    }
+
+    @Override
+    public WrappedScreenInfo getWrappedScreenInfo() {
+        return wrappedScreenInfo;
+    }
+
+    public int getTranslationX() {
+        return (this.width - Texture.GUILD_LOG_BACKGROUND.width()) / 2;
+    }
+
+    public int getTranslationY() {
+        return (this.height - Texture.GUILD_LOG_BACKGROUND.height()) / 2;
+    }
+}
