f00170a84f8f492e37ac334b2a04069e4da9243a
Jamie <JamCal117@gmail.com>
2025-01-18 19:00:54 +0000

    feat: Add custom guild log screen [skip ci] (#3071)
    feat: Add custom guild log screen

---
+++ common/src/main/java/com/wynntils/features/ui/CustomGuildLogScreenFeature.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright © Wynntils 2025.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.features.ui;
+
+import com.wynntils.core.components.Models;
+import com.wynntils.core.consumers.features.Feature;
+import com.wynntils.core.persisted.Persisted;
+import com.wynntils.core.persisted.config.Category;
+import com.wynntils.core.persisted.config.Config;
+import com.wynntils.core.persisted.config.ConfigCategory;
+import com.wynntils.core.text.StyledText;
+import com.wynntils.handlers.wrappedscreen.event.WrappedScreenOpenEvent;
+import com.wynntils.mc.event.ContainerClickEvent;
+import com.wynntils.models.containers.containers.GuildManagementContainer;
+import com.wynntils.screens.guildlog.GuildLogScreen;
+import com.wynntils.utils.mc.KeyboardUtils;
+import java.util.regex.Pattern;
+import net.neoforged.bus.api.SubscribeEvent;
+
+@ConfigCategory(Category.UI)
+public class CustomGuildLogScreenFeature extends Feature {
+    private static final Pattern GUILD_LOG_ITEM_PATTERN = Pattern.compile("§7§lGuild Log");
+
+    @Persisted
+    private final Config<ShiftBehavior> shiftBehaviorConfig = new Config<>(ShiftBehavior.DISABLED_IF_SHIFT_HELD);
+
+    private boolean shiftClickedLogItem = false;
+
+    @SubscribeEvent
+    public void onWrappedScreenOpen(WrappedScreenOpenEvent event) {
+        if (event.getWrappedScreenClass() != GuildLogScreen.class) return;
+
+        boolean shouldOpen = false;
+
+        switch (shiftBehaviorConfig.get()) {
+            case NONE -> {
+                shouldOpen = true;
+            }
+            case ENABLED_IF_SHIFT_HELD -> {
+                if (shiftClickedLogItem) {
+                    shouldOpen = true;
+                }
+            }
+            case DISABLED_IF_SHIFT_HELD -> {
+                if (!shiftClickedLogItem) {
+                    shouldOpen = true;
+                }
+            }
+        }
+
+        if (shouldOpen) {
+            event.setOpenScreen(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onContainerClick(ContainerClickEvent event) {
+        if (!(Models.Container.getCurrentContainer() instanceof GuildManagementContainer)) return;
+        if (!StyledText.fromComponent(event.getItemStack().getHoverName()).matches(GUILD_LOG_ITEM_PATTERN)) return;
+
+        shiftClickedLogItem = KeyboardUtils.isShiftDown();
+    }
+
+    private enum ShiftBehavior {
+        NONE,
+        ENABLED_IF_SHIFT_HELD,
+        DISABLED_IF_SHIFT_HELD
+    }
+}
