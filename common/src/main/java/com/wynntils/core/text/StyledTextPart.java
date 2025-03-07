d8662fe2ea6ee90bd7241e160a12ce1f7052f7f8
Jamie <JamCal117@gmail.com>
2025-03-07 19:36:08 +0000

    feat: Deserialize aspect data

--- common/src/main/java/com/wynntils/core/text/StyledTextPart.java
+++ common/src/main/java/com/wynntils/core/text/StyledTextPart.java
@@ -1,23 +1,32 @@
 /*
- * Copyright © Wynntils 2023-2024.
+ * Copyright © Wynntils 2023-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.core.text;
 
+import com.wynntils.core.WynntilsMod;
 import com.wynntils.utils.colors.CustomColor;
 import com.wynntils.utils.wynn.WynnUtils;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
+import net.minecraft.resources.ResourceLocation;
 
 public final class StyledTextPart {
+    private static final Pattern CLASS_PATTERN = Pattern.compile("class=['\"](.*?)['\"]");
+    private static final Pattern STYLE_PATTERN = Pattern.compile("style=['\"](.*?)['\"]");
+    private static final Pattern TOKEN_PATTERN = Pattern.compile("(<span[^>]*>|</span>|[^<]+)");
     private final String text;
     private final PartStyle style;
 
@@ -245,6 +254,142 @@ public final class StyledTextPart {
                         currentStyle.withClickEvent(style.getClickEvent()).withHoverEvent(style.getHoverEvent());
             }
             parts.add(new StyledTextPart(currentString.toString(), currentStyle, null, parentStyle));
+        }
+
+        return parts;
+    }
+
+    // This will convert HTML text to StyledTextParts based on the rules from the API documentation
+    // located at https://docs.wynncraft.com/docs/#api-markup-parser
+    static List<StyledTextPart> fromHtmlString(String htmlString) {
+        if (htmlString.trim().equals("</br>")) {
+            return List.of(new StyledTextPart("", Style.EMPTY, null, Style.EMPTY));
+        }
+
+        List<StyledTextPart> parts = new ArrayList<>();
+        Deque<Style> styles = new ArrayDeque<>();
+        styles.push(Style.EMPTY);
+        Style currentStyle = Style.EMPTY;
+
+        StringBuilder plainText = new StringBuilder();
+
+        Matcher matcher = TOKEN_PATTERN.matcher(htmlString);
+
+        while (matcher.find()) {
+            String token = matcher.group();
+
+            if (token.startsWith("<span")) {
+                if (!plainText.isEmpty()) {
+                    parts.add(new StyledTextPart(plainText.toString(), currentStyle, null, Style.EMPTY));
+                    plainText = new StringBuilder();
+                }
+
+                Style parentStyle = styles.peek();
+                Style newStyle = Style.EMPTY.withColor(parentStyle.getColor());
+
+                Matcher classMatcher = CLASS_PATTERN.matcher(token);
+                if (classMatcher.find()) {
+                    String fontName = classMatcher.group(1).trim();
+                    ResourceLocation font =
+                            switch (fontName) {
+                                case "font-ascii", "font-default" -> ResourceLocation.withDefaultNamespace("default");
+                                case "font-common" -> ResourceLocation.withDefaultNamespace("common");
+                                case "font-five" -> ResourceLocation.withDefaultNamespace("language/five");
+                                case "font-wynnic" -> ResourceLocation.withDefaultNamespace("language/wynnic");
+                                case "font-high_gavelian" ->
+                                    ResourceLocation.withDefaultNamespace("language/high_gavelian");
+                                default -> {
+                                    WynntilsMod.warn("Unknown font in HTML parsing: " + fontName);
+                                    yield ResourceLocation.withDefaultNamespace("default");
+                                }
+                            };
+                    newStyle = newStyle.withFont(font);
+                }
+
+                Matcher styleMatcher = STYLE_PATTERN.matcher(token);
+                if (styleMatcher.find()) {
+                    String style = styleMatcher.group(1);
+
+                    for (String styleEntry : style.split(";")) {
+                        styleEntry = styleEntry.trim();
+
+                        if (styleEntry.isEmpty()) continue;
+
+                        String[] stylePair = styleEntry.split(":");
+                        String styleKey = stylePair[0].trim();
+                        String styleValue = stylePair[1].trim();
+
+                        switch (styleKey) {
+                            case "text-decoration" -> {
+                                if (styleValue.equals("underline")) {
+                                    newStyle = newStyle.withUnderlined(true);
+                                } else if (styleValue.equals("line-through")) {
+                                    newStyle = newStyle.withStrikethrough(true);
+                                } else {
+                                    WynntilsMod.warn("Unknown text decoration type in HTML parsing: " + styleValue);
+                                }
+                            }
+                            case "font-style" -> {
+                                if (styleValue.equals("italic")) {
+                                    newStyle = newStyle.withItalic(true);
+                                } else {
+                                    WynntilsMod.warn("Unknown font style type in HTML parsing: " + styleValue);
+                                }
+                            }
+                            case "font-weight" -> {
+                                if (styleValue.equals("bolder")) {
+                                    newStyle = newStyle.withBold(true);
+                                } else {
+                                    WynntilsMod.warn("Unknown font weight type in HTML parsing: " + styleValue);
+                                }
+                            }
+                            case "color" -> {
+                                newStyle = newStyle.withColor(
+                                        CustomColor.fromHexString(styleValue).asInt());
+                            }
+                            case "margin-left" -> {
+                                if (styleValue.equals("7.5px")) {
+                                    // FIXME: Currently a guess, there are no items with this margin but it is
+                                    // a possible value according to the docs
+                                    plainText.append("À");
+                                } else if (styleValue.equals("20px")) {
+                                    plainText.append("ÀÀÀÀ");
+                                } else {
+                                    WynntilsMod.warn("Unexpected margin-left in HTML parsing: " + styleValue);
+                                }
+                            }
+                            default -> {
+                                WynntilsMod.warn("Unknown style type in HTML parsing: " + styleKey);
+                            }
+                        }
+                    }
+                }
+
+                styles.push(newStyle);
+                currentStyle = newStyle;
+            } else if (token.startsWith("</span>")) {
+                if (!plainText.isEmpty()) {
+                    parts.add(new StyledTextPart(plainText.toString(), currentStyle, null, Style.EMPTY));
+                    plainText = new StringBuilder();
+                }
+
+                if (!styles.isEmpty()) {
+                    styles.pop();
+                }
+
+                Style parentStyle = styles.isEmpty() ? Style.EMPTY : styles.peek();
+                if (!parentStyle.equals(currentStyle)) {
+                    parts.add(new StyledTextPart("", parentStyle, null, Style.EMPTY));
+                }
+
+                currentStyle = parentStyle;
+            } else {
+                plainText.append(token);
+            }
+        }
+
+        if (!plainText.isEmpty()) {
+            parts.add(new StyledTextPart(plainText.toString(), currentStyle, null, Style.EMPTY));
         }
 
         return parts;
--- common/src/main/java/com/wynntils/models/items/items/game/AspectItem.java
+++ common/src/main/java/com/wynntils/models/items/items/game/AspectItem.java
@@ -1,39 +1,43 @@
 /*
- * Copyright © Wynntils 2024.
+ * Copyright © Wynntils 2024-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.models.items.items.game;
 
+import com.wynntils.models.aspects.type.AspectInfo;
 import com.wynntils.models.character.type.ClassType;
 import com.wynntils.models.gear.type.GearTier;
+import com.wynntils.models.items.properties.ClassableItemProperty;
 import com.wynntils.models.items.properties.GearTierItemProperty;
+import com.wynntils.models.items.properties.NumberedTierItemProperty;
 
-public class AspectItem extends GameItem implements GearTierItemProperty {
-    private final ClassType classType;
-    private final GearTier gearTier;
+public class AspectItem extends GameItem
+        implements GearTierItemProperty, ClassableItemProperty, NumberedTierItemProperty {
+    private final AspectInfo aspectInfo;
     private final int aspectTier;
 
-    public AspectItem(ClassType classType, GearTier gearTier, int aspectTier) {
-        this.classType = classType;
-        this.gearTier = gearTier;
+    public AspectItem(AspectInfo aspectInfo, int aspectTier) {
+        this.aspectInfo = aspectInfo;
         this.aspectTier = aspectTier;
     }
 
-    public ClassType getClassType() {
-        return classType;
+    @Override
+    public ClassType getRequiredClass() {
+        return aspectInfo.classType();
     }
 
     @Override
     public GearTier getGearTier() {
-        return gearTier;
+        return aspectInfo.gearTier();
     }
 
-    public int getAspectTier() {
+    @Override
+    public int getTier() {
         return aspectTier;
     }
 
     @Override
     public String toString() {
-        return "AspectItem{" + "classType=" + classType + ", gearTier=" + gearTier + ", aspectTier=" + aspectTier + '}';
+        return "AspectItem{" + "aspectInfo=" + aspectInfo + ", aspectTier=" + aspectTier + '}';
     }
 }
