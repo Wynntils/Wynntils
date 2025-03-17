211f208093d54b8af2cc436986a462de72668eed
Ryan Zhou <57310593+ryanzhoudev@users.noreply.github.com>
2025-03-17 01:08:33 -0700

    refactor: Use dedicated ping packets for PingService

--- common/src/main/java/com/wynntils/services/ping/PingService.java
+++ common/src/main/java/com/wynntils/services/ping/PingService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2023-2024.
+ * Copyright © Wynntils 2023-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.services.ping;
@@ -13,15 +13,15 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import net.minecraft.network.protocol.game.ClientboundCommandSuggestionsPacket;
-import net.minecraft.network.protocol.game.ServerboundCommandSuggestionPacket;
+import net.minecraft.Util;
+import net.minecraft.network.protocol.ping.ClientboundPongResponsePacket;
+import net.minecraft.network.protocol.ping.ServerboundPingRequestPacket;
 import net.neoforged.bus.api.SubscribeEvent;
 
 public class PingService extends Service {
     private static final int MS_PER_PING = 1000;
     private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
-    private long lastPingSent = 0;
     private int lastPing = 0;
 
     public PingService() {
@@ -38,22 +38,14 @@ public class PingService extends Service {
         }
     }
 
-    // We are specifically looking for the packet itself, not it's processing. This is why we are using the PacketEvent.
     @SubscribeEvent
-    public void onCommandSuggestions(PacketEvent.PacketReceivedEvent<?> event) {
-        if (!(event.getPacket() instanceof ClientboundCommandSuggestionsPacket packet)) return;
-        if (packet.id() != -1) return;
-
-        lastPing = (int) (System.currentTimeMillis() - lastPingSent);
-
-        // We also need to cancel the packet, as it would be unexpected to Minecraft.
-        event.setCanceled(true);
+    public void onPongReceived(PacketEvent.PacketReceivedEvent<?> event) {
+        if (!(event.getPacket() instanceof ClientboundPongResponsePacket(long time))) return;
+        lastPing = (int) (Util.getMillis() - time);
     }
 
     private void sendPingPacket() {
-        // We use -1 as the id, as it is not used by Minecraft.
-        McUtils.sendPacket(new ServerboundCommandSuggestionPacket(-1, ""));
-        lastPingSent = System.currentTimeMillis();
+        McUtils.sendPacket(new ServerboundPingRequestPacket(Util.getMillis()));
     }
 
     public int getPing() {
