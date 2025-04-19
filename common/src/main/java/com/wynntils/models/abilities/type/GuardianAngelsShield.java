f2d034e125a0236d330fc05225a4e5b46c345dfa
Jamie <JamCal117@gmail.com>
2025-04-19 16:38:51 +0100

    feat: Add Guardian Angels tracker overlay [skip ci] (#3204)
    * feat: Add Guardian Angels tracker overlay
    
    * refactor: Save ShieldType instead of name

--- common/src/main/java/com/wynntils/models/abilities/type/GuardianAngelsShield.java
+++ common/src/main/java/com/wynntils/models/abilities/type/GuardianAngelsShield.java
@@ -1,27 +1,30 @@
 /*
- * Copyright © Wynntils 2024-2025.
+ * Copyright © Wynntils 2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.models.abilities.type;
 
 import com.wynntils.models.character.type.ClassType;
 import com.wynntils.models.spells.type.SpellType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
-public class ArrowShield extends ShieldType {
+public class GuardianAngelsShield extends ShieldType {
     private static final ClassType CLASS_TYPE = ClassType.ARCHER;
     private static final SpellType SPELL_TYPE = SpellType.ARROW_SHIELD;
-    private static final String NAME = "Arrow";
+    private static final String NAME = "Guardian Angels";
+    private static final int GUARDIAN_ANGEL_DAMAGE_VALUE = 7;
 
-    public ArrowShield() {
+    public GuardianAngelsShield() {
         super(CLASS_TYPE, SPELL_TYPE, NAME);
     }
 
     @Override
     protected boolean verifyArmorStand(ArmorStand armorStand) {
-        ItemStack heldItem = armorStand.getMainHandItem();
-        return heldItem.getItem().equals(Items.ARROW);
+        ItemStack headItem = armorStand.getItemBySlot(EquipmentSlot.HEAD);
+        return headItem.getItem().equals(Items.DIAMOND_SWORD)
+                && headItem.getDamageValue() == GUARDIAN_ANGEL_DAMAGE_VALUE;
     }
 }
