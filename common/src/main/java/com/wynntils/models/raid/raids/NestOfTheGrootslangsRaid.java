9192d69c3e2f471e06c2019327e30033d1790aed
Jamie <JamCal117@gmail.com>
2025-04-21 13:46:18 +0100

    feat: Rework raid tracking to track specific rooms (#3199)
    * feat: Rework raid tracking to track specific rooms
    
    * chore: Remove debug
    
    * refactor: Make maps unmodifiable
    
    * refactor: Make RaidKinds static fields in model
    
    * fix: Send correct event on fail
    
    * fix: Fix error if completing raid with no tracked challenges
    
    * fix: Fix error if dealing damage in unknown room
    
    * fix: Don't send personal best if no times
    
    * refactor: Use RaidKind instead of name for statistics
    
    * chore: Update room names
    
    * ci: spotless formatting
    
    * chore: Change argument name to roomNumber
    
    * chore: Remove outdated i18ns
    
    * refactor: Remove static fields, instanceof in statistics collector
    
    ---------
    
    Co-authored-by: ShadowCat117 <8337467+ShadowCat117@users.noreply.github.com>

---
+++ common/src/main/java/com/wynntils/models/raid/raids/NestOfTheGrootslangsRaid.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright © Wynntils 2025.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.models.raid.raids;
+
+import com.wynntils.core.text.StyledText;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class NestOfTheGrootslangsRaid extends RaidKind {
+    private static final String RAID_NAME = "Nest of the Grootslangs";
+    private static final String ABBREVIATION = "NOG";
+    private static final StyledText ENTRY_TITLE = StyledText.fromString("§2Nest of The Grootslangs");
+
+    public NestOfTheGrootslangsRaid() {
+        super(RAID_NAME, ABBREVIATION, ENTRY_TITLE, buildRoomMap(), buildMajorIdMap());
+    }
+
+    private static Map<Integer, Map<String, String>> buildRoomMap() {
+        Map<Integer, Map<String, String>> nameMap = new TreeMap<>();
+
+        Map<String, String> challenge1Map = Map.of(
+                "Hold the platform", "Slimey Platform",
+                "Hold and defend", "Tower Defense");
+        Map<String, String> challenge2Map = Map.of("Collect 10 Slimy Goo", "Slime Gathering");
+        Map<String, String> challenge3Map = Map.of(
+                "Have a player pick up", "Tunnel Traversal",
+                "2 players must", "Minibosses");
+
+        nameMap.put(1, challenge1Map);
+        nameMap.put(2, challenge2Map);
+        nameMap.put(3, challenge3Map);
+        nameMap.put(4, Map.of("Slay the Restless", "Grootslang Wyrmling"));
+
+        return nameMap;
+    }
+
+    private static Map<String, Map<Integer, String>> buildMajorIdMap() {
+        return Map.of(
+                "Beserk", Map.of(1, "Explosive Impact"),
+                "Lightbearer", Map.of(3, "Transcendence"),
+                "Pestilent", Map.of(3, "Plague"),
+                "Bedrock", Map.of(3, "Altruism"));
+    }
+}
