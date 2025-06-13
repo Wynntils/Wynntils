cf365a24bd4114f577151e02dea879b634af8b4d
Jamie <JamCal117@gmail.com>
2025-06-13 19:31:01 +0100

    feat: Add texture options to universal bars

--- common/src/main/java/com/wynntils/overlays/custombars/UniversalTexturedCustomBarOverlay.java
+++ common/src/main/java/com/wynntils/overlays/custombars/UniversalTexturedCustomBarOverlay.java
@@ -1,17 +1,24 @@
 /*
- * Copyright © Wynntils 2023.
+ * Copyright © Wynntils 2023-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.overlays.custombars;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.wynntils.core.consumers.overlays.OverlaySize;
 import com.wynntils.core.persisted.Persisted;
 import com.wynntils.core.persisted.config.Config;
 import com.wynntils.utils.colors.CommonColors;
 import com.wynntils.utils.colors.CustomColor;
 import com.wynntils.utils.render.Texture;
+import com.wynntils.utils.render.buffered.BufferedRenderUtils;
+import com.wynntils.utils.render.type.UniversalTexture;
+import net.minecraft.client.renderer.MultiBufferSource;
 
 public class UniversalTexturedCustomBarOverlay extends CustomBarOverlayBase {
+    @Persisted
+    public final Config<UniversalTexture> barTexture = new Config<>(UniversalTexture.A);
+
     @Persisted
     public final Config<CustomColor> color = new Config<>(CommonColors.WHITE);
 
@@ -31,7 +38,26 @@ public class UniversalTexturedCustomBarOverlay extends CustomBarOverlayBase {
 
     @Override
     protected float getTextureHeight() {
-        return Texture.UNIVERSAL_BAR.height() / 2f;
+        return barTexture.get().getHeight();
+    }
+
+    @Override
+    protected void renderBar(
+            PoseStack poseStack, MultiBufferSource bufferSource, float renderY, float renderHeight, float progress) {
+        BufferedRenderUtils.drawColoredProgressBar(
+                poseStack,
+                bufferSource,
+                Texture.UNIVERSAL_BAR,
+                color.get(),
+                getRenderX(),
+                renderY,
+                getRenderX() + getWidth(),
+                renderY + renderHeight,
+                0,
+                barTexture.get().getTextureY1(),
+                Texture.UNIVERSAL_BAR.width(),
+                barTexture.get().getTextureY2(),
+                progress);
     }
 
     @Override
