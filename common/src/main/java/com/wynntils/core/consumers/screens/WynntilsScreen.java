32cb8ccef06ccb99024d79e18cd2c5939c3b8ac9
Christoph Matthias Kohnen <mail@cmkohnen.de>
2025-06-28 21:44:54 +0200

    Make WynntilsScreen closable by pressing E.
    This makes WynntilsScreen and all of its subclasses be closable by
    pressing E on your keyboard. This mirrors WynntilsContainerScreen which
    inherits this behaviour, while WynntilsScreen does not.
    
    Signed-off-by: Christoph Matthias Kohnen <mail@cmkohnen.de>

--- common/src/main/java/com/wynntils/core/consumers/screens/WynntilsScreen.java
+++ common/src/main/java/com/wynntils/core/consumers/screens/WynntilsScreen.java
@@ -133,7 +133,15 @@ public abstract class WynntilsScreen extends Screen implements TextboxScreen {
             }
         }
 
-        return (getFocusedTextInput() != null && getFocusedTextInput().keyPressed(keyCode, scanCode, modifiers))
-                || super.keyPressed(keyCode, scanCode, modifiers);
+        if (getFocusedTextInput() != null) {
+            return getFocusedTextInput().keyPressed(keyCode, scanCode, modifiers);
+        }
+        
+        if (keyCode == GLFW.GLFW_KEY_E) {
+            this.onClose();
+            return true;
+        }
+
+        return super.keyPressed(keyCode, scanCode, modifiers);
     }
 }
