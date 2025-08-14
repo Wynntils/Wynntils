7a9f957927d1eea94879f18c77a29a7ff60a927e
Jamie <JamCal117@gmail.com>
2025-08-14 17:43:47 +0100

    feat: Annotate store items

--- common/src/main/java/com/wynntils/models/items/items/gui/StoreItem.java
+++ common/src/main/java/com/wynntils/models/items/items/gui/StoreItem.java
@@ -1,15 +1,15 @@
 /*
- * Copyright © Wynntils 2022-2023.
+ * Copyright © Wynntils 2022-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.models.items.items.gui;
 
 import com.wynntils.utils.colors.CustomColor;
 
-public class CosmeticItem extends GuiItem {
+public class StoreItem extends GuiItem {
     private final CustomColor highlightColor;
 
-    public CosmeticItem(CustomColor highlightColor) {
+    public StoreItem(CustomColor highlightColor) {
         this.highlightColor = highlightColor;
     }
 
@@ -19,6 +19,6 @@ public class CosmeticItem extends GuiItem {
 
     @Override
     public String toString() {
-        return "CosmeticItem{" + "highlightColor=" + highlightColor + '}';
+        return "StoreItem{" + "highlightColor=" + highlightColor + '}';
     }
 }
