47592a9e4ad6d5f216ce9bbcd79bf9f5c5622d5c
Jamie <JamCal117@gmail.com>
2025-08-16 22:51:20 +0100

    port: Port Item DataComponent changes

--- common/src/main/java/com/wynntils/screens/guides/emeraldpouch/GuideEmeraldPouchItemStack.java
+++ common/src/main/java/com/wynntils/screens/guides/emeraldpouch/GuideEmeraldPouchItemStack.java
@@ -13,12 +13,12 @@ import java.util.List;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.Unit;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.component.Unbreakable;
 
 public final class GuideEmeraldPouchItemStack extends GuideItemStack {
     private final int tier;
@@ -31,7 +31,7 @@ public final class GuideEmeraldPouchItemStack extends GuideItemStack {
 
         this.tier = tier;
 
-        this.set(DataComponents.UNBREAKABLE, new Unbreakable(false));
+        this.set(DataComponents.UNBREAKABLE, Unit.INSTANCE);
 
         generatedTooltip = generatePouchLore(tier);
     }
