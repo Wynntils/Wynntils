2a264cfedb9b4158091b685e3728dca968b35058
Jamie <JamCal117@gmail.com>
2025-08-16 13:54:11 +0100

    fix: Revert NeoForge patch to render player in character selection, crate opening and wardrobe

--- neoforge/src/main/java/com/wynntils/neoforge/mixins/ForgeLevelRendererMixin.java
+++ neoforge/src/main/java/com/wynntils/neoforge/mixins/ForgeLevelRendererMixin.java
@@ -11,16 +11,20 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.wynntils.core.events.MixinHelper;
 import com.wynntils.mc.event.RenderTileLevelLastEvent;
 import com.wynntils.utils.mc.McUtils;
+import java.util.List;
 import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.FogParameters;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.entity.Entity;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelRenderer.class)
@@ -51,5 +55,20 @@ public abstract class ForgeLevelRendererMixin {
             @Local PoseStack poseStack) {
         MixinHelper.post(new RenderTileLevelLastEvent(
                 McUtils.mc().levelRenderer, poseStack, deltaTracker, projectionMatrix, camera));
+    }
+
+    // This reverts the patch made by NeoForge here: https://github.com/neoforged/NeoForge/pull/858
+    // Wynncraft uses this behaviour to hide the local player in certain cases such as the character selection screen.
+    @Redirect(
+            method = "collectVisibleEntities(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/culling/Frustum;Ljava/util/List;)Z",
+            at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
+    private boolean removePlayerFromVisibleEntities(
+            List<Entity> list, Object obj, Camera camera, Frustum frustum, List<Entity> output) {
+        Entity entity = (Entity) obj;
+        if (entity instanceof LocalPlayer local && camera.getEntity() != local) {
+            return false;
+        }
+
+        return list.add((Entity) obj);
     }
 }
