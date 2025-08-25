0cfa68c4a6b76bb29dcf2a5eb76c14569d994f85
Jamie <JamCal117@gmail.com>
2025-08-25 10:36:12 +0100

    fix: Fix scrap menu title and slots

--- common/src/main/java/com/wynntils/models/containers/containers/ScrapMenuContainer.java
+++ common/src/main/java/com/wynntils/models/containers/containers/ScrapMenuContainer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2024.
+ * Copyright © Wynntils 2024-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.models.containers.containers;
@@ -12,7 +12,8 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class ScrapMenuContainer extends Container implements SearchableContainerProperty {
-    private static final Pattern TITLE_PATTERN = Pattern.compile("Scrap Rewards");
+    private static final Pattern TITLE_PATTERN =
+            Pattern.compile("\uDAFF\uDFE8\uE037\uDAFF\uDF42\uF024\uDAFF\uDF5B\uF030");
     private static final Pattern NEXT_PAGE_PATTERN = Pattern.compile("§7Next Page");
     private static final Pattern PREVIOUS_PAGE_PATTERN = Pattern.compile("§7Previous Page");
 
@@ -32,17 +33,17 @@ public class ScrapMenuContainer extends Container implements SearchableContainer
 
     @Override
     public int getNextItemSlot() {
-        return 8;
+        return 53;
     }
 
     @Override
     public int getPreviousItemSlot() {
-        return 0;
+        return 51;
     }
 
     @Override
     public ContainerBounds getBounds() {
-        return new ContainerBounds(1, 0, 5, 8);
+        return new ContainerBounds(2, 0, 4, 8);
     }
 
     @Override
