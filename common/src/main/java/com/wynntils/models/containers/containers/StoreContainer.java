931af446f3f715869f699134cab42dab9fdb2a63
Jamie <JamCal117@gmail.com>
2025-08-27 19:01:01 +0100

    feat: Container updates (#3445)
    * feat: Support 15th character slot
    
    * feat: Add new pages for misc bucket and bookshelf
    
    * fix: Fix scrap menu title and slots
    
    * feat: Update cosmetic containers, add store containers
    
    * feat: Allow searchable containers to set search bar offset
    
    * ci: spotless formatting
    
    * chore: Remove more unused i18n
    
    * ci: spotless formatting
    
    ---------
    
    Co-authored-by: ShadowCat117 <8337467+ShadowCat117@users.noreply.github.com>

--- common/src/main/java/com/wynntils/models/containers/containers/StoreContainer.java
+++ common/src/main/java/com/wynntils/models/containers/containers/StoreContainer.java
@@ -1,21 +1,21 @@
 /*
- * Copyright © Wynntils 2024-2025.
+ * Copyright © Wynntils 2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.models.containers.containers;
 
 import com.wynntils.models.containers.Container;
 import com.wynntils.models.containers.type.ScrollableContainerProperty;
+import com.wynntils.models.store.type.StoreItemType;
 import java.util.regex.Pattern;
 
-public class ScrapMenuContainer extends Container implements ScrollableContainerProperty {
-    private static final Pattern TITLE_PATTERN =
-            Pattern.compile("\uDAFF\uDFE8\uE037\uDAFF\uDF42\uF024\uDAFF\uDF5B\uF030");
+public class StoreContainer extends Container implements ScrollableContainerProperty {
+    private static final String TITLE_START = "\uDAFF\uDFF4\uE02D\uDAFF\uDF7C";
     private static final Pattern NEXT_PAGE_PATTERN = Pattern.compile("§7Next Page");
     private static final Pattern PREVIOUS_PAGE_PATTERN = Pattern.compile("§7Previous Page");
 
-    public ScrapMenuContainer() {
-        super(TITLE_PATTERN);
+    public StoreContainer(StoreItemType storeItemType) {
+        super(Pattern.compile(TITLE_START + storeItemType.getTitleCharacter() + ".*"));
     }
 
     @Override
