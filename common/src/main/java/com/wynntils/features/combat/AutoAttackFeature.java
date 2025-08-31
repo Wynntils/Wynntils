c2fc5d23573b335843e306d4aa6205db7ec6c135
theoplegends <45824947+theoplegends@users.noreply.github.com>
2025-08-31 17:47:48 +0300

    fix: Make auto attack work with archer (#3475)
    * archer auto attack fix
    
    * handle other events aswell
    
    * chore: Spotless
    
    ---------
    
    Co-authored-by: Jamie <JamCal117@gmail.com>

--- common/src/main/java/com/wynntils/features/combat/AutoAttackFeature.java
+++ common/src/main/java/com/wynntils/features/combat/AutoAttackFeature.java
@@ -52,22 +52,21 @@ public class AutoAttackFeature extends Feature {
 
     @SubscribeEvent
     public void onSwing(ArmSwingEvent event) {
-        if (Models.Character.getClassType() == ClassType.ARCHER) return;
-        if (spellInputs == 0) return;
+        if (Models.Character.getClassType() != ClassType.ARCHER && spellInputs == 0) return;
 
         handleInput();
     }
 
     @SubscribeEvent
     public void onUseItem(UseItemEvent event) {
-        if (Models.Character.getClassType() == ClassType.ARCHER) return;
+        if (Models.Character.getClassType() == ClassType.ARCHER && spellInputs == 0) return;
 
         handleInput();
     }
 
     @SubscribeEvent
     public void onInteract(PlayerInteractEvent.InteractAt event) {
-        if (Models.Character.getClassType() == ClassType.ARCHER) return;
+        if (Models.Character.getClassType() == ClassType.ARCHER && spellInputs == 0) return;
 
         if (event.getEntityHitResult() != null) {
             EntityType<?> entityType = event.getEntityHitResult().getEntity().getType();
@@ -81,8 +80,7 @@ public class AutoAttackFeature extends Feature {
     public void onTick(TickEvent event) {
         if (!Models.WorldState.onWorld()) return;
         if (!Models.Spell.isSpellQueueEmpty()) return;
-        // Archers already have auto attack
-        if (Models.Character.getClassType() == ClassType.ARCHER) return;
+
         int tickCount = McUtils.player().tickCount;
 
         if (lastSpellInput + Models.Spell.SPELL_COST_RESET_TICKS < tickCount) {
@@ -99,9 +97,11 @@ public class AutoAttackFeature extends Feature {
 
         if (weaponStatus != WeaponStatus.USABLE) return;
 
-        if (!McUtils.options().keyAttack.isDown()) return;
+        if (Models.Character.getClassType() == ClassType.ARCHER
+                ? !McUtils.options().keyUse.isDown()
+                : !McUtils.options().keyAttack.isDown()) return;
 
-        MouseUtils.sendAttackInput(false);
+        MouseUtils.sendAttackInput(Models.Character.getClassType() == ClassType.ARCHER);
     }
 
     private void handleInput() {
