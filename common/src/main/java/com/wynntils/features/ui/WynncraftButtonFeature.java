e055b44aad2309bebd8cc3fc1e735252c79c618b
Magnus Ihse Bursie <mag@icus.se>
2025-08-31 12:30:58 +0200

    fix: null check should be for server.getIconBytes(), not conversion to buffer

--- common/src/main/java/com/wynntils/features/ui/WynncraftButtonFeature.java
+++ common/src/main/java/com/wynntils/features/ui/WynncraftButtonFeature.java
@@ -329,15 +329,16 @@ public class WynncraftButtonFeature extends Feature {
         // Modified from
         // net.minecraft.client.gui.screens.multiplayer.ServerSelectionList#uploadServerIcon
         private synchronized void loadServerIcon(ResourceLocation destination) {
-            ByteBuffer iconBytes = ByteBuffer.wrap(server.getIconBytes());
-            // failed to ping server or icon wasn't sent
+            byte[] iconBytes = server.getIconBytes();
             if (iconBytes == null) {
+                // failed to ping server or icon wasn't sent
                 WynntilsMod.warn("Unable to load icon");
                 serverIconLocation = FALLBACK;
                 return;
             }
+            ByteBuffer iconBytesBuffer = ByteBuffer.wrap(iconBytes);
 
-            try (NativeImage nativeImage = NativeImage.read(iconBytes)) {
+            try (NativeImage nativeImage = NativeImage.read(iconBytesBuffer)) {
                 Validate.validState(nativeImage.getWidth() == 64, "Must be 64 pixels wide");
                 Validate.validState(nativeImage.getHeight() == 64, "Must be 64 pixels high");
 
