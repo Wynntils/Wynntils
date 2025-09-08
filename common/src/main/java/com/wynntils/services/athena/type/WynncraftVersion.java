3ebb84f29b97d92f6d32850aacdb34d8fe2141f4
Jamie <JamCal117@gmail.com>
2025-09-08 18:00:26 +0100

    fix: Handle dev Wynncraft version

--- common/src/main/java/com/wynntils/services/athena/type/WynncraftVersion.java
+++ common/src/main/java/com/wynntils/services/athena/type/WynncraftVersion.java
@@ -6,6 +6,8 @@ package com.wynntils.services.athena.type;
 
 public record WynncraftVersion(
         String versionGroup, String majorVersion, String minorVersion, String revision, boolean isBeta) {
+    public static final WynncraftVersion DEV = new WynncraftVersion("0", "0", "0", "0", false);
+
     @Override
     public String toString() {
         return "v" + versionGroup + "." + majorVersion + "." + minorVersion + "_" + revision + (isBeta ? "_BETA" : "");
