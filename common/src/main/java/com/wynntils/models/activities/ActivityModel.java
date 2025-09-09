2d6a9131aef8f3dc5645b9bddb18945b2f93da52
Jamie <JamCal117@gmail.com>
2025-09-09 19:48:36 +0100

    fix: Don't include world event fast travel lines in description

--- common/src/main/java/com/wynntils/models/activities/ActivityModel.java
+++ common/src/main/java/com/wynntils/models/activities/ActivityModel.java
@@ -33,6 +33,7 @@ import com.wynntils.models.activities.type.ActivityRewardType;
 import com.wynntils.models.activities.type.ActivityStatus;
 import com.wynntils.models.activities.type.ActivityTrackingState;
 import com.wynntils.models.activities.type.ActivityType;
+import com.wynntils.models.activities.type.WorldEventFastTravelStatus;
 import com.wynntils.models.beacons.event.BeaconEvent;
 import com.wynntils.models.beacons.event.BeaconMarkerEvent;
 import com.wynntils.models.beacons.type.Beacon;
@@ -87,7 +88,9 @@ public final class ActivityModel extends Model {
     private static final Pattern DIFFICULTY_PATTERN = Pattern.compile("^\uDB00\uDC0E§7Difficulty: (\\w*)$");
     private static final Pattern REWARD_HEADER_PATTERN = Pattern.compile("^\uDB00\uDC0E§dRewards:$");
     private static final Pattern REWARD_PATTERN = Pattern.compile("^§d\uDB00\uDC04(- )?§7\\+?(?<reward>.+)$");
-    private static final Pattern TRACKING_PATTERN = Pattern.compile(".+§f\uE000 §#[a-f0-9]{8}§lClick To (Untrack|Track)");
+    private static final Pattern TRACKING_PATTERN =
+            Pattern.compile(".+§f\uE000 §#[a-f0-9]{8}§lClick To (Untrack|Track)");
+    private static final Pattern REQUIRES_HERO_PLUS_PATTERN = Pattern.compile(".+§7Requires §f\uE08A");
     private static final Pattern OVERALL_PROGRESS_PATTERN = Pattern.compile("^\\S*§7(\\d+) of (\\d+) completed$");
     private static final Pattern WIKI_REDIRECT_PATTERN = Pattern.compile("#REDIRECT \\[\\[(?<redirectname>.+)\\]\\]");
 
@@ -246,6 +249,7 @@ public final class ActivityModel extends Model {
         ActivityTrackingState trackingState = ActivityTrackingState.UNTRACKABLE;
         List<Pair<Pair<ProfessionType, Integer>, Boolean>> professionLevels = new ArrayList<>();
         List<Pair<String, Boolean>> quests = new ArrayList<>();
+        WorldEventFastTravelStatus worldEventFastTravelStatus = null;
         Map<ActivityRewardType, List<StyledText>> rewards = new TreeMap<>();
         List<StyledText> descriptionLines = new ArrayList<>();
 
@@ -337,6 +341,19 @@ public final class ActivityModel extends Model {
                 continue;
             }
 
+            if (type == ActivityType.WORLD_EVENT) {
+                if (worldEventFastTravelStatus == null) {
+                    worldEventFastTravelStatus = WorldEventFastTravelStatus.fromLine(line);
+
+                    if (worldEventFastTravelStatus != null) continue;
+                }
+            }
+
+            if (worldEventFastTravelStatus == WorldEventFastTravelStatus.UNAVAILABLE
+                    && line.matches(REQUIRES_HERO_PLUS_PATTERN)) {
+                continue;
+            }
+
             // For all other lines, append it to the description
             descriptionLines.add(line);
         }
@@ -358,6 +375,7 @@ public final class ActivityModel extends Model {
                 Optional.ofNullable(distance),
                 Optional.ofNullable(distanceInfo),
                 Optional.ofNullable(difficulty),
+                Optional.ofNullable(worldEventFastTravelStatus),
                 requirements,
                 rewards,
                 trackingState);
