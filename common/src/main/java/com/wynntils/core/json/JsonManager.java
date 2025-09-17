aaa40fd55aac652c771e92dfdeeaeafc1d6e228d
Jamie <JamCal117@gmail.com>
2025-09-17 20:55:12 +0100

    fix: Only delete temp file before writing

--- common/src/main/java/com/wynntils/core/json/JsonManager.java
+++ common/src/main/java/com/wynntils/core/json/JsonManager.java
@@ -69,6 +69,11 @@ public final class JsonManager extends Manager {
 
         File tempFile = new File(jsonFile.getPath() + ".tmp");
 
+        if (tempFile.exists()) {
+            // Clear an old temp file if it exists
+            FileUtils.deleteFile(tempFile);
+        }
+
         // Write the temp file
         try (OutputStreamWriter fileWriter =
                 new OutputStreamWriter(new FileOutputStream(tempFile), StandardCharsets.UTF_8)) {
@@ -101,7 +106,6 @@ public final class JsonManager extends Manager {
             }
 
             FileUtils.moveFile(tempFile, jsonFile);
-            FileUtils.deleteFile(tempFile);
         } catch (Exception e) {
             WynntilsMod.error("Failed to replace temp file " + jsonFile, e);
         }
