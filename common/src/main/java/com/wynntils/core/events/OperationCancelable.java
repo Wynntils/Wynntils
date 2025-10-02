4f51c245b8ee43bbd580ddb253388c27ae51e91d
Magnus Ihse Bursie <mag@icus.se>
2025-10-02 16:28:08 +0200

    Add new OperationCancelable interface

---
+++ common/src/main/java/com/wynntils/core/events/OperationCancelable.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © Wynntils 2025.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.core.events;
+
+public interface OperationCancelable {
+    default void setCanceled(boolean canceled) {
+        ((BaseEvent) this).isOperationCanceled = canceled;
+    }
+
+    default boolean isCanceled() {
+        return ((BaseEvent) this).isOperationCanceled;
+    }
+}
