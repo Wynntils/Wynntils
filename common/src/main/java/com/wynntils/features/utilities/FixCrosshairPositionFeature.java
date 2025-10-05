05c3aa7d252d7239b516d66367e53e0f69812579
Jamie <JamCal117@gmail.com>
2025-10-05 15:23:32 +0100

    port: Port FixCrosshairPositionFeature (rendering changes)

--- common/src/main/java/com/wynntils/features/utilities/FixCrosshairPositionFeature.java
+++ common/src/main/java/com/wynntils/features/utilities/FixCrosshairPositionFeature.java
@@ -4,8 +4,6 @@
  */
 package com.wynntils.features.utilities;
 
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.wynntils.core.components.Managers;
 import com.wynntils.core.consumers.features.Feature;
 import com.wynntils.core.consumers.overlays.OverlayPosition;
@@ -16,7 +14,7 @@ import com.wynntils.mc.event.RenderEvent;
 import com.wynntils.utils.mc.McUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 
@@ -36,20 +34,12 @@ public class FixCrosshairPositionFeature extends Feature {
         if (!shouldOverrideCrosshair()) return;
         event.setCanceled(true);
 
-        RenderSystem.enableBlend();
-        RenderSystem.blendFuncSeparate(
-                GlStateManager.SourceFactor.ONE_MINUS_DST_COLOR,
-                GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR,
-                GlStateManager.SourceFactor.ONE,
-                GlStateManager.DestFactor.ZERO);
+        event.getGuiGraphics().nextStratum();
 
         SectionCoordinates section = Managers.Overlay.getSection(OverlayPosition.AnchorSection.MIDDLE);
         int x = (section.x1() + section.x2() - 15) / 2;
         int y = (section.y1() + section.y2() - 15) / 2;
-        event.getGuiGraphics().blitSprite(RenderType::crosshair, Gui.CROSSHAIR_SPRITE, x, y, 15, 15);
+        event.getGuiGraphics().blitSprite(RenderPipelines.CROSSHAIR, Gui.CROSSHAIR_SPRITE, x, y, 15, 15);
         // Don't need to render the attack indicator, since Wynncraft doesn't ever use it
-
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.disableBlend();
     }
 }
