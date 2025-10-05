bd9af6b1f16fbc2b49222bb84ba6d95f967461eb
Jamie <JamCal117@gmail.com>
2025-10-05 15:07:16 +0100

    port: Port LevelRendererMixins (parameter changes)

--- common/src/main/java/com/wynntils/mc/event/RenderTileLevelLastEvent.java
+++ common/src/main/java/com/wynntils/mc/event/RenderTileLevelLastEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2022-2024.
+ * Copyright © Wynntils 2022-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.mc.event;
@@ -9,25 +9,18 @@ import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.neoforged.bus.api.Event;
-import org.joml.Matrix4f;
 
 public class RenderTileLevelLastEvent extends Event {
     private final LevelRenderer levelRenderer;
     private final PoseStack poseStack;
     private final DeltaTracker deltaTracker;
-    private final Matrix4f projectionMatrix;
     private final Camera camera;
 
     public RenderTileLevelLastEvent(
-            LevelRenderer levelRenderer,
-            PoseStack poseStack,
-            DeltaTracker deltaTracker,
-            Matrix4f projectionMatrix,
-            Camera camera) {
+            LevelRenderer levelRenderer, PoseStack poseStack, DeltaTracker deltaTracker, Camera camera) {
         this.levelRenderer = levelRenderer;
         this.poseStack = poseStack;
         this.deltaTracker = deltaTracker;
-        this.projectionMatrix = projectionMatrix;
         this.camera = camera;
     }
 
@@ -41,10 +34,6 @@ public class RenderTileLevelLastEvent extends Event {
 
     public DeltaTracker getDeltaTracker() {
         return deltaTracker;
-    }
-
-    public Matrix4f getProjectionMatrix() {
-        return this.projectionMatrix;
     }
 
     public Camera getCamera() {
