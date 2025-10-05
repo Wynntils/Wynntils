3f62cfb5562719b7213ac863039b0d5a93434cd7
Jamie <JamCal117@gmail.com>
2025-10-05 16:05:23 +0100

    port: Port NpcDialogueFeature (Shift packet changed)

--- common/src/main/java/com/wynntils/features/overlays/NpcDialogueFeature.java
+++ common/src/main/java/com/wynntils/features/overlays/NpcDialogueFeature.java
@@ -35,7 +35,8 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.game.ServerboundPlayerCommandPacket;
+import net.minecraft.network.protocol.game.ServerboundPlayerInputPacket;
+import net.minecraft.world.entity.player.Input;
 import net.neoforged.bus.api.SubscribeEvent;
 import org.lwjgl.glfw.GLFW;
 
@@ -178,16 +179,16 @@ public class NpcDialogueFeature extends Feature {
 
     @SubscribeEvent
     public void onPacketSent(PacketEvent.PacketSentEvent<?> e) {
-        if (!(e.getPacket() instanceof ServerboundPlayerCommandPacket packet)) return;
-        if (packet.getAction() != ServerboundPlayerCommandPacket.Action.PRESS_SHIFT_KEY) return;
+        if (!(e.getPacket() instanceof ServerboundPlayerInputPacket(Input input))) return;
+        if (!input.shift()) return;
 
         if (scheduledAutoProgressKeyPress != null) {
             scheduledAutoProgressKeyPress.cancel(true);
 
             // Must be scheduled, can't be sent immediately
             autoProgressExecutor.schedule(
-                    () -> McUtils.sendPacket(new ServerboundPlayerCommandPacket(
-                            McUtils.player(), ServerboundPlayerCommandPacket.Action.RELEASE_SHIFT_KEY)),
+                    () -> McUtils.sendPacket(new ServerboundPlayerInputPacket(
+                            new Input(false, false, false, false, false, false, false))),
                     100,
                     TimeUnit.MILLISECONDS);
 
@@ -196,8 +197,8 @@ public class NpcDialogueFeature extends Feature {
 
         if (isReleaseShiftScheduled) {
             npcDialogKeyOverrideExecutor.schedule(
-                    () -> McUtils.sendPacket(new ServerboundPlayerCommandPacket(
-                            McUtils.player(), ServerboundPlayerCommandPacket.Action.RELEASE_SHIFT_KEY)),
+                    () -> McUtils.sendPacket(new ServerboundPlayerInputPacket(
+                            new Input(false, false, false, false, false, false, false))),
                     100,
                     TimeUnit.MILLISECONDS);
 
@@ -235,8 +236,8 @@ public class NpcDialogueFeature extends Feature {
     private void progressNPCDialogue() {
         if (Models.NpcDialogue.getCurrentDialogue().dialogueType() == NpcDialogueType.NORMAL) {
             isReleaseShiftScheduled = true;
-            McUtils.sendPacket(new ServerboundPlayerCommandPacket(
-                    McUtils.player(), ServerboundPlayerCommandPacket.Action.PRESS_SHIFT_KEY));
+            McUtils.sendPacket(
+                    new ServerboundPlayerInputPacket(new Input(false, false, false, false, false, true, false)));
         }
     }
 
@@ -244,8 +245,8 @@ public class NpcDialogueFeature extends Feature {
         long delay = Models.NpcDialogue.calculateMessageReadTime(dialogue);
 
         return autoProgressExecutor.schedule(
-                () -> McUtils.sendPacket(new ServerboundPlayerCommandPacket(
-                        McUtils.player(), ServerboundPlayerCommandPacket.Action.PRESS_SHIFT_KEY)),
+                () -> McUtils.sendPacket(
+                        new ServerboundPlayerInputPacket(new Input(false, false, false, false, false, true, false))),
                 delay,
                 TimeUnit.MILLISECONDS);
     }
