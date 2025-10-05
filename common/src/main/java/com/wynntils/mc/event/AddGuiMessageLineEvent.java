8177cce4d14d248d8e093d42dc52563587a402fd
Jamie <JamCal117@gmail.com>
2025-10-05 16:22:12 +0100

    port: Port ChatTimestampFeature (Mixin changes)

--- common/src/main/java/com/wynntils/mc/event/AddGuiMessageLineEvent.java
+++ common/src/main/java/com/wynntils/mc/event/AddGuiMessageLineEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2024.
+ * Copyright © Wynntils 2024-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.mc.event;
@@ -10,12 +10,10 @@ import net.neoforged.bus.api.Event;
 public class AddGuiMessageLineEvent extends Event {
     private final GuiMessage message;
     private final GuiMessage.Line line;
-    private final int index;
 
-    public AddGuiMessageLineEvent(GuiMessage message, GuiMessage.Line line, int index) {
+    public AddGuiMessageLineEvent(GuiMessage message, GuiMessage.Line line) {
         this.message = message;
         this.line = line;
-        this.index = index;
     }
 
     public GuiMessage getMessage() {
@@ -24,9 +22,5 @@ public class AddGuiMessageLineEvent extends Event {
 
     public GuiMessage.Line getLine() {
         return line;
-    }
-
-    public int getIndex() {
-        return index;
     }
 }
