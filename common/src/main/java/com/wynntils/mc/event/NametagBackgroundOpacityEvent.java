1c17424fb3de5fd5cd534a36763aeb3a70ceb9f9
Jamie <JamCal117@gmail.com>
2025-11-16 21:37:31 +0000

    port: Port nametag rendering (Rendering and mixin changes)

---
+++ common/src/main/java/com/wynntils/mc/event/NametagBackgroundOpacityEvent.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright © Wynntils 2025.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.mc.event;
+
+import net.neoforged.bus.api.Event;
+
+public class NametagBackgroundOpacityEvent extends Event {
+    private float opacity;
+
+    public NametagBackgroundOpacityEvent(float opacity) {
+        this.opacity = opacity;
+    }
+
+    public float getOpacity() {
+        return opacity;
+    }
+
+    public void setOpacity(float newOpacity) {
+        this.opacity = newOpacity;
+    }
+}
