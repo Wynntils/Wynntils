62842da2ebf0556ef6291298bb5649129db5d6f4
Jamie <JamCal117@gmail.com>
2025-11-20 19:42:57 +0000

    port: Port ForgeLevelRendererMixin (Entity -> EntityRenderState)

--- neoforge/src/main/java/com/wynntils/neoforge/mixins/ForgeLevelRendererMixin.java
+++ neoforge/src/main/java/com/wynntils/neoforge/mixins/ForgeLevelRendererMixin.java
@@ -4,11 +4,15 @@
  */
 package com.wynntils.neoforge.mixins;
 
+import com.wynntils.mc.extension.EntityRenderStateExtension;
 import java.util.List;
 import net.minecraft.client.Camera;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.client.renderer.state.LevelRenderState;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,15 +24,22 @@ public abstract class ForgeLevelRendererMixin {
     // Wynncraft uses this behaviour to hide the local player in certain cases such as the character selection screen.
     @Redirect(
             method =
-                    "collectVisibleEntities(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/culling/Frustum;Ljava/util/List;)Z",
+                    "extractVisibleEntities(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/culling/Frustum;Lnet/minecraft/client/DeltaTracker;Lnet/minecraft/client/renderer/state/LevelRenderState;)V",
             at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
     private boolean removePlayerFromVisibleEntities(
-            List<Entity> list, Object obj, Camera camera, Frustum frustum, List<Entity> output) {
-        Entity entity = (Entity) obj;
+            List<Object> list,
+            Object obj,
+            Camera camera,
+            Frustum frustum,
+            DeltaTracker deltaTracker,
+            LevelRenderState renderState) {
+        EntityRenderState state = (EntityRenderState) obj;
+        Entity entity = ((EntityRenderStateExtension) state).getEntity();
+
         if (entity instanceof LocalPlayer local && camera.entity() != local) {
             return false;
         }
 
-        return list.add((Entity) obj);
+        return list.add(obj);
     }
 }
