24f4fda6b0d9c55f3e441303898a0655855b05c7
Jamie <JamCal117@gmail.com>
2025-12-17 16:42:57 +0000

    fix: Fix player command not working with privacy settings (#3636)
    * fix: Fix player command not working with privacy settings
    
    * ci: spotless formatting
    
    * fix: Literal
    
    * fix: Fix issues with parsing character data
    
    * refactor: Make player guild info optional
    
    * feat: Add contribution rank and contributed xp to PlayerGuildInfo
    
    * fix: Fix skill point parsing
    
    * fix: Change xp to long
    
    ---------
    
    Co-authored-by: ShadowCat117 <8337467+ShadowCat117@users.noreply.github.com>

--- common/src/main/java/com/wynntils/models/guild/type/GuildMemberInfo.java
+++ common/src/main/java/com/wynntils/models/guild/type/GuildMemberInfo.java
@@ -1,10 +1,13 @@
 /*
- * Copyright © Wynntils 2024.
+ * Copyright © Wynntils 2024-2025.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.models.guild.type;
 
+import java.util.UUID;
+
 public record GuildMemberInfo(
+        UUID uuid,
         String username,
         GuildRank rank,
         boolean online,
