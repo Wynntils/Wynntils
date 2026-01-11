63ea36228112a90b095e8e3e0f5cae642da8c10e
Jamie <JamCal117@gmail.com>
2026-01-11 18:29:34 +0000

    fix: Reset streamer mode on world state change

--- common/src/main/java/com/wynntils/models/worlds/StreamerModeModel.java
+++ common/src/main/java/com/wynntils/models/worlds/StreamerModeModel.java
@@ -9,7 +9,9 @@ import com.wynntils.core.components.Handlers;
 import com.wynntils.core.components.Model;
 import com.wynntils.models.worlds.bossbars.StreamerModeBar;
 import com.wynntils.models.worlds.event.StreamModeEvent;
+import com.wynntils.models.worlds.event.WorldStateEvent;
 import java.util.List;
+import net.neoforged.bus.api.SubscribeEvent;
 
 public class StreamerModeModel extends Model {
     private static final StreamerModeBar streamerModeBar = new StreamerModeBar();
@@ -20,6 +22,15 @@ public class StreamerModeModel extends Model {
         super(List.of());
 
         Handlers.BossBar.registerBar(streamerModeBar);
+    }
+
+    @SubscribeEvent
+    public void onWorldStateChange(WorldStateEvent event) {
+        // When changing world state, even just switching worlds, streamer mode is always reset.
+        // If the boss bar reset isn't triggered with the state change, just manually reset it here
+        if (inStream) {
+            setStreamerMode(false);
+        }
     }
 
     public void setStreamerMode(boolean inStream) {
