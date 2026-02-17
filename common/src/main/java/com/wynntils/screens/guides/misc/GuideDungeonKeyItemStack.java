3bb2f074895f1b7effbc8cfe15d28b70293d873c
Tim Killenberger <timkillenberger@gmail.com>
2026-02-17 23:15:48 +0100

    fix: changed icon for corrupted keys to the broken key icon in MiscGuide

--- common/src/main/java/com/wynntils/screens/guides/misc/GuideDungeonKeyItemStack.java
+++ common/src/main/java/com/wynntils/screens/guides/misc/GuideDungeonKeyItemStack.java
@@ -8,6 +8,8 @@ import com.wynntils.core.components.Services;
 import com.wynntils.models.activities.type.Dungeon;
 import com.wynntils.models.items.items.game.DungeonKeyItem;
 import com.wynntils.screens.guides.GuideItemStack;
+import java.util.ArrayList;
+import java.util.List;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
@@ -19,16 +21,13 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.CustomModelData;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GuideDungeonKeyItemStack extends GuideItemStack {
     private final Dungeon dungeon;
     private final boolean corrupted;
     private List<Component> generatedTooltip;
 
     public GuideDungeonKeyItemStack(Dungeon dungeon, boolean corrupted) {
-        super(getItemStack(), new DungeonKeyItem(dungeon, corrupted), getName(dungeon, corrupted));
+        super(getItemStack(corrupted), new DungeonKeyItem(dungeon, corrupted), getName(dungeon, corrupted));
         this.dungeon = dungeon;
         this.corrupted = corrupted;
     }
@@ -67,38 +66,45 @@ public class GuideDungeonKeyItemStack extends GuideItemStack {
     private List<Component> generateLore() {
         List<Component> itemLore = new ArrayList<>();
 
-        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage1").withStyle(ChatFormatting.GRAY));
-        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage2", dungeon.getName()).withStyle(ChatFormatting.WHITE)
-                .append(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage3").withStyle(ChatFormatting.GRAY)));
+        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage1")
+                .withStyle(ChatFormatting.GRAY));
+        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage2", dungeon.getName())
+                .withStyle(ChatFormatting.WHITE)
+                .append(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage3")
+                        .withStyle(ChatFormatting.GRAY)));
 
         itemLore.add(Component.empty());
-        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage4").withStyle(isCorrupted() ? ChatFormatting.DARK_RED : ChatFormatting.GOLD));
+        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage4")
+                .withStyle(isCorrupted() ? ChatFormatting.DARK_RED : ChatFormatting.GOLD));
 
         Dungeon.DungeonData dungeonData = isCorrupted() ? dungeon.getCorruptedDungeonData() : dungeon.getDungeonData();
-        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage5").withStyle(ChatFormatting.GRAY)
+        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage5")
+                .withStyle(ChatFormatting.GRAY)
                 .append(Component.literal(dungeonData.getCombatLevel() + "").withStyle(ChatFormatting.WHITE)));
 
-
-        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage6").withStyle(ChatFormatting.GRAY)
-                .append(Component.literal(dungeonData.getXPos() + " " + dungeonData.getYPos()).withStyle(ChatFormatting.WHITE)));
+        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage6")
+                .withStyle(ChatFormatting.GRAY)
+                .append(Component.literal(dungeonData.getXPos() + " " + dungeonData.getYPos())
+                        .withStyle(ChatFormatting.WHITE)));
 
         itemLore.add(Component.empty());
 
-        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.obtain1").withStyle(ChatFormatting.DARK_GRAY)
-                .append(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.obtain2").withStyle(ChatFormatting.AQUA)));
+        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.obtain1")
+                .withStyle(ChatFormatting.DARK_GRAY)
+                .append(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.obtain2")
+                        .withStyle(ChatFormatting.AQUA)));
 
         return itemLore;
     }
 
-    private static ItemStack getItemStack() {
+    private static ItemStack getItemStack(boolean corrupted) {
         ItemStack itemStack = new ItemStack(Items.POTION);
 
-        CustomModelData customModelData = new CustomModelData(
-                List.of(Services.CustomModel.getFloat("dungeon_key")
-                        .orElse(-1f)),
-                List.of(),
-                List.of(),
-                List.of());
+        float model = corrupted
+                ? Services.CustomModel.getFloat("dungeon_key_broken").orElse(-1f)
+                : Services.CustomModel.getFloat("dungeon_key").orElse(0f);
+
+        CustomModelData customModelData = new CustomModelData(List.of(model), List.of(), List.of(), List.of());
         itemStack.set(DataComponents.CUSTOM_MODEL_DATA, customModelData);
 
         return itemStack;
