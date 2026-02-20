d1724bdb1308c56b2548ab96b1398f3d7fbf0712
Tim Killenberger <timkillenberger@gmail.com>
2026-02-20 15:34:43 +0100

    fix: added broken key variants to MiscScreen

--- common/src/main/java/com/wynntils/screens/guides/misc/GuideDungeonKeyItemStack.java
+++ common/src/main/java/com/wynntils/screens/guides/misc/GuideDungeonKeyItemStack.java
@@ -13,7 +13,6 @@ import java.util.List;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.Style;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -24,16 +23,24 @@ import net.minecraft.world.item.component.CustomModelData;
 public class GuideDungeonKeyItemStack extends GuideItemStack {
     private final Dungeon dungeon;
     private final boolean corrupted;
+    private final boolean broken;
+    private final ChatFormatting highlightColor;
     private List<Component> generatedTooltip;
 
-    public GuideDungeonKeyItemStack(Dungeon dungeon, boolean corrupted) {
-        super(getItemStack(corrupted), new DungeonKeyItem(dungeon, corrupted), getName(dungeon, corrupted));
+    public GuideDungeonKeyItemStack(Dungeon dungeon, boolean corrupted, boolean broken) {
+        super(getItemStack(broken), new DungeonKeyItem(dungeon, corrupted), getName(dungeon, corrupted, broken));
         this.dungeon = dungeon;
         this.corrupted = corrupted;
+        this.broken = broken;
+        this.highlightColor = calculateHighlightColor();
     }
 
     public Dungeon getDungeon() {
         return dungeon;
+    }
+
+    public ChatFormatting getHighlightColor() {
+        return highlightColor;
     }
 
     public boolean isCorrupted() {
@@ -59,11 +66,26 @@ public class GuideDungeonKeyItemStack extends GuideItemStack {
     @Override
     public Component getHoverName() {
         return Component.empty()
-                .withStyle(Style.EMPTY.withColor(isCorrupted() ? ChatFormatting.DARK_RED : ChatFormatting.GOLD))
-                .append(Component.literal(getName(dungeon, corrupted)));
+                .withStyle(highlightColor)
+                .append(Component.literal(getName(dungeon, corrupted, broken)));
     }
 
     private List<Component> generateLore() {
+        List<Component> itemLore = new ArrayList<>();
+
+        if (broken) {
+            itemLore.addAll(getBrokenKeyTooltip());
+        } else {
+            itemLore.addAll(getUnbrokenKeyTooltip());
+        }
+
+        itemLore.addAll(getDungeonInfoTooltipSection());
+        itemLore.addAll(getObtainTooltipSection());
+
+        return itemLore;
+    }
+
+    private List<Component> getUnbrokenKeyTooltip() {
         List<Component> itemLore = new ArrayList<>();
 
         itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage1")
@@ -73,9 +95,29 @@ public class GuideDungeonKeyItemStack extends GuideItemStack {
                 .append(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage3")
                         .withStyle(ChatFormatting.GRAY)));
 
+        return itemLore;
+    }
+
+    private List<Component> getBrokenKeyTooltip() {
+        List<Component> itemLore = new ArrayList<>();
+
+        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.broken.usage1")
+                .withStyle(ChatFormatting.GRAY));
+        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.broken.usage2")
+                .withStyle(ChatFormatting.WHITE)
+                .append(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.broken.usage3")
+                        .withStyle(ChatFormatting.GRAY)));
+        itemLore.add(Component.literal(getName(dungeon, corrupted, false)).withStyle(highlightColor));
+
+        return itemLore;
+    }
+
+    private List<Component> getDungeonInfoTooltipSection() {
+        List<Component> itemLore = new ArrayList<>();
+
         itemLore.add(Component.empty());
         itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.usage4")
-                .withStyle(isCorrupted() ? ChatFormatting.DARK_RED : ChatFormatting.GOLD));
+                .withStyle(highlightColor));
 
         Dungeon.DungeonData dungeonData =
                 (isCorrupted() ? dungeon.getCorruptedDungeonData() : dungeon.getDungeonData()).orElseThrow();
@@ -90,18 +132,29 @@ public class GuideDungeonKeyItemStack extends GuideItemStack {
 
         itemLore.add(Component.empty());
 
-        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.obtain1")
-                .withStyle(ChatFormatting.DARK_GRAY)
-                .append(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.obtain2")
-                        .withStyle(ChatFormatting.AQUA)));
+        return itemLore;
+    }
+
+    private List<Component> getObtainTooltipSection() {
+        List<Component> itemLore = new ArrayList<>();
+
+        itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.obtain1"));
+
+        if (broken) {
+            itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.broken.obtain2")
+                    .withStyle(ChatFormatting.AQUA));
+        } else {
+            itemLore.add(Component.translatable("screens.wynntils.wynntilsGuides.misc.keys.obtain2")
+                    .withStyle(ChatFormatting.AQUA));
+        }
 
         return itemLore;
     }
 
-    private static ItemStack getItemStack(boolean corrupted) {
+    private static ItemStack getItemStack(boolean broken) {
         ItemStack itemStack = new ItemStack(Items.POTION);
 
-        float model = corrupted
+        float model = broken
                 ? Services.CustomModel.getFloat("dungeon_key_broken").orElse(-1f)
                 : Services.CustomModel.getFloat("dungeon_key").orElse(-1f);
 
@@ -111,13 +164,27 @@ public class GuideDungeonKeyItemStack extends GuideItemStack {
         return itemStack;
     }
 
-    private static String getName(Dungeon dungeon, boolean corrupted) {
+    private static String getName(Dungeon dungeon, boolean corrupted, boolean broken) {
         StringBuilder name = new StringBuilder();
+
+        if (broken) {
+            name.append("Broken ");
+        }
 
         if (corrupted) {
             name.append("Corrupted ");
         }
 
         return name.append(dungeon.getName()).append(" Key").toString();
+    }
+
+    private ChatFormatting calculateHighlightColor() {
+        ChatFormatting highlightColor = ChatFormatting.GOLD;
+
+        if (corrupted || broken) {
+            highlightColor = ChatFormatting.DARK_RED;
+        }
+
+        return highlightColor;
     }
 }
