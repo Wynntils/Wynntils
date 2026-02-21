8da5f5db3450a1e669ac9e99c3e45ce1f0cd9b62
Jamie <JamCal117@gmail.com>
2026-02-21 18:25:22 +0000

    fix: Fix GatheringToolAnnotator (#6)

--- common/src/main/java/com/wynntils/models/items/annotators/game/GatheringToolAnnotator.java
+++ common/src/main/java/com/wynntils/models/items/annotators/game/GatheringToolAnnotator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2022-2024.
+ * Copyright © Wynntils 2022-2026.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.models.items.annotators.game;
@@ -9,20 +9,16 @@ import com.wynntils.handlers.item.GameItemAnnotator;
 import com.wynntils.handlers.item.ItemAnnotation;
 import com.wynntils.models.items.items.game.GatheringToolItem;
 import com.wynntils.models.profession.type.ToolProfile;
-import com.wynntils.utils.mc.McUtils;
+import com.wynntils.utils.mc.LoreUtils;
 import com.wynntils.utils.type.CappedValue;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
 
 public final class GatheringToolAnnotator implements GameItemAnnotator {
     private static final Pattern GATHERING_TOOL_PATTERN =
-            Pattern.compile("^§f[\uE003\uE001\uE000\uE002] Gathering (Axe|Rod|Scythe|Pickaxe) T(\\d+)$");
-    private static final Pattern DURABILITY_PATTERN = Pattern.compile("\\[(\\d+)/(\\d+) Durability\\]");
+            Pattern.compile("^\uDAFC\uDC00§fGathering (Axe|Rod|Scythe|Pickaxe) T(\\d+)\uDAFC\uDC00$");
+    private static final Pattern DURABILITY_PATTERN = Pattern.compile(".+?§7 Durability (\\d+)/(\\d+)");
 
     @Override
     public ItemAnnotation getAnnotation(ItemStack itemStack, StyledText name) {
@@ -41,12 +37,8 @@ public final class GatheringToolAnnotator implements GameItemAnnotator {
     }
 
     private CappedValue getDurability(ItemStack itemStack) {
-        List<Component> lore =
-                itemStack.getTooltipLines(Item.TooltipContext.of(McUtils.mc().level), null, TooltipFlag.NORMAL);
-        for (Component line : lore) {
-            Matcher matcher = DURABILITY_PATTERN.matcher(line.getString());
-            if (!matcher.find()) continue;
-
+        Matcher matcher = LoreUtils.matchLoreLine(itemStack, 4, DURABILITY_PATTERN);
+        if (matcher.matches()) {
             int currentDurability = Integer.parseInt(matcher.group(1));
             int maxDurability = Integer.parseInt(matcher.group(2));
             return new CappedValue(currentDurability, maxDurability);
