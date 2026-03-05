f69405c9027088ded707505a7654fae24d0c5782
Rafii2198 <rafii2198@gmail.com>
2026-03-05 04:02:30 +0100

    feat: Function Activity Icon

--- common/src/main/java/com/wynntils/models/activities/type/ActivityType.java
+++ common/src/main/java/com/wynntils/models/activities/type/ActivityType.java
@@ -1,40 +1,59 @@
 /*
- * Copyright © Wynntils 2023-2025.
+ * Copyright © Wynntils 2023-2026.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.models.activities.type;
 
+import com.wynntils.core.text.StyledTextPart;
 import com.wynntils.utils.colors.CustomColor;
 import com.wynntils.utils.render.Texture;
 import java.util.Objects;
+import net.minecraft.network.chat.FontDescription;
+import net.minecraft.network.chat.Style;
+import net.minecraft.resources.Identifier;
 
 public enum ActivityType {
-    RECOMMENDED("Recommended", "recommended", null, null),
-    QUEST("Quest", "quests", CustomColor.fromInt(0x29cc96), Texture.QUEST_ICON),
-    STORYLINE_QUEST("Quest", "quests", CustomColor.fromInt(0x33b33b), Texture.STORYLINE_QUEST_ICON),
-    MINI_QUEST("Mini-Quest", "mini-quests", CustomColor.fromInt(0xb38fad), Texture.MINI_QUEST_ICON),
-    WORLD_EVENT("World Event", "world events", CustomColor.fromInt(0x00bdbf), Texture.WORLD_EVENT_ICON),
-    SECRET_DISCOVERY("Secret Discovery", "secret discoveries", CustomColor.fromInt(0xa1c3e6), Texture.DISCOVERY_ICON),
-    WORLD_DISCOVERY("World Discovery", "world discoveries", CustomColor.fromInt(0xa1c3e6), Texture.DISCOVERY_ICON),
+    RECOMMENDED("Recommended", "recommended", null, null, null),
+    QUEST("Quest", "quests", CustomColor.fromInt(0x29cc96), Texture.QUEST_ICON, "quest"),
+    STORYLINE_QUEST("Quest", "quests", CustomColor.fromInt(0x33b33b), Texture.STORYLINE_QUEST_ICON, "story_quest"),
+    MINI_QUEST("Mini-Quest", "mini-quests", CustomColor.fromInt(0xb38fad), Texture.MINI_QUEST_ICON, "mini_quest"),
+    WORLD_EVENT("World Event", "world events", CustomColor.fromInt(0x00bdbf), Texture.WORLD_EVENT_ICON, "world_event"),
+    SECRET_DISCOVERY(
+            "Secret Discovery",
+            "secret discoveries",
+            CustomColor.fromInt(0xa1c3e6),
+            Texture.DISCOVERY_ICON,
+            "discovery"),
+    WORLD_DISCOVERY(
+            "World Discovery", "world discoveries", CustomColor.fromInt(0xa1c3e6), Texture.DISCOVERY_ICON, "discovery"),
     TERRITORIAL_DISCOVERY(
-            "Territorial Discovery", "territorial discoveries", CustomColor.fromInt(0xa1c3e6), Texture.DISCOVERY_ICON),
-    CAVE("Cave", "caves", CustomColor.fromInt(0xff8c19), Texture.CAVE),
-    DUNGEON("Dungeon", "dungeons", CustomColor.fromInt(0xcc6677), Texture.DUNGEON_ENTRANCE),
-    RAID("Raid", "raids", CustomColor.fromInt(0xd6401e), Texture.RAID_ENTRANCE),
-    BOSS_ALTAR("Boss Altar", "boss altars", CustomColor.fromInt(0xf2d349), Texture.BOSS_ALTAR),
-    LOOTRUN_CAMP("Lootrun Camp", "lootrun camps", CustomColor.fromInt(0x3399cc), Texture.LOOTRUN_CAMP);
+            "Territorial Discovery",
+            "territorial discoveries",
+            CustomColor.fromInt(0xa1c3e6),
+            Texture.DISCOVERY_ICON,
+            "discovery"),
+    CAVE("Cave", "caves", CustomColor.fromInt(0xff8c19), Texture.CAVE, "cave"),
+    DUNGEON("Dungeon", "dungeons", CustomColor.fromInt(0xcc6677), Texture.DUNGEON_ENTRANCE, "dungeon"),
+    RAID("Raid", "raids", CustomColor.fromInt(0xd6401e), Texture.RAID_ENTRANCE, "raid"),
+    BOSS_ALTAR("Boss Altar", "boss altars", CustomColor.fromInt(0xf2d349), Texture.BOSS_ALTAR, "boss_altar"),
+    LOOTRUN_CAMP("Lootrun Camp", "lootrun camps", CustomColor.fromInt(0x3399cc), Texture.LOOTRUN_CAMP, "loot_camp");
 
     private final String displayName;
     private final String groupName;
     private final CustomColor color;
     private final Texture texture;
+    private final FontDescription fontIcon;
 
-    ActivityType(String displayName, String groupName, CustomColor color, Texture texture) {
+    ActivityType(String displayName, String groupName, CustomColor color, Texture texture, String fontIcon) {
         this.displayName = displayName;
         this.groupName = groupName;
         this.color = color;
         this.texture = texture;
+        this.fontIcon = new FontDescription.AtlasSprite(
+                Identifier.withDefaultNamespace("items"),
+                Identifier.withDefaultNamespace("wynn/gui/content_book/" + fontIcon + "_active"));
     }
+    ;
 
     public static ActivityType from(CustomColor color, String displayName) {
         for (ActivityType type : values()) {
@@ -89,5 +108,9 @@ public enum ActivityType {
             case STORYLINE_QUEST, MINI_QUEST -> activityType == this || activityType == QUEST;
             default -> activityType == this;
         };
+    }
+
+    public StyledTextPart getIcon() {
+        return new StyledTextPart("A", Style.EMPTY.withFont(fontIcon), null, null);
     }
 }
