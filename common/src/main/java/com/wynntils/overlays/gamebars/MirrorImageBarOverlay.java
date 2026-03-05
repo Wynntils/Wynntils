31e3bc4d17ad594f2a01c02ac27c52b0f0492165
Run4fight-mc <run4fight.dev@gmail.com>
2026-03-05 19:36:55 +0100

    Copyright MirrorImageBarOverlay file

--- common/src/main/java/com/wynntils/overlays/gamebars/MirrorImageBarOverlay.java
+++ common/src/main/java/com/wynntils/overlays/gamebars/MirrorImageBarOverlay.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2024-2026.
+ * Copyright © Wynntils 2026.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.overlays.gamebars;
