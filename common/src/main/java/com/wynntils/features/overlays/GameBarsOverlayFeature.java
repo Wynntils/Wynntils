b3c251707980225b1322c3e2cdab129546753d1c
Run4fight <Run4fight.dev@gmail.com>
2026-03-05 18:32:35 +0100

    chore: Sorted GameBarsOverlayFeature registered overlay with alphabetical order

--- common/src/main/java/com/wynntils/features/overlays/GameBarsOverlayFeature.java
+++ common/src/main/java/com/wynntils/features/overlays/GameBarsOverlayFeature.java
@@ -27,11 +27,32 @@ import com.wynntils.utils.type.RenderElementType;
 
 @ConfigCategory(Category.OVERLAYS)
 public class GameBarsOverlayFeature extends Feature {
+    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
+    private final AwakenedProgressBarOverlay awakenedProgressBarOverlay = new AwakenedProgressBarOverlay();
+
+    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
+    private final BloodPoolBarOverlay bloodPoolBarOverlay = new BloodPoolBarOverlay();
+
     @RegisterOverlay(renderType = RenderElementType.ACTION_BAR)
     private final HealthBarOverlay healthBarOverlay = new HealthBarOverlay();
 
     @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final BloodPoolBarOverlay bloodPoolBarOverlay = new BloodPoolBarOverlay();
+    private final CommanderBarOverlay commanderBarOverlay = new CommanderBarOverlay();
+
+    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
+    private final CorruptedBarOverlay corruptedBarOverlay = new CorruptedBarOverlay();
+
+    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
+    private final DistortionBarOverlay distortionBarOverlay = new DistortionBarOverlay();
+
+    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
+    private final FocusBarOverlay focusBarOverlay = new FocusBarOverlay();
+
+    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
+    private final FocusedMobHealthBarOverlay focusedMobHealthBarOverlay = new FocusedMobHealthBarOverlay();
+
+    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
+    private final HolyPowerBarOverlay holyPowerBarOverlay = new HolyPowerBarOverlay();
 
     @RegisterOverlay(renderType = RenderElementType.ACTION_BAR)
     private final ManaBarOverlay manaBarOverlay = new ManaBarOverlay();
@@ -40,34 +61,13 @@ public class GameBarsOverlayFeature extends Feature {
     private final ManaBankBarOverlay manaBankBarOverlay = new ManaBankBarOverlay();
 
     @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final FocusBarOverlay focusBarOverlay = new FocusBarOverlay();
-
-    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final AwakenedProgressBarOverlay awakenedProgressBarOverlay = new AwakenedProgressBarOverlay();
-
-    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final CorruptedBarOverlay corruptedBarOverlay = new CorruptedBarOverlay();
-
-    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final OphanimBarOverlay ophanimBarOverlay = new OphanimBarOverlay();
-
-    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final HolyPowerBarOverlay holyPowerBarOverlay = new HolyPowerBarOverlay();
-
-    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final FocusedMobHealthBarOverlay focusedMobHealthBarOverlay = new FocusedMobHealthBarOverlay();
-
-    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final CommanderBarOverlay commanderBarOverlay = new CommanderBarOverlay();
+    private final MirrorImageBarOverlay mirrorImageBarOverlay = new MirrorImageBarOverlay();
 
     @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
     private final MomentumBarOverlay momentumBarOverlay = new MomentumBarOverlay();
 
     @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final DistortionBarOverlay distortionBarOverlay = new DistortionBarOverlay();
-
-    @RegisterOverlay(renderType = RenderElementType.BOSS_BARS)
-    private final MirrorImageBarOverlay mirrorImageBarOverlay = new MirrorImageBarOverlay();
+    private final OphanimBarOverlay ophanimBarOverlay = new OphanimBarOverlay();
 
     public GameBarsOverlayFeature() {
         super(ProfileDefault.onlyDefault());
