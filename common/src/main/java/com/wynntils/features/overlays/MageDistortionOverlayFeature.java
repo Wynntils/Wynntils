9a930a53cebfd6d0d567dc76f93bc5be4c5892de
Run4fight <Run4fight.dev@gmail.com>
2026-04-06 18:28:36 +0200

    > Feat : Changed distortion BarOverlay to a classic overlay > Feat : Distortion Bossbar hidden when overlay is active
    Note : Main attack cooldown display bugged, trying to figure out a fix

---
+++ common/src/main/java/com/wynntils/features/overlays/MageDistortionOverlayFeature.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright © Wynntils 2026.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.features.overlays;
+
+import com.wynntils.core.consumers.features.Feature;
+import com.wynntils.core.consumers.features.ProfileDefault;
+import com.wynntils.core.consumers.overlays.Overlay;
+import com.wynntils.core.consumers.overlays.annotations.RegisterOverlay;
+import com.wynntils.overlays.MageDistortionOverlay;
+
+public class MageDistortionOverlayFeature extends Feature {
+    @RegisterOverlay
+    private final Overlay distortionOverlay = new MageDistortionOverlay();
+
+    public MageDistortionOverlayFeature() {
+        super(ProfileDefault.onlyDefault());
+    }
+}
