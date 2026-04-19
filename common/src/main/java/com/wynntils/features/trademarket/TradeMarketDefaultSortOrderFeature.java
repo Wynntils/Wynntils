0afb0a79b86097fc2a255e452bd6986026b5bdc3
uTa4u <63360499+uTa4u@users.noreply.github.com>
2026-04-19 20:47:14 +0300

    Implement sort order button parsing

--- common/src/main/java/com/wynntils/features/trademarket/TradeMarketDefaultSortOrderFeature.java
+++ common/src/main/java/com/wynntils/features/trademarket/TradeMarketDefaultSortOrderFeature.java
@@ -4,6 +4,7 @@
  */
 package com.wynntils.features.trademarket;
 
+import com.wynntils.core.WynntilsMod;
 import com.wynntils.core.components.Models;
 import com.wynntils.core.consumers.features.Feature;
 import com.wynntils.core.consumers.features.ProfileDefault;
@@ -12,15 +13,24 @@ import com.wynntils.core.persisted.config.Category;
 import com.wynntils.core.persisted.config.Config;
 import com.wynntils.core.persisted.config.ConfigCategory;
 import com.wynntils.core.persisted.config.ConfigProfile;
-import com.wynntils.mc.event.ContainerClickEvent;
+import com.wynntils.core.text.StyledText;
+import com.wynntils.mc.event.ContainerSetContentEvent;
+import com.wynntils.mc.event.ScreenClosedEvent;
 import com.wynntils.mc.event.TickEvent;
-import com.wynntils.models.trademarket.event.TradeMarketStateEvent;
 import com.wynntils.models.trademarket.type.TradeMarketState;
+import com.wynntils.utils.mc.LoreUtils;
 import com.wynntils.utils.mc.McUtils;
 import com.wynntils.utils.wynn.ContainerUtils;
+import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import org.lwjgl.glfw.GLFW;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @ConfigCategory(Category.TRADEMARKET)
 public class TradeMarketDefaultSortOrderFeature extends Feature {
@@ -34,11 +44,11 @@ public class TradeMarketDefaultSortOrderFeature extends Feature {
     private final Config<Boolean> applySortOrderOnce = new Config<>(true);
 
     private static final int SORT_ORDER_SLOT = 52;
+    private static final Pattern SORT_ORDER_PATTERN = Pattern.compile("§6- §f(.*)");
 
-    private boolean appliedDefaultSortOrder = false;
     private int clickCountdown = 0;
     private boolean shouldRightClick = false;
-    private SortOrder currentSortOrder = SortOrder.MOST_RECENT;
+    private boolean appliedDefaultSortOrder = false;
 
     public TradeMarketDefaultSortOrderFeature() {
         super(new ProfileDefault.Builder()
@@ -47,28 +57,31 @@ public class TradeMarketDefaultSortOrderFeature extends Feature {
     }
 
     @SubscribeEvent
-    public void onTradeMarketState(TradeMarketStateEvent event) {
-        TradeMarketState newState = event.getNewState();
-        if (!(appliedDefaultSortOrder && applySortOrderOnce.get()) && newState == TradeMarketState.FILTERED_RESULTS) {
-            appliedDefaultSortOrder = true;
-            // Find the shortest path from current sort order to the one we want to apply
-            // Math.abs(path) is path's lenght, Math.sign(path) is path's direction (-1 = right, 1 == left)
-            final int path1 = defaultSortOrder.get().ordinal() - currentSortOrder.ordinal();
-            final int path2 = path1 + SortOrder.VALUES.length * (path1 > 0 ? 1 : -1);
-            if (Math.abs(path1) < Math.abs(path2)) {
-                clickCountdown = Math.abs(path1);
-                shouldRightClick = Mth.sign(path1) == -1;
-            } else {
-                clickCountdown = Math.abs(path2);
-                shouldRightClick = Mth.sign(path2) == -1;
-            }
-            currentSortOrder = defaultSortOrder.get();
-        } else if (newState == TradeMarketState.NOT_ACTIVE) {
-            appliedDefaultSortOrder = false;
-            clickCountdown = 0;
-            shouldRightClick = false;
-            currentSortOrder = SortOrder.MOST_RECENT;
+    public void onContainerSetContent(ContainerSetContentEvent.Pre event) {
+        if (Models.TradeMarket.getTradeMarketState() != TradeMarketState.FILTERED_RESULTS) return;
+        if (appliedDefaultSortOrder && applySortOrderOnce.get()) return;
+
+        SortOrder currentSortOrder = parseSortOrder(LoreUtils.getTooltipLines(event.getItems().get(SORT_ORDER_SLOT)));
+
+        // Find the shortest path from current sort order to the one we want to apply
+        // Math.abs(path) is path's lenght, path < 0 -> right click else left click
+        final int path1 = defaultSortOrder.get().ordinal() - currentSortOrder.ordinal();
+        final int path2 = path1 + SortOrder.LENGHT * (path1 > 0 ? 1 : -1);
+        if (Math.abs(path1) < Math.abs(path2)) {
+            clickCountdown = Math.abs(path1);
+            shouldRightClick = path1 < 0;
+        } else {
+            clickCountdown = Math.abs(path2);
+            shouldRightClick = path2 < 0;
         }
+        appliedDefaultSortOrder = true;
+    }
+
+    @SubscribeEvent
+    public void onScreenClose(ScreenClosedEvent.Post event) {
+        clickCountdown = 0;
+        shouldRightClick = false;
+        appliedDefaultSortOrder = false;
     }
 
     @SubscribeEvent
@@ -85,17 +98,16 @@ public class TradeMarketDefaultSortOrderFeature extends Feature {
         clickCountdown -= 1;
     }
 
-    @SubscribeEvent
-    public void onContainerClick(ContainerClickEvent event) {
-        if (Models.TradeMarket.getTradeMarketState() != TradeMarketState.FILTERED_RESULTS) return;
-        if (event.getSlotNum() != SORT_ORDER_SLOT) return;
-
-        final int mb = event.getMouseButton();
-        if (mb == GLFW.GLFW_MOUSE_BUTTON_LEFT) {
-            currentSortOrder = currentSortOrder.next();
-        } else if (mb == GLFW.GLFW_MOUSE_BUTTON_RIGHT) {
-            currentSortOrder = currentSortOrder.prev();
+    private static SortOrder parseSortOrder(List<Component> tooltip) {
+        for (Component component : tooltip) {
+            Matcher matcher = StyledText.fromComponent(component).getNormalized().getMatcher(SORT_ORDER_PATTERN);
+            if (matcher.matches()) {
+                return SortOrder.valueOf(matcher.group(1).trim().replace(" ", "_").toUpperCase(Locale.ROOT));
+            }
         }
+
+        WynntilsMod.error("Could not parse TM sort order button!");
+        return SortOrder.MOST_RECENT;
     }
 
     public enum SortOrder {
@@ -106,15 +118,7 @@ public class TradeMarketDefaultSortOrderFeature extends Feature {
         HIGHEST_LEVEL_RANGE,
         LOWEST_LEVEL_RANGE;
 
-        private static final SortOrder[] VALUES = SortOrder.values();
-
-        public SortOrder prev() {
-            return VALUES[Math.floorMod(ordinal() - 1, VALUES.length)];
-        }
-
-        public SortOrder next() {
-            return VALUES[(ordinal() + 1) % VALUES.length];
-        }
+        private static final int LENGHT = values().length;
     }
 
     // Values taken from BulkBuyFeature
