7e024fe04c784373100abc72d66cdc7e82e1a228
Dionysus <ollipausw1005@gmail.com>
2026-05-03 18:23:26 +0200

    fix: import

--- common/src/main/java/com/wynntils/models/npcdialogue/NpcDialogueModel.java
+++ common/src/main/java/com/wynntils/models/npcdialogue/NpcDialogueModel.java
@@ -14,6 +14,7 @@ import com.wynntils.models.npcdialogue.event.TranslationRequestEvent;
 import com.wynntils.utils.colors.ColorChatFormatting;
 import com.wynntils.utils.mc.McUtils;
 import com.wynntils.utils.wynn.DialogueUtils;
+import java.util.function.Consumer;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import net.minecraft.ChatFormatting;
