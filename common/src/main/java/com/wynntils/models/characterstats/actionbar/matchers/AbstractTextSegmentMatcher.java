ceac7e3e9e9f688005b717561a21722ba9d46ae8
zvyap <koidsusan82@gmail.com>
2026-05-22 02:20:37 +0800

    fix: mana & health actionbar segment failing due to dialog hide mana & health

--- common/src/main/java/com/wynntils/models/characterstats/actionbar/matchers/AbstractTextSegmentMatcher.java
+++ common/src/main/java/com/wynntils/models/characterstats/actionbar/matchers/AbstractTextSegmentMatcher.java
@@ -9,6 +9,7 @@ import com.wynntils.core.text.StyledText;
 import com.wynntils.handlers.actionbar.ActionBarSegment;
 import com.wynntils.handlers.actionbar.ActionBarSegmentMatcher;
 import com.wynntils.utils.type.CappedValue;
+
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -121,6 +122,10 @@ public abstract class AbstractTextSegmentMatcher implements ActionBarSegmentMatc
         Matcher matcher = VALUE_PATTERN.matcher(value);
 
         if (!matcher.matches()) {
+            // If dialogue is ongoing, the health/mana text is gone, ending out value = 0
+            if(value.equals("0")) {
+                return CappedValue.EMPTY;
+            }
             WynntilsMod.warn("Could not parse text action bar segment value as capped: " + value);
             return null;
         }
