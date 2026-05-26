b7e1cccefa4c317f41bd17e2edb9c3a651c37b8c
Jamie <JamCal117@gmail.com>
2026-05-26 17:44:18 +0100

    feat: Parse mount energy bar, add overlay

--- common/src/main/java/com/wynntils/core/consumers/functions/FunctionManager.java
+++ common/src/main/java/com/wynntils/core/consumers/functions/FunctionManager.java
@@ -682,6 +682,7 @@ public final class FunctionManager extends Manager {
         registerFunction(new MinecraftFunctions.TicksFunction());
 
         registerFunction(new MountFunctions.CappedMountStatFunction());
+        registerFunction(new MountFunctions.CurrentMountEnergyFunction());
         registerFunction(new MountFunctions.MountStatFunction());
         registerFunction(new MountFunctions.MountStatMaxFunction());
         registerFunction(new MountFunctions.MountNameFunction());
