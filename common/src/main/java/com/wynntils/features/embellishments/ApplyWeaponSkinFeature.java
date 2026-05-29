e41f6bc306c07916f707a40f8aa7c28195e99a2b
uTa4u <63360499+uTa4u@users.noreply.github.com>
2026-05-29 21:12:54 +0300

    Hopefully make glint work

--- common/src/main/java/com/wynntils/features/embellishments/ApplyWeaponSkinFeature.java
+++ common/src/main/java/com/wynntils/features/embellishments/ApplyWeaponSkinFeature.java
@@ -15,9 +15,11 @@ import com.wynntils.models.store.StoreModel;
 import com.wynntils.utils.mc.McUtils;
 import com.wynntils.utils.wynn.ItemUtils;
 import java.util.List;
+import java.util.Optional;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.item.component.CustomModelData;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -66,6 +68,24 @@ public class ApplyWeaponSkinFeature extends Feature {
         CustomModelData data = itemStack.get(DataComponents.CUSTOM_MODEL_DATA);
         if (data == null) return;
         if (!ItemUtils.isUsableWeapon(itemStack)) return;
+
+        // Transfer the glint
+        PotionContents potionContents = itemStack.get(DataComponents.POTION_CONTENTS);
+        if (potionContents != null) {
+            Optional<Integer> glintOpt = potionContents.customColor();
+            if (glintOpt.isPresent()) {
+                PotionContents handPotionContents = handItem.get(DataComponents.POTION_CONTENTS);
+                if (handPotionContents != null) {
+                    handItem.set(
+                            DataComponents.POTION_CONTENTS,
+                            new PotionContents(
+                                    handPotionContents.potion(),
+                                    glintOpt,
+                                    handPotionContents.customEffects(),
+                                    handPotionContents.customName()));
+                }
+            }
+        }
 
         // ItemInHandRenderer#tick() checks ItemStack changes by reference instead of ItemStack#matches
         // so to not trigger equipped animation we have to overwrite the ItemStack for selected slot
