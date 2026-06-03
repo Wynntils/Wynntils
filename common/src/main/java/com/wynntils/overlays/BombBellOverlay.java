5668490d0325716cdf328afb572262ba5d753ef6
Rafii2198 <Rafii2198@gmail.com>
2026-06-03 18:29:23 +0200

    hide BombBellOverlay in cutscenes

--- common/src/main/java/com/wynntils/overlays/BombBellOverlay.java
+++ common/src/main/java/com/wynntils/overlays/BombBellOverlay.java
@@ -100,7 +100,7 @@ public class BombBellOverlay extends Overlay {
 
     @Override
     protected boolean isVisible() {
-        return !Models.Bomb.getBombBells().isEmpty();
+        return !Models.Bomb.getBombBells().isEmpty() || !Models.Cutscene.isCutsceneActive();
     }
 
     @Override
