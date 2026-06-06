a55ca46c6aac1089ba63ec9c544ef66fd0ace473
Rafii2198 <Rafii2198@gmail.com>
2026-06-06 02:58:14 +0200

    feat: Highlight Gathering Nodes Feature

---
+++ common/src/main/java/com/wynntils/features/utilities/HighlightGatheringNodesFeature.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © Wynntils 2026.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.features.utilities;
+
+import com.wynntils.core.components.Models;
+import com.wynntils.core.consumers.features.Feature;
+import com.wynntils.core.consumers.features.ProfileDefault;
+import com.wynntils.core.persisted.Persisted;
+import com.wynntils.core.persisted.config.Category;
+import com.wynntils.core.persisted.config.Config;
+import com.wynntils.core.persisted.config.ConfigCategory;
+import com.wynntils.mc.event.RenderLevelEvent;
+import com.wynntils.models.items.items.game.GatheringToolItem;
+import com.wynntils.models.profession.label.ProfessionGatheringNodeLabelInfo;
+import com.wynntils.utils.colors.CommonColors;
+import com.wynntils.utils.colors.CustomColor;
+import com.wynntils.utils.mc.McUtils;
+import java.util.Optional;
+import java.util.stream.Stream;
+import net.minecraft.gizmos.GizmoStyle;
+import net.minecraft.gizmos.Gizmos;
+import net.neoforged.bus.api.SubscribeEvent;
+
+@ConfigCategory(Category.UTILITIES)
+public class HighlightGatheringNodesFeature extends Feature {
+    @Persisted
+    private final Config<CustomColor> highlightColor = new Config<>(CommonColors.WHITE);
+
+    @Persisted
+    private final Config<Boolean> alwaysShow = new Config<>(false);
+
+    public HighlightGatheringNodesFeature() {
+        super(ProfileDefault.ENABLED);
+    }
+
+    @SubscribeEvent
+    public void onRenderLevel(RenderLevelEvent.Pre event) {
+        Stream<ProfessionGatheringNodeLabelInfo> nodes = Models.Profession.getNodesSet().stream();
+
+        if (!alwaysShow.get()) {
+            Optional<GatheringToolItem> heldItem =
+                    Models.Item.asWynnItem(McUtils.player().getMainHandItem(), GatheringToolItem.class);
+            if (heldItem.isEmpty()) return;
+
+            nodes = nodes.filter(node -> heldItem.get()
+                    .getProfessionTypes()
+                    .contains(node.getMaterialType().getProfessionType()));
+        }
+
+        nodes.forEach(node -> {
+            Gizmos.cuboid(
+                    node.getLocation().toBlockPos(),
+                    GizmoStyle.stroke(highlightColor.get().asInt()));
+        });
+    }
+}
