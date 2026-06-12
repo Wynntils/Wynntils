7070577f367021bc13cf55ccd2b0ae3872056d91
Jamie <JamCal117@gmail.com>
2026-06-12 14:42:03 +0100

    refactor: Cache filtered gathering node pois (#4133)
    feat: Cache filtered gathering node pois

--- common/src/main/java/com/wynntils/screens/maps/MainMapScreen.java
+++ common/src/main/java/com/wynntils/screens/maps/MainMapScreen.java
@@ -317,8 +317,7 @@ public final class MainMapScreen extends AbstractMapScreen {
 
         pois = Stream.concat(pois, Services.Poi.getCombatPois());
         pois = Stream.concat(pois, Services.Poi.getLabelPois());
-        pois = Stream.concat(
-                pois, Services.Poi.getGatheringNodePois().filter(Services.Poi::isGatheringNodeTypeVisible));
+        pois = Stream.concat(pois, Services.Poi.getFilteredGatheringNodePois());
         pois = Stream.concat(pois, Managers.Feature.getFeatureInstance(MainMapFeature.class).customPois.get().stream());
         pois = Stream.concat(pois, Services.Poi.getProvidedCustomPois().stream());
         pois = Stream.concat(pois, Models.Marker.getAllPois());
