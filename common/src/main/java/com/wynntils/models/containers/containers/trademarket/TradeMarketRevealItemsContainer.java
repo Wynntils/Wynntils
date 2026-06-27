e2ac74175d07e2c9703cbf257de1c26834774099
Levente <60847596+lKLevente@users.noreply.github.com>
2026-06-27 18:54:11 +0200

    Add files via upload

---
+++ common/src/main/java/com/wynntils/models/containers/containers/trademarket/TradeMarketRevealItemsContainer.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright © Wynntils 2024-2026.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
+package com.wynntils.models.containers.containers.trademarket;
+
+import com.wynntils.models.containers.Container;
+import java.util.Locale;
+import java.util.function.Predicate;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class TradeMarketRevealItemsContainer extends Container {
+    public TradeMarketRevealItemsContainer() {
+        super((Predicate<Screen>) screen -> {
+            Component title = screen.getTitle();
+            if (title == null) return false;
+            String str = title.getString();
+            if (str == null) return false;
+            str = str.toLowerCase(Locale.ROOT);
+            return str.contains("reveal");
+        });
+    }
+}
