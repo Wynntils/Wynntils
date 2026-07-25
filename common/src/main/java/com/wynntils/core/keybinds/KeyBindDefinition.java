e1bdf9809d40dc5cf61d2f655d1aee7b253e02c7
Jamie <JamCal117@gmail.com>
2026-07-25 23:25:10 +0100

    chore: Remove specific guide keybinds

--- common/src/main/java/com/wynntils/core/keybinds/KeyBindDefinition.java
+++ common/src/main/java/com/wynntils/core/keybinds/KeyBindDefinition.java
@@ -416,73 +416,9 @@ public record KeyBindDefinition(
             GLFW.GLFW_KEY_UNKNOWN,
             true);
 
-    public static final KeyBindDefinition OPEN_POWDER_GUIDE = register(
-            "openPowerGuide",
-            "Open Powder Guide",
-            Managers.KeyBind.UI_CATEGORY,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_UNKNOWN,
-            true);
-
-    public static final KeyBindDefinition OPEN_ITEM_GUIDE = register(
-            "openItemGuide",
-            "Open Item Guide",
-            Managers.KeyBind.UI_CATEGORY,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_UNKNOWN,
-            true);
-
-    public static final KeyBindDefinition OPEN_INGREDIENT_GUIDE = register(
-            "openIngredientGuide",
-            "Open Ingredient Guide",
-            Managers.KeyBind.UI_CATEGORY,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_UNKNOWN,
-            true);
-
-    public static final KeyBindDefinition OPEN_CHARM_GUIDE = register(
-            "openCharmGuide",
-            "Open Charm Guide",
-            Managers.KeyBind.UI_CATEGORY,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_UNKNOWN,
-            true);
-
-    public static final KeyBindDefinition OPEN_TOME_GUIDE = register(
-            "openTomeGuide",
-            "Open Tome Guide",
-            Managers.KeyBind.UI_CATEGORY,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_UNKNOWN,
-            true);
-
-    public static final KeyBindDefinition OPEN_ASPECT_GUIDE = register(
-            "openAspectGuide",
-            "Open Aspect Guide",
-            Managers.KeyBind.UI_CATEGORY,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_UNKNOWN,
-            true);
-
-    public static final KeyBindDefinition OPEN_EMERALD_GUIDE = register(
-            "openEmeraldGuide",
-            "Open Emerald Pouch Guide",
-            Managers.KeyBind.UI_CATEGORY,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_UNKNOWN,
-            true);
-
     public static final KeyBindDefinition OPEN_EMOTE_WHEEL = register(
             "emoteWheel",
             "Emote Wheel",
-            Managers.KeyBind.UI_CATEGORY,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_UNKNOWN,
-            true);
-
-    public static final KeyBindDefinition OPEN_MISC_GUIDE = register(
-            "openMiscGuide",
-            "Open Misc Guide",
             Managers.KeyBind.UI_CATEGORY,
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_UNKNOWN,
--- common/src/main/java/com/wynntils/features/ui/WynntilsContentBookFeature.java
+++ common/src/main/java/com/wynntils/features/ui/WynntilsContentBookFeature.java
@@ -23,14 +23,6 @@ import com.wynntils.mc.event.UseItemEvent;
 import com.wynntils.screens.activities.WynntilsContentBookScreen;
 import com.wynntils.screens.base.WynntilsMenuScreenBase;
 import com.wynntils.screens.guides.WynntilsGuidesListScreen;
-import com.wynntils.screens.guides.aspect.WynntilsAspectGuideScreen;
-import com.wynntils.screens.guides.charm.WynntilsCharmGuideScreen;
-import com.wynntils.screens.guides.emerald.WynntilsEmeraldGuideScreen;
-import com.wynntils.screens.guides.gear.WynntilsItemGuideScreen;
-import com.wynntils.screens.guides.ingredient.WynntilsIngredientGuideScreen;
-import com.wynntils.screens.guides.misc.WynntilsMiscGuideScreen;
-import com.wynntils.screens.guides.powder.WynntilsPowderGuideScreen;
-import com.wynntils.screens.guides.tome.WynntilsTomeGuideScreen;
 import com.wynntils.screens.overlays.placement.OverlayManagementScreen;
 import com.wynntils.screens.overlays.selection.OverlaySelectionScreen;
 import com.wynntils.screens.wynntilsmenu.WynntilsMenuScreen;
@@ -62,38 +54,6 @@ public class WynntilsContentBookFeature extends Feature {
     @RegisterKeyBind
     private final KeyBind openOverlayFreeMove = KeyBindDefinition.OPEN_OVERLAY_FREE_MOVE.create(
             () -> McUtils.mc().setScreen(OverlayManagementScreen.create(null)));
-
-    @RegisterKeyBind
-    private final KeyBind openPowderGuide = KeyBindDefinition.OPEN_POWDER_GUIDE.create(
-            () -> WynntilsMenuScreenBase.openBook(WynntilsPowderGuideScreen.create()));
-
-    @RegisterKeyBind
-    private final KeyBind openItemGuide = KeyBindDefinition.OPEN_ITEM_GUIDE.create(
-            () -> WynntilsMenuScreenBase.openBook(WynntilsItemGuideScreen.create()));
-
-    @RegisterKeyBind
-    private final KeyBind openIngredientGuide = KeyBindDefinition.OPEN_INGREDIENT_GUIDE.create(
-            () -> WynntilsMenuScreenBase.openBook(WynntilsIngredientGuideScreen.create()));
-
-    @RegisterKeyBind
-    private final KeyBind openCharmGuide = KeyBindDefinition.OPEN_CHARM_GUIDE.create(
-            () -> WynntilsMenuScreenBase.openBook(WynntilsCharmGuideScreen.create()));
-
-    @RegisterKeyBind
-    private final KeyBind openTomeGuide = KeyBindDefinition.OPEN_TOME_GUIDE.create(
-            () -> WynntilsMenuScreenBase.openBook(WynntilsTomeGuideScreen.create()));
-
-    @RegisterKeyBind
-    private final KeyBind openAspectGuide = KeyBindDefinition.OPEN_ASPECT_GUIDE.create(
-            () -> WynntilsMenuScreenBase.openBook(WynntilsAspectGuideScreen.create()));
-
-    @RegisterKeyBind
-    private final KeyBind openEmeraldGuide = KeyBindDefinition.OPEN_EMERALD_GUIDE.create(
-            () -> WynntilsMenuScreenBase.openBook(WynntilsEmeraldGuideScreen.create()));
-
-    @RegisterKeyBind
-    private final KeyBind openMiscGuide = KeyBindDefinition.OPEN_MISC_GUIDE.create(
-            () -> WynntilsMenuScreenBase.openBook(WynntilsMiscGuideScreen.create()));
 
     @RegisterKeyBind
     private final KeyBind openGuidesList = KeyBindDefinition.OPEN_GUIDES_LIST.create(
