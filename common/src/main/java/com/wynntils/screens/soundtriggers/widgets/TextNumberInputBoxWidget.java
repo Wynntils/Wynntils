d2f2152b90e8233d74ad55cd8bffaff8e5dca008
Rafii2198 <Rafii2198@gmail.com>
2026-08-05 01:15:22 +0200

    Prevent pasting non number strings into number input boxes

--- common/src/main/java/com/wynntils/screens/soundtriggers/widgets/TextNumberInputBoxWidget.java
+++ common/src/main/java/com/wynntils/screens/soundtriggers/widgets/TextNumberInputBoxWidget.java
@@ -7,8 +7,10 @@ package com.wynntils.screens.soundtriggers.widgets;
 import com.wynntils.core.WynntilsMod;
 import com.wynntils.screens.base.TextboxScreen;
 import com.wynntils.screens.base.widgets.TextInputBoxWidget;
+import com.wynntils.utils.mc.McUtils;
 import java.util.function.Consumer;
 import net.minecraft.client.input.CharacterEvent;
+import net.minecraft.client.input.KeyEvent;
 
 class TextNumberInputBoxWidget extends TextInputBoxWidget {
     TextNumberInputBoxWidget(
@@ -23,5 +25,17 @@ class TextNumberInputBoxWidget extends TextInputBoxWidget {
         WynntilsMod.warn(event.codepointAsString());
         if (!(event.codepoint() >= 48 && event.codepoint() <= 57)) return false;
         return super.charTyped(event);
+    }
+
+    @Override
+    public boolean keyPressed(KeyEvent event) {
+        if (event.isPaste()) {
+            try {
+                Integer.parseInt(McUtils.mc().keyboardHandler.getClipboard());
+            } catch (NumberFormatException ignored) {
+                return false;
+            }
+        }
+        return super.keyPressed(event);
     }
 }
