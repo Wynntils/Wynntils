f071a265e62ccc3d88d637c81b2fe5a0b13d04b7
Jamie <JamCal117@gmail.com>
2026-09-09 19:57:56 +0100

    feat: Handle ultimate meter

--- common/src/main/java/com/wynntils/features/ui/MountJumpBarFeature.java
+++ common/src/main/java/com/wynntils/features/ui/MountJumpBarFeature.java
@@ -10,18 +10,27 @@ import com.wynntils.core.consumers.features.Feature;
 import com.wynntils.core.consumers.features.ProfileDefault;
 import com.wynntils.core.persisted.config.Category;
 import com.wynntils.core.persisted.config.ConfigCategory;
+import com.wynntils.handlers.actionbar.event.ActionBarUpdatedEvent;
 import com.wynntils.mc.event.RenderEvent;
+import com.wynntils.models.characterstats.actionbar.segments.MeterTransitionSegment;
 import com.wynntils.models.mount.type.MountType;
 import com.wynntils.utils.mc.McUtils;
 import com.wynntils.utils.render.RenderUtils;
 import com.wynntils.utils.render.Texture;
 import com.wynntils.utils.type.RenderElementType;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.util.Mth;
 import net.neoforged.bus.api.SubscribeEvent;
 
 @ConfigCategory(Category.UI)
 public class MountJumpBarFeature extends Feature {
+    private static final Pattern METER_CHARACTER_PATTERN = Pattern.compile(".+([\uE190-\uE19C]).+");
+    private static final int BAR_HEIGHT = 6;
+
+    private int padding = 0;
+
     public MountJumpBarFeature() {
         super(ProfileDefault.ENABLED);
     }
@@ -56,12 +65,17 @@ public class MountJumpBarFeature extends Feature {
                 renderX,
                 renderY,
                 renderX + Texture.MOUNT_JUMP_BAR.width(),
-                renderY + Texture.MOUNT_JUMP_BAR.height() / 2f,
+                renderY + BAR_HEIGHT / 2f,
                 0,
-                0,
+                padding * BAR_HEIGHT,
                 Texture.MOUNT_JUMP_BAR.width(),
-                5,
+                (BAR_HEIGHT - 1) + (padding * BAR_HEIGHT),
                 progress);
+    }
+
+    @SubscribeEvent
+    public void onActionBarUpdate(ActionBarUpdatedEvent event) {
+        event.runIfPresentOrElse(MeterTransitionSegment.class, this::updateMeterCharacter, this::clearMeterCharacter);
     }
 
     private int getRenderX(Window window) {
@@ -70,5 +84,21 @@ public class MountJumpBarFeature extends Feature {
 
     private int getRenderY(Window window) {
         return window.getGuiScaledHeight() - 28;
+    }
+
+    private void updateMeterCharacter(MeterTransitionSegment segment) {
+        Matcher matcher = METER_CHARACTER_PATTERN.matcher(segment.getSegmentText());
+        if (matcher.matches()) {
+            padding = switch (matcher.group(1)) {
+                case "\uE191", "\uE199" -> 1;
+                case "\uE192", "\uE198" -> 2;
+                case "\uE193", "\uE194", "\uE195", "\uE196", "\uE197" -> 3;
+                default -> 0;
+            };
+        }
+    }
+
+    private void clearMeterCharacter() {
+        padding = 0;
     }
 }
