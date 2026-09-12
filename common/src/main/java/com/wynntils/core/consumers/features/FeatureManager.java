dae2d5b1b8ad0eb8349122b49a4fda3cda4ff584
Jamie <JamCal117@gmail.com>
2026-09-12 10:12:06 +0000

    feat: Mount jump bar feature (#4297)
    * feat: Current mount type function
    
    * feat: Mount jump bar feature
    
    * fix: Handle more summon cases
    
    * feat: Handle ultimate meter

--- common/src/main/java/com/wynntils/core/consumers/features/FeatureManager.java
+++ common/src/main/java/com/wynntils/core/consumers/features/FeatureManager.java
@@ -154,6 +154,7 @@ import com.wynntils.features.ui.CustomTerritoryManagementScreenFeature;
 import com.wynntils.features.ui.CustomTradeMarketResultScreenFeature;
 import com.wynntils.features.ui.EmoteWheelFeature;
 import com.wynntils.features.ui.LobbyUptimeFeature;
+import com.wynntils.features.ui.MountJumpBarFeature;
 import com.wynntils.features.ui.ProfessionHighlightFeature;
 import com.wynntils.features.ui.WynncraftButtonFeature;
 import com.wynntils.features.ui.WynncraftPauseScreenFeature;
@@ -409,6 +410,7 @@ public final class FeatureManager extends Manager {
         registerFeature(new CustomTradeMarketResultScreenFeature());
         registerFeature(new EmoteWheelFeature());
         registerFeature(new LobbyUptimeFeature());
+        registerFeature(new MountJumpBarFeature());
         registerFeature(new ProfessionHighlightFeature());
         registerFeature(new WynncraftButtonFeature());
         registerFeature(new WynncraftPauseScreenFeature());
