6b7ab3b2791a2c0b5997a58ce036d7735aedbff3
Jamie <JamCal117@gmail.com>
2026-09-17 20:35:47 +0100

    port: Port ChatTabService (gui.chat -> gui.hud.chat)

--- common/src/main/java/com/wynntils/services/chat/ChatTabService.java
+++ common/src/main/java/com/wynntils/services/chat/ChatTabService.java
@@ -165,7 +165,7 @@ public final class ChatTabService extends Service {
         });
 
         vanillaChatComponent = McUtils.getChat();
-        McUtils.mc().gui.chat = new WrappingChatComponent(McUtils.mc());
+        McUtils.mc().gui.hud.chat = new WrappingChatComponent(McUtils.mc());
 
         setFocusedTab(getChatTabs().getFirst());
     }
@@ -173,7 +173,7 @@ public final class ChatTabService extends Service {
     public void disable() {
         if (!isEnabled()) return;
 
-        McUtils.mc().gui.chat = vanillaChatComponent;
+        McUtils.mc().gui.hud.chat = vanillaChatComponent;
         vanillaChatComponent = null;
 
         reset();
