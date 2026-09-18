1ebf2acfa359da9f8df86894554dfbca04237a51
Jamie <JamCal117@gmail.com>
2026-09-19 00:29:59 +0100

    port: Port submitEntities mixin (Package change)

--- common/src/main/java/com/wynntils/mc/mixin/LevelRendererMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/LevelRendererMixin.java
@@ -81,12 +81,12 @@ public abstract class LevelRendererMixin {
 
     @WrapWithCondition(
             method =
-                    "submitEntities(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/state/LevelRenderState;Lnet/minecraft/client/renderer/SubmitNodeCollector;)V",
+                    "submitEntities(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/state/level/LevelRenderState;Lnet/minecraft/client/renderer/SubmitNodeCollector;)V",
             at =
                     @At(
                             value = "INVOKE",
                             target =
-                                    "Lnet/minecraft/client/renderer/entity/EntityRenderDispatcher;submit(Lnet/minecraft/client/renderer/entity/state/EntityRenderState;Lnet/minecraft/client/renderer/state/CameraRenderState;DDDLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;)V"))
+                                    "Lnet/minecraft/client/renderer/entity/EntityRenderDispatcher;submit(Lnet/minecraft/client/renderer/entity/state/EntityRenderState;Lnet/minecraft/client/renderer/state/level/CameraRenderState;DDDLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;)V"))
     private boolean onSubmitEntity(
             EntityRenderDispatcher entityRenderDispatcher,
             EntityRenderState renderState,
