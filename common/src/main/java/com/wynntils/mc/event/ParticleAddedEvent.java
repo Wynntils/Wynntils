2bfdc56b64ec01353940710027776f7407e9945a
Jamie <JamCal117@gmail.com>
2026-09-18 19:41:06 +0100

    port: Port ParticleAddedEvent (Class converted to record)

--- common/src/main/java/com/wynntils/mc/event/ParticleAddedEvent.java
+++ common/src/main/java/com/wynntils/mc/event/ParticleAddedEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Wynntils 2023-2024.
+ * Copyright © Wynntils 2023-2026.
  * This file is released under LGPLv3. See LICENSE for full license details.
  */
 package com.wynntils.mc.event;
@@ -15,22 +15,26 @@ public class ParticleAddedEvent extends Event {
     private final float xDist;
     private final float yDist;
     private final float zDist;
-    private final float maxSpeed;
+    private final float xMaxSpeed;
+    private final float yMaxSpeed;
+    private final float zMaxSpeed;
     private final int count;
     private final boolean overrideLimiter;
     private final ParticleOptions particle;
 
     public ParticleAddedEvent(ClientboundLevelParticlesPacket packet) {
-        this.x = packet.getX();
-        this.y = packet.getY();
-        this.z = packet.getZ();
-        this.xDist = packet.getXDist();
-        this.yDist = packet.getYDist();
-        this.zDist = packet.getZDist();
-        this.maxSpeed = packet.getMaxSpeed();
-        this.count = packet.getCount();
-        this.overrideLimiter = packet.isOverrideLimiter();
-        this.particle = packet.getParticle();
+        this.x = packet.x();
+        this.y = packet.y();
+        this.z = packet.z();
+        this.xDist = packet.xDist();
+        this.yDist = packet.yDist();
+        this.zDist = packet.zDist();
+        this.xMaxSpeed = packet.xMaxSpeed();
+        this.yMaxSpeed = packet.yMaxSpeed();
+        this.zMaxSpeed = packet.zMaxSpeed();
+        this.count = packet.count();
+        this.overrideLimiter = packet.overrideLimiter();
+        this.particle = packet.particle();
     }
 
     public double getX() {
