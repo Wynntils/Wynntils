06f2e3524fd4225ae1a09347e16ec0a1739dc696
Eshreal <96093563+EshrealDev@users.noreply.github.com>
2026-09-19 21:35:00 +0200

    fix: fix canReset parsing

--- common/src/main/java/com/wynntils/models/items/annotators/gui/AbilityTreeAnnotator.java
+++ common/src/main/java/com/wynntils/models/items/annotators/gui/AbilityTreeAnnotator.java
@@ -5,7 +5,6 @@
 package com.wynntils.models.items.annotators.gui;
 
 import com.wynntils.core.text.StyledText;
-import com.wynntils.core.text.type.StyleType;
 import com.wynntils.handlers.item.GuiItemAnnotator;
 import com.wynntils.handlers.item.ItemAnnotation;
 import com.wynntils.models.items.items.gui.AbilityTreeItem;
@@ -20,6 +19,8 @@ public final class AbilityTreeAnnotator implements GuiItemAnnotator {
 
     // Deals with the reset button in the ability tree screen
     private static final StyledText TREE_ABILITY_POINTS_NAME = StyledText.fromString("§#82eff4ff§lAbility Points");
+
+    // The ALT is for when we have tree manipulation and can reset the tree
     private static final StyledText TREE_ABILITY_POINTS_NAME_ALT = StyledText.fromString("§e§lAbility Points");
     private static final Pattern TREE_ABILITY_POINTS_RESET_PATTERN =
             Pattern.compile("§eShift Click to reset your tree");
@@ -27,43 +28,41 @@ public final class AbilityTreeAnnotator implements GuiItemAnnotator {
     // Test in AbilityTreeAnnotator_ABILITY_POINTS_PATTERN
     private static final Pattern ABILITY_POINTS_PATTERN =
             Pattern.compile("§b✦ Available Points: §(?:#a0c84bff|f)(\\d+)§7\\/(\\d+)");
-    private static final Pattern LOANED_ABILITY_POINTS_PATTERN = Pattern.compile("(\\d+) early points? from .+");
+    private static final Pattern LOANED_ABILITY_POINTS_PATTERN =
+            Pattern.compile("§#a0c84bff(\\d+) early points from §..");
 
     @Override
     public ItemAnnotation getAnnotation(ItemStack itemStack, StyledText name) {
-        if (name.equals(COMPASS_ABILITY_POINTS_NAME)) {
-            Matcher matcher = LoreUtils.matchLoreLine(itemStack, 3, ABILITY_POINTS_PATTERN);
-            if (!matcher.matches()) return null;
-            int count = Integer.parseInt(matcher.group(1)); // available points
-            int totalPoints = Integer.parseInt(matcher.group(2));
-            int loanedPoints = getLoanedAbilityPoints(itemStack);
-            return new AbilityTreeItem(count, totalPoints, loanedPoints, false);
-        } else if (name.equals(TREE_ABILITY_POINTS_NAME) || name.equals(TREE_ABILITY_POINTS_NAME_ALT)) {
-            Matcher matcher = LoreUtils.matchLoreLine(itemStack, 3, ABILITY_POINTS_PATTERN);
-            if (!matcher.matches()) return null;
-            int count = Integer.parseInt(matcher.group(1));
-            int totalPoints = Integer.parseInt(matcher.group(2));
-            int loanedPoints = getLoanedAbilityPoints(itemStack);
+        boolean isCompass = name.equals(COMPASS_ABILITY_POINTS_NAME);
+        boolean isTreeAlt = name.equals(TREE_ABILITY_POINTS_NAME_ALT);
+        boolean isTree = isTreeAlt || name.equals(TREE_ABILITY_POINTS_NAME);
+        if (!isCompass && !isTree) return null;
 
-            boolean isReset = false;
-            if (name.equals(TREE_ABILITY_POINTS_NAME_ALT)) {
-                Matcher resetMatcher = LoreUtils.matchLoreLine(itemStack, 8, TREE_ABILITY_POINTS_RESET_PATTERN);
-                isReset = resetMatcher.matches();
+        Matcher matcher = LoreUtils.matchLoreLine(itemStack, 3, ABILITY_POINTS_PATTERN);
+        if (!matcher.matches()) return null;
+
+        int count = Integer.parseInt(matcher.group(1)); // available points
+        int totalPoints = Integer.parseInt(matcher.group(2));
+        int loanedPoints = 0;
+        boolean canReset = false;
+
+        for (StyledText styledText : LoreUtils.getLore(itemStack)) {
+            String line = styledText.getString();
+
+            if (loanedPoints == 0) {
+                Matcher loanMatcher = LOANED_ABILITY_POINTS_PATTERN.matcher(line);
+                if (loanMatcher.matches()) {
+                    loanedPoints = Integer.parseInt(loanMatcher.group(1));
+                }
             }
 
-            return new AbilityTreeItem(count, totalPoints, loanedPoints, isReset);
-        } else {
-            return null;
-        }
-    }
-
-    private static int getLoanedAbilityPoints(ItemStack itemStack) {
-        for (StyledText line : LoreUtils.getLore(itemStack)) {
-            Matcher matcher = LOANED_ABILITY_POINTS_PATTERN.matcher(line.getString(StyleType.NONE));
-            if (matcher.matches()) {
-                return Integer.parseInt(matcher.group(1));
+            if (isTreeAlt
+                    && !canReset
+                    && TREE_ABILITY_POINTS_RESET_PATTERN.matcher(line).matches()) {
+                canReset = true;
             }
         }
-        return 0;
+
+        return new AbilityTreeItem(count, totalPoints, loanedPoints, canReset);
     }
 }
