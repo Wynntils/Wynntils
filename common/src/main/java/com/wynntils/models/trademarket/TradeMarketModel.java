5777b72d404c5278e435120ce5ce202446f58fb5
vy <switzr@tuta.io>
2026-09-19 10:05:58 +0000

    feat: TM sell buttons removed for crafteds and ID'd gear (#4306)
    * fix: TM sell all only returns 1 for identified items
    
    Signed-off-by: vy <switzr@tuta.io>
    Coded-by: my own two paws
    
    * fix: fix sell all buttons for materials
    
    Signed-off-by: vy <switzr@tuta.io>
    Coded-by: my own two paws
    
    * fix: fix sell item name regex
    
    Signed-off-by: vy <switzr@tuta.io>
    Coded-by: my own two paws
    
    * revert: revert 7e92589
    
    Signed-off-by: vy <switzr@tuta.io>
    Coded-by: my own two paws
    
    * feat: TM sell buttons removed for crafteds and ID'd gear
    
    * refactor: rename `displaySellButtons`, make boolean
    
    * fix: regex now matches items without color code
    
    * fix: add MountItem check
    
    ---------
    
    Signed-off-by: vy <switzr@tuta.io>
    Co-authored-by: Jamie <JamCal117@gmail.com>

--- common/src/main/java/com/wynntils/models/trademarket/TradeMarketModel.java
+++ common/src/main/java/com/wynntils/models/trademarket/TradeMarketModel.java
@@ -27,7 +27,10 @@ import com.wynntils.models.containers.containers.trademarket.TradeMarketRevealIt
 import com.wynntils.models.containers.containers.trademarket.TradeMarketSellContainer;
 import com.wynntils.models.containers.containers.trademarket.TradeMarketTradesContainer;
 import com.wynntils.models.containers.type.ContainerBounds;
+import com.wynntils.models.items.items.game.GearItem;
 import com.wynntils.models.items.items.game.MaterialItem;
+import com.wynntils.models.items.items.game.MountItem;
+import com.wynntils.models.items.properties.CraftedItemProperty;
 import com.wynntils.models.trademarket.event.TradeMarketChatInputEvent;
 import com.wynntils.models.trademarket.event.TradeMarketSellDialogueUpdatedEvent;
 import com.wynntils.models.trademarket.event.TradeMarketStateEvent;
@@ -102,7 +105,9 @@ public final class TradeMarketModel extends Model {
     private static final Pattern PRICE_PATTERN = Pattern.compile(
             "§[67] - (?:§f(?<amount>[\\d,]+) §7x )?§(?:(?:(?:c✖|a✔) §f)|f§m|f)(?<price>[\\d,]+)§7(?:§m)?²(?:§b ✮ (?<silverbullPrice>[\\d,]+)§3²)?(?: .+)?");
 
-    private static final Pattern SELL_ITEM_NAME_PATTERN = Pattern.compile("\uDAFC\uDC00§.(.+)\uDAFC\uDC00");
+    // Test in TradeMarketModel_SELL_ITEM_NAME_PATTERN
+    private static final Pattern SELL_ITEM_NAME_PATTERN =
+            Pattern.compile("(?:\uDAFC\uDC00)?(?:§.)?(.+)(?:\uDAFC\uDC00)?");
     private static final String EMPTY_ITEM_SLOT = "Empty Item Slot";
 
     public static final int SORT_ORDER_SLOT = 52;
@@ -124,6 +129,7 @@ public final class TradeMarketModel extends Model {
 
     private String soldItemName = null;
     private Optional<Integer> soldItemTier = Optional.empty();
+    private boolean shouldDisableSellButtons = false;
 
     public TradeMarketModel() {
         super(List.of());
@@ -380,6 +386,10 @@ public final class TradeMarketModel extends Model {
         return soldItemTier;
     }
 
+    public boolean getShouldDisableSellButtons() {
+        return shouldDisableSellButtons;
+    }
+
     private void handleSellDialogueUpdate() {
         if (tradeMarketState != TradeMarketState.SELLING) return;
 
@@ -387,6 +397,7 @@ public final class TradeMarketModel extends Model {
 
         soldItemName = null;
         soldItemTier = Optional.empty();
+        shouldDisableSellButtons = false;
 
         ItemStack itemStack = cs.getMenu().getSlot(SELLABLE_ITEM_SLOT).getItem();
         if (itemStack != ItemStack.EMPTY) {
@@ -399,6 +410,19 @@ public final class TradeMarketModel extends Model {
                     MaterialItem materialItem = materialItemOpt.get();
                     soldItemName = materialItem.getName();
                     soldItemTier = Optional.of(materialItem.getQualityTier());
+                }
+                Optional<GearItem> gearItemOpt = Models.Item.asWynnItem(itemStack, GearItem.class);
+                if (gearItemOpt.isPresent() && !gearItemOpt.get().isUnidentified()) {
+                    shouldDisableSellButtons = true;
+                }
+                Optional<CraftedItemProperty> craftedItemPropertyOpt =
+                        Models.Item.asWynnItemProperty(itemStack, CraftedItemProperty.class);
+                if (craftedItemPropertyOpt.isPresent()) {
+                    shouldDisableSellButtons = true;
+                }
+                Optional<MountItem> mountItemOpt = Models.Item.asWynnItem(itemStack, MountItem.class);
+                if (mountItemOpt.isPresent()) {
+                    shouldDisableSellButtons = true;
                 }
             }
         }
